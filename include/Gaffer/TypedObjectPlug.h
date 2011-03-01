55cddded63d7789351f2bc322af7e5a224c21dcc
John Haddon <john@image-engine.com>
2011-03-01 20:37:50 +0000

    Fixing warning.

--- include/Gaffer/TypedObjectPlug.h
+++ include/Gaffer/TypedObjectPlug.h
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////
 //  
 //  Copyright (c) 2011, John Haddon. All rights reserved.
+//  Copyright (c) 2011, Image Engine Design Inc. All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -107,4 +108,4 @@ typedef FilteredChildIterator<PlugPredicate<Plug::Out, ObjectPlug> > OutputObjec
 
 } // namespace Gaffer
 
-#endif GAFFER_TYPEDOBJECTPLUG_H
+#endif // GAFFER_TYPEDOBJECTPLUG_H
