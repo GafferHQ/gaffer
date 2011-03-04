1085a006f0dfd5b85b509aa206696c7e75f88abc
John Haddon <john@image-engine.com>
2011-03-04 19:15:15 +0000

    Added a todo.

--- include/GafferUI/Gadget.h
+++ include/GafferUI/Gadget.h
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////
 //  
 //  Copyright (c) 2011, John Haddon. All rights reserved.
+//  Copyright (c) 2011, Image Engine Design Inc. All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -77,6 +78,7 @@ IE_CORE_FORWARDDECLARE( Style );
 ///   scheme, but otherwise the container widget is responsible for passing events on itself, and
 ///   it gets slightly ugly.
 ///
+/// \todo Why not traverse up from the leaf like Qt does?
 class Gadget : public Gaffer::GraphComponent
 {
 
