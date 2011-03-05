b7299e2b7ad9aaf922d124d8649acc84f6d3a14b
John Haddon <john@image-engine.com>
2011-03-05 02:40:41 +0000

    Tidying.

--- include/GafferBindings/SetBinding.h
+++ include/GafferBindings/SetBinding.h
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////
 //  
 //  Copyright (c) 2011, John Haddon. All rights reserved.
+//  Copyright (c) 2011, Image Engine Design Inc. All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -44,6 +45,4 @@ void bindSet();
 
 } // namespace GafferBindings
 
-#include "GafferBindings/SetBinding.inl"
-
-#endif GAFFERBINDINGS_SETBINDING_H
+#endif // GAFFERBINDINGS_SETBINDING_H
