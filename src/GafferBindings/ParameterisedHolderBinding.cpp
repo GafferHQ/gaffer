e14a8465a914b31f1f0d2ab69be0c5d377b86274
John Haddon <thehaddonyoof@gmail.com>
2012-02-18 21:45:18 +0000

    Fixing node function bindings for ParameterisedHolder.

--- src/GafferBindings/ParameterisedHolderBinding.cpp
+++ src/GafferBindings/ParameterisedHolderBinding.cpp
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////
 //  
 //  Copyright (c) 2011, Image Engine Design Inc. All rights reserved.
-//  Copyright (c) 2011, John Haddon. All rights reserved.
+//  Copyright (c) 2011-2012, John Haddon. All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -122,7 +122,7 @@ void GafferBindings::bindParameterisedHolder()
 					)
 				)
 		)
-		.GAFFERBINDINGS_DEFGRAPHCOMPONENTWRAPPERFNS( ParameterisedHolderNode )
+		.GAFFERBINDINGS_DEFNODEWRAPPERFNS( ParameterisedHolderNode )
 		.def(
 			"setParameterised",
 			(void (ParameterisedHolderNode::*)( IECore::RunTimeTypedPtr, bool ))&ParameterisedHolderNode::setParameterised,
