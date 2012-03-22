31d2d99a951cd2f9d743c8adaf536fec32872335
John Haddon <thehaddonyoof@gmail.com>
2012-03-22 18:12:56 +0000

    Missing header.

---
+++ include/GafferScene/SceneProcessor.h
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////
+//  
+//  Copyright (c) 2012, John Haddon. All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//  
+//      * Redistributions of source code must retain the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer.
+//  
+//      * Redistributions in binary form must reproduce the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer in the documentation and/or other materials provided with
+//        the distribution.
+//  
+//      * Neither the name of John Haddon nor the names of
+//        any other contributors to this software may be used to endorse or
+//        promote products derived from this software without specific prior
+//        written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef GAFFERSCENE_SCENEPROCESSOR_H
+#define GAFFERSCENE_SCENEPROCESSOR_H
+
+#include "GafferScene/SceneNode.h"
+
+namespace GafferScene
+{
+
+/// The SceneProcessor class provides a base class for nodes which will take a scene input
+/// and modify it in some way. See the SceneElementProcessor and the SceneHierarchyProcessor
+/// for more useful base classes for scene processing.
+class SceneProcessor : public SceneNode
+{
+
+	public :
+
+		SceneProcessor( const std::string &name=staticTypeName() );
+		virtual ~SceneProcessor();
+
+		IE_CORE_DECLARERUNTIMETYPEDEXTENSION( SceneProcessor, SceneProcessorTypeId, SceneNode );
+		
+		/// Scene elements enter the node through inPlug() and are output in processed form on SceneNode::outPlug()
+		ScenePlug *inPlug();
+		const ScenePlug *inPlug() const;
+		
+};
+
+} // namespace GafferScene
+
+#endif // GAFFERSCENE_SCENEPROCESSOR_H
