9dd9928b54b239251b852dc4b1678d926b06ec64
John Haddon <thehaddonyoof@gmail.com>
2012-05-21 11:24:47 +0000

    More notes.

--- contrib/jh/notes/thingsToBreakBeforeVersion1.py
+++ contrib/jh/notes/thingsToBreakBeforeVersion1.py
@@ -14,6 +14,9 @@ move things into more modules :
 	
 distinguish widgets and gadgets by module namespace?
 
+rename borderwidth parameters to something more accurate
+	- consider the ContainerGadget::padding naming too
+
 have module name in typeName() ?
 
 change serialisation to use parent().addChild(), and allow nested group nodes
