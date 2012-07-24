fd70925576e9f06a861c018ed37b850ae41996e5
John Haddon <thehaddonyoof@gmail.com>
2012-07-24 11:06:32 +0000

    Safely ignoring child nodes when making ui for plugs.

--- python/GafferUI/NodeUI.py
+++ python/GafferUI/NodeUI.py
@@ -87,7 +87,7 @@ class NodeUI( GafferUI.Widget ) :
 		column = GafferUI.ListContainer( GafferUI.ListContainer.Orientation.Vertical, spacing=4 )
 		self.__plugsWidget.setChild( column )
 				
-		for plug in self.node().children() :
+		for plug in self.node().children( Gaffer.Plug.staticTypeId() ) :
 			if plug.getName().startswith( "__" ) or plug.direction() == Gaffer.Plug.Direction.Out :
 				continue
 			plugValueWidget = GafferUI.PlugValueWidget.create( plug )
