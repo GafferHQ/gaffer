e7e97f2fd24040f21665b0fa4d7cc4bf3d5340e3
John Haddon <thehaddonyoof@gmail.com>
2012-10-24 13:40:53 +0000

    Fixed bug where colour chooser would remain after the parent swatch died, but be inoperative. It now remains but is still functional. This also fixes a crash on closing the dialogue introduced by the Window.addChildWindow() change - caused by a bug in qt mac.

--- python/GafferUI/ColorPlugValueWidget.py
+++ python/GafferUI/ColorPlugValueWidget.py
@@ -35,6 +35,8 @@
 #  
 ##########################################################################
 
+import weakref
+
 import IECore
 
 import Gaffer
@@ -69,39 +71,77 @@ class ColorPlugValueWidget( GafferUI.CompoundNumericPlugValueWidget ) :
 		if not self._editable() :
 			return False
 				
-		if not self.__colorChooserDialogue :	
-			self.__colorChooserDialogue = GafferUI.ColorChooserDialogue()
-		
-		self.ancestor( GafferUI.Window ).addChildWindow( self.__colorChooserDialogue )
-		self.__colorChooserDialogue.setTitle( self.getPlug().relativeName( self.getPlug().ancestor( Gaffer.ScriptNode.staticTypeId() ) ) )
-		self.__colorChooserDialogue.colorChooser().setColor( self.getPlug().getValue() )
-		self.__colorChooserDialogue.colorChooser().setInitialColor( self.getPlug().getValue() )
-		
-		self.__colorChangedConnection = self.__colorChooserDialogue.colorChooser().colorChangedSignal().connect( Gaffer.WeakMethod( self.__colorChanged ) )
-		self.__confirmClickedConnection = self.__colorChooserDialogue.confirmButton.clickedSignal().connect( Gaffer.WeakMethod( self.__buttonClicked ) )
-		self.__cancelClickedConnection = self.__colorChooserDialogue.cancelButton.clickedSignal().connect( Gaffer.WeakMethod( self.__buttonClicked ) )
-		self.__dialogueClosedConnection = self.__colorChooserDialogue.closedSignal().connect( Gaffer.WeakMethod( self.__dialogueClosed ) )
-		
-		self.__colorChooserDialogue.setVisible( True )
+		# we only store a weak reference to the dialogue, because we want to allow it
+		# to manage its own lifetime. this allows it to exist after we've died, which
+		# can be useful for the user - they can bring up a node editor to get access to
+		# the color chooser, and then close the node editor but keep the floating color
+		# chooser. the only reason we keep a reference to the dialogue at all is so that
+		# we can avoid opening two at the same time.
+		if self.__colorChooserDialogue is None or self.__colorChooserDialogue() is None :
+			self.__colorChooserDialogue = weakref.ref(
+				_ColorPlugValueDialogue(
+					self.getPlug(),
+					self.ancestor( GafferUI.Window )
+				)
+			)
+
+		self.__colorChooserDialogue().setVisible( True )
 				
 		return True
-		
-	def __colorChanged( self, colorDialogue ) :
-	
-		color = colorDialogue.getColor()
-		self.getPlug().setValue( color )
-		
-	def __buttonClicked( self, button ) :
-	
-		if button is self.__colorChooserDialogue.cancelButton :
-			self.getPlug().setValue( self.__colorChooserDialogue.colorChooser().getInitialColor() )
-		
-		self.__colorChooserDialogue.parent().removeChild( self.__colorChooserDialogue )	
-		self.__colorChooserDialogue = None
-		
-	def __dialogueClosed( self, dialogue ) :
-	
-		self.__colorChooserDialogue = None
-				
+					
 GafferUI.PlugValueWidget.registerType( Gaffer.Color3fPlug.staticTypeId(), ColorPlugValueWidget )
 GafferUI.PlugValueWidget.registerType( Gaffer.Color4fPlug.staticTypeId(), ColorPlugValueWidget )
+
+## \todo Perhaps we could make this a part of the public API and give it an acquire()
+# method which the ColorPlugValueWidget uses?
+## \todo Support undo properly, with concatenation of repeated operations.
+class _ColorPlugValueDialogue( GafferUI.ColorChooserDialogue ) :
+
+	def __init__( self, plug, parentWindow ) :
+	
+		GafferUI.ColorChooserDialogue.__init__( 
+			self,
+			title = plug.relativeName( plug.ancestor( Gaffer.ScriptNode.staticTypeId() ) ),
+			color = plug.getValue()
+		)
+
+		self.__plug = plug
+
+		node = plug.node()
+		self.__nodeParentChangedConnection = node.parentChangedSignal().connect( Gaffer.WeakMethod( self.__destroy ) )
+		self.__plugSetConnection = plug.node().plugSetSignal().connect( Gaffer.WeakMethod( self.__plugSet ) )
+		
+		self.__closedConnection = self.closedSignal().connect( Gaffer.WeakMethod( self.__destroy ) )
+		self.__colorChangedConnection = self.colorChooser().colorChangedSignal().connect( Gaffer.WeakMethod( self.__colorChanged ) )
+		self.__confirmClickedConnection = self.confirmButton.clickedSignal().connect( Gaffer.WeakMethod( self.__buttonClicked ) )
+		self.__cancelClickedConnection = self.cancelButton.clickedSignal().connect( Gaffer.WeakMethod( self.__buttonClicked ) )
+
+		parentWindow.addChildWindow( self )
+
+	def __plugSet( self, plug ) :
+		
+		if plug.isSame( self.__plug ) :
+			self.colorChooser().setColor( self.__plug.getValue() )
+
+	def __colorChanged( self, colorChooser ) :
+	
+		self.__plug.setValue( self.colorChooser().getColor() )
+	
+	def __buttonClicked( self, button ) :
+		
+		if button is self.cancelButton :
+			self.__plug.setValue( self.colorChooser().getInitialColor() )
+		
+		# ideally we'd just remove ourselves from our parent immediately, but that would
+		# trigger this bug :
+		#
+		# 	https://bugreports.qt-project.org/browse/QTBUG-26761
+		#
+		# so instead we destroy ourselves on the next idle event.
+		
+		GafferUI.EventLoop.addIdleCallback( self.__destroy )
+	
+	def __destroy( self, *unused ) :
+	
+		self.parent().removeChild( self )
+		return False # to remove idle callback
