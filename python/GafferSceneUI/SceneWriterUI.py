5d9b9954f2c7c5924972dbb7ddfe97e07f79346f
David Minor <david.imageengine@gmail.com>
2013-02-02 00:33:22 +0000

    fixed copyright message

--- python/GafferSceneUI/SceneWriterUI.py
+++ python/GafferSceneUI/SceneWriterUI.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #  
-#  Copyright (c) 2012, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2013, Image Engine Design inc. All rights reserved.
 #  
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
