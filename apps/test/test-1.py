2ca530299400b859bf9fbee254767f73d39ccc84
John Haddon <john@image-engine.com>
2013-03-20 21:29:39 +0000

    Actually running GafferImageTest and GafferImageUITest by default.

--- apps/test/test-1.py
+++ apps/test/test-1.py
@@ -1,7 +1,7 @@
 ##########################################################################
 #  
 #  Copyright (c) 2011-2012, John Haddon. All rights reserved.
-#  Copyright (c) 2011, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2011-2013, Image Engine Design Inc. All rights reserved.
 #  
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -80,7 +80,7 @@ class test( Gaffer.Application ) :
 			
 		else :
 		
-			for module in ( GafferTest, GafferUITest, GafferSceneTest ) :
+			for module in ( GafferTest, GafferUITest, GafferSceneTest, GafferImageTest, GafferImageUITest ) :
 		
 				moduleTestSuite = unittest.defaultTestLoader.loadTestsFromModule( module )
 				testSuite.addTest( moduleTestSuite )
