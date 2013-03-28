7f6ee85c45e1decfd8090b4a9d48ee44bf970d09
goddardl <lukeg@image-engine.com>
2013-03-28 14:23:46 -0700

    Changed the EnumPlugValueWidget and the FormatPlugValueWidget to use the MultiSelectionMenu.

--- python/GafferImageUI/FormatPlugValueWidget.py
+++ python/GafferImageUI/FormatPlugValueWidget.py
@@ -45,8 +45,8 @@ QtGui = GafferUI._qtImport( "QtGui" )
 class FormatPlugValueWidget( GafferUI.PlugValueWidget ) :
 
 	def __init__( self, plug, **kw ) :
-		self.__selectionMenu = GafferUI.SelectionMenu()
-		GafferUI.PlugValueWidget.__init__( self, self.__selectionMenu, plug, **kw )
+		self.__multiSelectionMenu = GafferUI.MultiSelectionMenu( allowMultipleSelection = False, alwaysHaveASelection = True)
+		GafferUI.PlugValueWidget.__init__( self, self.__multiSelectionMenu, plug, **kw )
 		
 		self.__formatAddedConnection = GafferImage.Format.formatAddedSignal().connect( Gaffer.WeakMethod( self.__formatAdded ) )
 		self.__formatRemovedConnection = GafferImage.Format.formatRemovedSignal().connect( Gaffer.WeakMethod( self.__formatRemoved ) )
@@ -59,87 +59,79 @@ class FormatPlugValueWidget( GafferUI.PlugValueWidget ) :
 		
 		# Populate the widget with the names of the available formats
 		formatNames = GafferImage.Format.formatNames()
-		
-		self.__formatNameAndValues = []
-		
-		# If this plug is on the script node then do not add any "NULL" (empty) formats.
-		for i in range( 0, len( formatNames ) ) :
-			name = formatNames[i]
-			format = GafferImage.Format.getFormat( name )
-			self.__selectionMenu.addItem( name )
-			self.__formatNameAndValues.append( ( name, format ) )
+		self.__multiSelectionMenu[:] = formatNames
 			
-		# Do not add the "Default Format" option if this is in fact the default format plug!
+		# Create a mapping of the names to the formats.
+		self.__formats = {}.fromkeys( formatNames )
+		for name in formatNames :
+			self.__formats[name] = GafferImage.Format.getFormat( name )
+			
+		# Add the "Default Format" option if this is not the default format plug!
 		if not self.__isDefaultFormatPlug :
-			self.__selectionMenu.insertItem( 0, "Default Format" )
-			self.__formatNameAndValues.insert( 0, ( "Default Format", GafferImage.Format() ) )
+			self.__multiSelectionMenu.insert( 0, "Default Format" )
+			self.__formats["Default Format"] = GafferImage.Format()
 			
-		self.__currentChangedConnection = self.__selectionMenu.currentIndexChangedSignal().connect( Gaffer.WeakMethod( self.__currentChanged ) )
+		self.__currentChangedConnection = self.__multiSelectionMenu.selectionChangedSignal().connect( Gaffer.WeakMethod( self.__currentChanged ) )
 		self._updateFromPlug()
 	
 	def __formatAdded( self, name ) :
-		self.__formatNameAndValues.append( ( name, GafferImage.Format.getFormat( name ) ) )
-		self.__selectionMenu.insertItem( self.__selectionMenu.getTotal()-1, name )
+		self.__formats[name] = GafferImage.Format.getFormat( name )
+		self.__multiSelectionMenu.append( name )
 	
 	def __formatRemoved( self, name ) :
-		for index, formatNameAndValue in enumerate( self.__formatNameAndValues ) :
-			if formatNameAndValue[0] == name:
-				self.__selectionMenu.removeIndex( index )
-				del self.__formatNameAndValues[index]
-				return
+		self.__multiSelectionMenu.remove( name )
+		self.__formats.remove( name )
 	
 	def _updateFromPlug( self ) :
+		self.__multiSelectionMenu.setEnabled( self._editable() )
 		if self.getPlug() is not None:
 			with self.getContext() :
-			
+		
 				# Check to see if the format is the default instance. If so then it means we are tracking
 				# the default format and therefore do not need to change the UI.
 				if self.getPlug().getValue().getDisplayWindow().isEmpty() and not self.__isDefaultFormatPlug :
-					self.__selectionMenu.setCurrentIndex( 0 )
+					self.__multiSelectionMenu.setSelected( "Default Format" )
 					return
-				
+			
+				# Otherwise update the UI from the plug.	
 				plugValue = self.getPlug().getValue()
-				
-				matchingIndex = None
-				for index, formatNameAndValue in enumerate( self.__formatNameAndValues ) :
-					format = formatNameAndValue[1]
+				for name in self.__formats.keys() :
+					format = self.__formats[name]
 					if format == plugValue :
-						matchingIndex = index
-						break
-				if matchingIndex is not None :
-					with Gaffer.BlockedConnection( self.__currentChangedConnection ) :
-						self.__selectionMenu.setCurrentIndex( matchingIndex )
-				else:
-					GafferImage.Format.registerFormat( plugValue )
-					with Gaffer.BlockedConnection( self.__currentChangedConnection ) :
-						self.__selectionMenu.setCurrentIndex( self.__selectionMenu.getTotal()-1 )
-		
-		self.__selectionMenu.setEnabled( self._editable() )
+						with Gaffer.BlockedConnection( self.__currentChangedConnection ) :
+							self.__multiSelectionMenu.setSelected( name )
+							
+						return
+				
+				# The format is new so we should add it to our menu...
+				GafferImage.Format.registerFormat( plugValue )
+				with Gaffer.BlockedConnection( self.__currentChangedConnection ) :
+					self.__multiSelectionMenu.setSelected( self.__multiSelectionMenu[-1] )
 	
-	def __currentChanged( self, selectionMenu ) :
-		index = self.__selectionMenu.getCurrentIndex()
-		nameAndValue = self.__formatNameAndValues[ index ]
+	def __currentChanged( self, multiSelectionMenu ) :
+		selected = self.__multiSelectionMenu.getSelected()
+		format = self.__formats[ selected[0] ]
+		
 		if self.__isDefaultFormatPlug :
-			GafferImage.Format.setDefaultFormat( self.__scriptNode, nameAndValue[1] )
+			GafferImage.Format.setDefaultFormat( self.__scriptNode, format )
 		else :
-			self.getPlug().setValue( nameAndValue[1] )
+			self.getPlug().setValue( format )
 		
 		with Gaffer.UndoContext( self.__scriptNode ) :
-			self.getPlug().setValue( nameAndValue[1] )
-	
+			self.getPlug().setValue( format )
+
 	def __getitem__( self, key ) :
 		if isinstance( key, int ) :
-			return self.__formatNameAndValues[ key ]
+			return ( self.__multiSelectionMenu[key], self.__formats[self.__multiSelectionMenu[key]] )
 		if isinstance( key, str ) :
-			for index, formatNameAndValue in enumerate( self.__formatNameAndValues ) :
-				if formatNameAndValue[0] == key :
-					return formatNameAndValue[1]
+			return self.__formats[key]
 		if isinstance( key, GafferImage.Format ) :
-			for index, formatNameAndValue in enumerate( self.__formatNameAndValues ) :
-				if formatNameAndValue[1] == key :
-					return formatNameAndValue[0]
+			for name in self.__formats.keys() :
+				if self.__formats[name] == key :
+					return name
 	
 	def __len__( self ) :
-		return self.__selectionMenu.getTotal()
-	
+		return len( self.__multiSelectionMenu )
+														
 GafferUI.PlugValueWidget.registerType( GafferImage.FormatPlug.staticTypeId(), FormatPlugValueWidget )
+
--- python/GafferUI/EnumPlugValueWidget.py
+++ python/GafferUI/EnumPlugValueWidget.py
@@ -43,35 +43,32 @@ class EnumPlugValueWidget( GafferUI.PlugValueWidget ) :
 
 	def __init__( self, plug, labelsAndValues, **kw ) :
 	
-		self.__selectionMenu = GafferUI.SelectionMenu()
+		self.__selectionMenu = GafferUI.MultiSelectionMenu( allowMultipleSelection = False, alwaysHaveASelection = True)
 		GafferUI.PlugValueWidget.__init__( self, self.__selectionMenu, plug, **kw )
-	
+
 		self.__labelsAndValues = labelsAndValues
 		for label, value in self.__labelsAndValues :
-			self.__selectionMenu.addItem( label )
+			self.__selectionMenu.append( label )
 	
-		self.__currentChangedConnection = self.__selectionMenu.currentIndexChangedSignal().connect( Gaffer.WeakMethod( self.__currentChanged ) )
+		self.__currentChangedConnection = self.__selectionMenu.selectionChangedSignal().connect( Gaffer.WeakMethod( self.__currentChanged ) )
 		
 		self._updateFromPlug()
 	
 	def _updateFromPlug( self ) :
 	
+		self.__selectionMenu.setEnabled( self._editable() )
+		
 		if self.getPlug() is not None :
 			with self.getContext() :
 				plugValue = self.getPlug().getValue()
-				matchingIndex = None
-				for index, labelAndValue in enumerate( self.__labelsAndValues ) :
+				for labelAndValue in self.__labelsAndValues :
 					if labelAndValue[1] == plugValue :
-						matchingIndex = index
-						break
-				if matchingIndex is not None :
-					with Gaffer.BlockedConnection( self.__currentChangedConnection ) :
-						self.__selectionMenu.setCurrentIndex( matchingIndex )
-							
-		self.__selectionMenu.setEnabled( self._editable() )
+						with Gaffer.BlockedConnection( self.__currentChangedConnection ) :
+							self.__selectionMenu.setSelected( labelAndValue[0] )
 	
 	def __currentChanged( self, selectionMenu ) :
 	
 		with Gaffer.UndoContext( self.getPlug().ancestor( Gaffer.ScriptNode.staticTypeId() ) ) :
-			self.getPlug().setValue( self.__labelsAndValues[selectionMenu.getCurrentIndex()][1] )
+			name = selectionMenu.getSelected()[0]
+			self.getPlug().setValue( self.__labelsAndValues[ selectionMenu.index(name) ][1] )
 		
