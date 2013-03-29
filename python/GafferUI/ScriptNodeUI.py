5dd36d5a48adf4fa29dfd559f2bf2e2f452efbd8
John Haddon <john@image-engine.com>
2013-03-28 18:28:18 -0700

    Hiding unsaved changes plug in the Settings... window.

--- python/GafferUI/ScriptNodeUI.py
+++ python/GafferUI/ScriptNodeUI.py
@@ -1,6 +1,7 @@
 ##########################################################################
 #  
 #  Copyright (c) 2012, John Haddon. All rights reserved.
+#  Copyright (c) 2013, Image Engine Design Inc. All rights reserved.
 #  
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -36,6 +37,12 @@
 
 import Gaffer
 import GafferUI
+
+GafferUI.PlugValueWidget.registerCreator(
+	Gaffer.ScriptNode.staticTypeId(),
+	"unsavedChanges",
+	None
+)
 
 GafferUI.PlugValueWidget.registerCreator(
 	Gaffer.ScriptNode.staticTypeId(),
