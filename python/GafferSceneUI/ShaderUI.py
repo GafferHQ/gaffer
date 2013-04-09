e40bb74548871e64f9a9362c30536440fd34c690
John Haddon <john@image-engine.com>
2013-04-09 13:04:45 -0700

    Removed the unwanted visualisation of the "name" plug on shaders in the Node Graph.

--- python/GafferSceneUI/ShaderUI.py
+++ python/GafferSceneUI/ShaderUI.py
@@ -121,6 +121,7 @@ def __parametersNoduleCreator( plug ) :
 	return GafferUI.CompoundNodule( plug, GafferUI.LinearContainer.Orientation.Y )
 
 GafferUI.Nodule.registerNodule( GafferScene.Shader.staticTypeId(), "parameters", __parametersNoduleCreator )
+GafferUI.Nodule.registerNodule( GafferScene.Shader.staticTypeId(), "name", lambda plug : None )
 
 # we leave it to the derived class uis to register creators for the parameters.* plugs, because only the derived classes know whether
 # or not networkability makes sense in each case.
