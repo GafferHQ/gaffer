ad1ac27b5401bdf0733645e330aac088a8fb3c1a
davidsminor <davidm@image-engine.com>
2013-04-15 10:37:39 -0700

    SceneProcedural now holds a ref to the parent script node, to prevent it from getting blown away by the python garbage collector in certain cases

--- src/GafferScene/SceneProcedural.cpp
+++ src/GafferScene/SceneProcedural.cpp
@@ -45,6 +45,7 @@
 #include "IECore/MotionBlock.h"
 
 #include "Gaffer/Context.h"
+#include "Gaffer/ScriptNode.h"
 
 #include "GafferScene/SceneProcedural.h"
 #include "GafferScene/ScenePlug.h"
@@ -58,6 +59,9 @@ using namespace GafferScene;
 SceneProcedural::SceneProcedural( ScenePlugPtr scenePlug, const Gaffer::Context *context, const ScenePlug::ScenePath &scenePath, const IECore::PathMatcherData *pathsToExpand )
 	:	m_scenePlug( scenePlug ), m_context( new Context( *context ) ), m_scenePath( scenePath ), m_pathsToExpand( pathsToExpand ? pathsToExpand->copy() : 0 )
 {
+	// get a reference to the script node to prevent it being destroyed by the python garbage collector:
+	m_scriptNode = m_scenePlug->ancestor<ScriptNode>();
+	
 	m_context->set( ScenePlug::scenePathContextName, m_scenePath );
 
 	// options
@@ -90,6 +94,9 @@ SceneProcedural::SceneProcedural( const SceneProcedural &other, const ScenePlug:
 	:	m_scenePlug( other.m_scenePlug ), m_context( new Context( *(other.m_context) ) ), m_scenePath( scenePath ),
 		m_pathsToExpand( other.m_pathsToExpand ), m_options( other.m_options ), m_attributes( other.m_attributes )
 {
+	// get a reference to the script node to prevent it being destroyed by the python garbage collector:
+	m_scriptNode = m_scenePlug->ancestor<ScriptNode>();
+	
 	m_context->set( ScenePlug::scenePathContextName, m_scenePath );
 
 	updateAttributes( false );
