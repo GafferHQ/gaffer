e49954e4dd2b2336e45f3c315af6a87c0be94141
goddardl <lukeg@image-engine.com>
2013-05-03 12:46:47 -0700

    Added basic UI elements for GafferImage.

--- include/GafferUI/Style.h
+++ include/GafferUI/Style.h
@@ -42,6 +42,7 @@
 
 #include "IECore/RunTimeTyped.h"
 #include "IECore/LineSegment.h"
+#include "IECoreGL/GL.h"
 
 #include "GafferUI/TypeIds.h"
 
@@ -94,8 +95,10 @@ class Style : public IECore::RunTimeTyped
 		/// The tangents give an indication of which direction is "out" from a node.
 		virtual void renderConnection( const Imath::V3f &srcPosition, const Imath::V3f &srcTangent, const Imath::V3f &dstPosition, const Imath::V3f &dstTangent, State state = NormalState ) const = 0;
 		virtual void renderSelectionBox( const Imath::Box2f &box ) const = 0;
-		virtual void renderImage( const Imath::Box2f &box, const IECoreGL::Texture *texture ) const = 0;
+		virtual void renderImage( const Imath::Box2f &box, const IECoreGL::Texture *texture, int textureFilter = GL_LINEAR ) const = 0;
 		virtual void renderLine( const IECore::LineSegment3f &line ) const = 0;
+		virtual void renderSolidRectangle( const Imath::Box2f &box ) const = 0;
+		virtual void renderRectangle( const Imath::Box2f &box ) const = 0;
 				
 		//! @name Default style
 		/// There always exists a default style which is
