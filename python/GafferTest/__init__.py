f761e14ea719207c9dd80a61b2582ee3fa5b7228
goddardl <lukeg@image-engine.com>
2013-05-03 12:53:30 -0700

    Fixed a typo.

--- python/GafferTest/__init__.py
+++ python/GafferTest/__init__.py
@@ -100,7 +100,7 @@ from ExpressionTest import ExpressionTest
 from BlockedConnectionTest import BlockedConnectionTest
 from TimeWarpDependencyNodeTest import TimeWarpDependencyNodeTest
 from TransformPlugTest import TransformPlugTest
-from Transform2dPlugTest import Transform2dPlugTest
+from Transform2DPlugTest import Transform2DPlugTest
 from SequencePathTest import SequencePathTest
 from OpMatcherTest import OpMatcherTest
 from WeakMethodTest import WeakMethodTest
