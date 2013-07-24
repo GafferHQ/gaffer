1e6e0e94d573c170f6c4a2933db5a0798e7cea01
Ben Toogood <bent@image-engine.com>
2013-07-24 13:55:15 -0700

    updated for new way of setting layout\n\nalso, gfr file now doesn't redundantly serialsie (consequence of opening and saving script)

--- doc/GafferUserGuide/images/autoGenerated_source/interface_sceneInspector_figAB.py
+++ doc/GafferUserGuide/images/autoGenerated_source/interface_sceneInspector_figAB.py
@@ -1,6 +1,7 @@
 scriptWindow = GafferUI.ScriptWindow.acquire( script ) #load a suitable layout - shouldn't need this once layout is stored in script
-layout = GafferUI.Layouts.create( 'user:screenGrabTest', scriptWindow.scriptNode() ) #load a suitable layout - shouldn't need this once layout is stored in script
+layouts = GafferUI.Layouts.acquire( d['application'] ) #load a suitable layout - shouldn't need this once layout is stored in script
+layout = layouts.create( 'user:screenGrabTest', scriptWindow.scriptNode() ) #load a suitable layout - shouldn't need this once layout is stored in script
 scriptWindow.setLayout( layout ) #load a suitable layout - shouldn't need this once layout is stored in script
 script.selection().clear() #make sure the Display node is active - shouldn't need this once selection is stored in script
 script.selection().add(script['Display']) #make sure the Display node is active - shouldn't need this once selection is stored in script
-script['RenderManRender'].execute( [script.context()] ) #
+script['RenderManRender'].execute( [script.context()] )
