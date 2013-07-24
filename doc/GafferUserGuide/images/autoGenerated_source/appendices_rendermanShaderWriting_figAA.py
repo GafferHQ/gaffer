b2163073f7e6b832ceafadc9c73514a626ce83a8
Ben Toogood <bent@image-engine.com>
2013-07-24 14:00:40 -0700

    functional version, properly selects node

--- doc/GafferUserGuide/images/autoGenerated_source/appendices_rendermanShaderWriting_figAA.py
+++ doc/GafferUserGuide/images/autoGenerated_source/appendices_rendermanShaderWriting_figAA.py
@@ -1,17 +1,15 @@
 import GafferRenderMan
 import os
-
 scriptFile = script['fileName'].getValue()
 scriptPath = os.path.dirname(scriptFile)
-shaderNode = GafferRenderMan.RenderManShader("shaderNode")
+shaderNode = GafferRenderMan.RenderManShader('ShaderNode')
 script.addChild( shaderNode )
-shaderNode["name"].setValue('annotationsDemo')
-shaderNode["type"].setValue('ri:surface')
-shaderPath = os.path.join( scriptPath, 'annotationsExample' )
+os.environ['DL_SHADERS_PATH'] = os.environ['DL_SHADERS_PATH'] + ':' + scriptPath
+shaderPath = 'annotationsExample'
 shaderNode.loadShader( shaderPath )
-
 scriptWindow = GafferUI.ScriptWindow.acquire( script ) #load a suitable layout - shouldn't need this once layout is stored in script
-layout = GafferUI.Layouts.create( 'user:screenGrabTest', scriptWindow.scriptNode() ) #load a suitable layout - shouldn't need this once layout is stored in script
+layouts = GafferUI.Layouts.acquire( d['application'] ) #load a suitable layout - shouldn't need this once layout is stored in script
+layout = layouts.create( 'user:screenGrabJustNodeEditor', scriptWindow.scriptNode() ) #load a suitable layout - shouldn't need this once layout is stored in script
 scriptWindow.setLayout( layout ) #load a suitable layout - shouldn't need this once layout is stored in script
 script.selection().clear() #make sure the Shader node is active - shouldn't need this once selection is stored in script
-script.selection().add(script['annotationsDemo']) #make sure the Shader node is active - shouldn't need this once selection is stored in script
+script.selection().add(shaderNode) #make sure the Shader node is active - shouldn't need this once selection is stored in script
