62919500809647d0d892865c23e975d370683a3c
Ben Toogood <bent@image-engine.com>
2013-07-25 10:57:17 -0700

    redoing fixes

--- doc/GafferUserGuide/images/autoGenerated_source/appendices_rendermanShaderWriting_figAA.py
+++ doc/GafferUserGuide/images/autoGenerated_source/appendices_rendermanShaderWriting_figAA.py
@@ -8,7 +8,7 @@ os.environ['DL_SHADERS_PATH'] = os.environ['DL_SHADERS_PATH'] + ':' + scriptPath
 shaderPath = 'annotationsExample'
 shaderNode.loadShader( shaderPath )
 scriptWindow = GafferUI.ScriptWindow.acquire( script ) #load a suitable layout - shouldn't need this once layout is stored in script
-layouts = GafferUI.Layouts.acquire( d['application'] ) #load a suitable layout - shouldn't need this once layout is stored in script
+layouts = GafferUI.Layouts.acquire( application ) #load a suitable layout - shouldn't need this once layout is stored in script
 layout = layouts.create( 'user:screenGrabJustNodeEditor', scriptWindow.scriptNode() ) #load a suitable layout - shouldn't need this once layout is stored in script
 scriptWindow.setLayout( layout ) #load a suitable layout - shouldn't need this once layout is stored in script
 script.selection().clear() #make sure the Shader node is active - shouldn't need this once selection is stored in script
--- doc/GafferUserGuide/images/autoGenerated_source/interface_sceneInspector_figAB.py
+++ doc/GafferUserGuide/images/autoGenerated_source/interface_sceneInspector_figAB.py
@@ -1,5 +1,5 @@
 scriptWindow = GafferUI.ScriptWindow.acquire( script ) #load a suitable layout - shouldn't need this once layout is stored in script
-layouts = GafferUI.Layouts.acquire( d['application'] ) #load a suitable layout - shouldn't need this once layout is stored in script
+layouts = GafferUI.Layouts.acquire( application ) #load a suitable layout - shouldn't need this once layout is stored in script
 layout = layouts.create( 'user:screenGrabTest', scriptWindow.scriptNode() ) #load a suitable layout - shouldn't need this once layout is stored in script
 scriptWindow.setLayout( layout ) #load a suitable layout - shouldn't need this once layout is stored in script
 script.selection().clear() #make sure the Display node is active - shouldn't need this once selection is stored in script
