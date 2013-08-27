9140a9f8bd767956fa08037b9dd3263bcd4c82c5
Ben Toogood <bent@image-engine.com>
2013-08-26 17:57:48 -0700

    added some new quickstart image setups. changed some of the older ones to just use default layout

--- doc/GafferUserGuide/images/autoGenerated_source/quickstart_tutorial_figBA.py
+++ doc/GafferUserGuide/images/autoGenerated_source/quickstart_tutorial_figBA.py
@@ -5,8 +5,6 @@ import GafferSceneUI
 import os
 scriptNode = script
 scriptWindow = GafferUI.ScriptWindow.acquire( script )
-layout = eval( "GafferUI.CompoundEditor( scriptNode, children = ( GafferUI.SplitContainer.Orientation.Vertical, 0.972222, ( ( GafferUI.SplitContainer.Orientation.Horizontal, 0.586207, ( ( GafferUI.SplitContainer.Orientation.Vertical, 0.478221, ( {'tabs': (GafferUI.Viewer( scriptNode ),), 'tabsVisible': True, 'currentTab': 0}, {'tabs': (GafferUI.NodeGraph( scriptNode ),), 'tabsVisible': True, 'currentTab': 0} ) ), ( GafferUI.SplitContainer.Orientation.Vertical, 0.543095, ( {'tabs': (GafferUI.NodeEditor( scriptNode ), GafferSceneUI.SceneInspector( scriptNode )), 'tabsVisible': True, 'currentTab': 0}, {'tabs': (GafferSceneUI.SceneHierarchy( scriptNode ), GafferUI.ScriptEditor( scriptNode )), 'tabsVisible': True, 'currentTab': 0} ) ) ) ), {'tabs': (GafferUI.Timeline( scriptNode ),), 'tabsVisible': False, 'currentTab': 0} ) ) )" )
-scriptWindow.setLayout( layout )
 scriptWindow._Widget__qtWidget.resize(900,720)
 for nodeName in ['Sphere']:
 	script.selection().add( script.descendant( nodeName ) )
