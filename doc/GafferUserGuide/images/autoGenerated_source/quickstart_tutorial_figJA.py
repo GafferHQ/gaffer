5f46ee273eec80f3fbcd354f47c9473cd90849f3
Ben Toogood <bent@image-engine.com>
2013-08-28 17:50:20 -0700

    expanded quick start tutorial
    * added extra images
    * misc fixes in a couple of other chapters
    * added shader files for use in tutorials

--- doc/GafferUserGuide/images/autoGenerated_source/quickstart_tutorial_figJA.py
+++ doc/GafferUserGuide/images/autoGenerated_source/quickstart_tutorial_figJA.py
@@ -8,7 +8,7 @@ scriptWindow = GafferUI.ScriptWindow.acquire( script )
 layout = eval( "GafferUI.CompoundEditor( scriptNode, children = ( GafferUI.SplitContainer.Orientation.Vertical, 0.955145, ( ( GafferUI.SplitContainer.Orientation.Horizontal, 0.55, ( ( GafferUI.SplitContainer.Orientation.Vertical, 0.5, ( {'tabs': (GafferUI.Viewer( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [True]}, {'tabs': (GafferUI.NodeGraph( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [None]} ) ), ( GafferUI.SplitContainer.Orientation.Vertical, 0.538997, ( {'tabs': (GafferUI.NodeEditor( scriptNode ), GafferSceneUI.SceneInspector( scriptNode )), 'tabsVisible': True, 'currentTab': 0, 'pinned': [False, False]}, {'tabs': (GafferSceneUI.SceneHierarchy( scriptNode ), GafferUI.ScriptEditor( scriptNode )), 'tabsVisible': True, 'currentTab': 0, 'pinned': [False, None]} ) ) ) ), {'tabs': (GafferUI.Timeline( scriptNode ),), 'tabsVisible': False, 'currentTab': 0, 'pinned': [None]} ) ) )" )
 scriptWindow.setLayout( layout )
 scriptWindow._Widget__qtWidget.resize(900,800)
-for nodeName in ['ShaderAssignment']:
+for nodeName in ['demoLight']:
 	script.selection().add( script.descendant( nodeName ) )
 script.context()["ui:scene:expandedPaths"] = GafferScene.PathMatcherData( GafferScene.PathMatcher( ['/'] ) )
 script.context()["ui:scene:selectedPaths"] = IECore.StringVectorData( [  ] )
