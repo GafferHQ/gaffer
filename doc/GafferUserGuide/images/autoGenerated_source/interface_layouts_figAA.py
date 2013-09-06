26ffb29780e30261fe9c654e8b844b2460528ca9
Ben Toogood <bent@image-engine.com>
2013-09-06 16:33:58 -0700

    checkpointing some wide ranging improvements.
    - misc copy fixes and rewordings
    - added new auto-screen-grabs and some hand made pictures
    - adding kitten.png generic 2d resource for use in screen-grabs and tutorials

--- doc/GafferUserGuide/images/autoGenerated_source/interface_layouts_figAA.py
+++ doc/GafferUserGuide/images/autoGenerated_source/interface_layouts_figAA.py
@@ -5,12 +5,12 @@ import GafferSceneUI
 import os
 scriptNode = script
 scriptWindow = GafferUI.ScriptWindow.acquire( script )
-layout = eval( "GafferUI.CompoundEditor( scriptNode, children = ( GafferUI.SplitContainer.Orientation.Horizontal, 0.495437, ( ( GafferUI.SplitContainer.Orientation.Vertical, 0.528027, ( {'tabs': (GafferUI.Viewer( scriptNode ),), 'currentTab': 0}, ( GafferUI.SplitContainer.Orientation.Vertical, 0.840964, ( {'tabs': (GafferUI.NodeGraph( scriptNode ),), 'currentTab': 0}, {'tabs': (GafferUI.Timeline( scriptNode ),), 'currentTab': 0} ) ) ) ), ( GafferUI.SplitContainer.Orientation.Horizontal, 0.494647, ( ( GafferUI.SplitContainer.Orientation.Vertical, 0.651345, ( {'tabs': (GafferUI.NodeEditor( scriptNode ),), 'currentTab': 0}, {'tabs': (GafferSceneUI.SceneInspector( scriptNode ),), 'currentTab': 0} ) ), {'tabs': (GafferSceneUI.SceneHierarchy( scriptNode ),), 'currentTab': 0} ) ) ) ) )" )
+layout = eval( "GafferUI.CompoundEditor( scriptNode, children = ( GafferUI.SplitContainer.Orientation.Horizontal, 0.495974, ( ( GafferUI.SplitContainer.Orientation.Vertical, 0.529083, ( {'tabs': (GafferUI.Viewer( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [False]}, ( GafferUI.SplitContainer.Orientation.Vertical, 0.918072, ( {'tabs': (GafferUI.NodeGraph( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [None]}, {'tabs': (GafferUI.Timeline( scriptNode ),), 'tabsVisible': False, 'currentTab': 0, 'pinned': [None]} ) ) ) ), ( GafferUI.SplitContainer.Orientation.Horizontal, 0.494105, ( ( GafferUI.SplitContainer.Orientation.Vertical, 0.651007, ( {'tabs': (GafferUI.NodeEditor( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [False]}, {'tabs': (GafferSceneUI.SceneInspector( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [False]} ) ), {'tabs': (GafferSceneUI.SceneHierarchy( scriptNode ),), 'tabsVisible': True, 'currentTab': 0, 'pinned': [False]} ) ) ) ) )" )
 scriptWindow.setLayout( layout )
-scriptWindow._Widget__qtWidget.resize(1869,923)
+scriptWindow._Widget__qtWidget.resize(995,500)
 for nodeName in ['Group5']:
 	script.selection().add( script.descendant( nodeName ) )
-script.context()["ui:scene:expandedPaths"] = GafferScene.PathMatcherData( GafferScene.PathMatcher( ['/', '/group', '/group/group', '/group/group/plane', '/group/group/sphere', '/group/group1', '/group/group1/group', '/group/group1/group/plane', '/group/group1/group/sphere', '/group/group1/group1', '/group/group1/group1/group', '/group/group1/group1/text', '/group/group2', '/group/group2/group'] ) )
-script.context()["ui:scene:selectedPaths"] = IECore.StringVectorData( [ "/group/group1/group1/text" ] )
+script.context()["ui:scene:expandedPaths"] = GafferScene.PathMatcherData( GafferScene.PathMatcher( ['/', '/group', '/group/group', '/group/group/plane', '/group/group/sphere', '/group/group1', '/group/group1/group', '/group/group1/group/plane', '/group/group1/group/sphere', '/group/group1/group1', '/group/group1/group1/group', '/group/group1/group1/text', '/group/group2', '/group/group2/group', '/group/group2/group/plane', '/group/group2/group/sphere'] ) )
+script.context()["ui:scene:selectedPaths"] = IECore.StringVectorData( [ "/group/group2/group/sphere" ] )
 ##############################################################
 ## IMAGE SPECIFIC COMMANDS BELOW #############################
