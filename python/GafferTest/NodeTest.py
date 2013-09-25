2dea62c3ce6360852f877f3134a51656491ad2b2
John Haddon <john@image-engine.com>
2013-09-25 15:55:09 -0700

    Made Plug::acceptsInput() consider current output connections.
    A plug will now never accept an input if any of its immediate outputs would not themselves accept the same connection directly. This allows the RenderManShader node to accept connections to Boxes without them being used a loophole to bypass the usual connection checks for coshaders.
    
    Fixes #532.

--- python/GafferTest/NodeTest.py
+++ python/GafferTest/NodeTest.py
@@ -194,7 +194,10 @@ class NodeTest( GafferTest.TestCase ) :
 				
 			def acceptsInput( self, plug, inputPlug ) :
 			
-				return isinstance( inputPlug.node(), AcceptsInputTestNode )
+				if plug.isSame( self["in"] ) :
+					return isinstance( inputPlug.source().node(), AcceptsInputTestNode )
+					
+				return True
 	
 		n1 = AcceptsInputTestNode()
 		n2 = AcceptsInputTestNode()
@@ -202,7 +205,16 @@ class NodeTest( GafferTest.TestCase ) :
 	
 		self.assertEqual( n1["in"].acceptsInput( n2["out"] ), True )
 		self.assertEqual( n1["in"].acceptsInput( n3["sum"] ), False )
-			
+		
+		# check that we can't use a pass-through connection as
+		# a loophole.
+		
+		# this particular connection makes no sense but breaks
+		# no rules - we're just using it to test the loophole.
+		n2["out"].setInput( n3["sum"] )
+		
+		self.assertEqual( n1["in"].acceptsInput( n2["out"] ), False )
+		
 	def testPlugFlagsChangedSignal( self ) :
 	
 		n = Gaffer.Node()
