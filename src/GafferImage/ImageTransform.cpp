6c60043376c11f47fb0790e9ca4daf659b9ed863
John Haddon <thehaddonyoof@gmail.com>
2014-05-12 16:15:37 +0100

    Simplified ImageTransform::channelDataHash() and removed todo.
    Using the less verbose method of hashing all the children of a CompoundPlug.
    
    The comment in the todo was inaccurate - ImageProcessor no longer does any hashing of the tile origin. We also won't be changing the coordinate space that tile origin is specified in - tile origin, display window, and data window are all specified relative to the same origin for good reasons - sanity and ease of understanding. There's no reason the described optimisation couldn't work with the origin we have, but it seems best to implement in a node which specifically does axis-aligned integer shifts in the image without any filtering.

--- src/GafferImage/ImageTransform.cpp
+++ src/GafferImage/ImageTransform.cpp
@@ -239,17 +239,7 @@ void Implementation::hashChannelData( const GafferImage::ImagePlug *output, cons
 	filterPlug()->hash( h );
 	
 	// Finally we hash the transformation.
-
-	///\ todo: Ideally we should only hash the offset of the transform from the data window so that when translated by ImagePlug::tileSize()
-	/// we can reuse the cache. However this involves changing the ImageProcessor so that it hashes each tile relative to it's data window
-	/// rather than it's absolute coordinates. When this is implemented, uncomment the line below and delete the line that hashes translatePlug(). 
-	// h.append( Imath::V2f( t.x - floor( t.x ), t.y - floor( t.y ) ) );
-	
-	transformPlug()->translatePlug()->hash( h );
-	
-	transformPlug()->scalePlug()->hash( h );
-	transformPlug()->pivotPlug()->hash( h );
-	transformPlug()->rotatePlug()->hash( h );
+	transformPlug()->hash( h );
 }
 
 Imath::Box2i Implementation::computeDataWindow( const Gaffer::Context *context, const ImagePlug *parent ) const
