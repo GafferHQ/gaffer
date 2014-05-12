GafferHQ/gaffer

commit 78b8739f1867ecd3b660838d384cdd6083734d92

Refactored ImageNode hashing for improved performance and clarity.
The refactoring has helped my understanding of things in hunting down a hash bug affecting disabled nodes downstream from a Display. It now matches the SceneNode scheme much more closely, with the following improvements :

- No longer calls ComputeNode::hash() only to overwrite it in the case of disabled nodes - performance improvement.
- Much simpler implementation for ImageProcessor::hash() and ImageProcessor::compute().
- No longer appends enabledPlug() to the hash output of all plugs (bugfix).
- Clearer documentation and rules for derived class hash*() methods. Before they would need to call the base class implementation if they were derived down a few levels, but not if they were derived from the base class. Now they must always call the base class implementation unless wishing to pass through a hash.
- Clear documentation of the effects of enabled() and channelEnabled() with regard to not calling any of the hash*() or compute*() methods.

src/GafferImage/ImageTransform.cpp
@@ -211,17 +211,21 @@ void Implementation::hashFormat( const GafferImage::ImagePlug *output, const Gaf
 
 void Implementation::hashDataWindow( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashDataWindow( output, context, h );
 	inPlug()->dataWindowPlug()->hash( h );
 	transformPlug()->hash( h );
 }
 
 void Implementation::hashChannelNames( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashChannelNames( output, context, h );
 	inPlug()->channelNamesPlug()->hash( h );
 }
 
 void Implementation::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashChannelData( output, context, h );
+
 	// Hash all of the tiles that the sample requires for this tile.	
 	Imath::V2i tileOrigin( Context::current()->get<Imath::V2i>( ImagePlug::tileOriginContextName ) );
 	std::string channelName( Context::current()->get<std::string>( ImagePlug::channelNameContextName ) );

include/GafferImage/ImageNode.h
@@ -76,24 +76,32 @@ class ImageNode : public Gaffer::ComputeNode
 		/// enabled() is called to query the nodes state when hashing and computing the image plug.
 		/// When computing or hashing the channelData plug channelEnabled( channel ) is also called
 		/// to query if the particular channel is enabled or not.
-		/// The default implementation of enabled returns the value of the enabled plug.
 		///
 		/// Derived classes can overide one or both methods to return false if their plugs are in
 		/// a state that makes the node produce no effect. This stops duplication of data in the
-		/// cache and improves performance and memory efficiency.
+		/// cache and improves performance and memory efficiency. It is guaranteed that the hash*()
+		/// methods below and compute*() methods below will never be called if enabled() is false,
+		/// or channelEnabled() is false (in the case of *channelData()).
+		///
 		/// Any derived classes that do reimplement these methods need to call the respective method
 		/// on the base class before then computing whether or not it is in fact enabled.
 		virtual bool channelEnabled( const std::string &channel ) const { return true; };
+		/// The default implementation of enabled returns the value of the enabled plug.
 		virtual bool enabled() const;
 			
-		/// Implemented to append the image:channelName and image:tileOrigin context entries to the hash where appropriate,
-		/// and then call the hash*() methods below whenever output is part of an ImagePlug. Derived classes should reimplement
-		/// the specific hash*() methods rather than hash() itself.
+		/// Implemented to call the hash*() methods below whenever output is part of an ImagePlug and the node is enabled.
 		virtual void hash( const Gaffer::ValuePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const;
-		virtual void hashFormat( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
-		virtual void hashDataWindow( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
-		virtual void hashChannelNames( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
-		virtual void hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
+		/// Hash methods for the individual children of an image output - these must be implemented by derived classes.
+		/// An implementation must do one or the other of the following :
+		///
+		/// - Call the base class implementation and then append to the hash with any plugs and context items they
+		///   will use in the corresponding compute*() method.
+		/// - Assign directly to the hash from some input hash to signify that an input will be passed through
+		///   unchanged by the corresponding compute*() method.
+		virtual void hashFormat( const GafferImage::ImagePlug *parent, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
+		virtual void hashDataWindow( const GafferImage::ImagePlug *parent, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
+		virtual void hashChannelNames( const GafferImage::ImagePlug *parent, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
+		virtual void hashChannelData( const GafferImage::ImagePlug *parent, const Gaffer::Context *context, IECore::MurmurHash &h ) const = 0;
 
 		/// Implemented to call the compute*() methods below whenever output is part of an ImagePlug and the node is enabled.
 		/// Derived classes should reimplement the specific compute*() methods rather than compute() itself.
@@ -102,8 +110,6 @@ class ImageNode : public Gaffer::ComputeNode
 		virtual Imath::Box2i computeDataWindow( const Gaffer::Context *context, const ImagePlug *parent ) const = 0;
 		virtual IECore::ConstStringVectorDataPtr computeChannelNames( const Gaffer::Context *context, const ImagePlug *parent ) const = 0;
 		virtual IECore::ConstFloatVectorDataPtr computeChannelData( const std::string &channelName, const Imath::V2i &tileOrigin, const Gaffer::Context *context, const ImagePlug *parent ) const = 0;
-		
-		void computeImagePlugs( Gaffer::ValuePlug *output, const Gaffer::Context *context ) const;
 		
 		/// Implemented to initialize the default format settings if they don't exist already.
 		void parentChanging( Gaffer::GraphComponent *newParent );

include/GafferImage/ImagePrimitiveSource.inl
@@ -96,24 +96,28 @@ void ImagePrimitiveSource<BaseType>::hash( const Gaffer::ValuePlug *output, cons
 template<typename BaseType>
 void ImagePrimitiveSource<BaseType>::hashFormat( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	BaseType::hashFormat( output, context, h );
 	inputImagePrimitivePlug()->hash( h );
 }
 
 template<typename BaseType>
 void ImagePrimitiveSource<BaseType>::hashChannelNames( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	BaseType::hashChannelNames( output, context, h );
 	inputImagePrimitivePlug()->hash( h );
 }
 
 template<typename BaseType>
 void ImagePrimitiveSource<BaseType>::hashDataWindow( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	BaseType::hashDataWindow( output, context, h );
 	inputImagePrimitivePlug()->hash( h );
 }
 
 template<typename BaseType>
 void ImagePrimitiveSource<BaseType>::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	BaseType::hashChannelData( output, context, h );
 	inputImagePrimitivePlug()->hash( h );
 }
 		

include/GafferImage/ImageProcessor.h
@@ -64,11 +64,9 @@ class ImageProcessor : public ImageNode
 
 	protected :
 	
-		/// Reimplemented to pass through the hashes of the first input when the node is disabled. When it is not
-		/// it will call hash*Plug() so that derived classes can implement their own hashing functions.
+		/// Reimplemented to pass through the hashes of the inPlug() when the node is disabled.
 		virtual void hash( const Gaffer::ValuePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const;	
-		/// Reimplements the functionality of ImageNode::compute to pass through
-		/// the first input if the node is disabled.
+		/// Reimplemented from ImageNode to pass through the inPlug() computations when the node is disabled.
 		virtual void compute( Gaffer::ValuePlug *output, const Gaffer::Context *context ) const;
 		
 	private :

src/GafferImage/ColorProcessor.cpp
@@ -185,6 +185,7 @@ IECore::ConstStringVectorDataPtr ColorProcessor::computeChannelNames( const Gaff
 
 void ColorProcessor::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashChannelData( output, context, h );
 	colorDataPlug()->hash( h );
 }
 

src/GafferImage/Constant.cpp
@@ -38,6 +38,8 @@
 #include "OpenImageIO/imagecache.h"
 OIIO_NAMESPACE_USING
 
+#include "Gaffer/Context.h"
+
 #include "GafferImage/Constant.h"
 
 using namespace std;
@@ -114,22 +116,28 @@ void Constant::affects( const Gaffer::Plug *input, AffectedPlugsContainer &outpu
 
 void Constant::hashFormat( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageNode::hashFormat( output, context, h );
 	h.append( formatPlug()->hash() );
 }
 
 void Constant::hashChannelNames( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageNode::hashChannelNames( output, context, h );
 	colorPlug()->hash( h );
 }
 
 void Constant::hashDataWindow( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageNode::hashDataWindow( output, context, h );
 	h.append( formatPlug()->hash() );
 }
 
 void Constant::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
-	// Don't bother hashing the format here as we couldn't care less about the size of the canvas, only the colour!
+	ImageNode::hashChannelData( output, context, h );
+	// Don't bother hashing the format or tile origin here as we couldn't care less about the
+	// position on the canvas, only the colour!
+	h.append( context->get<std::string>( ImagePlug::channelNameContextName ) );
 	colorPlug()->hash( h );
 }
 

src/GafferImage/FilterProcessor.cpp
@@ -100,6 +100,8 @@ bool FilterProcessor::enabled() const
 
 void FilterProcessor::hashFormat( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashFormat( output, context, h );
+	
 	const ImagePlugList& inputs( m_inputs.inputs() );
 	const ImagePlugList::const_iterator end( m_inputs.endIterator() );
 	for( ImagePlugList::const_iterator it( inputs.begin() ); it != end; it++ )
@@ -110,6 +112,8 @@ void FilterProcessor::hashFormat( const GafferImage::ImagePlug *output, const Ga
 
 void FilterProcessor::hashDataWindow( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashDataWindow( output, context, h );
+
 	const ImagePlugList& inputs( m_inputs.inputs() );
 	const ImagePlugList::const_iterator end( m_inputs.endIterator() );
 	for( ImagePlugList::const_iterator it( inputs.begin() ); it != end; it++ )
@@ -120,6 +124,8 @@ void FilterProcessor::hashDataWindow( const GafferImage::ImagePlug *output, cons
 
 void FilterProcessor::hashChannelNames( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashChannelNames( output, context, h );
+
 	const ImagePlugList& inputs( m_inputs.inputs() );
 	const ImagePlugList::const_iterator end( m_inputs.endIterator() );
 	for( ImagePlugList::const_iterator it( inputs.begin() ); it != end; it++ )
@@ -130,6 +136,8 @@ void FilterProcessor::hashChannelNames( const GafferImage::ImagePlug *output, co
 
 void FilterProcessor::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ImageProcessor::hashChannelData( output, context, h );
+
 	const ImagePlugList& inputs( m_inputs.inputs() );
 	const ImagePlugList::const_iterator end( m_inputs.endIterator() );
 	for( ImagePlugList::const_iterator it( inputs.begin() ); it != end; it++ )

src/GafferImage/Grade.cpp
@@ -218,6 +218,8 @@ void Grade::affects( const Gaffer::Plug *input, AffectedPlugsContainer &outputs 
 
 void Grade::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
+	ChannelDataProcessor::hashChannelData( output, context, h );
+
 	inPlug()->channelDataPlug()->hash( h );
 
 	std::string channelName = context->get<std::string>( ImagePlug::channelNameContextName );

commit df2882ac837acbcb96855336b3d185e0fd3d28f0

Fixed bug in ImageTransform.
This was caused by ImageTransform::hashChannelData() not correctly including the tile origin in the hash. For transforms where two output tiles share the exact same set of input tiles affecting them, we were computing exactly the same hash for both output tiles, even though they each would filter the pixels in the input tiles totally differently.

This bug was introduced in 78b8739f1867ecd3b660838d384cdd6083734d92, where I refactored the hash computation for images. Specifically, in that commit it became the responsibility of derived classes to hash in the tile origin where necessary whereas before the SceneProcessor _always_ hashed in the tile origin whether it was necessary or not. The old code resulted in unnecessary computation for networks like Constant->Grade, where although all the tiles have the same hash in the Constant, they would have different hashes in the Grade. The new way of doing things is for sure better, provided you're not a numpty like I was and fail to hash in the tile origin where relevant.

Also renamed the test case, so it actually makes sense now we've fixed things - in any case we should have used @GafferTest.expectedFailure to decorate it in the first place so we could still give it a meaningful name.

src/GafferImage/ImageTransform.cpp
@@ -231,6 +231,10 @@ void Implementation::hashChannelData( const GafferImage::ImagePlug *output, cons
 	Sampler sampler( inPlug(), channelName, tile, filter );
 	sampler.hash( h );
 	
+	// Hash in the origin of the output tile. Multiple output tiles may share the exact same set of input
+	// tiles, but will reference different parts of them depending on the tile origin.
+	h.append( tileOrigin );
+
 	// Hash the filter type that we are using.	
 	filterPlug()->hash( h );
 	

python/GafferImageTest/ImageTransformTest.py
@@ -63,8 +63,13 @@ class ImageTransformTest( unittest.TestCase ) :
 			h2 = r["out"].hash()	
 			self.assertEqual( h1, h2 )	
 
-	def testKnownFailure( self ) :
-		'''Highlights a bug that has been introduced since a previous version (tested against 0.83.0).'''
+	def testTilesWithSameInputTiles( self ) :
+
+		# This particular transform (along with many others) has output tiles
+		# which share the exact same set of input tiles affecting their result.
+		# This revealed a bug in ImageTransform::hashChannelData() whereby the
+		# tile origin wasn't being hashed in to break the hashes for these output
+		# tiles apart.
 
 		r = GafferImage.ImageReader()
 		r["fileName"].setValue( os.path.join( self.path, "rgb.100x100.exr" ) )