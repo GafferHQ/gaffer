cb28a7a4607ccf8ca2e3f356188619892cd988e5
John Haddon <thehaddonyoof@gmail.com>
2014-08-26 12:41:26 +0100

    Added a GafferScene config to provide Displays back-compatibility.
    Keeping this sort of stuff out of the main codebase as much as possible seems pretty reasonable.

---
+++ startup/GafferScene/displaysCompatibility.py
@@ -0,0 +1,57 @@
+##########################################################################
+#  
+#  Copyright (c) 2014, Image Engine Design Inc. All rights reserved.
+#  
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#  
+#      * Redistributions of source code must retain the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer.
+#  
+#      * Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided with
+#        the distribution.
+#  
+#      * Neither the name of John Haddon nor the names of
+#        any other contributors to this software may be used to endorse or
+#        promote products derived from this software without specific prior
+#        written permission.
+#  
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#  
+##########################################################################
+
+import IECore
+
+import GafferScene
+
+## A little wrapper sufficient to load old Displays nodes
+# and cause them to be turned into Outputs nodes on the
+# next save.
+class Displays( GafferScene.Outputs ) :
+
+	def __init__( self, name = "Displays" ) :
+	
+		GafferScene.Outputs.__init__( self, name )
+		
+	def __getitem__( self, key ) :
+		
+		key = "outputs" if key == "displays" else key
+		return GafferScene.Outputs.__getitem__( self, key )
+
+IECore.registerRunTimeTyped( Displays, typeName = "GafferScene::Displays" )
+
+GafferScene.Displays = Displays
