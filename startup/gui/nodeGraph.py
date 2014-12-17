68cb1c77c69b7810b58fbd64d4ddf9d649943a61
John Haddon <thehaddonyoof@gmail.com>
2014-12-17 12:31:14 +0000

    Added basic colour scheme for nodes via startup/gui/nodeGraph.py.
    Kindof fixes #89 and kindof fixes #1098 and either fixes or lays the foundations for #154, but I'm sure other people will have different ideas on what colours are best.

--- startup/gui/nodeGraph.py
+++ startup/gui/nodeGraph.py
@@ -37,8 +37,55 @@
 
 import IECore
 
+import Gaffer
 import GafferUI
+import GafferScene
 import GafferSceneUI
+
+##########################################################################
+# Colour
+##########################################################################
+
+Gaffer.Metadata.registerNodeValue( Gaffer.ExecutableNode, "nodeGadget:color", IECore.Color3f( 0.61, 0.1525, 0.1525 ) )
+Gaffer.Metadata.registerPlugValue( Gaffer.ExecutableNode, "requirement", "nodule:color", IECore.Color3f( 0.645, 0.2483, 0.2483 ) )
+Gaffer.Metadata.registerPlugValue( Gaffer.ExecutableNode, "requirements.*", "nodule:color", IECore.Color3f( 0.645, 0.2483, 0.2483 ) )
+Gaffer.Metadata.registerPlugValue( Gaffer.ExecutableNode, "requirement", "connectionGadget:color", IECore.Color3f( 0.315, 0.0787, 0.0787 ) )
+Gaffer.Metadata.registerPlugValue( Gaffer.ExecutableNode, "requirements.*", "connectionGadget:color", IECore.Color3f( 0.315, 0.0787, 0.0787 ) )
+
+Gaffer.Metadata.registerNodeValue( Gaffer.SubGraph, "nodeGadget:color", IECore.Color3f( 0.225 ) )
+
+Gaffer.Metadata.registerPlugValue( GafferScene.SceneNode, "in*", "nodule:color", IECore.Color3f( 0.2346, 0.326, 0.46 ) )
+Gaffer.Metadata.registerPlugValue( GafferScene.SceneNode, "out", "nodule:color", IECore.Color3f( 0.2346, 0.326, 0.46 ) )
+Gaffer.Metadata.registerPlugValue( GafferScene.ExecutableRender, "in", "nodule:color", IECore.Color3f( 0.2346, 0.326, 0.46 ) )
+Gaffer.Metadata.registerPlugValue( GafferScene.InteractiveRender, "in", "nodule:color", IECore.Color3f( 0.2346, 0.326, 0.46 ) )
+Gaffer.Metadata.registerPlugValue( GafferScene.Parent, "child", "nodule:color", IECore.Color3f( 0.2346, 0.326, 0.46 ) )
+
+Gaffer.Metadata.registerNodeValue( GafferScene.SceneProcessor, "nodeGadget:color", IECore.Color3f( 0.495, 0.2376, 0.4229 ) )
+Gaffer.Metadata.registerNodeValue( GafferScene.SceneElementProcessor, "nodeGadget:color", IECore.Color3f( 0.1886, 0.2772, 0.41 ) )
+
+Gaffer.Metadata.registerPlugValue( GafferScene.FilteredSceneProcessor, "filter", "nodule:color", IECore.Color3f( 0.69, 0.5378, 0.2283 ) )
+Gaffer.Metadata.registerPlugValue( GafferScene.Filter, "match", "nodule:color", IECore.Color3f( 0.69, 0.5378, 0.2283 ) )
+
+Gaffer.Metadata.registerNodeValue( GafferScene.Transform, "nodeGadget:color", IECore.Color3f( 0.485, 0.3112, 0.2255 ) )
+Gaffer.Metadata.registerNodeValue( GafferScene.Constraint, "nodeGadget:color", IECore.Color3f( 0.485, 0.3112, 0.2255 ) )
+
+Gaffer.Metadata.registerNodeValue( GafferScene.GlobalsProcessor, "nodeGadget:color", IECore.Color3f( 0.255, 0.505, 0.28 ) )
+
+__shaderNoduleColors = {
+	Gaffer.FloatPlug.staticTypeId() : IECore.Color3fData( IECore.Color3f( 0.2467, 0.3762, 0.47 ) ),
+	Gaffer.Color3fPlug.staticTypeId() : IECore.Color3fData( IECore.Color3f( 0.69, 0.5378, 0.2283 ) ),
+	Gaffer.V3fPlug.staticTypeId() : IECore.Color3fData( IECore.Color3f( 0.47, 0.181, 0.181 ) ),
+}
+
+def __shaderNoduleColor( plug ) :
+
+	return __shaderNoduleColors.get( plug.typeId(), None )
+
+Gaffer.Metadata.registerPlugValue( GafferScene.Shader, "*", "nodule:color", __shaderNoduleColor )
+
+##########################################################################
+# Behaviour
+##########################################################################
 
 ## \todo Make this behaviour a part of the preferences.
 def __nodeDoubleClick( nodeGraph, node ) :
