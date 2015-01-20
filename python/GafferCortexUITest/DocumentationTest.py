9abb97151e3bd4224981a38a139d5f90b66f3d76
John Haddon <thehaddonyoof@gmail.com>
2015-01-20 18:50:25 +0000

    Added test case verifying all GafferCortex nodes are documented.

--- python/GafferCortexUITest/DocumentationTest.py
+++ python/GafferCortexUITest/DocumentationTest.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2014, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -34,9 +34,16 @@
 #
 ##########################################################################
 
-from CompoundParameterValueWidgetTest import CompoundParameterValueWidgetTest
-from ParameterValueWidgetTest import ParameterValueWidgetTest
-from DocumentationTest import DocumentationTest
+import GafferUITest
+import GafferCortex
+import GafferCortexUI
+
+class DocumentationTest( GafferUITest.TestCase ) :
+
+	def test( self ) :
+
+		self.maxDiff = None
+		self.assertNodesAreDocumented( GafferCortex )
 
 if __name__ == "__main__":
 	unittest.main()
