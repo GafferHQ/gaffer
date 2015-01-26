ecc3ab40d008d8030e40defebc596c13c8c1c3d7
Cedric Launay <cedricl@image-engine.com>
2015-01-26 10:46:17 -0800

    Expose actual traceBias value.

--- python/GafferRenderManUI/RenderManAttributesUI.py
+++ python/GafferRenderManUI/RenderManAttributesUI.py
@@ -90,7 +90,7 @@ def __raytracingSummary( plug ) :
 	if plug["traceDisplacements"]["enabled"].getValue() :
 		info.append( "Displacements %s" % ( "On" if plug["traceDisplacements"]["value"].getValue() else "Off" ) )
 	if plug["traceBias"]["enabled"].getValue() :
-		info.append( "Trace Bias %s" % ( "On" if plug["traceBias"]["value"].getValue() else "Off" ) )
+		info.append( "Trace Bias %g" % plug["traceBias"]["value"].getValue() )
 
 	return ", ".join( info )
 
