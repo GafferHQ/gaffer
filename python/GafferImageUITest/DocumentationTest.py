102344585f298689f8419fd31db0054789d900bb
John Haddon <thehaddonyoof@gmail.com>
2015-02-17 12:28:24 +0000

    Added test case verifying all GafferImage nodes are documented.

--- python/GafferImageUITest/DocumentationTest.py
+++ python/GafferImageUITest/DocumentationTest.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2013, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -34,9 +34,20 @@
 #
 ##########################################################################
 
-from FormatPlugValueWidgetTest import FormatPlugValueWidgetTest
-from ImageViewTest import ImageViewTest
-from DocumentationTest import DocumentationTest
+import GafferUITest
+
+import GafferImage
+import GafferImageUI
+
+class DocumentationTest( GafferUITest.TestCase ) :
+
+	def test( self ) :
+
+		self.maxDiff = None
+		self.assertNodesAreDocumented(
+			GafferImage,
+			additionalTerminalPlugTypes = ( GafferImage.ImagePlug, )
+		)
 
 if __name__ == "__main__":
 	unittest.main()
