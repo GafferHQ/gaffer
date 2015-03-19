10f0bd2806ab93adf62767ae6bc687f24893a1b0
Andrew Kaufman <andrewk.imageengine@gmail.com>
2015-03-18 17:17:14 -0700

    Fixed failing ImageWriter test.
    I have no idea why this is passing on the Travis builds, but failing at Image Engine. But it seems to be failing back to Gaffer 0.8.0.0 at Image Engine, so I'm fairly confident it wasn't introduced by this branch. In any case, the change doesn't affect the integrity of the test, so I'm going to leave that mystery for another time...

--- python/GafferImageTest/ImageWriterTest.py
+++ python/GafferImageTest/ImageWriterTest.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2013-2014, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2013-2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -255,7 +255,11 @@ class ImageWriterTest( unittest.TestCase ) :
 		s["w"] = GafferImage.ImageWriter()
 		s["w"]["in"].setInput( s["c"]["out"] )
 
-		self.assertEqual( s["c"]["out"].image(), s["w"]["out"].image() )
+		with s.context() :
+			ci = s["c"]["out"].image()
+			wi = s["w"]["out"].image()
+		
+		self.assertEqual( ci, wi )
 
 	def testPassThroughSerialisation( self ) :
 
