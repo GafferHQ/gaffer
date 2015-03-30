eb95b38cdbe5e67888708c0e0d1376d489ec4912
Andrew Kaufman <andrewk.imageengine@gmail.com>
2015-03-30 15:30:00 -0700

    Labels for plugs set to their userDefault no longer display as changed.
    Note that for plugs with userDefaults, the labels now display as changed when set to the real default value. Some people seemed confused that there is any difference between the real default and the userDefault. After using it a bit, it seemed more natural that the user-facing UI should consider the userDefault as the true default, even if the API doesn't.
    
    Fixes #1216

--- python/GafferUI/LabelPlugValueWidget.py
+++ python/GafferUI/LabelPlugValueWidget.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2013, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2013-2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -126,7 +126,10 @@ class LabelPlugValueWidget( GafferUI.PlugValueWidget ) :
 
 		valueChanged = plug.getInput() is not None
 		if not valueChanged and isinstance( plug, Gaffer.ValuePlug ) :
-			valueChanged = valueChanged or not plug.isSetToDefault()
+			if Gaffer.NodeAlgo.hasUserDefault( plug ) :
+				valueChanged = valueChanged or not Gaffer.NodeAlgo.isSetToUserDefault( plug )
+			else :
+				valueChanged = valueChanged or not plug.isSetToDefault()
 		self.__setValueChanged( valueChanged )
 
 	# Sets whether or not the label be rendered in a ValueChanged state.
