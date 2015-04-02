0a77a14c34d069a4ac21e2f50c1f98eca616b1d2
John Haddon <thehaddonyoof@gmail.com>
2015-04-02 11:50:16 +0100

    Documented AimConstraint node.

--- python/GafferSceneUI/AimConstraintUI.py
+++ python/GafferSceneUI/AimConstraintUI.py
@@ -1,7 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2012, John Haddon. All rights reserved.
-#  Copyright (c) 2012-2014, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -35,68 +34,43 @@
 #
 ##########################################################################
 
-from _GafferSceneUI import *
+import Gaffer
+import GafferScene
 
-from SceneHierarchy import SceneHierarchy
-from SceneInspector import SceneInspector
-from FilterPlugValueWidget import FilterPlugValueWidget
-import SceneNodeUI
-import SceneReaderUI
-import SceneProcessorUI
-import FilteredSceneProcessorUI
-import PruneUI
-import SubTreeUI
-import OutputsUI
-import OptionsUI
-import OpenGLAttributesUI
-import SceneContextVariablesUI
-import SceneWriterUI
-import StandardOptionsUI
-import StandardAttributesUI
-import ShaderUI
-import OpenGLShaderUI
-import ObjectSourceUI
-import TransformUI
-import AttributesUI
-import LightUI
-import InteractiveRenderUI
-import SphereUI
-import MapProjectionUI
-import MapOffsetUI
-import CustomAttributesUI
-import CustomOptionsUI
-import SceneViewToolbar
-import SceneSwitchUI
-import ShaderSwitchUI
-import ShaderAssignmentUI
-import ParentConstraintUI
-import ParentUI
-import PrimitiveVariablesUI
-import DuplicateUI
-import GridUI
-import SetFilterUI
-import DeleteGlobalsUI
-import DeleteOptionsUI
-import DeleteSetsUI
-import ExternalProceduralUI
-import ExecutableRenderUI
-import IsolateUI
-import SelectionToolUI
-import CropWindowToolUI
-import CameraUI
-import SetUI
-import ClippingPlaneUI
-import FilterUI
-import FilterSwitchUI
-import PointsTypeUI
-import ParametersUI
-import TextUI
-import AimConstraintUI
+Gaffer.Metadata.registerNode(
 
-# then all the PathPreviewWidgets. note that the order
-# of import controls the order of display.
+	GafferScene.AimConstraint,
 
-from AlembicPathPreview import AlembicPathPreview
-from SceneReaderPathPreview import SceneReaderPathPreview
+	"description",
+	"""
+	Transforms objects so that they are aimed at
+	a specified target.
+	""",
 
-__import__( "IECore" ).loadConfig( "GAFFER_STARTUP_PATHS", {}, subdirectory = "GafferSceneUI" )
+	plugs = {
+
+		"aim" : [
+
+			"description",
+			"""
+			The aim vector, specified in object space. The
+			object will be transformed so that this vector
+			points at the target.
+			""",
+
+		],
+
+		"up" : [
+
+			"description",
+			"""
+			The up vector, specified in object space. The
+			object will be transformed so that this vector
+			points up in world space, as far as is possible.
+			""",
+
+		],
+
+	}
+
+)
