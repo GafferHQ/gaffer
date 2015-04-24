GafferHQ/gaffer

commit 6b7851c77aabdc7f66f57b5495073304d8003562

Added support for functools.partial( WeakMethod ) in Menu commands.

python/GafferUI/Menu.py
@@ -137,7 +137,7 @@ class Menu( GafferUI.Widget ) :
 		elif isinstance( function, Gaffer.WeakMethod ) :
 			return inspect.getargspec( function.method() )[0][1:]
 		elif isinstance( function, functools.partial ) :
-			return inspect.getargspec( function.func )[0]
+			return self.__argNames( function.func )[0]
 
 		return []
 

commit 44ff67aea97465ca90fe68d2fab7aaf537ad29a5

Menu : Fixed support for functools.partial( classMethod ) commands.
This was broken by 6b7851c77aabdc7f66f57b5495073304d8003562, which manifested itself as the node "Set Color..." command not working any more.

python/GafferUI/Menu.py
@@ -134,10 +134,12 @@ class Menu( GafferUI.Widget ) :
 
 		if isinstance( function, types.FunctionType ) :
 			return inspect.getargspec( function )[0]
+		elif isinstance( function, types.MethodType ) :
+			return inspect.getargspec( function )[0][1:]
 		elif isinstance( function, Gaffer.WeakMethod ) :
 			return inspect.getargspec( function.method() )[0][1:]
 		elif isinstance( function, functools.partial ) :
-			return self.__argNames( function.func )[0]
+			return self.__argNames( function.func )
 
 		return []
 