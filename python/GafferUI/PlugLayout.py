3996a676fff94865bf6b25235c3956a479a8da43
John Haddon <thehaddonyoof@gmail.com>
2015-04-27 16:37:18 +0100

    PlugLayout : Added support for "layout:section" metadata.
    This allows the layout to be split into sections, and will provide the basis for replacing the Sectioned* widgets, adding support for sections in the UIEditor, and replacing the section management code in the StandardNodeUI.

--- python/GafferUI/PlugLayout.py
+++ python/GafferUI/PlugLayout.py
@@ -35,6 +35,8 @@
 #
 ##########################################################################
 
+import collections
+
 import Gaffer
 import GafferUI
 
@@ -43,6 +45,7 @@ import GafferUI
 # Supported metadata :
 #
 #	- "layout:index" controls ordering of plugs within the layout
+#	- "layout:section" places the plug in a named section of the layout
 #	- "divider" specifies whether or not a plug should be followed by a divider
 #	- "layout:widgetType" the class name for the widget type of a particular plug
 #
@@ -50,11 +53,11 @@ class PlugLayout( GafferUI.Widget ) :
 
 	def __init__( self, parent, orientation = GafferUI.ListContainer.Orientation.Vertical, **kw ) :
 
-		assert( isinstance( parent, ( Gaffer.Node, Gaffer.CompoundPlug ) ) )
+		assert( isinstance( parent, ( Gaffer.Node, Gaffer.Plug ) ) )
 
-		self.__mainContainer = GafferUI.ListContainer( orientation, spacing = 4 )
+		self.__layout = _TabLayout( orientation ) if isinstance( parent, Gaffer.Node ) else _CollapsibleLayout( orientation )
 
-		GafferUI.Widget.__init__( self, self.__mainContainer, **kw )
+		GafferUI.Widget.__init__( self, self.__layout, **kw )
 
 		self.__parent = parent
 		self.__readOnly = False
@@ -141,24 +144,34 @@ class PlugLayout( GafferUI.Widget ) :
 			( plug, widget ) for plug, widget in self.__plugsToWidgets.items() if plug in plugsSet
 		)
 
-		# make (or reuse existing) uis for each plug
-		orderedWidgets = []
+		# make (or reuse existing) widgets for each plug, and sort them into
+		# sections.
+		rootSection = _Section()
 		for plug in plugs :
+
 			if plug not in self.__plugsToWidgets :
 				widget = self.__createPlugWidget( plug )
 				self.__plugsToWidgets[plug] = widget
 			else :
 				widget = self.__plugsToWidgets[plug]
 
-			if widget is not None :
-				orderedWidgets.append( widget )
-				if Gaffer.Metadata.plugValue( plug, "divider" ) :
-					orderedWidgets.append( GafferUI.Divider(
-						GafferUI.Divider.Orientation.Horizontal if self.__mainContainer.orientation() == GafferUI.ListContainer.Orientation.Vertical else GafferUI.Divider.Orientation.Vertical
-					) )
+			if widget is None :
+				continue
 
-		# and update the column to display them in the right order
-		self.__mainContainer[:] = orderedWidgets
+			section = rootSection
+			for sectionName in self.__sectionPath( plug ) :
+				section = section.subsections.setdefault( sectionName, _Section() )
+
+			section.widgets.append( widget )
+
+			if Gaffer.Metadata.plugValue( plug, "divider" ) :
+				section.widgets.append( GafferUI.Divider(
+					GafferUI.Divider.Orientation.Horizontal if self.__layout.orientation() == GafferUI.ListContainer.Orientation.Vertical else GafferUI.Divider.Orientation.Vertical
+				) )
+
+		# delegate to our layout class to create a concrete
+		# layout from the section definitions.
+		self.__layout.update( rootSection )
 
  	def __createPlugWidget( self, plug ) :
 
@@ -182,7 +195,7 @@ class PlugLayout( GafferUI.Widget ) :
 
  		if not result.hasLabel() and Gaffer.Metadata.plugValue( plug, "label" ) != "" :
  			result = GafferUI.PlugWidget( result )
-			if self.__mainContainer.orientation() == GafferUI.ListContainer.Orientation.Horizontal :
+			if self.__layout.orientation() == GafferUI.ListContainer.Orientation.Horizontal :
 				# undo the annoying fixed size the PlugWidget has applied
 				# to the label.
 				## \todo Shift all the label size fixing out of PlugWidget and just fix the
@@ -193,6 +206,21 @@ class PlugLayout( GafferUI.Widget ) :
 		self.__applyReadOnly( result )
 
  		return result
+
+	def __sectionPath( self, plug ) :
+
+		m = None
+		if isinstance( self.__parent, Gaffer.Node ) :
+			# Backwards compatibility with old metadata entry
+			## \todo Remove
+			m = Gaffer.Metadata.plugValue( plug, "nodeUI:section" )
+			if m == "header" :
+				m = ""
+
+		if m is None :
+			m = Gaffer.Metadata.plugValue( plug, "layout:section" )
+
+		return m.split( "." ) if m else []
 
 	def __visibilityChanged( self, widget ) :
 
@@ -249,7 +277,101 @@ class PlugLayout( GafferUI.Widget ) :
 		if not node.isInstanceOf( node.typeId() ) :
 			return
 
-		if key in ( "divider", "layout:index" ) :
+		if key in ( "divider", "layout:index", "layout:section" ) :
 			# we often see sequences of several metadata changes - so
 			# we schedule an update on idle to batch them into one ui update.
 			self.__scheduleUpdate()
+
+# The _Section class provides a simple abstract representation of a hierarchical
+# layout. Each section contains a list of widgets, and an OrderedDict of named
+# subsections.
+class _Section( object ) :
+
+	def __init__( self ) :
+
+		self.widgets = []
+		self.subsections = collections.OrderedDict()
+
+# The PlugLayout class deals with all the details of plugs, metadata and
+# signals to define an abstract layout in terms of _Sections. It then
+# delegates to the _Layout classes to create an actual layout.
+class _Layout( GafferUI.Widget ) :
+
+	def __init__( self, topLevelWidget, orientation, **kw ) :
+
+		GafferUI.Widget.__init__( self, topLevelWidget, **kw )
+
+		self.__orientation = orientation
+
+	def orientation( self ) :
+
+		return self.__orientation
+
+	def update( self, section ) :
+
+		raise NotImplementedError
+
+class _TabLayout( _Layout ) :
+
+	def __init__( self, orientation, **kw ) :
+
+		self.__mainColumn = GafferUI.ListContainer( GafferUI.ListContainer.Orientation.Vertical )
+
+		_Layout.__init__( self, self.__mainColumn, orientation, **kw )
+
+		with self.__mainColumn :
+			self.__widgetsColumn = GafferUI.ListContainer( self.orientation(), spacing = 4, borderWidth = 4 )
+			self.__tabbedContainer = GafferUI.TabbedContainer()
+
+	def update( self, section ) :
+
+		self.__widgetsColumn[:] = section.widgets
+
+		existingTabs = collections.OrderedDict()
+		for tab in self.__tabbedContainer[:] :
+			existingTabs[self.__tabbedContainer.getLabel( tab )] = tab
+
+		updatedTabs = collections.OrderedDict()
+		for name, subsection in section.subsections.items() :
+			tab = existingTabs.get( name )
+			if tab is None :
+				tab = GafferUI.ScrolledContainer( borderWidth = 8 )
+				if self.orientation() == GafferUI.ListContainer.Orientation.Vertical :
+					tab.setHorizontalMode( GafferUI.ScrolledContainer.ScrollMode.Never )
+				else :
+					tab.setVerticalMode( GafferUI.ScrolledContainer.ScrollMode.Never )
+				tab.setChild( _CollapsibleLayout( self.orientation() ) )
+			tab.getChild().update( subsection )
+			updatedTabs[name] = tab
+
+		if existingTabs.keys() != updatedTabs.keys() :
+			del self.__tabbedContainer[:]
+			for name, tab in updatedTabs.items() :
+				self.__tabbedContainer.append( tab, label = name )
+
+		self.__widgetsColumn.setVisible( len( section.widgets ) )
+		self.__tabbedContainer.setVisible( len( self.__tabbedContainer ) )
+
+class _CollapsibleLayout( _Layout ) :
+
+	def __init__( self, orientation, **kw ) :
+
+		self.__column = GafferUI.ListContainer( orientation, spacing = 4 )
+
+		_Layout.__init__( self, self.__column, orientation, **kw )
+
+		self.__collapsibles = {} # Indexed by section name
+
+	def update( self, section ) :
+
+		widgets = list( section.widgets )
+
+		for name, subsection in section.subsections.items() :
+			collapsible = self.__collapsibles.get( name )
+			if collapsible is None :
+				collapsible = GafferUI.Collapsible( name, _CollapsibleLayout( self.orientation() ), borderWidth = 2, collapsed = True )
+				self.__collapsibles[name] = collapsible
+			collapsible.getChild().update( subsection )
+			widgets.append( collapsible )
+
+		self.__column[:] = widgets
