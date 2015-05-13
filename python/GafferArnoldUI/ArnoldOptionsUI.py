bb2fdf8c0d9f12f5d52c08cdfa2b466dc800e071
John Haddon <thehaddonyoof@gmail.com>
2015-05-13 17:23:45 +0100

    Documented ArnoldOptions node.

--- python/GafferArnoldUI/ArnoldOptionsUI.py
+++ python/GafferArnoldUI/ArnoldOptionsUI.py
@@ -95,6 +95,13 @@ Gaffer.Metadata.registerNode(
 
 	GafferArnold.ArnoldOptions,
 
+	"description",
+	"""
+	Sets global scene options applicable to the Arnold
+	renderer. Use the StandardOptions node to set
+	global options applicable to all renderers.
+	""",
+
 	plugs = {
 
 		# Sections
@@ -112,12 +119,32 @@ Gaffer.Metadata.registerNode(
 
 		"options.aaSamples" : [
 
+			"description",
+			"""
+			Controls the number of rays per pixel
+			traced from the camera. The more samples,
+			the better the quality of antialiasing,
+			motion blur and depth of field. The actual
+			number of rays per pixel is the square of
+			the AA samples value - so a value of 3
+			means 9 rays are traced, 4 means 16 rays are
+			traced and so on.
+			""",
+
 			"layout:section", "Sampling",
 			"label", "AA Samples",
 
 		],
 
 		"options.giDiffuseSamples" : [
+
+			"description",
+			"""
+			Controls the number of rays traced when
+			computing indirect illumination ("bounce light").
+			The number of actual diffuse rays traced is the
+			square of this number.
+			""",
 
 			"layout:section", "Sampling",
 			"label", "Diffuse Samples",
@@ -126,12 +153,27 @@ Gaffer.Metadata.registerNode(
 
 		"options.giGlossySamples" : [
 
+			"description",
+			"""
+			Controls the number of rays traced when
+			computing glossy specular reflections.
+			The number of actual specular rays traced
+			is the square of this number.
+			""",
+
 			"layout:section", "Sampling",
 			"label", "Glossy Samples",
 
 		],
 
 		"options.giRefractionSamples" : [
+
+			"description",
+			"""
+			Controls the number of rays traced when
+			computing refractions. The number of actual
+			specular rays traced is the square of this number.
+			""",
 
 			"layout:section", "Sampling",
 			"label", "Refraction Samples",
@@ -142,11 +184,23 @@ Gaffer.Metadata.registerNode(
 
 		"options.ignoreTextures" : [
 
+			"description",
+			"""
+			Ignores all file textures, rendering as
+			if they were all white.
+			""",
+
 			"layout:section", "Features",
 
 		],
 
 		"options.ignoreShaders" : [
+
+			"description",
+			"""
+			Ignores all shaders, rendering as a
+			simple facing ratio shader instead.
+			""",
 
 			"layout:section", "Features",
 
@@ -154,11 +208,21 @@ Gaffer.Metadata.registerNode(
 
 		"options.ignoreAtmosphere" : [
 
+			"description",
+			"""
+			Ignores all atmosphere shaders.
+			""",
+
 			"layout:section", "Features",
 
 		],
 
 		"options.ignoreLights" : [
+
+			"description",
+			"""
+			Ignores all lights.
+			""",
 
 			"layout:section", "Features",
 
@@ -166,11 +230,22 @@ Gaffer.Metadata.registerNode(
 
 		"options.ignoreShadows" : [
 
+			"description",
+			"""
+			Skips all shadow calculations.
+			""",
+
 			"layout:section", "Features",
 
 		],
 
 		"options.ignoreSubdivision" : [
+
+			"description",
+			"""
+			Treats all subdivision surfaces
+			as simple polygon meshes instead.
+			""",
 
 			"layout:section", "Features",
 
@@ -178,11 +253,21 @@ Gaffer.Metadata.registerNode(
 
 		"options.ignoreDisplacement" : [
 
+			"description",
+			"""
+			Ignores all displacement shaders.
+			""",
+
 			"layout:section", "Features",
 
 		],
 
 		"options.ignoreBump" : [
+
+			"description",
+			"""
+			Ignores all bump mapping.
+			""",
 
 			"layout:section", "Features",
 
@@ -190,11 +275,24 @@ Gaffer.Metadata.registerNode(
 
 		"options.ignoreMotionBlur" : [
 
+			"description",
+			"""
+			Ignores motion blur. Note that the turn
+			off motion blur completely, it is more
+			efficient to use the motion blur controls
+			in the StandardOptions node.
+			""",
+
 			"layout:section", "Features",
 
 		],
 
 		"options.ignoreSSS" : [
+
+			"description",
+			"""
+			Disables all subsurface scattering.
+			""",
 
 			"layout:section", "Features",
 
@@ -204,6 +302,12 @@ Gaffer.Metadata.registerNode(
 
 		"options.textureSearchPath" : [
 
+			"description",
+			"""
+			The locations used to search for texture
+			files.
+			""",
+
 			"layout:section", "Search Paths",
 			"label", "Textures",
 
@@ -211,12 +315,23 @@ Gaffer.Metadata.registerNode(
 
 		"options.proceduralSearchPath" : [
 
+			"description",
+			"""
+			The locations used to search for procedural
+			DSOs.
+			""",
+
 			"layout:section", "Search Paths",
 			"label", "Procedurals",
 
 		],
 
 		"options.shaderSearchPath" : [
+
+			"description",
+			"""
+			The locations used to search for shader plugins.
+			""",
 
 			"layout:section", "Search Paths",
 			"label", "Shaders",
@@ -227,12 +342,24 @@ Gaffer.Metadata.registerNode(
 
 		"options.errorColorBadTexture" : [
 
+			"description",
+			"""
+			The colour to display if an attempt is
+			made to use a bad or non-existent texture.
+			""",
+
 			"layout:section", "Error Colors",
 			"label", "Bad Texture",
 
 		],
 
 		"options.errorColorBadMesh" : [
+
+			"description",
+			"""
+			The colour to display if bad geometry
+			is encountered.
+			""",
 
 			"layout:section", "Error Colors",
 			"label", "Bad Mesh",
@@ -241,12 +368,24 @@ Gaffer.Metadata.registerNode(
 
 		"options.errorColorBadPixel" : [
 
+			"description",
+			"""
+			The colour to display for a pixel where
+			a NaN is encountered.
+			""",
+
 			"layout:section", "Error Colors",
 			"label", "Bad Pixel",
 
 		],
 
 		"options.errorColorBadShader" : [
+
+			"description",
+			"""
+			The colour to display if a problem occurs
+			in a shader.
+			""",
 
 			"layout:section", "Error Colors",
 			"label", "Bad Shader",
