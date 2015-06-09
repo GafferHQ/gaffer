c35853ff3eb3cd8f5dd99473b5f9ef8d2b0b559c
John Haddon <thehaddonyoof@gmail.com>
2015-06-09 19:45:24 +0100

    PathFilterUI : Added support for dropping objects into NodeGraph.
    Objects can be dragged to either a PathFilter, a FilteredSceneProcessor with a PathFilter attached, or a FilteredSceneProcessor with no filter (in which case one will be created). Shift+Drag adds objects to the filter, Control+Drag removes them, and a plain drag replaces the current value.
    
    Unfortunately, because we must store the connection returned by `connect()` to keep it alive, we currently have to leak connections by keeping them forever in our __connections list.

--- python/GafferSceneUI/PathFilterUI.py
+++ python/GafferSceneUI/PathFilterUI.py
@@ -34,6 +34,10 @@
 #
 ##########################################################################
 
+import types
+
+import IECore
+
 import Gaffer
 import GafferUI
 
@@ -107,3 +111,134 @@ GafferUI.Nodule.registerNodule(
 	"paths",
 	lambda plug : None,
 )
+
+##########################################################################
+# NodeGadget drop handler
+##########################################################################
+
+GafferUI.Pointer.registerPointer( "addObjects", GafferUI.Pointer( "addObjects.png", IECore.V2i( 36, 18 ) ) )
+GafferUI.Pointer.registerPointer( "removeObjects", GafferUI.Pointer( "removeObjects.png", IECore.V2i( 36, 18 ) ) )
+GafferUI.Pointer.registerPointer( "replaceObjects", GafferUI.Pointer( "replaceObjects.png", IECore.V2i( 36, 18 ) ) )
+
+__DropMode = IECore.Enum.create( "None", "Add", "Remove", "Replace" )
+
+# Ideally we'd store these as attributes on the relevant NodeGadgets,
+# but the attributes would get lost due to IECorePython's RefCounted
+# binding not guaranteeing a one-to-one mapping between C++ and Python
+# objects.
+__originalDragPointer = None
+__connections = []
+
+def __dropMode( nodeGadget, event ) :
+
+	if not isinstance( nodeGadget.node(), GafferScene.PathFilter ) :
+		filter = None
+		if nodeGadget.node()["filter"].getInput() is not None :
+			filter = nodeGadget.node()["filter"].source().node()
+		if filter is None :
+			return __DropMode.Replace
+		elif not isinstance( filter, GafferScene.PathFilter ) :
+			return __DropMode.None
+
+	if event.modifiers & event.Modifiers.Shift :
+		return __DropMode.Add
+	elif event.modifiers & event.Modifiers.Control :
+		return __DropMode.Remove
+	else :
+		return __DropMode.Replace
+
+def __dragEnter( nodeGadget, event ) :
+
+	if not isinstance( event.data, IECore.StringVectorData ) :
+		return False
+
+	if not len( event.data ) :
+		return False
+
+	if not event.data[0].startswith( "/" ) :
+		return False
+
+	if __dropMode( nodeGadget, event ) == __DropMode.None :
+		return False
+
+	global __originalDragPointer
+	__originalDragPointer = GafferUI.Pointer.getCurrent()
+
+	return True
+
+def __dragLeave( nodeGadget, event ) :
+
+	global __originalDragPointer
+
+	GafferUI.Pointer.setCurrent( __originalDragPointer )
+	__originalDragPointer = None
+
+	return True
+
+def __dragMove( nodeGadget, event ) :
+
+	global __originalDragPointer
+	if __originalDragPointer is None :
+		return False
+
+	GafferUI.Pointer.setCurrent( str( __dropMode( nodeGadget, event ) ).lower() + "Objects" )
+
+	return True
+
+def __drop( nodeGadget, event ) :
+
+	global __originalDragPointer
+	if __originalDragPointer is None :
+		return False
+
+	pathFilter = None
+	if isinstance( nodeGadget.node(), GafferScene.PathFilter ) :
+		pathFilter = nodeGadget.node()
+	elif nodeGadget.node()["filter"].getInput() :
+		pathFilter = nodeGadget.node()["filter"].source().node()
+
+	dropMode = __dropMode( nodeGadget, event )
+	if dropMode == __DropMode.Replace :
+		paths = sorted( event.data )
+	elif dropMode == __DropMode.Add :
+		paths = set( pathFilter["paths"].getValue() )
+		paths.update( event.data )
+		paths = sorted( paths )
+	else :
+		paths = set( pathFilter["paths"].getValue() )
+		paths.difference_update( event.data )
+		paths = sorted( paths )
+
+	with Gaffer.UndoContext( nodeGadget.node().ancestor( Gaffer.ScriptNode ) ) :
+		
+		if pathFilter is None :
+		
+			pathFilter = GafferScene.PathFilter()
+			nodeGadget.node().parent().addChild( pathFilter )
+			nodeGadget.node()["filter"].setInput( pathFilter["out"] )
+
+			graphGadget = nodeGadget.ancestor( GafferUI.GraphGadget )
+			graphGadget.getLayout().positionNode( graphGadget, pathFilter )
+
+		pathFilter["paths"].setValue( IECore.StringVectorData( paths ) )
+
+	GafferUI.Pointer.setCurrent( __originalDragPointer )
+	__originalDragPointer = None
+
+	return True
+
+def addObjectDropTarget( nodeGadget ) :
+
+	__connections.append( nodeGadget.dragEnterSignal().connect( __dragEnter ) )
+	__connections.append( nodeGadget.dragLeaveSignal().connect( __dragLeave ) )
+	__connections.append( nodeGadget.dragMoveSignal().connect( __dragMove ) )
+	__connections.append( nodeGadget.dropSignal().connect( __drop ) )
+
+def __nodeGadget( pathFilter ) :
+
+	nodeGadget = GafferUI.StandardNodeGadget( pathFilter )
+	addObjectDropTarget( nodeGadget )
+
+	return nodeGadget
+
+GafferUI.NodeGadget.registerNodeGadget( GafferScene.PathFilter, __nodeGadget )
