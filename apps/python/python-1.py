0055210df68246428382be117a9bd95f3e542dbb
John Haddon <thehaddonyoof@gmail.com>
2015-07-29 17:45:22 +0100

    Python app : Set __name__ to "__main__".
    Fixes #1405.

--- apps/python/python-1.py
+++ apps/python/python-1.py
@@ -105,7 +105,13 @@ class python( Gaffer.Application ) :
 		try:
 			sys.argv = [ args[ "file" ].value ] + list( args[ "arguments" ] )
 			try :
-				execfile( args["file"].value, { "argv" : args["arguments"] } )
+				execfile(
+					args["file"].value,
+					{
+						"argv" : args["arguments"],
+						"__name__" : "__main__",
+					}
+				)
 				return 0
 			except SystemExit as e :
 				# don't print traceback when a sys.exit was called, but return the exit code as the result
