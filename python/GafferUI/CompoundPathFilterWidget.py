77719780b4b761ea1c30ab79a363296ee87e986f
Andrew Kaufman <andrewk.imageengine@gmail.com>
2015-08-19 15:41:34 -0700

    CompoundPathFilterWidget allows more widgets on the same row.
    This doesn't really seem like the best way to accomplish the layout of PathFilters, but it gets us by as well as before anyways...

--- python/GafferUI/CompoundPathFilterWidget.py
+++ python/GafferUI/CompoundPathFilterWidget.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2012, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2012-2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -69,7 +69,7 @@ class CompoundPathFilterWidget( GafferUI.PathFilterWidget ) :
 
 			self.__grid[gridPos.x, gridPos.y] = filterWidget
 
-			if gridPos.x < 2 :
+			if gridPos.x < 3 :
 				gridPos.x += 1
 			else :
 				gridPos.x = 0
