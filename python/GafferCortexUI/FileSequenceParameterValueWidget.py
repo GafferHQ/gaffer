6dbf28433fa887be4485cdf16ba776a8e517e645
Andrew Kaufman <andrewk.imageengine@gmail.com>
2015-08-20 14:20:20 -0700

    FileSequenceParameterValueWidget uses a FileSystemPath with sequences enabled.

--- python/GafferCortexUI/FileSequenceParameterValueWidget.py
+++ python/GafferCortexUI/FileSequenceParameterValueWidget.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2012, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2012-2015, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -48,6 +48,10 @@ class FileSequenceParameterValueWidget( GafferCortexUI.PathParameterValueWidget 
 
 	def _path( self ) :
 
-		return Gaffer.SequencePath( "/", filter = self._filter() )
+		return Gaffer.FileSystemPath( "/", filter = self._filter(), includeSequences = True )
+
+	def _filter( self ) :
+
+		return Gaffer.FileSystemPath.createStandardFilter( includeSequenceFilter = True )
 
 GafferCortexUI.ParameterValueWidget.registerType( IECore.FileSequenceParameter, FileSequenceParameterValueWidget )
