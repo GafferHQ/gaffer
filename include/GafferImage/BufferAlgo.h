46679fc9c548ab045282b210ee766749a9fe57c1
Hugh Macdonald <hugh.macdonald@nvizible.com>
2015-11-27 09:47:58 +0000

    BufferAlgo : Shift methods out of ImageAlgo

---
+++ include/GafferImage/BufferAlgo.h
@@ -0,0 +1,75 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2015, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      * Redistributions of source code must retain the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer.
+//
+//      * Redistributions in binary form must reproduce the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer in the documentation and/or other materials provided with
+//        the distribution.
+//
+//      * Neither the name of John Haddon nor the names of
+//        any other contributors to this software may be used to endorse or
+//        promote products derived from this software without specific prior
+//        written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef GAFFERIMAGE_BUFFERALGO_H
+#define GAFFERIMAGE_BUFFERALGO_H
+
+#include <vector>
+#include "OpenEXR/ImathBox.h"
+
+namespace GafferImage
+{
+
+/// Image window utility functions. The GafferImage convention is that
+/// the minimum coordinate is included within the window and the
+/// maximum coordinate is outside it - these functions take that into
+/// account and should therefore be used in favour of the Imath equivalents.
+////////////////////////////////////////////////////////////////////////////
+
+/// Returns true if the window contains no pixels, and false otherwise.
+inline bool empty( const Imath::Box2i &window );
+
+/// Returns true if the image windows intersect.
+inline bool intersects( const Imath::Box2i &window1, const Imath::Box2i &window2 );
+
+/// Return the intersection of the two image windows.
+inline Imath::Box2i intersection( const Imath::Box2i &window1, const Imath::Box2i &window2 );
+
+/// Returns true if the given point is inside the window.
+inline bool contains( const Imath::Box2i &window, const Imath::V2i &point );
+
+/// Returns true if the given area is inside the window.
+inline bool contains( const Imath::Box2i &window, const Imath::Box2i &area );
+
+/// Clamps the point so that it is contained inside the window.
+inline Imath::V2i clamp( const Imath::V2i &point, const Imath::Box2i &window );
+
+} // namespace GafferImage
+
+#include "GafferImage/BufferAlgo.inl"
+
+#endif // GAFFERIMAGE_BUFFERALGO_H
+
