0fc90d77afc77bbfc7fd320f59ae3d204bc1ad60
Hugh Macdonald <hugh.macdonald@nvizible.com>
2015-12-21 11:07:56 +0000

    ObjectToImageTest : Add test for deep plugs

--- python/GafferImageTest/ObjectToImageTest.py
+++ python/GafferImageTest/ObjectToImageTest.py
@@ -58,6 +58,18 @@ class ObjectToImageTest( GafferImageTest.ImageTestCase ) :
 
 		self.assertEqual( GafferImage.ImageAlgo.image( n["out"] ), i )
 
+	def testDeepPlugs( self ) :
+
+		ts = GafferImage.ImagePlug.tileSize()
+
+		i = IECore.Reader.create( self.fileName ).read()
+
+		n = GafferImage.ObjectToImage()
+		n["object"].setValue( i )
+
+		self.assertEqual( n["out"]["deep"].getValue(), False )
+		self.assertEqual( n["out"].sampleOffsets( imath.V2i( 0 ) ), GafferImage.ImagePlug.flatTileSampleOffsets() )
+
 	def testImageWithANegativeDataWindow( self ) :
 
 		i = IECore.Reader.create( self.negFileName ).read()
