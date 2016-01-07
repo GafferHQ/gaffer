GafferHQ/gaffer

commit 2362d005b69b7172e545e13a97d9bbdca2d2460d

Multithreaded child procedural instantiation in SceneProcedural
Both the constructor and the bound() method in SceneProcedural involve potentially expensive graph evaluations, which this commit performs in parallel.

src/GafferScene/SceneProcedural.cpp
@@ -34,6 +34,8 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
+#include "tbb/parallel_for.h"
+
 #include "OpenEXR/ImathBoxAlgo.h"
 #include "OpenEXR/ImathFun.h"
 
@@ -97,6 +99,7 @@ SceneProcedural::SceneProcedural( ConstScenePlugPtr scenePlug, const Gaffer::Con
 	const IntData *deformationBlurSegmentsData = globals->member<IntData>( "attribute:gaffer:deformationBlurSegments" );
 	m_attributes.deformationBlurSegments = deformationBlurSegmentsData ? deformationBlurSegmentsData->readable() : 1;
 
+	computeBound();
 	updateAttributes( true );
 	++g_pendingSceneProcedurals;
 
@@ -111,6 +114,7 @@ SceneProcedural::SceneProcedural( const SceneProcedural &other, const ScenePlug:
 
 	m_context->set( ScenePlug::scenePathContextName, m_scenePath );
 
+	computeBound();
 	updateAttributes( false );
 	++g_pendingSceneProcedurals;
 }
@@ -123,7 +127,7 @@ SceneProcedural::~SceneProcedural()
 	}
 }
 
-Imath::Box3f SceneProcedural::bound() const
+void SceneProcedural::computeBound()
 {
 	/// \todo I think we should be able to remove this exception handling in the future.
 	/// Either when we do better error handling in ValuePlug computations, or when
@@ -165,23 +169,73 @@ Imath::Box3f SceneProcedural::bound() const
 		motionTimes( ( m_options.deformationBlur && m_attributes.deformationBlur ) ? m_attributes.deformationBlurSegments : 0, times );
 		motionTimes( ( m_options.transformBlur && m_attributes.transformBlur ) ? m_attributes.transformBlurSegments : 0, times );
 
-		Box3f result;
+		m_bound = Imath::Box3f();
 		for( std::set<float>::const_iterator it = times.begin(), eIt = times.end(); it != eIt; it++ )
 		{
 			timeContext->setFrame( *it );
 			Box3f b = m_scenePlug->boundPlug()->getValue();
 			M44f t = m_scenePlug->transformPlug()->getValue();
-			result.extendBy( transform( b, t ) );
+			m_bound.extendBy( transform( b, t ) );
 		}
-
-		return result;
 	}
 	catch( const std::exception &e )
 	{
+		m_bound = Imath::Box3f();
 		IECore::msg( IECore::Msg::Error, "SceneProcedural::bound()", e.what() );
 	}
-	return Box3f();
 }
+
+Imath::Box3f SceneProcedural::bound() const
+{
+	return m_bound;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// SceneProceduralCreate implementation
+//
+// This uses tbb::parallel_for to fill up a preallocated array of
+// SceneProceduralPtrs with new SceneProcedurals, based on the parent
+// SceneProcedural and the child names we supply.
+//
+//////////////////////////////////////////////////////////////////////////
+
+class SceneProcedural::SceneProceduralCreate
+{
+
+	public:
+		typedef std::vector<SceneProceduralPtr> SceneProceduralContainer;
+
+		SceneProceduralCreate(
+			SceneProceduralContainer& childProcedurals,
+			const SceneProcedural& parent,
+			const vector<InternedString>& childNames
+			
+		) :
+			m_childProcedurals( childProcedurals ),
+			m_parent( parent ),
+			m_childNames( childNames )
+		{
+		}
+
+		void operator()( const tbb::blocked_range<int>& range ) const
+		{
+			for( int i=range.begin(); i!=range.end(); ++i )
+			{
+				ScenePlug::ScenePath childScenePath = m_parent.m_scenePath;
+				childScenePath.push_back( m_childNames[i] );
+				SceneProceduralPtr sceneProcedural = new SceneProcedural( m_parent, childScenePath );
+				m_childProcedurals[ i ] = sceneProcedural;
+			}
+		}
+	
+	private:
+	
+		SceneProceduralContainer& m_childProcedurals;
+		const SceneProcedural& m_parent;
+		const vector<InternedString>& m_childNames;
+		
+};
+
 
 void SceneProcedural::render( Renderer *renderer ) const
 {
@@ -301,12 +355,26 @@ void SceneProcedural::render( Renderer *renderer ) const
 		ConstInternedStringVectorDataPtr childNames = m_scenePlug->childNamesPlug()->getValue();
 		if( childNames->readable().size() )
 		{
-			ScenePlug::ScenePath childScenePath = m_scenePath;
-			childScenePath.push_back( InternedString() ); // for the child name
-			for( vector<InternedString>::const_iterator it=childNames->readable().begin(); it!=childNames->readable().end(); it++ )
+			// Creating a SceneProcedural involves an attribute/bound evaluation, which are
+			// potentially expensive, so we're parallelizing them.
+
+			// allocate space for child procedurals:
+			SceneProceduralCreate::SceneProceduralContainer childProcedurals( childNames->readable().size() );
+
+			// create procedurals in parallel:
+			SceneProceduralCreate s(
+				childProcedurals,
+				*this,
+				childNames->readable()
+			);
+			tbb::parallel_for( tbb::blocked_range<int>( 0, childNames->readable().size() ), s );
+
+			// send to the renderer in series:
+
+			std::vector<SceneProceduralPtr>::const_iterator procIt = childProcedurals.begin(), procEit = childProcedurals.end();
+			for( ; procIt != procEit; ++procIt )
 			{
-				childScenePath[m_scenePath.size()] = *it;
-				renderer->procedural( new SceneProcedural( *this, childScenePath ) );
+				renderer->procedural( *procIt );
 			}
 		}
 	}
@@ -346,6 +414,9 @@ IECore::MurmurHash SceneProcedural::hash() const
 void SceneProcedural::updateAttributes( bool full )
 {
 	Context::Scope scopedContext( m_context.get() );
+	
+	// \todo: Investigate if it's worth keeping these around and reusing them in SceneProcedural::render().
+	
 	ConstCompoundObjectPtr attributes;
 	if( full )
 	{

include/GafferScene/SceneProcedural.h
@@ -115,6 +115,7 @@ class SceneProcedural : public IECore::Renderer::Procedural
 	private :
 
 		void updateAttributes( bool full );
+		void computeBound();
 		void motionTimes( unsigned segments, std::set<float> &times ) const;
 		
 		// A global counter of all the scene procedurals that are hanging around but haven't been rendered yet, which 
@@ -129,6 +130,15 @@ class SceneProcedural : public IECore::Renderer::Procedural
 		mutable bool m_rendered;
 		
 		void decrementPendingProcedurals() const;
+		
+		// We use this variable for caching the bound computation, so we can compute bounds for
+		// a SceneProcedural's children in parallel, and avoid computing them again when we send
+		// them all to the renderer in serial
+		Imath::Box3f m_bound;
+		
+		// struct for creating child procedurals in parallel and computing their bounds, using
+		// tbb::parallel_for:
+		class SceneProceduralCreate;
 		
 		static tbb::mutex g_allRenderedMutex;
 		static AllRenderedSignal g_allRenderedSignal;

commit 30fb30be74a996d5e960f28212b423d7cd0a9806

SceneProcedural : Fix use of motion blur attributes.
This was broken by 2362d005b69b7172e545e13a97d9bbdca2d2460d, because it moved the computation of the bounds before the call to updateAttributes(), meaning that we used the attribute values from the parent location rather than the current location.

src/GafferScene/SceneProcedural.cpp
@@ -176,8 +176,8 @@ SceneProcedural::SceneProcedural( ConstScenePlugPtr scenePlug, const Gaffer::Con
 	const IntData *deformationBlurSegmentsData = globals->member<IntData>( "attribute:gaffer:deformationBlurSegments" );
 	m_attributes.deformationBlurSegments = deformationBlurSegmentsData ? deformationBlurSegmentsData->readable() : 1;
 
+	updateAttributes( /* full = */ true );
 	computeBound();
-	updateAttributes( true );
 	++g_pendingSceneProcedurals;
 
 }
@@ -194,8 +194,8 @@ SceneProcedural::SceneProcedural( const SceneProcedural &other, const ScenePlug:
 
 	m_context->set( ScenePlug::scenePathContextName, m_scenePath );
 
+	updateAttributes( /* full = */ false );
 	computeBound();
-	updateAttributes( false );
 	++g_pendingSceneProcedurals;
 }
 

python/GafferSceneTest/SceneProceduralTest.py
@@ -240,5 +240,47 @@ class SceneProceduralTest( unittest.TestCase ) :
 
 		self.assertEqual( t.allRenderedSignalCalled, True )
 
+	def testTransformBlurAttributeScope( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		script["sphere"] = GafferScene.Sphere()
+		script["sphere"]["type"].setValue( GafferScene.Sphere.Type.Primitive )
+
+		script["expression1"] = Gaffer.Expression()
+		script["expression1"].setExpression( 'parent["sphere"]["transform"]["translate"]["x"] = context.getFrame()' )
+
+		script["filter"] = GafferScene.PathFilter()
+		script["filter"]["paths"].setValue( IECore.StringVectorData( [ "/sphere" ] ) )
+
+		script["attributes"] = GafferScene.StandardAttributes()
+		script["attributes"]["in"].setInput( script["sphere"]["out"] )
+		script["attributes"]["filter"].setInput( script["filter"]["out"] )
+		script["attributes"]["attributes"]["transformBlur"]["enabled"].setValue( True )
+
+		script["options"] = GafferScene.StandardOptions()
+		script["options"]["in"].setInput( script["attributes"]["out"] )
+		script["options"]["options"]["transformBlur"]["enabled"].setValue( True )
+		script["options"]["options"]["transformBlur"]["value"].setValue( True )
+		script["options"]["options"]["shutter"]["enabled"].setValue( True )
+		script["options"]["options"]["shutter"]["value"].setValue( IECore.V2f( -0.5, 0.5 ) )
+
+		sphereBound = IECore.Box3f( IECore.V3f( -1 ), IECore.V3f( 1 ) )
+		velocity = IECore.V3f( 1, 0, 0 )
+
+		context = Gaffer.Context()
+		for frame in range( 0, 10 ) :
+			for transformBlur in ( False, True ) :
+
+				context.setFrame( frame )
+				script["attributes"]["attributes"]["transformBlur"]["value"].setValue( transformBlur )
+
+				procedural = GafferScene.SceneProcedural( script["options"]["out"], context, "/sphere" )
+				bound = procedural.bound()
+				if transformBlur :
+					self.assertEqual( bound, IECore.Box3f( sphereBound.min + velocity * ( frame - 0.5 ), sphereBound.max + velocity * ( frame + 0.5 ) ) )
+				else :
+					self.assertEqual( bound, IECore.Box3f( sphereBound.min + velocity * frame, sphereBound.max + velocity * frame ) )
+
 if __name__ == "__main__":
 	unittest.main()