ced5a66c9b4793a13a4c2506e5ccafef724e89d2
John Haddon <thehaddonyoof@gmail.com>
2016-04-14 12:40:59 +0100

    Sphinx config : Add favicon.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -139,12 +139,12 @@ html_theme = "sphinx_rtd_theme"
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = []
+html_static_path = [ "_static" ]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
