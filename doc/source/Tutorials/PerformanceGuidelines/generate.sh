b944117650b29d6bda17720d902d3d0262b36ee4
John Haddon <thehaddonyoof@gmail.com>
2016-05-03 11:42:21 +0100

    Documentation : Mention PerformanceMonitor.

--- doc/source/Tutorials/PerformanceGuidelines/generate.sh
+++ doc/source/Tutorials/PerformanceGuidelines/generate.sh
@@ -2,3 +2,4 @@
 
 gaffer screengrab scripts/groupFirst.gfr -image images/groupFirst.png -editor NodeGraph
 gaffer screengrab scripts/groupSecond.gfr -image images/groupSecond.png -editor NodeGraph
+gaffer screengrab scripts/performanceMonitor.gfr -image images/performanceMonitor.png -editor NodeEditor -selection StandardOptions -nodeEditor.reveal StandardOptions.options.performanceMonitor
