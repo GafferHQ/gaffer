71acd6a5cb27bb05e939b62a142abf3adb43f242
est77 <ramenhdr@gmail.com>
2016-08-21 13:23:00 +0200

    Added appleseed nodes to help strings

--- python/GafferSceneUI/CustomAttributesUI.py
+++ python/GafferSceneUI/CustomAttributesUI.py
@@ -49,7 +49,7 @@ Gaffer.Metadata.registerNode(
 	"""
 	Applies arbitrary user-defined attributes to locations in the scene. Note
 	that for most common cases the StandardAttributes, OpenGLAttributes, RenderManAttributes,
-	and ArnoldAttributes nodes should be used in preference - they provide predefined
+	ArnoldAttributes and AppleseedAttributes nodes should be used in preference - they provide predefined
 	sets of attributes with customised user interfaces. The CustomAttributes node is of most use when
 	needing to set a custom attribute not supported by the specialised nodes.
 	""",
