7bb969123e90082e27a90d7c7b41755c00433021
John Haddon <thehaddonyoof@gmail.com>
2016-09-07 11:23:43 +0100

    OSLShaderUI : Fix out nodule when switching between shader/surface types.

--- src/GafferOSL/OSLShader.cpp
+++ src/GafferOSL/OSLShader.cpp
@@ -48,6 +48,7 @@
 #include "Gaffer/CompoundNumericPlug.h"
 #include "Gaffer/StringPlug.h"
 #include "Gaffer/SplinePlug.h"
+#include "Gaffer/Metadata.h"
 
 #include "GafferScene/RendererAlgo.h"
 
@@ -699,6 +700,7 @@ void OSLShader::loadShader( const std::string &shaderName, bool keepExistingValu
 		throw Exception( query.geterror() );
 	}
 
+	const bool outPlugHadChildren = existingOut ? existingOut->children().size() : false;
 	if( !keepExistingValues )
 	{
 		// If we're not preserving existing values then remove all existing
@@ -740,6 +742,14 @@ void OSLShader::loadShader( const std::string &shaderName, bool keepExistingValu
 
 	namePlug()->setValue( shaderName );
 	typePlug()->setValue( std::string( "osl:" ) + query.shadertype().c_str() );
+
+	if( static_cast<bool>( outPlug()->children().size() ) != outPlugHadChildren )
+	{
+		// OSLShaderUI registers a dynamic metadata entry which depends on whether or
+		// not the plug has children, so we must notify the world that the value will
+		// have changed.
+		Metadata::plugValueChangedSignal()( staticTypeId(), "out", "nodule:type", outPlug() );
+	}
 
 	m_metadata = NULL;
 }
