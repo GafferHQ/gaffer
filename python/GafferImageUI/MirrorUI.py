a818fd3aec5ac06987602fff7e17f5b6c8a3ceb2
John Haddon <thehaddonyoof@gmail.com>
2016-10-25 19:12:51 +0100

    GafferImageUI : Add Mirror UI.

--- python/GafferImageUI/MirrorUI.py
+++ python/GafferImageUI/MirrorUI.py
@@ -1,7 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2012, John Haddon. All rights reserved.
-#  Copyright (c) 2012-2015, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2016, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -35,51 +34,40 @@
 #
 ##########################################################################
 
-from _GafferImageUI import *
+import Gaffer
+import GafferImage
 
-import DisplayUI
-from FormatPlugValueWidget import FormatPlugValueWidget
-from ChannelMaskPlugValueWidget import ChannelMaskPlugValueWidget
+Gaffer.Metadata.registerNode(
 
-import OpenImageIOReaderUI
-import ImageReaderUI
-import ImageViewUI
-import ImageTransformUI
-import ConstantUI
-import ImageSwitchUI
-import ColorSpaceUI
-import ImageContextVariablesUI
-import ImageStatsUI
-import DeleteChannelsUI
-import ObjectToImageUI
-import ClampUI
-import ImageWriterUI
-import GradeUI
-import ImageTimeWarpUI
-import ImageSamplerUI
-import MergeUI
-import ImageNodeUI
-import ChannelDataProcessorUI
-import ImageProcessorUI
-import ImageMetadataUI
-import DeleteImageMetadataUI
-import CopyImageMetadataUI
-import ImageLoopUI
-import ShuffleUI
-import PremultiplyUI
-import UnpremultiplyUI
-import CropUI
-import ResizeUI
-import ResampleUI
-import LUTUI
-import CDLUI
-import DisplayTransformUI
-import OffsetUI
-import BlurUI
-import ShapeUI
-import TextUI
-import WarpUI
-import UVWarpUI
-import MirrorUI
+	GafferImage.Mirror,
 
-__import__( "IECore" ).loadConfig( "GAFFER_STARTUP_PATHS", {}, subdirectory = "GafferImageUI" )
+	"description",
+	"""
+	Mirrors the image, flipping it in the horizontal and/or
+	vertical directions. Unlike the ImageTransform node, this
+	performs no filtering, so pixel values are not changed.
+	""",
+
+	plugs = {
+
+		"horizontal" : [
+
+			"description",
+			"""
+			Mirrors horizontally, flopping the image left to right.
+			""",
+
+		],
+
+		"vertical" : [
+
+			"description",
+			"""
+			Mirrors vertically, flipping the image top to bottom.
+			""",
+
+		],
+
+	}
+
+)
