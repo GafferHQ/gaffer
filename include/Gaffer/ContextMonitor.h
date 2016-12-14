ea9b77a9b0c6c174c1c5748d629757624c6d17b8
John Haddon <thehaddonyoof@gmail.com>
2016-12-14 11:08:42 +0000

    Gaffer : Add ContextMonitor class.
    This is a companion to the PerformanceMonitor class, but rather than collecting statistics about the frequency and duration of hash/compute processes, it collects statistics about the number of unique contexts used and the variables within them.

---
+++ include/Gaffer/ContextMonitor.h
@@ -0,0 +1,129 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2016, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      * Redistributions of source code must retain the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer.
+//
+//      * Redistributions in binary form must reproduce the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer in the documentation and/or other materials provided with
+//        the distribution.
+//
+//      * Neither the name of John Haddon nor the names of
+//        any other contributors to this software may be used to endorse or
+//        promote products derived from this software without specific prior
+//        written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef GAFFER_CONTEXTMONITOR_H
+#define GAFFER_CONTEXTMONITOR_H
+
+#include <vector>
+
+#include "tbb/enumerable_thread_specific.h"
+
+#include "boost/unordered_map.hpp"
+#include "boost/unordered_set.hpp"
+
+#include "IECore/RefCounted.h"
+#include "IECore/MurmurHash.h"
+
+#include "Gaffer/Monitor.h"
+
+namespace Gaffer
+{
+
+IE_CORE_FORWARDDECLARE( GraphComponent )
+IE_CORE_FORWARDDECLARE( Plug )
+IE_CORE_FORWARDDECLARE( Context )
+
+/// A monitor which collects statistics about
+/// what contexts plugs are evaluated in.
+class ContextMonitor : public Monitor
+{
+
+	public :
+
+		/// Statistics are only collected for the root and its
+		/// descendants.
+		ContextMonitor( const GraphComponent *root = NULL );
+		virtual ~ContextMonitor();
+
+		struct Statistics
+		{
+
+			size_t numUniqueContexts() const;
+			std::vector<IECore::InternedString> variableNames() const;
+			size_t numUniqueValues( IECore::InternedString variableName ) const;
+
+			Statistics & operator += ( const Context *rhs );
+			Statistics & operator += ( const Statistics &rhs );
+
+			bool operator == ( const Statistics &rhs );
+			bool operator != ( const Statistics &rhs );
+
+			private :
+
+				typedef boost::unordered_set<IECore::MurmurHash> ContextSet;
+				typedef boost::unordered_map<IECore::MurmurHash, size_t> CountingMap;
+				typedef std::map<IECore::InternedString, CountingMap> VariableMap;
+
+				ContextSet m_contexts;
+				VariableMap m_variables;
+
+		};
+
+		typedef boost::unordered_map<ConstPlugPtr, Statistics> StatisticsMap;
+
+		const StatisticsMap &allStatistics() const;
+		const Statistics &plugStatistics( const Plug *plug ) const;
+		const Statistics &combinedStatistics() const;
+
+	protected :
+
+		virtual void processStarted( const Process *process );
+		virtual void processFinished( const Process *process );
+
+	private :
+
+		const GraphComponent *m_root;
+
+		// For performance reasons we accumulate our statistics into
+		// thread local storage while computations are running.
+		struct ThreadData
+		{
+			// Stores the per-plug statistics captured by this thread.
+			StatisticsMap statistics;
+		};
+
+		tbb::enumerable_thread_specific<ThreadData, tbb::cache_aligned_allocator<ThreadData>, tbb::ets_key_per_instance> m_threadData;
+
+		// Then when we want to query it, we collate it into m_statistics.
+		void collate() const;
+		mutable StatisticsMap m_statistics;
+		mutable Statistics m_combinedStatistics;
+
+};
+
+} // namespace Gaffer
+
+#endif // GAFFER_CONTEXTMONITOR_H
---
+++ src/Gaffer/ContextMonitor.cpp
@@ -0,0 +1,178 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2016, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      * Redistributions of source code must retain the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer.
+//
+//      * Redistributions in binary form must reproduce the above
+//        copyright notice, this list of conditions and the following
+//        disclaimer in the documentation and/or other materials provided with
+//        the distribution.
+//
+//      * Neither the name of John Haddon nor the names of
+//        any other contributors to this software may be used to endorse or
+//        promote products derived from this software without specific prior
+//        written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "Gaffer/ContextMonitor.h"
+#include "Gaffer/Process.h"
+#include "Gaffer/Plug.h"
+#include "Gaffer/Context.h"
+
+using namespace std;
+using namespace IECore;
+using namespace Gaffer;
+
+static ContextMonitor::Statistics g_emptyStatistics;
+
+//////////////////////////////////////////////////////////////////////////
+// ContextMonitor::Statistics
+//////////////////////////////////////////////////////////////////////////
+
+size_t ContextMonitor::Statistics::numUniqueContexts() const
+{
+	return m_contexts.size();
+}
+
+std::vector<IECore::InternedString> ContextMonitor::Statistics::variableNames() const
+{
+	vector<InternedString> result;
+	for( VariableMap::const_iterator it = m_variables.begin(), eIt = m_variables.end(); it != eIt; ++it )
+	{
+		result.push_back( it->first );
+	}
+	return result;
+}
+
+size_t ContextMonitor::Statistics::numUniqueValues( IECore::InternedString variableName ) const
+{
+	VariableMap::const_iterator it = m_variables.find( variableName );
+	if( it != m_variables.end() )
+	{
+		return it->second.size();
+	}
+	return 0;
+}
+
+ContextMonitor::Statistics & ContextMonitor::Statistics::operator += ( const Context *context )
+{
+	m_contexts.insert( context->hash() );
+	vector<InternedString> names;
+	context->names( names );
+	for( vector<InternedString>::const_iterator it = names.begin(), eIt = names.end(); it != eIt; ++it )
+	{
+		const Data *d = context->get<Data>( *it );
+		m_variables[*it][d->Object::hash()] += 1;
+	}
+	return *this;
+}
+
+ContextMonitor::Statistics & ContextMonitor::Statistics::operator += ( const Statistics &rhs )
+{
+	m_contexts.insert( rhs.m_contexts.begin(), rhs.m_contexts.end() );
+	for( VariableMap::const_iterator it = rhs.m_variables.begin(), eIt = rhs.m_variables.end(); it != eIt; ++it )
+	{
+		CountingMap &c = m_variables[it->first];
+		for( CountingMap::const_iterator cIt = it->second.begin(), ceIt = it->second.end(); cIt != ceIt; ++cIt )
+		{
+			c[cIt->first] += cIt->second;
+		}
+	}
+	return *this;
+}
+
+bool ContextMonitor::Statistics::operator == ( const Statistics &rhs )
+{
+	return m_contexts == rhs.m_contexts && m_variables == rhs.m_variables;
+}
+
+bool ContextMonitor::Statistics::operator != ( const Statistics &rhs )
+{
+	return !( *this == rhs );
+}
+
+//////////////////////////////////////////////////////////////////////////
+// PerformanceMonitor
+//////////////////////////////////////////////////////////////////////////
+
+ContextMonitor::ContextMonitor( const GraphComponent *root )
+	:	m_root( root )
+{
+}
+
+ContextMonitor::~ContextMonitor()
+{
+}
+
+const ContextMonitor::StatisticsMap &ContextMonitor::allStatistics() const
+{
+	collate();
+	return m_statistics;
+}
+
+const ContextMonitor::Statistics &ContextMonitor::plugStatistics( const Plug *plug ) const
+{
+	collate();
+	StatisticsMap::const_iterator it = m_statistics.find( plug );
+	if( it == m_statistics.end() )
+	{
+		return g_emptyStatistics;
+	}
+	return it->second;
+}
+
+const ContextMonitor::Statistics &ContextMonitor::combinedStatistics() const
+{
+	collate();
+	return m_combinedStatistics;
+}
+
+void ContextMonitor::processStarted( const Process *process )
+{
+	if( m_root && m_root != process->plug() && !m_root->isAncestorOf( process->plug() ) )
+	{
+		return;
+	}
+	const Context *context = Context::current();
+	ThreadData &threadData = m_threadData.local();
+	threadData.statistics[process->plug()] += context;
+}
+
+void ContextMonitor::processFinished( const Process *process )
+{
+}
+
+void ContextMonitor::collate() const
+{
+	tbb::enumerable_thread_specific<ThreadData, tbb::cache_aligned_allocator<ThreadData>, tbb::ets_key_per_instance>::iterator it, eIt;
+	for( it = m_threadData.begin(), eIt = m_threadData.end(); it != eIt; ++it )
+	{
+		StatisticsMap &m = it->statistics;
+		for( StatisticsMap::const_iterator mIt = m.begin(), meIt = m.end(); mIt != meIt; ++mIt )
+		{
+			m_statistics[mIt->first] += mIt->second;
+			m_combinedStatistics += mIt->second;
+		}
+		m.clear();
+	}
+}
