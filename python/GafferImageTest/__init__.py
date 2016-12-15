78fe84e044a6bb3dff074b9041753c9cf2e97686
John Haddon <thehaddonyoof@gmail.com>
2016-12-15 18:58:57 +0000

    GafferImageTest : Add CopyChannelsTest

--- python/GafferImageTest/__init__.py
+++ python/GafferImageTest/__init__.py
@@ -84,6 +84,7 @@ from TextTest import TextTest
 from OpenColorIOTransformTest import OpenColorIOTransformTest
 from UVWarpTest import UVWarpTest
 from MirrorTest import MirrorTest
+from CopyChannelsTest import CopyChannelsTest
 
 if __name__ == "__main__":
 	import unittest
