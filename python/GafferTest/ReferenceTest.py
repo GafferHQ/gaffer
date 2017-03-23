GafferHQ/gaffer

commit 6c9c14b34c80ed38c5b40691873d7b13f0779996

CompoundNumericPlugBinding: support binding and serialisation

src/GafferBindings/CompoundNumericPlugBinding.cpp
@@ -36,6 +36,8 @@
 //////////////////////////////////////////////////////////////////////////
 
 #include "boost/python.hpp"
+#include "boost/algorithm/string/replace.hpp"
+#include "boost//format.hpp"
 
 #include "IECorePython/IECoreBinding.h"
 #include "IECorePython/RunTimeTypedBinding.h"
@@ -54,6 +56,24 @@ namespace
 {
 
 template<typename T>
+std::string maskedCompoundNumericPlugRepr( const T *plug, unsigned flagsMask )
+{
+	std::string extraArgs = "";
+
+	const IECore::GeometricData::Interpretation interpretation = plug->interpretation();
+
+	if( interpretation != IECore::GeometricData::None )
+	{
+		boost::python::object interpretationAsPythonObject( interpretation );
+		boost::python::object interpretationRepr = interpretationAsPythonObject.attr( "__repr__" )();
+		extraArgs = "interpretation = " + std::string( boost::python::extract<std::string>( interpretationRepr ) );
+		boost::replace_first( extraArgs, "_IECore", "GeometricData" );
+	}
+
+	return ValuePlugSerialiser::repr( plug, flagsMask, extraArgs );
+}
+
+template<typename T>
 std::string compoundNumericPlugRepr( const T *plug )
 {
 	return maskedCompoundNumericPlugRepr( plug, Plug::All );
@@ -62,6 +82,12 @@ std::string compoundNumericPlugRepr( const T *plug )
 template<typename T>
 class CompoundNumericPlugSerialiser : public ValuePlugSerialiser
 {
+  public:
+
+	virtual std::string constructor( const Gaffer::GraphComponent *graphComponent, const Serialisation &serialisation ) const
+	{
+		return maskedCompoundNumericPlugRepr( static_cast<const T *>( graphComponent ), Plug::All & ~Plug::ReadOnly );
+	}
 
 	protected :
 
@@ -128,14 +154,15 @@ void bind()
 	typedef typename T::ValueType V;
 
 	PlugClass<T>()
-		.def( init<const char *, Plug::Direction, V, V, V, unsigned>(
+		.def( init<const char *, Plug::Direction, V, V, V, unsigned, IECore::GeometricData::Interpretation>(
 				(
 					boost::python::arg_( "name" )=GraphComponent::defaultName<T>(),
 					boost::python::arg_( "direction" )=Plug::In,
 					boost::python::arg_( "defaultValue" )=V( 0 ),
 					boost::python::arg_( "minValue" )=V(Imath::limits<typename V::BaseType>::min()),
 					boost::python::arg_( "maxValue" )=V(Imath::limits<typename V::BaseType>::max()),
-					boost::python::arg_( "flags" )=Plug::Default
+					boost::python::arg_( "flags" )=Plug::Default,
+					boost::python::arg_( "interpretation" )=IECore::GeometricData::None
 				)
 			)
 		)
@@ -146,10 +173,12 @@ void bind()
 		.def( "maxValue", &T::maxValue )
 		.def( "setValue", &setValue<T> )
 		.def( "getValue", &getValue<T> )
+		.def( "interpretation", &T::interpretation )
 		.def( "canGang", &T::canGang )
 		.def( "gang", &gang<T> )
 		.def( "isGanged", &T::isGanged )
 		.def( "ungang", &ungang<T> )
+		.def( "__repr__", &compoundNumericPlugRepr<T> )
 	;
 
 	Serialisation::registerSerialiser( T::staticTypeId(), new CompoundNumericPlugSerialiser<T>() );

commit f01aca661c582716b80aa3b3ed3db8fb4c8b6694

CompoundNumericPlugBinding : Fix export of references from Box
This was broken by 6c9c14b34c80ed38c5b40691873d7b13f0779996.

src/GafferBindings/CompoundNumericPlugBinding.cpp
@@ -56,7 +56,7 @@ namespace
 {
 
 template<typename T>
-std::string maskedCompoundNumericPlugRepr( const T *plug, unsigned flagsMask )
+std::string maskedCompoundNumericPlugRepr( const T *plug, unsigned flagsMask, const Serialisation *serialisation = NULL )
 {
 	std::string extraArgs = "";
 
@@ -70,7 +70,7 @@ std::string maskedCompoundNumericPlugRepr( const T *plug, unsigned flagsMask )
 		boost::replace_first( extraArgs, "_IECore", "GeometricData" );
 	}
 
-	return ValuePlugSerialiser::repr( plug, flagsMask, extraArgs );
+	return ValuePlugSerialiser::repr( plug, flagsMask, extraArgs, serialisation );
 }
 
 template<typename T>
@@ -86,7 +86,7 @@ class CompoundNumericPlugSerialiser : public ValuePlugSerialiser
 
 	virtual std::string constructor( const Gaffer::GraphComponent *graphComponent, const Serialisation &serialisation ) const
 	{
-		return maskedCompoundNumericPlugRepr( static_cast<const T *>( graphComponent ), Plug::All & ~Plug::ReadOnly );
+		return maskedCompoundNumericPlugRepr( static_cast<const T *>( graphComponent ), Plug::All & ~Plug::ReadOnly, &serialisation );
 	}
 
 	protected :

python/GafferTest/ReferenceTest.py
@@ -607,6 +607,8 @@ class ReferenceTest( GafferTest.TestCase ) :
 		s["b"] = Gaffer.Box()
 		s["b"]["p"] = Gaffer.IntPlug( defaultValue = 1, flags = Gaffer.Plug.Flags.Default | Gaffer.Plug.Flags.Dynamic )
 		s["b"]["p"].setValue( 2 )
+		s["b"]["c"] = Gaffer.Color3fPlug( defaultValue = IECore.Color3f( 1 ), flags = Gaffer.Plug.Flags.Default | Gaffer.Plug.Flags.Dynamic )
+		s["b"]["c"].setValue( IECore.Color3f( 0.5 ) )
 		s["b"].exportForReference( self.temporaryDirectory() + "/test.grf" )
 
 		s["r"] = Gaffer.Reference()
@@ -621,6 +623,10 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["r"]["p"].defaultValue(), 2 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
 
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 0.5 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 0.5 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
+
 		# And we should be able to save and reload the script
 		# and have that still be the case.
 
@@ -633,11 +639,17 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["b"]["p"].getValue(), 2 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
 
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 0.5 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 0.5 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 0.5 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
+
 		# If we change the value on the box and reexport,
 		# then the reference should pick up both the new value
 		# and the new default.
 
 		s["b"]["p"].setValue( 3 )
+		s["b"]["c"].setValue( IECore.Color3f( 0.25 ) )
 		s["b"].exportForReference( self.temporaryDirectory() + "/test.grf" )
 		s["r"].load( self.temporaryDirectory() + "/test.grf" )
 
@@ -645,6 +657,11 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["r"]["p"].defaultValue(), 3 )
 		self.assertEqual( s["b"]["p"].getValue(), 3 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
+
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
 
 		# And that should still hold after saving and reloading the script.
 
@@ -655,15 +672,26 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["b"]["p"].getValue(), 3 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
 
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
+
 		# But if the user changes the value on the reference node,
 		# it should be kept.
 
 		s["r"]["p"].setValue( 100 )
+		s["r"]["c"].setValue( IECore.Color3f( 100 ) )
 
 		self.assertEqual( s["r"]["p"].getValue(), 100 )
 		self.assertEqual( s["r"]["p"].defaultValue(), 3 )
 		self.assertEqual( s["b"]["p"].getValue(), 3 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
+
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 100 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
 
 		# And a save and load shouldn't change that.
 
@@ -675,11 +703,17 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["b"]["p"].getValue(), 3 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
 
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 100 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 0.25 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
+
 		# And now the user has changed a value, only the
 		# default value should be updated if we load a new
 		# reference.
 
 		s["b"]["p"].setValue( 4 )
+		s["b"]["c"].setValue( IECore.Color3f( 4 ) )
 		s["b"].exportForReference( self.temporaryDirectory() + "/test.grf" )
 		s["r"].load( self.temporaryDirectory() + "/test.grf" )
 
@@ -687,6 +721,11 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["r"]["p"].defaultValue(), 4 )
 		self.assertEqual( s["b"]["p"].getValue(), 4 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
+
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 100 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 4 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 4 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
 
 		# And a save and load shouldn't change anything.
 
@@ -697,6 +736,11 @@ class ReferenceTest( GafferTest.TestCase ) :
 		self.assertEqual( s["r"]["p"].defaultValue(), 4 )
 		self.assertEqual( s["b"]["p"].getValue(), 4 )
 		self.assertEqual( s["b"]["p"].defaultValue(), 1 )
+
+		self.assertEqual( s["r"]["c"].getValue(), IECore.Color3f( 100 ) )
+		self.assertEqual( s["r"]["c"].defaultValue(), IECore.Color3f( 4 ) )
+		self.assertEqual( s["b"]["c"].getValue(), IECore.Color3f( 4 ) )
+		self.assertEqual( s["b"]["c"].defaultValue(), IECore.Color3f( 1 ) )
 
 		# And since we know that all plugs in box exports
 		# have had their default values set to the current