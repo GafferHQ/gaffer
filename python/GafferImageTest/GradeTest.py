GafferHQ/gaffer

commit 93c9c148bb9ac4c8ce11f84b85cca97ec8de9b46

GafferImage : Used ImagePlug::GlobalScope in a large number of places where global plugs are evaluated

src/GafferImage/ImageProcessor.cpp
@@ -134,15 +134,19 @@ void ImageProcessor::hash( const Gaffer::ValuePlug *output, const Gaffer::Contex
 
 	// we're computing a component of the output image. if we're disabled,
 	// then we wish to pass through the hash from the input.
-	bool passThrough = !enabled();
-	if( !passThrough )
+	bool passThrough;
 	{
-		// even if we're enabled at the image level, the channel might be disabled
-		// at the channelData level.
-		if( output == imagePlug->channelDataPlug() )
+		ImagePlug::GlobalScope c( context );
+		passThrough = !enabled();
+		if( !passThrough )
 		{
-			const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
-			passThrough = !channelEnabled( channel );
+			// even if we're enabled at the image level, the channel might be disabled
+			// at the channelData level.
+			if( output == imagePlug->channelDataPlug() )
+			{
+				const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
+				passThrough = !channelEnabled( channel );
+			}
 		}
 	}
 
@@ -166,15 +170,19 @@ void ImageProcessor::compute( ValuePlug *output, const Context *context ) const
 		return;
 	}
 
-	bool passThrough = !enabled();
-	if( !passThrough )
+	bool passThrough;
 	{
-		// even if we're enabled at the image level, the channel might be disabled
-		// at the channelData level.
-		if( output == imagePlug->channelDataPlug() )
+		ImagePlug::GlobalScope c( context );
+		passThrough = !enabled();
+		if( !passThrough )
 		{
-			const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
-			passThrough = !channelEnabled( channel );
+			// even if we're enabled at the image level, the channel might be disabled
+			// at the channelData level.
+			if( output == imagePlug->channelDataPlug() )
+			{
+				const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
+				passThrough = !channelEnabled( channel );
+			}
 		}
 	}
 

src/GafferImage/CopyChannels.cpp
@@ -253,13 +253,23 @@ IECore::ConstStringVectorDataPtr CopyChannels::computeChannelNames( const Gaffer
 
 void CopyChannels::hashChannelData( const GafferImage::ImagePlug *parent, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
-	ConstCompoundObjectPtr mapping = mappingPlug()->getValue();
+	ConstCompoundObjectPtr mapping;
+	{
+		ImagePlug::GlobalScope c( context );
+		mapping = mappingPlug()->getValue();
+	}
 	if( const IntData *i = mapping->member<const IntData>( context->get<string>( ImagePlug::channelNameContextName ) ) )
 	{
 		const ImagePlug *inputImage = inPlugs()->getChild<ImagePlug>( i->readable() );
 		const V2i tileOrigin = context->get<V2i>( ImagePlug::tileOriginContextName );
 		const Box2i tileBound( tileOrigin, tileOrigin + V2i( ImagePlug::tileSize() ) );
-		const Box2i inputDataWindow = inputImage->dataWindowPlug()->getValue();
+
+		Box2i inputDataWindow;
+		{
+			ImagePlug::GlobalScope c( context );
+			inputDataWindow = inputImage->dataWindowPlug()->getValue();
+		}
+
 		const Box2i validBound = BufferAlgo::intersection( tileBound, inputDataWindow );
 		if( validBound == tileBound )
 		{
@@ -283,13 +293,21 @@ void CopyChannels::hashChannelData( const GafferImage::ImagePlug *parent, const 
 
 IECore::ConstFloatVectorDataPtr CopyChannels::computeChannelData( const std::string &channelName, const Imath::V2i &tileOrigin, const Gaffer::Context *context, const ImagePlug *parent ) const
 {
-	ConstCompoundObjectPtr mapping = mappingPlug()->getValue();
+	ConstCompoundObjectPtr mapping;
+	{
+		ImagePlug::GlobalScope c( context );
+		mapping = mappingPlug()->getValue();
+	}
 	if( const IntData *i = mapping->member<const IntData>( channelName ) )
 	{
 		const ImagePlug *inputImage = inPlugs()->getChild<ImagePlug>( i->readable() );
 		const V2i tileOrigin = context->get<V2i>( ImagePlug::tileOriginContextName );
 		const Box2i tileBound( tileOrigin, tileOrigin + V2i( ImagePlug::tileSize() ) );
-		const Box2i inputDataWindow = inputImage->dataWindowPlug()->getValue();
+		Box2i inputDataWindow;
+		{
+			ImagePlug::GlobalScope c( context );
+			inputDataWindow = inputImage->dataWindowPlug()->getValue();
+		}
 		const Box2i validBound = BufferAlgo::intersection( tileBound, inputDataWindow );
 		if( validBound == tileBound )
 		{

src/GafferImage/Crop.cpp
@@ -325,6 +325,7 @@ void Crop::hash( const Gaffer::ValuePlug *output, const Gaffer::Context *context
 	}
 	else if( output->parent<Plug>() == offsetPlug() )
 	{
+		ImagePlug::GlobalScope c( context );
 		affectDisplayWindowPlug()->hash( h );
 		resetOriginPlug()->hash( h );
 		cropWindowPlug()->hash( h );
@@ -378,6 +379,7 @@ void Crop::compute( Gaffer::ValuePlug *output, const Gaffer::Context *context ) 
 	}
 	else if( output->parent<Plug>() == offsetPlug() )
 	{
+		ImagePlug::GlobalScope c( context );
 		V2i offset( 0 );
 		if( affectDisplayWindowPlug()->getValue() )
 		{

src/GafferImage/ImageNode.cpp
@@ -91,7 +91,12 @@ bool ImageNode::enabled() const
 void ImageNode::hash( const Gaffer::ValuePlug *output, const Gaffer::Context *context, IECore::MurmurHash &h ) const
 {
 	const ImagePlug *imagePlug = output->parent<ImagePlug>();
-	if( imagePlug && enabled() )
+	bool enabledValue;
+	{
+		ImagePlug::GlobalScope c( context );
+		enabledValue = enabled();
+	}
+	if( imagePlug && enabledValue )
 	{
 		// We don't call ComputeNode::hash() immediately here, because for subclasses which
 		// want to pass through a specific hash in the hash*() methods it's a waste of time (the
@@ -200,7 +205,13 @@ void ImageNode::compute( ValuePlug *output, const Context *context ) const
 
 	// we're computing part of an ImagePlug
 
-	if( !enabled() )
+	bool enabledValue;
+	{
+		ImagePlug::GlobalScope c( context );
+		enabledValue = enabled();
+	}
+
+	if( !enabledValue )
 	{
 		// disabled nodes just output a default black image.
 		output->setToDefault();

src/GafferImage/ImageTransform.cpp
@@ -276,7 +276,10 @@ void ImageTransform::hashChannelData( const GafferImage::ImagePlug *parent, cons
 	else
 	{
 		// Rotation of either the input or the resampled input.
-		ImageTransform::hashDataWindow( parent, context, h );
+		{
+			ImagePlug::GlobalScope c( context );
+			ImageTransform::hashDataWindow( parent, context, h );
+		}
 
 		const ImagePlug *samplerImage; M33f samplerMatrix;
 		const Box2i samplerRegion = sampler( op, matrix, resampleMatrix, context->get<V2i>( ImagePlug::tileOriginContextName ), samplerImage, samplerMatrix );

src/GafferImage/Merge.cpp
@@ -204,7 +204,15 @@ void Merge::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer:
 			continue;
 		}
 
-		IECore::ConstStringVectorDataPtr channelNamesData = (*it)->channelNamesPlug()->getValue();
+		IECore::ConstStringVectorDataPtr channelNamesData;
+		Box2i dataWindow;
+
+		{
+			ImagePlug::GlobalScope c( context );
+			channelNamesData = (*it)->channelNamesPlug()->getValue();
+			dataWindow = (*it)->dataWindowPlug()->getValue();
+		}
+		
 		const std::vector<std::string> &channelNames = channelNamesData->readable();
 
 		if( ImageAlgo::channelExists( channelNames, channelName ) )
@@ -227,7 +235,7 @@ void Merge::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer:
 		// input data windows, we may be using/revealing the invalid parts of a tile. We
 		// deal with this in computeChannelData() by treating the invalid parts as black,
 		// and must therefore hash in the valid bound here to take that into account.
-		const Box2i validBound = boxIntersection( tileBound, (*it)->dataWindowPlug()->getValue() );
+		const Box2i validBound = boxIntersection( tileBound, dataWindow );
 		h.append( validBound );
 	}
 
@@ -287,7 +295,14 @@ IECore::ConstFloatVectorDataPtr Merge::merge( F f, const std::string &channelNam
 			continue;
 		}
 
-		IECore::ConstStringVectorDataPtr channelNamesData = (*it)->channelNamesPlug()->getValue();
+		IECore::ConstStringVectorDataPtr channelNamesData;
+		Box2i dataWindow;
+		{
+			ImagePlug::GlobalScope c( Context::current() );
+			channelNamesData = (*it)->channelNamesPlug()->getValue();
+			dataWindow = (*it)->dataWindowPlug()->getValue();
+		}
+
 		const std::vector<std::string> &channelNames = channelNamesData->readable();
 
 		ConstFloatVectorDataPtr channelData;
@@ -311,7 +326,7 @@ IECore::ConstFloatVectorDataPtr Merge::merge( F f, const std::string &channelNam
 			alphaData = ImagePlug::blackTile();
 		}
 
-		const Box2i validBound = boxIntersection( tileBound, (*it)->dataWindowPlug()->getValue() );
+		const Box2i validBound = boxIntersection( tileBound, dataWindow );
 
 		if( !resultData )
 		{

src/GafferImage/Mirror.cpp
@@ -213,12 +213,17 @@ void Mirror::hashChannelData( const GafferImage::ImagePlug *parent, const Gaffer
 	const std::string &channelName = context->get<string>( ImagePlug::channelNameContextName );
 	const V2i tileOrigin = context->get<V2i>( ImagePlug::tileOriginContextName );
 	const Box2i tileBound( tileOrigin, tileOrigin + V2i( ImagePlug::tileSize() ) );
+	Box2i displayWindow;
+	{
+		ImagePlug::GlobalScope c( context );
+		displayWindow = inPlug()->formatPlug()->getValue().getDisplayWindow();
+	}
 
 	const Box2i sampleWindow = mirror(
 		tileBound,
 		horizontal,
 		vertical,
-		inPlug()->formatPlug()->getValue().getDisplayWindow()
+		displayWindow
 	);
 
 	Sampler sampler( inPlug(), channelName, sampleWindow );
@@ -238,7 +243,11 @@ IECore::ConstFloatVectorDataPtr Mirror::computeChannelData( const std::string &c
 		return inPlug()->channelDataPlug()->getValue();
 	}
 
-	const Box2i displayWindow = inPlug()->formatPlug()->getValue().getDisplayWindow();
+	Box2i displayWindow;
+	{
+		ImagePlug::GlobalScope c( context );
+		displayWindow = inPlug()->formatPlug()->getValue().getDisplayWindow();
+	}
 	const Box2i tileBound( tileOrigin, tileOrigin + V2i( ImagePlug::tileSize() ) );
 	const Box2i sampleWindow = mirror(
 		tileBound,

src/GafferImage/Mix.cpp
@@ -261,7 +261,14 @@ void Mix::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::C
 			continue;
 		}
 
-		IECore::ConstStringVectorDataPtr channelNamesData = (*it)->channelNamesPlug()->getValue();
+		IECore::ConstStringVectorDataPtr channelNamesData;
+		Box2i dataWindow;
+		{
+			ImagePlug::GlobalScope c( Context::current() );
+			channelNamesData = (*it)->channelNamesPlug()->getValue();
+			dataWindow = (*it)->dataWindowPlug()->getValue();
+		}
+
 		const std::vector<std::string> &channelNames = channelNamesData->readable();
 
 		if( ImageAlgo::channelExists( channelNames, channelName ) )
@@ -280,18 +287,26 @@ void Mix::hashChannelData( const GafferImage::ImagePlug *output, const Gaffer::C
 		// input data windows, we may be using/revealing the invalid parts of a tile. We
 		// deal with this in computeChannelData() by treating the invalid parts as black,
 		// and must therefore hash in the valid bound here to take that into account.
-		const Box2i validBound = boxIntersection( tileBound, (*it)->dataWindowPlug()->getValue() );
+		const Box2i validBound = boxIntersection( tileBound, dataWindow );
 		h.append( validBound );
+	}
+
+	IECore::ConstStringVectorDataPtr maskChannelNamesData;
+	Box2i maskDataWindow;
+	{
+		ImagePlug::GlobalScope c( Context::current() );
+		maskChannelNamesData = maskPlug()->channelNamesPlug()->getValue();
+		maskDataWindow = maskPlug()->dataWindowPlug()->getValue();
 	}
 
 
 	const std::string &maskChannel = maskChannelPlug()->getValue();
-	if( maskPlug()->getInput<ValuePlug>() && ImageAlgo::channelExists( maskPlug(), maskChannel ) )
+	if( maskPlug()->getInput<ValuePlug>() && ImageAlgo::channelExists( maskChannelNamesData->readable(), maskChannel ) )
 	{
 		h.append( maskPlug()->channelDataHash( maskChannel, tileOrigin ) );
 	}
 
-	const Box2i maskValidBound = boxIntersection( tileBound, maskPlug()->dataWindowPlug()->getValue() );
+	const Box2i maskValidBound = boxIntersection( tileBound, maskDataWindow );
 	h.append( maskValidBound );
 }
 
@@ -311,13 +326,21 @@ IECore::ConstFloatVectorDataPtr Mix::computeChannelData( const std::string &chan
 
 	const Box2i tileBound( tileOrigin, tileOrigin + V2i( ImagePlug::tileSize() ) );
 
+	IECore::ConstStringVectorDataPtr maskChannelNamesData;
+	Box2i maskDataWindow;
+	{
+		ImagePlug::GlobalScope c( Context::current() );
+		maskChannelNamesData = maskPlug()->channelNamesPlug()->getValue();
+		maskDataWindow = maskPlug()->dataWindowPlug()->getValue();
+	}
+
 	const std::string &maskChannel = maskChannelPlug()->getValue();
 	ConstFloatVectorDataPtr maskData = NULL;
 	Box2i maskValidBound;
-	if( maskPlug()->getInput<ValuePlug>() && ImageAlgo::channelExists( maskPlug(), maskChannel ) )
+	if( maskPlug()->getInput<ValuePlug>() && ImageAlgo::channelExists( maskChannelNamesData->readable(), maskChannel ) )
 	{
 		maskData = maskPlug()->channelData( maskChannel, tileOrigin );
-		maskValidBound = boxIntersection( tileBound, maskPlug()->dataWindowPlug()->getValue() );
+		maskValidBound = boxIntersection( tileBound, maskDataWindow );
 	}
 
 	ConstFloatVectorDataPtr channelData[2];
@@ -326,14 +349,21 @@ IECore::ConstFloatVectorDataPtr Mix::computeChannelData( const std::string &chan
 	int i = 0;
 	for( ImagePlugIterator it( inPlugs() ); !it.done(); ++it,++i )
 	{
-		IECore::ConstStringVectorDataPtr channelNamesData = (*it)->channelNamesPlug()->getValue();
+		IECore::ConstStringVectorDataPtr channelNamesData;
+		Box2i dataWindow;
+		{
+			ImagePlug::GlobalScope c( Context::current() );
+			channelNamesData = (*it)->channelNamesPlug()->getValue();
+			dataWindow = (*it)->dataWindowPlug()->getValue();
+		}
+
 		const std::vector<std::string> &channelNames = channelNamesData->readable();
 
 
 		if( ImageAlgo::channelExists( channelNames, channelName ) )
 		{
 			channelData[i] = (*it)->channelDataPlug()->getValue();
-			validBound[i] = boxIntersection( tileBound, (*it)->dataWindowPlug()->getValue() );
+			validBound[i] = boxIntersection( tileBound, dataWindow );
 		}
 		else
 		{

commit fbb78307a4aa885819ac056b804d1b6d3fe9b491

ImageProcessor : Fix `channelEnabled()` mechanism
This was broken in 93c9c148bb9ac4c8ce11f84b85cca97ec8de9b46. We can now use expressions to perform per-layer grading and suchlike again.

src/GafferImage/ImageProcessor.cpp
@@ -138,15 +138,15 @@ void ImageProcessor::hash( const Gaffer::ValuePlug *output, const Gaffer::Contex
 	{
 		ImagePlug::GlobalScope c( context );
 		passThrough = !enabled();
-		if( !passThrough )
+	}
+	if( !passThrough )
+	{
+		// even if we're enabled at the image level, the channel might be disabled
+		// at the channelData level.
+		if( output == imagePlug->channelDataPlug() )
 		{
-			// even if we're enabled at the image level, the channel might be disabled
-			// at the channelData level.
-			if( output == imagePlug->channelDataPlug() )
-			{
-				const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
-				passThrough = !channelEnabled( channel );
-			}
+			const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
+			passThrough = !channelEnabled( channel );
 		}
 	}
 
@@ -174,15 +174,15 @@ void ImageProcessor::compute( ValuePlug *output, const Context *context ) const
 	{
 		ImagePlug::GlobalScope c( context );
 		passThrough = !enabled();
-		if( !passThrough )
+	}
+	if( !passThrough )
+	{
+		// even if we're enabled at the image level, the channel might be disabled
+		// at the channelData level.
+		if( output == imagePlug->channelDataPlug() )
 		{
-			// even if we're enabled at the image level, the channel might be disabled
-			// at the channelData level.
-			if( output == imagePlug->channelDataPlug() )
-			{
-				const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
-				passThrough = !channelEnabled( channel );
-			}
+			const std::string &channel = context->get<std::string>( ImagePlug::channelNameContextName );
+			passThrough = !channelEnabled( channel );
 		}
 	}
 

python/GafferImageTest/GradeTest.py
@@ -35,6 +35,7 @@
 ##########################################################################
 
 import os
+import inspect
 import unittest
 
 import IECore
@@ -170,7 +171,7 @@ class GradeTest( GafferImageTest.ImageTestCase ) :
 		c = GafferImage.Constant()
 		c["format"].setValue( GafferImage.Format( 50, 50, 1.0 ) )
 		c["color"].setValue( IECore.Color4f( 0.125, 0.25, 0.5, 0.75 ) )
-	
+
 		s = GafferImage.Shuffle()
 		s["channels"].addChild( GafferImage.Shuffle.ChannelPlug( 'customChannel', '__white' ) )
 		s["in"].setInput( c["out"] )
@@ -206,4 +207,42 @@ class GradeTest( GafferImageTest.ImageTestCase ) :
 
 		g["channels"].setValue( IECore.StringVectorData( [ "R", "G", "B", "A", "customChannel" ] ) )
 		self.assertEqual( sample( 25, 25 ), [ 3.125, 3.25, 3.5, 3.75, 4.0 ] )
-		
+
+	def testPerLayerExpression( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		script["c1"] = GafferImage.Constant()
+		script["c1"]["color"].setValue( IECore.Color4f( 1 ) )
+
+		script["c2"] = GafferImage.Constant()
+		script["c2"]["color"].setValue( IECore.Color4f( 1 ) )
+		script["c2"]["layer"].setValue( "B" )
+
+		script["copyChannels"] = GafferImage.CopyChannels()
+		script["copyChannels"]["in"][0].setInput( script["c1"]["out"] )
+		script["copyChannels"]["in"][1].setInput( script["c2"]["out"] )
+		script["copyChannels"]["channels"].setValue( "*" )
+
+		script["grade"] = GafferImage.Grade()
+		script["grade"]["in"].setInput( script["copyChannels"]["out"] )
+		script["grade"]["channels"].setValue( "*" )
+
+		script["expression"] = Gaffer.Expression()
+		script["expression"].setExpression( inspect.cleandoc(
+			"""
+			import GafferImage
+			layerName = GafferImage.ImageAlgo.layerName( context["image:channelName" ] )
+			parent["grade"]["gain"] = IECore.Color4f( 1 if layerName == "B" else 0.5 )
+			"""
+		) )
+
+		sampler = GafferImage.ImageSampler()
+		sampler["image"].setInput( script["grade"]["out"] )
+		sampler["pixel"].setValue( IECore.V2f( 10.5 ) )
+
+		sampler["channels"].setValue( IECore.StringVectorData( [ "R", "G", "B", "A" ] ) )
+		self.assertEqual( sampler["color"].getValue(), IECore.Color4f( 0.5 ) )
+
+		sampler["channels"].setValue( IECore.StringVectorData( [ "B.R", "B.G", "B.B", "B.A" ] ) )
+		self.assertEqual( sampler["color"].getValue(), IECore.Color4f( 1 ) )