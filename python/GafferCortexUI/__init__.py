895384fe90f96771f16c73a8cecc7e36e084f7cd
John Haddon <thehaddonyoof@gmail.com>
2017-08-31 12:16:37 +0100

    GafferCortexUI : Remove ImageReaderPathPreview
    It wasn't that good, and it relied on being able to render an ImagePrimitive directly via RenderableGadget, which is no longer possible.

--- python/GafferCortexUI/__init__.py
+++ python/GafferCortexUI/__init__.py
@@ -38,7 +38,6 @@
 # PathPreviewWidgets and browser modes
 
 from FileIndexedIOPathPreview import FileIndexedIOPathPreview
-from ImageReaderPathPreview import ImageReaderPathPreview
 from OpPathPreview import OpPathPreview
 import OpBrowserMode
 
