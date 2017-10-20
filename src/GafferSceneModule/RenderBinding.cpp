bb66477c4b83a539281f09a168f86aecfc7b6252
John Haddon <thehaddonyoof@gmail.com>
2017-10-20 17:17:06 +0100

    Scene module : Bind Geometry class

--- src/GafferSceneModule/RenderBinding.cpp
+++ src/GafferSceneModule/RenderBinding.cpp
@@ -49,11 +49,13 @@
 #include "GafferScene/Preview/Render.h"
 #include "GafferScene/Private/IECoreScenePreview/Renderer.h"
 #include "GafferScene/Private/IECoreScenePreview/Procedural.h"
+#include "GafferScene/Private/IECoreScenePreview/Geometry.h"
 
 #include "RenderBinding.h"
 
 using namespace boost::python;
 
+using namespace Imath;
 using namespace IECoreScenePreview;
 using namespace Gaffer;
 using namespace GafferBindings;
@@ -306,6 +308,24 @@ void GafferSceneModule::bindRender()
 		IECorePython::RunTimeTypedClass<IECoreScenePreview::Procedural, ProceduralWrapper>()
 			.def( init<>() )
 			.def( "render", (void (Procedural::*)( IECoreScenePreview::Renderer *)const)&Procedural::render )
+		;
+
+
+		IECorePython::RunTimeTypedClass<Geometry>()
+			.def(
+				init<const std::string &, const Box3f &, const IECore::CompoundDataPtr &>(
+					(
+						arg( "type" ) = "",
+						arg( "bound" ) = Box3f(),
+						arg( "parameters" ) = object()
+					)
+				)
+			)
+			.def( "setType", &Geometry::setType )
+			.def( "getType", &Geometry::getType, return_value_policy<copy_const_reference>() )
+			.def( "setBound", &Geometry::setBound )
+			.def( "getBound", &Geometry::getBound, return_value_policy<copy_const_reference>() )
+			.def( "parameters", (IECore::CompoundData *(Geometry::*)())&Geometry::parameters, return_value_policy<IECorePython::CastToIntrusivePtr>() )
 		;
 
 	}
