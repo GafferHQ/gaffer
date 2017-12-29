c144769d1203aae7f75559dc7b3c161b0d1d44d4
Alex Fuller <boberfly@gmail.com>
2017-12-28 16:58:41 -0800

    GafferUI : Ensure forward slashes in Style Sheet
    - Windows `GAFFER_ROOT` will use backslashes which makes style sheet URLs fail to resolve.

--- python/GafferUI/_StyleSheet.py
+++ python/GafferUI/_StyleSheet.py
@@ -139,7 +139,7 @@ _themeVariables = {
 }
 
 substitutions = {
-	"GAFFER_ROOT" : os.environ["GAFFER_ROOT"]
+	"GAFFER_ROOT" : os.environ["GAFFER_ROOT"].replace("\\", "/"),
 }
 
 for k, v in _styleColors.items() :
