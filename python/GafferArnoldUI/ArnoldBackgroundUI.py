00baae0a5576667bd4b8779ec9f377e2cfb425a7
John Haddon <thehaddonyoof@gmail.com>
2018-02-12 12:18:07 +0000

    GafferArnoldUI : Add ArnoldBackgroundUI

--- python/GafferArnoldUI/ArnoldBackgroundUI.py
+++ python/GafferArnoldUI/ArnoldBackgroundUI.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2012, John Haddon. All rights reserved.
+#  Copyright (c) 2018, John Haddon. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -34,24 +34,18 @@
 #
 ##########################################################################
 
-__import__( "GafferSceneUI" )
+import Gaffer
+import GafferArnold
 
-from _GafferArnoldUI import *
+Gaffer.Metadata.registerNode(
 
-import ArnoldShaderUI
-import ArnoldRenderUI
-import ShaderMenu
-import ArnoldOptionsUI
-import ArnoldAttributesUI
-import ArnoldLightUI
-import ArnoldVDBUI
-import InteractiveArnoldRenderUI
-import ArnoldDisplacementUI
-import ArnoldMeshLightUI
-import ArnoldShaderBallUI
-import ArnoldAOVShaderUI
-import ArnoldAtmosphereUI
-import ArnoldBackgroundUI
-import CacheMenu
+	GafferArnold.ArnoldBackground,
 
-__import__( "IECore" ).loadConfig( "GAFFER_STARTUP_PATHS", {}, subdirectory = "GafferArnoldUI" )
+	"description",
+	"""
+	Assigns a background shader. This is stored as an "ai:background" option
+	in Gaffer's globals, and translated onto the `options.background` parameter
+	in Arnold.
+	""",
+
+)
