4dfca3f6a938d9430673e45fee5fc6d0a889f35d
John Haddon <thehaddonyoof@gmail.com>
2018-03-15 11:57:51 +0000

    FileMenu : Add `addScript()` function
    I've deliberately kept the dialogueParentWindow as an internal implementation detail for now, as I think in future we'll probably have a single ApplicationWindow that we can automatically use for the parent.

--- python/GafferUI/FileMenu.py
+++ python/GafferUI/FileMenu.py
@@ -88,9 +88,13 @@ def open( menu ) :
 
 	__open( scriptWindow.scriptNode(), str( path ) )
 
-def __open( currentScript, fileName ) :
+## Opens a script and adds it to the application, as if the user
+# had done so via the File Open dialogue.
+def addScript( application, fileName ) :
 
-	application = currentScript.ancestor( Gaffer.ApplicationRoot )
+	return __addScript( application, fileName )
+
+def __addScript( application, fileName, dialogueParentWindow = None ) :
 
 	recoveryFileName = None
 	backups = GafferUI.Backups.acquire( application, createIfNecessary = False )
@@ -103,8 +107,9 @@ def __open( currentScript, fileName ) :
 				confirmLabel = "Open Backup",
 				cancelLabel = "Open",
 			)
-			if not dialogue.waitForConfirmation( parentWindow = GafferUI.ScriptWindow.acquire( currentScript ) ) :
+			if not dialogue.waitForConfirmation( parentWindow = dialogueParentWindow ) :
 				recoveryFileName = None
+			del dialogue
 
 	script = Gaffer.ScriptNode()
 	script["fileName"].setValue( recoveryFileName or fileName )
@@ -112,7 +117,7 @@ def __open( currentScript, fileName ) :
 	with GafferUI.ErrorDialogue.ErrorHandler(
 		title = "Errors Occurred During Loading",
 		closeLabel = "Oy vey",
-		parentWindow = GafferUI.ScriptWindow.acquire( currentScript )
+		parentWindow = dialogueParentWindow
 	) :
 		script.load( continueOnError = True )
 
@@ -126,12 +131,20 @@ def __open( currentScript, fileName ) :
 
 	addRecentFile( application, fileName )
 
+	return script
+
+def __open( currentScript, fileName ) :
+
+	application = currentScript.ancestor( Gaffer.ApplicationRoot )
+	currentWindow = GafferUI.ScriptWindow.acquire( currentScript )
+
+	script = __addScript( application, fileName, dialogueParentWindow = currentWindow )
+
 	removeCurrentScript = False
 	if not currentScript["fileName"].getValue() and not currentScript["unsavedChanges"].getValue() :
 		# the current script is empty - the user will think of the operation as loading
 		# the new script into the current window, rather than adding a new window. so make it
 		# look like that.
-		currentWindow = GafferUI.ScriptWindow.acquire( currentScript )
 		newWindow = GafferUI.ScriptWindow.acquire( script )
 		## \todo We probably want a way of querying and setting geometry in the public API
 		newWindow._qtWidget().restoreGeometry( currentWindow._qtWidget().saveGeometry() )
