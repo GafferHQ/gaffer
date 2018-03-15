704c8e6e653caeee85ae18b6ad5dc9bd850321bf
John Haddon <thehaddonyoof@gmail.com>
2018-03-15 15:31:51 +0000

    SceneViewUI : Highlight camera button when look-through enabled

--- python/GafferSceneUI/SceneViewUI.py
+++ python/GafferSceneUI/SceneViewUI.py
@@ -297,7 +297,6 @@ class _CameraPlugValueWidget( GafferUI.PlugValueWidget ) :
 	def __init__( self, plug, **kw ) :
 
 		self.__menuButton = GafferUI.MenuButton(
-			image = "camera.png",
 			menu = GafferUI.Menu( Gaffer.WeakMethod( self.__menuDefinition ), title="Camera" ),
 			hasFrame = False,
 		)
@@ -310,6 +309,8 @@ class _CameraPlugValueWidget( GafferUI.PlugValueWidget ) :
 		self.__dragEnterConnection = self.dragEnterSignal().connect( 0, Gaffer.WeakMethod( self.__dragEnter ) )
 		self.__dropConnection = self.dropSignal().connect( 0, Gaffer.WeakMethod( self.__drop ) )
 
+		self._updateFromPlug()
+
 	def setHighlighted( self, highlighted ) :
 
 		GafferUI.PlugValueWidget.setHighlighted( self, highlighted )
@@ -318,7 +319,9 @@ class _CameraPlugValueWidget( GafferUI.PlugValueWidget ) :
 
 	def _updateFromPlug( self ) :
 
-		pass
+		self.__menuButton.setImage(
+			"cameraOn.png" if self.getPlug()["lookThroughEnabled"].getValue() else "cameraOff.png"
+		)
 
 	def __menuDefinition( self ) :
 
