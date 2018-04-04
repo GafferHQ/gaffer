GafferHQ/gaffer

commit e81835e2ca987e5684c9ecdd308e562c9c1e1f47

BoxIO : Preserve plug values when promoting

src/Gaffer/BoxIO.cpp
@@ -134,6 +134,29 @@ void applyDynamicFlag( Plug *plug )
 	}
 }
 
+// \todo This also exists in PlugAlgo.cpp. Should it be a public method,
+// and if so, what should happen when the plugs don't match (the asserts
+// wouldn't be appropriate). Or, more radically, should the `setFrom()`
+// virtual method be moved from ValuePlug to Plug?
+void setFrom( Plug *dst, const Plug *src )
+{
+	assert( dst->typeId() == src->typeId() );
+	if( ValuePlug *dstValuePlug = IECore::runTimeCast<ValuePlug>( dst ) )
+	{
+		dstValuePlug->setFrom( static_cast<const ValuePlug *>( src ) );
+	}
+	else
+	{
+		for( PlugIterator it( dst ); !it.done(); ++it )
+		{
+			Plug *dstChild = it->get();
+			const Plug *srcChild = src->getChild<Plug>( dstChild->getName() );
+			assert( srcChild );
+			setFrom( dstChild, srcChild );
+		}
+	}
+}
+
 } // namespace
 
 //////////////////////////////////////////////////////////////////////////
@@ -189,6 +212,11 @@ void BoxIO::setup( const Plug *plug )
 		}
 		addChild( plug->createCounterpart( inPlugName(), Plug::In ) );
 		addChild( plug->createCounterpart( outPlugName(), Plug::Out ) );
+
+		if( plug->direction() == Plug::In )
+		{
+			setFrom( inPlugInternal(), plug );
+		}
 
 		inPlugInternal()->setFlags( Plug::Serialisable, true );
 		outPlugInternal()->setFlags( Plug::Serialisable, true );

python/GafferTest/BoxInTest.py
@@ -346,6 +346,8 @@ class BoxInTest( GafferTest.TestCase ) :
 		s = Gaffer.ScriptNode()
 		s["b"] = Gaffer.Box()
 		s["b"]["n"] = GafferTest.AddNode()
+		s["b"]["n"]["op1"].setValue( 1 )
+		s["b"]["n"]["op2"].setValue( 2 )
 
 		Gaffer.Metadata.registerValue( s["b"]["n"]["op2"], "nodule:type", "" )
 
@@ -358,6 +360,11 @@ class BoxInTest( GafferTest.TestCase ) :
 		self.assertTrue( s["b"]["n"]["op2"].getInput().node().isSame( s["b"] ) )
 		self.assertEqual( len( s["b"]["n"]["sum"].outputs() ), 1 )
 		self.assertTrue( isinstance( s["b"]["n"]["sum"].outputs()[0].parent(), Gaffer.BoxOut ) )
+
+		self.assertEqual( s["b"]["n"]["op1"].getValue(), 1 )
+		self.assertEqual( s["b"]["n"]["op1"].source().getValue(), 1 )
+		self.assertEqual( s["b"]["n"]["op2"].getValue(), 2 )
+		self.assertEqual( s["b"]["n"]["op2"].source().getValue(), 2 )
 
 	def testInsert( self ) :
 

commit a94d6cd4c5b322b3c642c28a2e19a549e9c5acc1

BoxIO : Fix creation of Boxes around existing SceneNode graphs
This was broken by e81835e2ca987e5684c9ecdd308e562c9c1e1f47.

src/Gaffer/BoxIO.cpp
@@ -213,11 +213,6 @@ void BoxIO::setup( const Plug *plug )
 		addChild( plug->createCounterpart( inPlugName(), Plug::In ) );
 		addChild( plug->createCounterpart( outPlugName(), Plug::Out ) );
 
-		if( plug->direction() == Plug::In )
-		{
-			setFrom( inPlugInternal(), plug );
-		}
-
 		inPlugInternal()->setFlags( Plug::Serialisable, true );
 		outPlugInternal()->setFlags( Plug::Serialisable, true );
 		applyDynamicFlag( inPlugInternal() );
@@ -503,6 +498,11 @@ Plug *BoxIO::promote( Plug *plug )
 	box->addChild( boxIO );
 	boxIO->namePlug()->setValue( promotedName( plug ) );
 	boxIO->setup( plug );
+
+	if( plug->direction() == Plug::In )
+	{
+		setFrom( boxIO->promotedPlug(), plug );
+	}
 
 	connect( plug, boxIO->plug() );
 

python/GafferSceneTest/ShaderAssignmentTest.py
@@ -359,5 +359,21 @@ class ShaderAssignmentTest( GafferSceneTest.SceneTestCase ) :
 
 		self.assertTrue( s2["a"]["shader"].source().isSame( s2["Box"]["s"]["out"] ) )
 
+	def testInsertBoxIO( self ) :
+
+		s = Gaffer.ScriptNode()
+
+		s["n1"] = GafferScene.ShaderAssignment()
+		s["n2"] = GafferScene.ShaderAssignment()
+		s["n2"]["in"].setInput( s["n1"]["out"] )
+		s["n3"] = GafferScene.ShaderAssignment()
+		s["n3"]["in"].setInput( s["n2"]["out"] )
+
+		box = Gaffer.Box.create( s, Gaffer.StandardSet( { s["n2"] } ) )
+		Gaffer.BoxIO.insert( box )
+
+		self.assertTrue( box["n2"]["in"].source().isSame( s["n1"]["out"] ) )
+		self.assertTrue( s["n3"]["in"].source().isSame( box["n2"]["out"] ) )
+
 if __name__ == "__main__":
 	unittest.main()