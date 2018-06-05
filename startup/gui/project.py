c81d4b032ca7edae8d4e2ded1eaba38abbfe3345
Andrew Kaufman <andrewk.imageengine@gmail.com>
2018-06-05 11:26:07 -0700

    gui and dispatch apps : Separate non-project LocalDispatcher startup settings.

--- startup/gui/project.py
+++ startup/gui/project.py
@@ -112,6 +112,3 @@ for dispatcher in dispatchers :
 	Gaffer.Metadata.registerPlugValue( dispatcher, "jobName", "userDefault", "${script:name}" )
 	directoryName = dispatcher.staticTypeName().rpartition( ":" )[2].replace( "Dispatcher", "" ).lower()
 	Gaffer.Metadata.registerPlugValue( dispatcher, "jobsDirectory", "userDefault", "${project:rootDirectory}/dispatcher/" + directoryName )
-
-Gaffer.Metadata.registerPlugValue( GafferDispatch.LocalDispatcher, "executeInBackground", "userDefault", True )
-GafferDispatch.Dispatcher.setDefaultDispatcherType( "Local" )
