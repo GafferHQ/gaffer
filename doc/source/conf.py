8b8b024313ed17581bf5575ec8602ba6060e9909
medubelko <michael.dubelko@gmail.com>
2018-06-12 15:15:07 -0700

    Include: custom stylesheet Enable: logo, smart quotation marks Disable: links to reST sources

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -121,7 +121,9 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -135,7 +137,7 @@ html_theme = "sphinx_rtd_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/GafferLogoMini.svg"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -159,7 +161,7 @@ html_static_path = [ "_static" ]
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
+html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
@@ -178,7 +180,7 @@ html_static_path = [ "_static" ]
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
@@ -381,3 +383,6 @@ def setup( app ) :
     app.add_transform( GafferTransform )
 
     app.connect( "source-read", gafferSourceSubstitutions )
+    
+    # Add the custom stylesheet; used in all .md and .rst files in source
+    app.add_stylesheet( 'gaffer.css' )
