GafferHQ/gaffer

commit af3247ae90afd712821f2ddf093ca2589d817e24

Bitwise mask correction '&' instead of '&&'

src/GafferSceneUI/SceneGadget.cpp
@@ -923,7 +923,7 @@ void SceneGadget::updateSceneGraph() const
 		UpdateTask *task = new( tbb::task::allocate_root() ) UpdateTask( this, m_sceneGraph.get(), m_dirtyFlags, ScenePlug::ScenePath() );
 		tbb::task::spawn_root_and_wait( *task );
 
-		if( m_dirtyFlags && UpdateTask::ChildNamesDirty )
+		if( m_dirtyFlags & UpdateTask::ChildNamesDirty )
 		{
 			m_sceneGraph->applySelection( m_selection );
 		}

commit d25f88773608f43e4de0e3ae11c172bd5abf489d

SceneGadget : Apply selection after expanding
This was broken by `af3247ae90afd712821f2ddf093ca2589d817e24`, which by fixing one problem had exposed another error in the original code.

src/GafferSceneUI/SceneGadget.cpp
@@ -923,7 +923,7 @@ void SceneGadget::updateSceneGraph() const
 		UpdateTask *task = new( tbb::task::allocate_root() ) UpdateTask( this, m_sceneGraph.get(), m_dirtyFlags, ScenePlug::ScenePath() );
 		tbb::task::spawn_root_and_wait( *task );
 
-		if( m_dirtyFlags & UpdateTask::ChildNamesDirty )
+		if( m_dirtyFlags & ( UpdateTask::ChildNamesDirty | UpdateTask::ExpansionDirty ) )
 		{
 			m_sceneGraph->applySelection( m_selection );
 		}