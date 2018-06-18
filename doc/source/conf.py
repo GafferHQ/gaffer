eec261103e7acaf8c55b8418a557793890e125e2
medubelko <michael.dubelko@gmail.com>
2018-06-18 14:48:32 -0700

    conf.py : Enable automatic header bookmarking

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,12 @@ import Gaffer
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autosectionlabel'
+]
+
+# Whether to use unique IDs for automatic section labels (articleName:section).
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
