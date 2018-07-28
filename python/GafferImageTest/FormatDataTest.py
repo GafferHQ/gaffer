GafferHQ/gaffer

commit ad07e064d04b9f2205632f26a98346eab14ef45f

DataBinding : Replace `despatchTypedData()` with `DataAlgo::dispatch()`

src/GafferBindings/DataBinding.cpp
@@ -36,9 +36,10 @@
 
 #include "boost/python.hpp"
 
-#include "GafferBindings/DataBinding.h"
+#include "IECore/DataAlgo.h"
+#include "IECore/TypeTraits.h"
 
-#include "IECore/DespatchTypedData.h"
+#include "GafferBindings/DataBinding.h"
 
 using namespace boost::python;
 using namespace IECore;
@@ -46,15 +47,29 @@ using namespace IECore;
 namespace
 {
 
-struct SimpleTypedDataGetter
+struct DataToPython
 {
-	typedef object ReturnType;
+
+	DataToPython( bool copy )
+		:	m_copy( copy )
+	{
+	}
 
 	template<typename T>
-	object operator()( T *data )
+	object operator()( const T *data, typename std::enable_if<TypeTraits::IsSimpleTypedData<T>::value>::type *enabler = nullptr )
 	{
 		return object( data->readable() );
 	}
+
+	object operator()( Data *data )
+	{
+		return object( m_copy ? data->copy() : DataPtr( data ) );
+	}
+
+	private :
+
+		bool m_copy;
+
 };
 
 boost::python::object dataToPythonInternal( IECore::Data *data, bool copy, boost::python::object nullValue )
@@ -64,23 +79,7 @@ boost::python::object dataToPythonInternal( IECore::Data *data, bool copy, boost
 		return nullValue;
 	}
 
-	try
-	{
-		return despatchTypedData<SimpleTypedDataGetter, TypeTraits::IsSimpleTypedData>( data );
-	}
-	catch( const InvalidArgumentException &e )
-	{
-		// In an ideal world, we wouldn't be using exception handling to detect
-		// the cases where IsSimpleTypedData is false. We _could_ use a null error
-		// handler for the despatchTypedData() call, except for two annoying facts :
-		//
-		//	- Error handlers don't get to return a value
-		//  - The error handler isn't used if a totally unknown datatype is
-		//    found, and Gaffer adds new datatypes.
-		//
-		// It might be nice to fix that in Cortex.
-		return object( copy ? data->copy() : DataPtr( data ) );
-	}
+	return dispatch( data, DataToPython( copy ) );
 }
 
 } // namespace

commit 88e7fc739abc87b0276552ee7caf682a6e6880b2

DataBinding : Fix `dataToPython()` for non-IECore data types
This was broken by ad07e064d04b9f2205632f26a98346eab14ef45f.

src/GafferBindings/DataBinding.cpp
@@ -79,7 +79,18 @@ boost::python::object dataToPythonInternal( IECore::Data *data, bool copy, boost
 		return nullValue;
 	}
 
-	return dispatch( data, DataToPython( copy ) );
+	try
+	{
+		return dispatch( data, DataToPython( copy ) );
+	}
+	catch( ... )
+	{
+		// `dispatch()` doesn't know about our custom data types,
+		// so we deal with those here.
+		/// \todo Should `dispatch()` just call `Functor( Data * )`
+		/// for unknown types?
+		return object( DataPtr( data ) );
+	}
 }
 
 } // namespace

python/GafferImageTest/FormatDataTest.py
@@ -91,5 +91,13 @@ class FormatDataTest( GafferImageTest.ImageTestCase ) :
 		d["f"] = f
 		self.assertEqual( d["f"], GafferImage.FormatData( f ) )
 
+	def testStoreInContext( self ) :
+
+		f = GafferImage.Format( imath.Box2i( imath.V2i( 0 ), imath.V2i( 200, 100 ) ), 0.5 )
+		d = GafferImage.FormatData( f )
+		c = Gaffer.Context()
+		c["f"] = d
+		self.assertEqual( c["f"], d )
+
 if __name__ == "__main__":
 	unittest.main()