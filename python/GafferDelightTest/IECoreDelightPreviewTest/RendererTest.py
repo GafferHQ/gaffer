3413ae14ee18985ea0930e96f144c167971fe8f8
John Haddon <thehaddonyoof@gmail.com>
2018-10-10 12:17:03 +0100

    IECoreDelightPreview::NodeAlgo : Don't use NSIParamIndirect
    It no longer exists in the latest 3Delight version (1.1.7 at the time of writing).

--- python/GafferDelightTest/IECoreDelightPreviewTest/RendererTest.py
+++ python/GafferDelightTest/IECoreDelightPreviewTest/RendererTest.py
@@ -179,9 +179,9 @@ class RendererTest( GafferTest.TestCase ) :
 		nsi = self.__parse( self.temporaryDirectory() + "/test.nsi" )
 
 		self.__assertInNSI( '"P.indices" "int" 8 [ 0 1 4 3 1 2 5 4 ]', nsi )
-		self.__assertInNSI( '"P" "vi point" 6 [ -1 -1 0 0 -1 0 1 -1 0 -1 1 0 0 1 0 1 1 0 ]', nsi )
+		self.__assertInNSI( '"P" "v point" 6 [ -1 -1 0 0 -1 0 1 -1 0 -1 1 0 0 1 0 1 1 0 ]', nsi )
 		self.__assertInNSI( '"nvertices" "int" 2 [ 4 4 ]', nsi )
-		self.__assertInNSI( '"uv" "i float[2]" 6 [ 0 0 0.5 0 1 0 0 1 0.5 1 1 1 ]', nsi )
+		self.__assertInNSI( '"uv" "float[2]" 6 [ 0 0 0.5 0 1 0 0 1 0.5 1 1 1 ]', nsi )
 		self.__assertInNSI( '"uv.indices" "int" 8 [ 0 1 4 3 1 2 5 4 ]', nsi )
 
 	def testAnimatedMesh( self ) :
@@ -209,8 +209,8 @@ class RendererTest( GafferTest.TestCase ) :
 
 		self.__assertInNSI( '"P.indices" "int" 4 [ 0 1 3 2 ]', nsi )
 		self.assertEqual( nsi.count( '"P.indices"' ), 1 )
-		self.__assertInNSI( '"P" "vi point" 4 [ -1 -1 0 1 -1 0 -1 1 0 1 1 0 ]', nsi )
-		self.__assertInNSI( '"P" "vi point" 4 [ -2 -2 0 2 -2 0 -2 2 0 2 2 0 ]', nsi )
+		self.__assertInNSI( '"P" "v point" 4 [ -1 -1 0 1 -1 0 -1 1 0 1 1 0 ]', nsi )
+		self.__assertInNSI( '"P" "v point" 4 [ -2 -2 0 2 -2 0 -2 2 0 2 2 0 ]', nsi )
 		self.__assertInNSI( '"nvertices" "int" 1 4', nsi )
 		self.assertEqual( nsi.count( '"nvertices"' ), 1 )
 
