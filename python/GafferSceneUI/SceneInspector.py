GafferHQ/gaffer

commit 0185372c076b089ee705b352b56d9c7434e609a5

SceneInspector : Disable sections rather than entire editor
This allows keyboard shortcuts to be processed still. Perhaps we should be deferring the enabled status updates to the sections themselves anyway?

python/GafferSceneUI/SceneInspector.py
@@ -302,8 +302,7 @@ class SceneInspector( GafferUI.NodeSetEditor ) :
 
 			for section in self.__sections :
 				section.update( targets )
-
-			self.setEnabled( bool( targets ) )
+				section.setEnabled( section.getEnabled() and bool( targets ) )
 
 			return False # remove idle callback
 

commit dc142b0d48b151a847be06b99012f97cf1feff0c

SceneInspector : Fix section disabling
The global sections would fail to be re-enabled after their first disabling. This was broken in 0185372c076b089ee705b352b56d9c7434e609a5.

python/GafferSceneUI/SceneInspector.py
@@ -302,7 +302,6 @@ class SceneInspector( GafferUI.NodeSetEditor ) :
 
 			for section in self.__sections :
 				section.update( targets )
-				section.setEnabled( section.getEnabled() and bool( targets ) )
 
 			return False # remove idle callback
 
@@ -1161,6 +1160,8 @@ class Section( GafferUI.Widget ) :
 	# UI to reflect the state of the targets. Implementations should
 	# first call the base class implementation.
 	def update( self, targets ) :
+
+		self.setEnabled( bool( targets ) )
 
 		if self.__collapsible is None :
 			return