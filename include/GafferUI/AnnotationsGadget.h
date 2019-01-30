fa7731055450595ded1d9c935f9f97383f8782f5
John Haddon <thehaddonyoof@gmail.com>
2019-01-30 22:01:43 +0000

    GafferUI : Add AnnotationsGadget
    This is currently just capable of drawing the bookmark status for each node, but will soon be extended to render arbitrary annotations.

--- include/GafferUI/AnnotationsGadget.h
+++ include/GafferUI/AnnotationsGadget.h
@@ -1,7 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2011-2012, John Haddon. All rights reserved.
-//  Copyright (c) 2011-2014, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2019, John Haddon. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -35,57 +34,73 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
-#ifndef GAFFERUI_TYPEIDS_H
-#define GAFFERUI_TYPEIDS_H
+#ifndef GAFFERUI_ANNOTATIONSGADGET_H
+#define GAFFERUI_ANNOTATIONSGADGET_H
+
+#include "GafferUI/Gadget.h"
+
+#include <unordered_map>
+
+namespace Gaffer
+{
+
+class Plug;
+class Node;
+
+} // namespace Gaffer
 
 namespace GafferUI
 {
 
-enum TypeId
-{
-	GadgetTypeId = 110251,
-	NodeGadgetTypeId = 110252,
-	GraphGadgetTypeId = 110253,
-	ContainerGadgetTypeId = 110254,
-	AuxiliaryConnectionsGadgetTypeId = 110255,
-	TextGadgetTypeId = 110256,
-	NameGadgetTypeId = 110257,
-	IndividualContainerTypeId = 110258,
-	FrameTypeId = 110259,
-	StyleTypeId = 110260,
-	StandardStyleTypeId = 110261,
-	NoduleTypeId = 110262,
-	LinearContainerTypeId = 110263,
-	ConnectionGadgetTypeId = 110264,
-	StandardNodeGadgetTypeId = 110265,
-	AuxiliaryNodeGadgetTypeId = 110266,
-	StandardNoduleTypeId = 110267,
-	CompoundNoduleTypeId = 110268,
-	ImageGadgetTypeId = 110269,
-	ViewportGadgetTypeId = 110270,
-	ViewTypeId = 110271,
-	ConnectionCreatorTypeId = 110272,
-	CompoundNumericNoduleTypeId = 110273,
-	PlugGadgetTypeId = 110274,
-	GraphLayoutTypeId = 110275,
-	StandardGraphLayoutTypeId = 110276,
-	BackdropNodeGadgetTypeId = 110277,
-	SpacerGadgetTypeId = 110278,
-	StandardConnectionGadgetTypeId = 110279,
-	HandleTypeId = 110280,
-	ToolTypeId = 110281,
-	DotNodeGadgetTypeId = 110282,
-	PlugAdderTypeId = 110283,
-	NoduleLayoutTypeId = 110284,
-	TranslateHandleTypeId = 110285,
-	ScaleHandleTypeId = 110286,
-	RotateHandleTypeId = 110287,
-	AnimationGadgetTypeId = 110288,
-	AnnotationsGadgetTypeId = 110289,
+class GraphGadget;
+class NodeGadget;
 
-	LastTypeId = 110450
+class GAFFERUI_API AnnotationsGadget : public Gadget
+{
+
+	public :
+
+		~AnnotationsGadget() override;
+
+		IE_CORE_DECLARERUNTIMETYPEDEXTENSION( GafferUI::AnnotationsGadget, AnnotationsGadgetTypeId, Gadget );
+
+		bool acceptsParent( const GraphComponent *potentialParent ) const override;
+
+	protected :
+
+		// Protected constructor and friend status so only GraphGadget can
+		// construct us.
+		AnnotationsGadget();
+		friend class GraphGadget;
+
+		void parentChanging( Gaffer::GraphComponent *newParent ) override;
+		void doRenderLayer( Layer layer, const Style *style ) const override;
+
+	private :
+
+		GraphGadget *graphGadget();
+		const GraphGadget *graphGadget() const;
+
+		void graphGadgetChildAdded( GraphComponent *child );
+		void graphGadgetChildRemoved( const GraphComponent *child );
+		void nodeMetadataChanged( IECore::TypeId nodeTypeId, IECore::InternedString key, Gaffer::Node *node );
+
+		struct Annotations
+		{
+			bool dirty = true;
+			bool bookmarked = false;
+			IECore::InternedString numericBookmark;
+			bool renderable = false;
+		};
+
+		boost::signals::scoped_connection m_graphGadgetChildAddedConnection;
+		boost::signals::scoped_connection m_graphGadgetChildRemovedConnection;
+
+		using AnnotationsContainer = std::unordered_map<const NodeGadget *, Annotations>;
+		mutable AnnotationsContainer m_annotations;
+
 };
 
 } // namespace GafferUI
 
-#endif // GAFFERUI_TYPEIDS_H
+#endif // GAFFERUI_ANNOTATIONSGADGET_H
