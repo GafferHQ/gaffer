479bb74b8e9e041d667a41b9c8e910c3adfaf880
Michael DuBelko <michaeldu@image-engine.com>
2019-02-26 14:50:30 -0800

    "Camera" : Add automated image scripts

---
+++ doc/source/WorkingWithScenes/Camera/generate.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# BuildTarget: images/gafferSceneUIRotateTool.png
+
+set -e
+
+cp $GAFFER_ROOT/graphics/gafferSceneUIRotateTool.png images
+cp $GAFFER_ROOT/graphics/gafferSceneUITranslateTool.png images
+cp $GAFFER_ROOT/graphics/cameraOff.png images
+cp $GAFFER_ROOT/graphics/gafferSceneUICameraTool.png images
+cp $GAFFER_ROOT/graphics/toggleOff.png images
+cp $GAFFER_ROOT/graphics/plus.png images
