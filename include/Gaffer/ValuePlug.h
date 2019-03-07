334836efc129fb89b34e850a93ad713604f18b43
John Haddon <thehaddonyoof@gmail.com>
2019-03-07 12:35:13 +0000

    ComputeNode/ValuePlug : Introduce cache policies
    This provides us with a mechanism to fix longstanding weaknesses in the interaction between Gaffer's caching and TBB tasks. To recap, our assumption was that the majority of computes in Gaffer were in themselves entirely single-threaded, not spawning any TBB tasks. We instead gained the majority of our parallelism from outer loops implemented using TBB, for instance by visiting all scene locations in parallel, or computing the tiles of an image in parallel. Furthermore, each of these parallel computes was assumed to be independent, not relying on any shared intermediate upstream values. These assumptions have never been entirely true, and have become increasingly invalid as Gaffer's usage has broadened in scope. What happened when the assumptions were broken? If parallel computes required the _same_ upstream value, then that value would be computed redundantly on each thread, and the first thread to complete would put it in the cache. And if a compute spawned TBB tasks, then we had no protection against TBB's predilection for "stealing" _outer_ tasks, and a thread waiting for an upstream parallel compute to complete might find itself suddenly performing an outer task (location or tile) which then re-requested the _same_ upstream compute it was trying to do in the first place. These two problems combined could lead to huge amounts of redundant work being performed for certain node graphs.
    
    The new cache policies allow us to block threads that are waiting for the same upstream value, instead waiting for a single thread to do the work. And in the case of waiting for an upstream compute spawning TBB tasks, the waiting threads can now perform useful work on behalf of that compute while they wait. This is all built on top of the recent TaskMutex/LRUCache improvements.
    
    Note that the default compute policy is currently `Legacy` rather than `Standard`. This is because the `Standard` policy requires placing the compute behind a mutex, and can lead to deadlock if an upstream task-spawning compute "steals" an outer task. Once all task-spawning computes have declared themselves with an appropriate task-based policy, we can remove the `Legacy` policy and default everything to `Standard`.

--- include/Gaffer/ValuePlug.h
+++ include/Gaffer/ValuePlug.h
@@ -100,6 +100,34 @@ class GAFFER_API ValuePlug : public Plug
 		/// Convenience function to append the hash to h.
 		void hash( IECore::MurmurHash &h ) const;
 
+		/// Specifies the methodology used to cache the value
+		/// and hash for output plugs.
+		enum class CachePolicy
+		{
+			/// No caching is performed. Suitable for
+			/// extremely quick processes. Also useful
+			/// to avoid double-counting of cache memory when
+			/// a compute always returns a sub-object of another
+			/// cache entry.
+			Uncached,
+			/// Suitable for regular processes that don't spawn
+			/// TBB tasks. It is essential that any task-spawning
+			/// processes use one of the dedicated policies below.
+			Standard,
+			/// Suitable for processes that spawn TBB tasks.
+			/// Threads waiting for the same result will collaborate
+			/// to perform tasks together until the work is complete.
+			TaskCollaboration,
+			/// Suitable for processes that spawn TBB tasks. Threads
+			/// waiting for an in-progress compute will block until
+			/// it is complete. In theory this is inferior to TaskCollaboration,
+			/// but due to TBB overhead it may be preferable for small
+			/// but frequent computes.
+			TaskIsolation,
+			/// Legacy policy, to be removed.
+			Legacy
+		};
+
 		/// @name Cache management
 		/// ValuePlug optimises repeated computation by storing a cache of
 		/// recently computed values. These functions allow for management
