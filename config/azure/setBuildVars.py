427834735fa551636495a8a0624ef00eb2b9e273
John Haddon <thehaddonyoof@gmail.com>
2019-06-01 19:51:00 +0100

    Azure : Publish build archive and link to source commit
    The scons packaged build is uploaded to the gafferhq/builds blob store
    and a commit status message is added to the source commit linking to
    the download. This allows easy access to a build from PRs/etc...
    
    Presently, we only publish release builds.

---
+++ config/azure/setBuildVars.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+##########################################################################
+#
+#  Copyright (c) 2019, Cinesite VFX Ltd. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#      * Redistributions of source code must retain the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided with
+#        the distribution.
+#
+#      * Neither the name of John Haddon nor the names of
+#        any other contributors to this software may be used to endorse or
+#        promote products derived from this software without specific prior
+#        written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##########################################################################
+
+import datetime
+import os
+import sys
+
+# Azure pipeline variables can be populated at run-time by echoing special
+# strings to a process output. The following allows vars to be set:
+#
+#    ##vso[task.setvariable variable=<name>;]<value>
+#
+# NOTE: The variable must first be defined in the Pipeline settings and have
+# the 'Settable at queue time' checkbox checked.
+#
+# We make use of this mechanism to allow custom logic to define the build name
+# as well as determine the correct commit hash depending on the nature of the
+# trigger. These variables can be referenced in a pipeline yaml file downstream
+# of the step that runs this script.
+
+def getCommitHash() :
+	commit = os.environ.get( 'BUILD_SOURCEVERSION' )
+	# Azure merges the branch into its target in PR build, so
+	# BUILD_SOURCEVERSION isn't correct as it references the ephemeral merge.
+	if os.environ.get( 'BUILD_REASON', '' ) == 'PullRequest' :
+		commit = os.environ.get( 'SYSTEM_PULLREQUEST_SOURCECOMMITID' )
+	return commit
+
+formatVars = {
+	"buildType" : os.environ.get( "BUILD_TYPE", "UNKNOWN" ).title(),
+	"platform" : "MacOS" if sys.platform == "darwin" else "Linux",
+	"timestamp" : datetime.datetime.now().strftime( "%Y%m%d%H%M" ),
+	"pullRequest" : os.environ.get( "SYSTEM_PULLREQUEST_PULLREQUESTNUMBER", "UNKNOWN" ),
+	"commit" : getCommitHash()
+}
+
+nameFormats = {
+	"default" : "gaffer-{timestamp}-{commit}-{platform}-{buildType}",
+	"PullRequest" : "gaffer-PR{pullRequest}-{commit}-{platform}-{buildType}"
+}
+
+trigger = os.environ.get( 'BUILD_REASON', 'Manual' )
+buildName = nameFormats.get( trigger, nameFormats['default'] ).format( **formatVars )
+
+print( "Setting $(buildName) to %s" % buildName )
+print( "##vso[task.setvariable variable=buildName;]%s" % buildName )
+
+# To make sure our publish always matches the one we use in the build name
+print( "Setting $(buildSourceCommit) to %s" % formatVars['commit'] )
+print( "##vso[task.setvariable variable=buildSourceCommit;]%s" % formatVars['commit'] )
+
