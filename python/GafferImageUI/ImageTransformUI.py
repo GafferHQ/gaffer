30dd9fcc6ccc58e30e3937bd178793bb1788bdc2
John Haddon <thehaddonyoof@gmail.com>
2020-01-30 17:22:28 +0000

    ImageTransformUI : Update for concatentation

--- python/GafferImageUI/ImageTransformUI.py
+++ python/GafferImageUI/ImageTransformUI.py
@@ -92,7 +92,25 @@ Gaffer.Metadata.registerNode(
 			"""
 			Apply the inverse transformation to the image.
 			"""
-		]
+		],
+
+		"concatenate" : [
+
+			"description",
+
+			"""
+			Combines the processing for a series of ImageTransforms so that
+			transformation and filtering is only applied once. This gives better
+			image quality and performance.
+
+			> Note : When concatenation is in effect, the filter settings on upstream
+			> ImageTransforms are ignored.
+			""",
+
+			"layout:section", "Node",
+			"layout:index", -1,
+
+		],
 
 	}
 
