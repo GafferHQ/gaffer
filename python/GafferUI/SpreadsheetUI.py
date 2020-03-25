GafferHQ/gaffer

commit 3a8ba8b83c3a1d1ad24405ba8a5448785cb789e7

SpreadsheetUI : Reapply column widths when columns are deleted
Fixes #3630. Fixes #3631.

Fixes
-----

- Spreadsheet : Fixed bug which caused incorrect column widths after deleting a column (#3630, #3631).

python/GafferUI/SpreadsheetUI.py
@@ -510,12 +510,7 @@ class _PlugTableView( GafferUI.Widget ) :
 
 		if mode in ( self.Mode.Cells, self.Mode.Defaults ) :
 
-			for index, plug in enumerate( plug["default"]["cells"] ) :
-				width = Gaffer.Metadata.value( plug, "spreadsheet:columnWidth" )
-				if width is not None :
-					tableView.horizontalHeader().resizeSection( index, width )
-				visualIndex = Gaffer.Metadata.value( plug, "spreadsheet:columnIndex" )
-
+			self.__applyColumnWidthMetadata()
 			self.__applySectionOrderMetadata()
 
 			tableView.horizontalHeader().setSectionsMovable( True )
@@ -642,6 +637,31 @@ class _PlugTableView( GafferUI.Widget ) :
 			with Gaffer.BlockedConnection( self.__plugMetadataChangedConnection ) :
 				Gaffer.Metadata.registerValue( plug, "spreadsheet:columnWidth", newSize )
 
+	def __applyColumnWidthMetadata( self, cellPlug = None ) :
+
+		defaultCells = self._qtWidget().model().rowsPlug()["default"]["cells"]
+
+		if cellPlug is not None :
+			indicesAndPlugs = [ ( defaultCells.children().index( cellPlug ), cellPlug ) ]
+		else :
+			indicesAndPlugs = enumerate( defaultCells )
+
+		try :
+
+			self.__ignoreSectionResized = True
+
+			for index, plug in indicesAndPlugs :
+
+				width = Gaffer.Metadata.value( plug, "spreadsheet:columnWidth" )
+				if width is None :
+					width = self._qtWidget().horizontalHeader().defaultSectionSize()
+
+				self._qtWidget().horizontalHeader().resizeSection( index, width )
+
+		finally :
+
+			self.__ignoreSectionResized = False
+
 	def __applySectionOrderMetadata( self ) :
 
 		rowsPlug = self._qtWidget().model().rowsPlug()
@@ -688,13 +708,8 @@ class _PlugTableView( GafferUI.Widget ) :
 
 		if key == "spreadsheet:columnWidth" :
 
-			column = rowsPlug["default"]["cells"].children().index( plug )
-			self.__ignoreSectionResized = True
-			width = Gaffer.Metadata.value( plug, "spreadsheet:columnWidth" )
-			if width is None :
-				width = self._qtWidget().horizontalHeader().defaultSectionSize()
-			self._qtWidget().horizontalHeader().resizeSection( column, width )
-			self.__ignoreSectionResized = False
+			if plug.parent() == rowsPlug["default"]["cells"] :
+				self.__applyColumnWidthMetadata( cellPlug = plug )
 
 		elif key == "spreadsheet:columnIndex" :
 
@@ -817,6 +832,7 @@ class _PlugTableView( GafferUI.Widget ) :
 	def __modelReset( self ) :
 
 		self.__applyColumnVisibility()
+		self.__applyColumnWidthMetadata()
 
 	def __moveToSection( self, cellPlug, sectionName = None ) :
 

commit ea242ed5b3d810bb5fde8b12c85f56343e5b1628

SpreadsheetUI : Fix row name sizing bug
This was introduced by 3a8ba8b83c3a1d1ad24405ba8a5448785cb789e7, with the root cause being an attempt to apply the column widths for the main columns to the two columns in the row name section. Added similar safeguards to the application of section order and column visibility.

python/GafferUI/SpreadsheetUI.py
@@ -639,6 +639,9 @@ class _PlugTableView( GafferUI.Widget ) :
 
 	def __applyColumnWidthMetadata( self, cellPlug = None ) :
 
+		if self._qtWidget().model().mode() == self.Mode.RowNames :
+			return
+
 		defaultCells = self._qtWidget().model().rowsPlug().defaultRow()["cells"]
 
 		if cellPlug is not None :
@@ -664,6 +667,9 @@ class _PlugTableView( GafferUI.Widget ) :
 
 	def __applySectionOrderMetadata( self ) :
 
+		if self._qtWidget().model().mode() == self.Mode.RowNames :
+			return
+
 		rowsPlug = self._qtWidget().model().rowsPlug()
 		header = self._qtWidget().horizontalHeader()
 		for index, plug in enumerate( rowsPlug.defaultRow()["cells"] ) :
@@ -673,6 +679,9 @@ class _PlugTableView( GafferUI.Widget ) :
 			self.__callingMoveSection = False
 
 	def __applyColumnVisibility( self ) :
+
+		if self._qtWidget().model().mode() == self.Mode.RowNames :
+			return
 
 		# Changing column visibility seems to cause the
 		# `sectionResized()` signal to be emitted unnecessarily,
@@ -871,6 +880,10 @@ class _PlugTableModel( QtCore.QAbstractTableModel ) :
 
 	# Methods of our own
 	# ------------------
+
+	def mode( self ) :
+
+		return self.__mode
 
 	def rowsPlug( self ) :
 