5592940912d05057b91662b0a6baf813a4d0dbaf
John Haddon <thehaddonyoof@gmail.com>
2020-04-01 17:48:42 +0100

    CoreBinding : Avoid `PyString_Check()`

--- src/GafferSceneModule/CoreBinding.cpp
+++ src/GafferSceneModule/CoreBinding.cpp
@@ -105,7 +105,8 @@ struct ScenePathFromString
 
 	static void *convertible( PyObject *obj )
 	{
-		if( PyString_Check( obj ) )
+		extract<std::string> e( obj );
+		if( e.check() )
 		{
 			return obj;
 		}
