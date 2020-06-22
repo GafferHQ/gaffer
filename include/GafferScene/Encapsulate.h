GafferHQ/gaffer

commit c113e2f9f9f8137e49c3d3c492aaff2581b9160e

FilterPlug : Add `sceneAffectsMatch()` method
This is more convenient than the `Filter::sceneAffectsMatch()` method it replaces, and better fits our general design whereby plugs provide the public interface to the internal capabilities of nodes.

src/GafferScene/Encapsulate.cpp
@@ -104,25 +104,22 @@ bool Encapsulate::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *in
 
 	if( plug == filterPlug() )
 	{
-		if( const Filter *filter = runTimeCast<const Filter>( inputPlug->source()->node() ) )
+		if(
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
+		)
 		{
-			if(
-				filter->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-			)
-			{
-				// We make a single call to filterHash() in hashSet(), to account for
-				// the fact that the filter is used in remapping sets. This wouldn't
-				// work for filter types which actually vary based on data within the
-				// scene hierarchy, because then multiple calls would be necessary.
-				// We could make more calls here, but that would be expensive.
-				/// \todo In an ideal world we'd be able to compute a hash for the
-				/// filter across a whole hierarchy.
-				return false;
-			}
+			// We make a single call to filterHash() in hashSet(), to account for
+			// the fact that the filter is used in remapping sets. This wouldn't
+			// work for filter types which actually vary based on data within the
+			// scene hierarchy, because then multiple calls would be necessary.
+			// We could make more calls here, but that would be expensive.
+			/// \todo In an ideal world we'd be able to compute a hash for the
+			/// filter across a whole hierarchy.
+			return false;
 		}
 	}
 

src/GafferScene/Isolate.cpp
@@ -199,25 +199,22 @@ bool Isolate::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputP
 
 	if( plug == filterPlug() )
 	{
-		if( const Filter *filter = runTimeCast<const Filter>( inputPlug->source()->node() ) )
+		if(
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
+		)
 		{
-			if(
-				filter->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-			)
-			{
-				// We make a single call to filterHash() in hashSet(), to account for
-				// the fact that the filter is used in remapping sets. This wouldn't
-				// work for filter types which actually vary based on data within the
-				// scene hierarchy, because then multiple calls would be necessary.
-				// We could make more calls here, but that would be expensive.
-				/// \todo In an ideal world we'd be able to compute a hash for the
-				/// filter across a whole hierarchy.
-				return false;
-			}
+			// We make a single call to filterHash() in hashSet(), to account for
+			// the fact that the filter is used in remapping sets. This wouldn't
+			// work for filter types which actually vary based on data within the
+			// scene hierarchy, because then multiple calls would be necessary.
+			// We could make more calls here, but that would be expensive.
+			/// \todo In an ideal world we'd be able to compute a hash for the
+			/// filter across a whole hierarchy.
+			return false;
 		}
 	}
 

src/GafferScene/LightToCamera.cpp
@@ -295,25 +295,22 @@ bool LightToCamera::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *
 
 	if( plug == filterPlug() )
 	{
-		if( const Filter *filter = runTimeCast<const Filter>( inputPlug->source()->node() ) )
+		if(
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
+		)
 		{
-			if(
-				filter->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-			)
-			{
-				// We make a single call to filterHash() in hashSet(), to account for
-				// the fact that the filter is used in remapping sets. This wouldn't
-				// work for filter types which actually vary based on data within the
-				// scene hierarchy, because then multiple calls would be necessary.
-				// We could make more calls here, but that would be expensive.
-				/// \todo In an ideal world we'd be able to compute a hash for the
-				/// filter across a whole hierarchy.
-				return false;
-			}
+			// We make a single call to filterHash() in hashSet(), to account for
+			// the fact that the filter is used in remapping sets. This wouldn't
+			// work for filter types which actually vary based on data within the
+			// scene hierarchy, because then multiple calls would be necessary.
+			// We could make more calls here, but that would be expensive.
+			/// \todo In an ideal world we'd be able to compute a hash for the
+			/// filter across a whole hierarchy.
+			return false;
 		}
 	}
 

src/GafferScene/Prune.cpp
@@ -104,25 +104,22 @@ bool Prune::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlu
 
 	if( plug == filterPlug() )
 	{
-		if( const Filter *filter = runTimeCast<const Filter>( inputPlug->source()->node() ) )
+		if(
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
+			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
+		)
 		{
-			if(
-				filter->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-				filter->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-			)
-			{
-				// We make a single call to filterHash() in hashSet(), to account for
-				// the fact that the filter is used in remapping sets. This wouldn't
-				// work for filter types which actually vary based on data within the
-				// scene hierarchy, because then multiple calls would be necessary.
-				// We could make more calls here, but that would be expensive.
-				/// \todo In an ideal world we'd be able to compute a hash for the
-				/// filter across a whole hierarchy.
-				return false;
-			}
+			// We make a single call to filterHash() in hashSet(), to account for
+			// the fact that the filter is used in remapping sets. This wouldn't
+			// work for filter types which actually vary based on data within the
+			// scene hierarchy, because then multiple calls would be necessary.
+			// We could make more calls here, but that would be expensive.
+			/// \todo In an ideal world we'd be able to compute a hash for the
+			/// filter across a whole hierarchy.
+			return false;
 		}
 	}
 

include/GafferScene/Filter.h
@@ -70,6 +70,7 @@ class GAFFERSCENE_API Filter : public Gaffer::ComputeNode
 		const FilterPlug *outPlug() const;
 
 		void affects( const Gaffer::Plug *input, AffectedPlugsContainer &outputs ) const override;
+		/// Use `FilterPlug::sceneAffectsMatch()` instead.
 		virtual bool sceneAffectsMatch( const ScenePlug *scene, const Gaffer::ValuePlug *child ) const;
 
 		/// \deprecated Use FilterPlug::SceneScope instead.

include/GafferScene/FilterPlug.h
@@ -81,6 +81,8 @@ class GAFFERSCENE_API FilterPlug : public Gaffer::IntPlug
 		bool acceptsInput( const Gaffer::Plug *input ) const override;
 		Gaffer::PlugPtr createCounterpart( const std::string &name, Direction direction ) const override;
 
+		bool sceneAffectsMatch( const ScenePlug *scene, const Gaffer::ValuePlug *child ) const;
+
 		/// Name of a context variable used to provide the input
 		/// scene to the filter
 		static const IECore::InternedString inputSceneContextName;

src/GafferScene/FilterPlug.cpp
@@ -126,6 +126,22 @@ Gaffer::PlugPtr FilterPlug::createCounterpart( const std::string &name, Directio
 	return new FilterPlug( name, direction, defaultValue(), minValue(), maxValue(), getFlags() );
 }
 
+bool FilterPlug::sceneAffectsMatch( const ScenePlug *scene, const Gaffer::ValuePlug *child ) const
+{
+	const Plug *source = this->source();
+	if( source == this )
+	{
+		// No input
+		return false;
+	}
+
+	if( const Filter *filter = runTimeCast<const Filter>( source->node() ) )
+	{
+		return filter->sceneAffectsMatch( scene, child );
+	}
+	return false;
+}
+
 FilterPlug::SceneScope::SceneScope( const Gaffer::Context *context, const ScenePlug *scenePlug )
 	:	EditableScope( context )
 {

src/GafferScene/FilterProcessor.cpp
@@ -104,10 +104,9 @@ const Gaffer::ArrayPlug *FilterProcessor::inPlugs() const
 
 bool FilterProcessor::sceneAffectsMatch( const ScenePlug *scene, const Gaffer::ValuePlug *child ) const
 {
-	for( InputIntPlugIterator it( inPlugs() ); !it.done(); ++it )
+	for( InputFilterPlugIterator it( inPlugs() ); !it.done(); ++it )
 	{
-		const Filter *filter = IECore::runTimeCast<const Filter>( (*it)->source()->node() );
-		if( filter && filter != this && filter->sceneAffectsMatch( scene, child ) )
+		if( (*it)->sceneAffectsMatch( scene, child ) )
 		{
 			return true;
 		}

commit 079e48420dd1e95513806d62d74a1b0d3d9544c1

GafferScene : Remove broken `acceptsInput()` checks for filters
These were broken in c113e2f9f9f8137e49c3d3c492aaff2581b9160e, which inadvertently moved the `sceneAffectsMatch()` call from the candidate input filter to `this->filterPlug()`. We could fix this, but :

- The bug has been in the wild since 0.51.0.0, and fixing it could prevent scripts from loading.
- The bug is irrelevant for all native Gaffer filters.
- I've just reviewed all the proprietary Image Engine filters where this is relevant, and they perform their own checks (at the compute stage rather than the input acceptance stage).
- I have a rough plan for lifting this restriction via an overhaul of the filter API : https://github.com/GafferHQ/gaffer/issues/3804.

include/GafferScene/Encapsulate.h
@@ -56,8 +56,6 @@ class GAFFERSCENE_API Encapsulate : public FilteredSceneProcessor
 
 	protected :
 
-		bool acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const override;
-
 		void hashObject( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;
 		IECore::ConstObjectPtr computeObject( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent ) const override;
 

include/GafferScene/Isolate.h
@@ -75,8 +75,6 @@ class GAFFERSCENE_API Isolate : public FilteredSceneProcessor
 
 	protected :
 
-		bool acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const override;
-
 		void hashBound( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;
 		void hashChildNames( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;
 		void hashSet( const IECore::InternedString &setName, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;

include/GafferScene/LightToCamera.h
@@ -56,7 +56,6 @@ class GAFFERSCENE_API LightToCamera : public SceneElementProcessor
 		const Gaffer::IntPlug *filmFitPlug() const;
 
 		void affects( const Gaffer::Plug *input, AffectedPlugsContainer &outputs ) const override;
-		bool acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const override;
 
 	protected :
 

include/GafferScene/Prune.h
@@ -59,8 +59,6 @@ class GAFFERSCENE_API Prune : public FilteredSceneProcessor
 
 	protected :
 
-		bool acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const override;
-
 		void hashBound( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;
 		void hashChildNames( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;
 		void hashSet( const IECore::InternedString &setName, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const override;

src/GafferScene/Encapsulate.cpp
@@ -95,37 +95,6 @@ void Encapsulate::affects( const Plug *input, AffectedPlugsContainer &outputs ) 
 	}
 }
 
-bool Encapsulate::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const
-{
-	if( !FilteredSceneProcessor::acceptsInput( plug, inputPlug ) )
-	{
-		return false;
-	}
-
-	if( plug == filterPlug() )
-	{
-		if(
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-		)
-		{
-			// We make a single call to filterHash() in hashSet(), to account for
-			// the fact that the filter is used in remapping sets. This wouldn't
-			// work for filter types which actually vary based on data within the
-			// scene hierarchy, because then multiple calls would be necessary.
-			// We could make more calls here, but that would be expensive.
-			/// \todo In an ideal world we'd be able to compute a hash for the
-			/// filter across a whole hierarchy.
-			return false;
-		}
-	}
-
-	return true;
-}
-
 void Encapsulate::hashObject( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const
 {
 	if( filterValue( context ) & IECore::PathMatcher::ExactMatch )

src/GafferScene/Isolate.cpp
@@ -190,37 +190,6 @@ void Isolate::affects( const Gaffer::Plug *input, AffectedPlugsContainer &output
 	}
 }
 
-bool Isolate::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const
-{
-	if( !FilteredSceneProcessor::acceptsInput( plug, inputPlug ) )
-	{
-		return false;
-	}
-
-	if( plug == filterPlug() )
-	{
-		if(
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-		)
-		{
-			// We make a single call to filterHash() in hashSet(), to account for
-			// the fact that the filter is used in remapping sets. This wouldn't
-			// work for filter types which actually vary based on data within the
-			// scene hierarchy, because then multiple calls would be necessary.
-			// We could make more calls here, but that would be expensive.
-			/// \todo In an ideal world we'd be able to compute a hash for the
-			/// filter across a whole hierarchy.
-			return false;
-		}
-	}
-
-	return true;
-}
-
 void Isolate::hashBound( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const
 {
 	const SetsToKeep setsToKeep( this );

src/GafferScene/LightToCamera.cpp
@@ -294,38 +294,6 @@ void LightToCamera::affects( const Gaffer::Plug *input, AffectedPlugsContainer &
 	}
 }
 
-bool LightToCamera::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const
-{
-	if( !SceneElementProcessor::acceptsInput( plug, inputPlug ) )
-	{
-		return false;
-	}
-
-	if( plug == filterPlug() )
-	{
-		if(
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-		)
-		{
-			// We make a single call to filterHash() in hashSet(), to account for
-			// the fact that the filter is used in remapping sets. This wouldn't
-			// work for filter types which actually vary based on data within the
-			// scene hierarchy, because then multiple calls would be necessary.
-			// We could make more calls here, but that would be expensive.
-			/// \todo In an ideal world we'd be able to compute a hash for the
-			/// filter across a whole hierarchy.
-			return false;
-		}
-	}
-
-	return true;
-}
-
-
 bool LightToCamera::processesObject() const
 {
 	return true;

src/GafferScene/Prune.cpp
@@ -95,37 +95,6 @@ void Prune::affects( const Gaffer::Plug *input, AffectedPlugsContainer &outputs 
 	}
 }
 
-bool Prune::acceptsInput( const Gaffer::Plug *plug, const Gaffer::Plug *inputPlug ) const
-{
-	if( !FilteredSceneProcessor::acceptsInput( plug, inputPlug ) )
-	{
-		return false;
-	}
-
-	if( plug == filterPlug() )
-	{
-		if(
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->boundPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->transformPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->attributesPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->objectPlug() ) ||
-			filterPlug()->sceneAffectsMatch( inPlug(), inPlug()->childNamesPlug() )
-		)
-		{
-			// We make a single call to filterHash() in hashSet(), to account for
-			// the fact that the filter is used in remapping sets. This wouldn't
-			// work for filter types which actually vary based on data within the
-			// scene hierarchy, because then multiple calls would be necessary.
-			// We could make more calls here, but that would be expensive.
-			/// \todo In an ideal world we'd be able to compute a hash for the
-			/// filter across a whole hierarchy.
-			return false;
-		}
-	}
-
-	return true;
-}
-
 void Prune::hashBound( const ScenePath &path, const Gaffer::Context *context, const ScenePlug *parent, IECore::MurmurHash &h ) const
 {
 	if( adjustBoundsPlug()->getValue() )