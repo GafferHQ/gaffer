GafferHQ/gaffer

commit 8be634295180ed353eda81a4eb9c915211981c78

TransformTool : Fix `sceneToTransformSpace()` for AimConstraints
Our approach was to find a matrix that aligned the full transforms for `path()` and `upstreamPath()`, and this worked in the vast majority of cases. But in the case of an intervening AimConstraint, the local transforms at the end of each path were different (`path()` has aim applied, `upstreamPath()` doesn't), and this caused TranslateTool to translate in wild directions. The fix is to align the full transforms of the _parent_ paths instead.

This code is at the heart of lots of fairly subtle juggling of transforms in the TransformTools, so I am a little wary to be messing with it. But two things reassure me :

- We have a reasonable amount of test coverage, having added tests for many similar problems we've fixed in the past.
- This should only cause a change of behaviour when `upstreamScene()` is _not_ the final author of the local transform, and that can only happen when there's an intervening node - such as an AimConstraint - that `initFromSceneNode()` isn't savvy to. This should not change behaviour for the standard case, because the standard case is that `upstreamScene()` had the final say in the local transform.

src/GafferSceneUI/TransformTool.cpp
@@ -547,16 +547,33 @@ Imath::M44f TransformTool::Selection::sceneToTransformSpace() const
 {
 	throwIfNotEditable();
 
+	// The scenes produced by `scene()` and `upstreamScene()` may
+	// be completely different :
+	//
+	// - The hierarchies may be different, often due to something like
+	//   a Group node inserted between the two.
+	// - The parent transforms of `path()` and `upstreamPath()` may
+	//   be different, either due to a Group or an intermediate edit
+	//   to the parent location.
+	//
+	// To account for this we align the two scenes by finding the
+	// difference between the parent transforms of `path()` and
+	// `upstreamPath()`.
+
 	M44f downstreamMatrix;
+	if( path().size() )
 	{
+		ScenePlug::ScenePath parentPath = path(); parentPath.pop_back();
 		Context::Scope scopedContext( context() );
-		downstreamMatrix = scene()->fullTransform( path() );
+		downstreamMatrix = scene()->fullTransform( parentPath );
 	}
 
 	M44f upstreamMatrix;
+	if( upstreamPath().size() )
 	{
+		ScenePlug::ScenePath upstreamParentPath = upstreamPath(); upstreamParentPath.pop_back();
 		Context::Scope scopedContext( upstreamContext() );
-		upstreamMatrix = upstreamScene()->fullTransform( upstreamPath() );
+		upstreamMatrix = upstreamScene()->fullTransform( upstreamParentPath );
 	}
 
 	return downstreamMatrix.inverse() * upstreamMatrix * transformSpace().inverse();

python/GafferSceneUITest/TranslateToolTest.py
@@ -1045,5 +1045,55 @@ class TranslateToolTest( GafferUITest.TestCase ) :
 			{ "Location does not exist" }
 		)
 
+	def testInteractionWithAimConstraints( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		# Cube at ( 0, 10, 0 ), aimed at a sphere at the origin.
+
+		script["sphere"] = GafferScene.Sphere()
+
+		script["cube"] = GafferScene.Cube()
+		script["cube"]["transform"]["translate"]["y"].setValue( 10 )
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["parent"].setValue( "/" )
+		script["parent"]["in"].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][0].setInput( script["cube"]["out"] )
+
+		script["cubeFilter"] = GafferScene.PathFilter()
+		script["cubeFilter"]["paths"].setValue( IECore.StringVectorData( [ "/cube" ] ) )
+
+		script["aim"] = GafferScene.AimConstraint()
+		script["aim"]["in"].setInput( script["parent"]["out"] )
+		script["aim"]["filter"].setInput( script["cubeFilter"]["out"] )
+		script["aim"]["target"].setValue( "/sphere" )
+
+		# Translate in Z (parent space) and check that we moved to
+		# where we expected.
+
+		self.assertEqual(
+			script["aim"]["out"].transform( "/cube" ).translation(),
+			imath.V3f( 0, 10, 0 )
+		)
+
+		view = GafferSceneUI.SceneView()
+		view["in"].setInput( script["aim"]["out"] )
+		GafferSceneUI.ContextAlgo.setSelectedPaths( view.getContext(), IECore.PathMatcher( [ "/cube" ] ) )
+
+		tool = GafferSceneUI.TranslateTool( view )
+		tool["active"].setValue( True )
+
+		self.assertEqual( len( tool.selection() ), 1 )
+		self.assertEqual( tool.selection()[0].path(), "/cube" )
+		self.assertEqual( tool.selection()[0].upstreamPath(), "/cube" )
+		self.assertEqual( tool.selection()[0].editTarget(), script["cube"]["transform"] )
+
+		tool.translate( imath.V3f( 0, 0, 10 ) )
+		self.assertEqual(
+			script["aim"]["out"].transform( "/cube" ).translation(),
+			imath.V3f( 0, 10, 10 )
+		)
+
 if __name__ == "__main__":
 	unittest.main()

commit cdb965dcf09209e8a9c4aa35c844b0898682f968

TransformTool : Fix pivot position when constraints are present
This was broken by 8be634295180ed353eda81a4eb9c915211981c78. In that commit we stopped considering the local transform in `sceneToTransformSpace()`, but when positioning the handles we want to take the local matrix into account, not matter how it was generated.

include/GafferSceneUI/TransformTool.h
@@ -172,6 +172,7 @@ class GAFFERSCENEUI_API TransformTool : public GafferSceneUI::SelectionTool
 				void initFromSceneNode( const GafferScene::SceneAlgo::History *history );
 				void initWalk( const GafferScene::SceneAlgo::History *history, bool &editScopeFound );
 				void throwIfNotEditable() const;
+				Imath::M44f transformToLocalSpace() const;
 
 				GafferScene::ConstScenePlugPtr m_scene;
 				GafferScene::ScenePlug::ScenePath m_path;

src/GafferSceneUI/TransformTool.cpp
@@ -609,6 +609,25 @@ Imath::M44f TransformTool::Selection::sceneToTransformSpace() const
 	return downstreamMatrix.inverse() * upstreamMatrix * transformSpace().inverse();
 }
 
+Imath::M44f TransformTool::Selection::transformToLocalSpace() const
+{
+	throwIfNotEditable();
+
+	M44f downstreamMatrix;
+	{
+		Context::Scope scopedContext( context() );
+		downstreamMatrix = scene()->fullTransform( path() );
+	}
+
+	M44f upstreamMatrix;
+	{
+		Context::Scope scopedContext( upstreamContext() );
+		upstreamMatrix = upstreamScene()->fullTransform( upstreamPath() );
+	}
+
+	return transformSpace() * upstreamMatrix.inverse() * downstreamMatrix;
+}
+
 Imath::M44f TransformTool::Selection::orientedTransform( Orientation orientation ) const
 {
 	throwIfNotEditable();
@@ -645,7 +664,7 @@ Imath::M44f TransformTool::Selection::orientedTransform( Orientation orientation
 	transform( translate, rotate, scale, pivot );
 
 	const V3f transformSpacePivot = pivot + translate;
-	const V3f downstreamWorldPivot = transformSpacePivot * sceneToTransformSpace().inverse();
+	const V3f downstreamWorldPivot = transformSpacePivot * transformToLocalSpace();
 
 	result[3][0] = downstreamWorldPivot[0];
 	result[3][1] = downstreamWorldPivot[1];

python/GafferSceneUITest/RotateToolTest.py
@@ -332,5 +332,76 @@ class RotateToolTest( GafferUITest.TestCase ) :
 			imath.M44f().translate( imath.V3f( 1, 0, 0 ) ).rotate( imath.V3f( 0, math.pi / 2, 0 ) ),
 		)
 
+	def testInteractionWithPointConstraint( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		script["sphere"] = GafferScene.Sphere()
+
+		script["cube"] = GafferScene.Cube()
+		script["cube"]["transform"]["translate"].setValue( imath.V3f( 5, 5, 0 ) )
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["in"].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][0].setInput( script["cube"]["out"] )
+		script["parent"]["parent"].setValue( "/" )
+
+		script["sphereFilter"] = GafferScene.PathFilter()
+		script["sphereFilter"]["paths"].setValue( IECore.StringVectorData( [ "/sphere" ] ) )
+
+		script["constraint"] = GafferScene.PointConstraint()
+		script["constraint"]["in"].setInput( script["parent"]["out"] )
+		script["constraint"]["filter"].setInput( script["sphereFilter"]["out"] )
+		script["constraint"]["target"].setValue( "/cube" )
+
+		view = GafferSceneUI.SceneView()
+		view["in"].setInput( script["constraint"]["out"] )
+
+		GafferSceneUI.ContextAlgo.setSelectedPaths( view.getContext(), IECore.PathMatcher( [ "/sphere" ] ) )
+
+		tool = GafferSceneUI.RotateTool( view )
+		tool["active"].setValue( True )
+
+		for orientation in ( tool.Orientation.Local, tool.Orientation.Parent, tool.Orientation.World ) :
+			tool["orientation"].setValue( orientation )
+			self.assertEqual( tool.handlesTransform(), imath.M44f().translate( script["cube"]["transform"]["translate"].getValue() ) )
+
+	def testInteractionWithParentConstraint( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		script["sphere"] = GafferScene.Sphere()
+
+		script["cube"] = GafferScene.Cube()
+		script["cube"]["transform"]["translate"].setValue( imath.V3f( 5, 5, 0 ) )
+		script["cube"]["transform"]["rotate"]["x"].setValue( 90 )
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["in"].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][0].setInput( script["cube"]["out"] )
+		script["parent"]["parent"].setValue( "/" )
+
+		script["sphereFilter"] = GafferScene.PathFilter()
+		script["sphereFilter"]["paths"].setValue( IECore.StringVectorData( [ "/sphere" ] ) )
+
+		script["constraint"] = GafferScene.ParentConstraint()
+		script["constraint"]["in"].setInput( script["parent"]["out"] )
+		script["constraint"]["filter"].setInput( script["sphereFilter"]["out"] )
+		script["constraint"]["target"].setValue( "/cube" )
+
+		view = GafferSceneUI.SceneView()
+		view["in"].setInput( script["constraint"]["out"] )
+
+		GafferSceneUI.ContextAlgo.setSelectedPaths( view.getContext(), IECore.PathMatcher( [ "/sphere" ] ) )
+
+		tool = GafferSceneUI.RotateTool( view )
+		tool["active"].setValue( True )
+
+		tool["orientation"].setValue( tool.Orientation.Parent )
+		self.assertEqual( tool.handlesTransform(), imath.M44f().translate( script["cube"]["transform"]["translate"].getValue() ) )
+
+		tool["orientation"].setValue( tool.Orientation.Local )
+		self.assertEqual( tool.handlesTransform(), script["constraint"]["out"].transform( "/sphere" ) )
+
 if __name__ == "__main__":
 	unittest.main()

python/GafferSceneUITest/ScaleToolTest.py
@@ -142,5 +142,38 @@ class ScaleToolTest( GafferUITest.TestCase ) :
 		self.assertTrue( GafferScene.EditScopeAlgo.hasTransformEdit( script["editScope"], "/sphere" ) )
 		self.assertEqual( script["editScope"]["out"].transform( "/sphere" ), imath.M44f().translate( imath.V3f( 1, 0, 0 ) ).scale( imath.V3f( 2, 2, 2 ) ) )
 
+	def testHandleOriginsRespectPointConstraint( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		script["sphere"] = GafferScene.Sphere()
+
+		cubeTranslate = imath.V3f( 5, 5, 0 )
+		script["cube"] = GafferScene.Cube()
+		script["cube"]["transform"]["translate"].setValue( cubeTranslate )
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["in"].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][0].setInput( script["cube"]["out"] )
+		script["parent"]["parent"].setValue( "/" )
+
+		script["sphereFilter"] = GafferScene.PathFilter()
+		script["sphereFilter"]["paths"].setValue( IECore.StringVectorData( [ "/sphere" ] ) )
+
+		script["constraint"] = GafferScene.PointConstraint()
+		script["constraint"]["in"].setInput( script["parent"]["out"] )
+		script["constraint"]["filter"].setInput( script["sphereFilter"]["out"] )
+		script["constraint"]["target"].setValue( "/cube" )
+
+		view = GafferSceneUI.SceneView()
+		view["in"].setInput( script["constraint"]["out"] )
+
+		GafferSceneUI.ContextAlgo.setSelectedPaths( view.getContext(), IECore.PathMatcher( [ "/sphere" ] ) )
+
+		tool = GafferSceneUI.ScaleTool( view )
+		tool["active"].setValue( True )
+
+		self.assertEqual( tool.handlesTransform(), imath.M44f().translate( cubeTranslate ) )
+
 if __name__ == "__main__":
 	unittest.main()

python/GafferSceneUITest/TranslateToolTest.py
@@ -1156,5 +1156,58 @@ class TranslateToolTest( GafferUITest.TestCase ) :
 			imath.V3f( 0, 10, 10 )
 		)
 
+	def testInteractionWithParentConstraints( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		# Cube with identity transform, parent constrained to sphere
+		# rotated 90 around X and translated to ( 5, 5, 0 ).
+
+		script["sphere"] = GafferScene.Sphere()
+		script["sphere"]["transform"]["rotate"]["x"].setValue( 90 )
+		script["sphere"]["transform"]["translate"].setValue( imath.V3f( 5, 5, 0 ) )
+
+		script["cube"] = GafferScene.Cube()
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["parent"].setValue( "/" )
+		script["parent"]["in"].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][0].setInput( script["cube"]["out"] )
+
+		script["cubeFilter"] = GafferScene.PathFilter()
+		script["cubeFilter"]["paths"].setValue( IECore.StringVectorData( [ "/cube" ] ) )
+
+		script["constraint"] = GafferScene.ParentConstraint()
+		script["constraint"]["in"].setInput( script["parent"]["out"] )
+		script["constraint"]["filter"].setInput( script["cubeFilter"]["out"] )
+		script["constraint"]["target"].setValue( "/sphere" )
+
+		self.assertEqual(
+			script["constraint"]["out"].fullTransform( "/cube" ),
+			script["constraint"]["out"].fullTransform( "/sphere" )
+		)
+
+		# View and Tool
+
+		view = GafferSceneUI.SceneView()
+		view["in"].setInput( script["constraint"]["out"] )
+		GafferSceneUI.ContextAlgo.setSelectedPaths( view.getContext(), IECore.PathMatcher( [ "/cube" ] ) )
+
+		tool = GafferSceneUI.TranslateTool( view )
+		tool["active"].setValue( True )
+
+		# Check handle orientation
+
+		tool["orientation"].setValue( tool.Orientation.Local )
+		self.assertEqual( tool.handlesTransform(), script["constraint"]["out"].fullTransform( "/cube" ) )
+
+		tool["orientation"].setValue( tool.Orientation.Parent )
+		self.assertEqual(
+			tool.handlesTransform(),
+			imath.M44f().translate(
+				script["constraint"]["out"].fullTransform( "/cube" ).translation()
+			)
+		)
+
 if __name__ == "__main__":
 	unittest.main()