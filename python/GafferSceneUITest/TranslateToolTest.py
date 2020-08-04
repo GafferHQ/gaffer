GafferHQ/gaffer

commit 8be634295180ed353eda81a4eb9c915211981c78

TransformTool : Fix `sceneToTransformSpace()` for AimConstraints
Our approach was to find a matrix that aligned the full transforms for `path()` and `upstreamPath()`, and this worked in the vast majority of cases. But in the case of an intervening AimConstraint, the local transforms at the end of each path were different (`path()` has aim applied, `upstreamPath()` doesn't), and this caused TranslateTool to translate in wild directions. The fix is to align the full transforms of the _parent_ paths instead.

This code is at the heart of lots of fairly subtle juggling of transforms in the TransformTools, so I am a little wary to be messing with it. But two things reassure me :

- We have a reasonable amount of test coverage, having added tests for many similar problems we've fixed in the past.
- This should only cause a change of behaviour when `upstreamScene()` is _not_ the final author of the local transform, and that can only happen when there's an intervening node - such as an AimConstraint - that `initFromSceneNode()` isn't savvy to. This should not change behaviour for the standard case, because the standard case is that `upstreamScene()` had the final say in the local transform.

src/GafferSceneUI/TransformTool.cpp
@@ -547,16 +547,33 @@ Imath::M44f TransformTool::Selection::sceneToTransformSpace() const
 {
 	throwIfNotEditable();
 
+	// The scenes produced by `scene()` and `upstreamScene()` may
+	// be completely different :
+	//
+	// - The hierarchies may be different, often due to something like
+	//   a Group node inserted between the two.
+	// - The parent transforms of `path()` and `upstreamPath()` may
+	//   be different, either due to a Group or an intermediate edit
+	//   to the parent location.
+	//
+	// To account for this we align the two scenes by finding the
+	// difference between the parent transforms of `path()` and
+	// `upstreamPath()`.
+
 	M44f downstreamMatrix;
+	if( path().size() )
 	{
+		ScenePlug::ScenePath parentPath = path(); parentPath.pop_back();
 		Context::Scope scopedContext( context() );
-		downstreamMatrix = scene()->fullTransform( path() );
+		downstreamMatrix = scene()->fullTransform( parentPath );
 	}
 
 	M44f upstreamMatrix;
+	if( upstreamPath().size() )
 	{
+		ScenePlug::ScenePath upstreamParentPath = upstreamPath(); upstreamParentPath.pop_back();
 		Context::Scope scopedContext( upstreamContext() );
-		upstreamMatrix = upstreamScene()->fullTransform( upstreamPath() );
+		upstreamMatrix = upstreamScene()->fullTransform( upstreamParentPath );
 	}
 
 	return downstreamMatrix.inverse() * upstreamMatrix * transformSpace().inverse();

python/GafferSceneUITest/TranslateToolTest.py
@@ -1045,5 +1045,55 @@ class TranslateToolTest( GafferUITest.TestCase ) :
 			{ "Location does not exist" }
 		)
 
+	def testInteractionWithAimConstraints( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		# Cube at ( 0, 10, 0 ), aimed at a sphere at the origin.
+
+		script["sphere"] = GafferScene.Sphere()
+
+		script["cube"] = GafferScene.Cube()
+		script["cube"]["transform"]["translate"]["y"].setValue( 10 )
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["parent"].setValue( "/" )
+		script["parent"]["in"].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][0].setInput( script["cube"]["out"] )
+
+		script["cubeFilter"] = GafferScene.PathFilter()
+		script["cubeFilter"]["paths"].setValue( IECore.StringVectorData( [ "/cube" ] ) )
+
+		script["aim"] = GafferScene.AimConstraint()
+		script["aim"]["in"].setInput( script["parent"]["out"] )
+		script["aim"]["filter"].setInput( script["cubeFilter"]["out"] )
+		script["aim"]["target"].setValue( "/sphere" )
+
+		# Translate in Z (parent space) and check that we moved to
+		# where we expected.
+
+		self.assertEqual(
+			script["aim"]["out"].transform( "/cube" ).translation(),
+			imath.V3f( 0, 10, 0 )
+		)
+
+		view = GafferSceneUI.SceneView()
+		view["in"].setInput( script["aim"]["out"] )
+		GafferSceneUI.ContextAlgo.setSelectedPaths( view.getContext(), IECore.PathMatcher( [ "/cube" ] ) )
+
+		tool = GafferSceneUI.TranslateTool( view )
+		tool["active"].setValue( True )
+
+		self.assertEqual( len( tool.selection() ), 1 )
+		self.assertEqual( tool.selection()[0].path(), "/cube" )
+		self.assertEqual( tool.selection()[0].upstreamPath(), "/cube" )
+		self.assertEqual( tool.selection()[0].editTarget(), script["cube"]["transform"] )
+
+		tool.translate( imath.V3f( 0, 0, 10 ) )
+		self.assertEqual(
+			script["aim"]["out"].transform( "/cube" ).translation(),
+			imath.V3f( 0, 10, 10 )
+		)
+
 if __name__ == "__main__":
 	unittest.main()

commit bd9a0236fdb3eb01610028cefe0fa3f7b8cc46c9

TransformTool : Fix interactions with ParentConstraints
This was broken by 8be6342.

include/GafferSceneUI/TransformTool.h
@@ -187,6 +187,7 @@ class GAFFERSCENEUI_API TransformTool : public GafferSceneUI::SelectionTool
 				Gaffer::EditScopePtr m_editScope;
 				mutable boost::optional<TransformEdit> m_transformEdit;
 				Imath::M44f m_transformSpace;
+				bool m_aimConstraint;
 
 		};
 

src/GafferSceneUI/TransformTool.cpp
@@ -39,6 +39,7 @@
 #include "GafferSceneUI/SceneView.h"
 #include "GafferSceneUI/ContextAlgo.h"
 
+#include "GafferScene/AimConstraint.h"
 #include "GafferScene/EditScopeAlgo.h"
 #include "GafferScene/Group.h"
 #include "GafferScene/ObjectSource.h"
@@ -213,7 +214,7 @@ TransformTool::Selection::Selection(
 	const Gaffer::ConstContextPtr &context,
 	const Gaffer::EditScopePtr &editScope
 )
-	:	m_scene( scene ), m_path( path ), m_context( context ), m_editable( false ), m_editScope( editScope )
+	:	m_scene( scene ), m_path( path ), m_context( context ), m_editable( false ), m_editScope( editScope ), m_aimConstraint( false )
 {
 	Context::Scope scopedContext( context.get() );
 	if( path.empty() )
@@ -273,6 +274,8 @@ TransformTool::Selection::Selection(
 
 void TransformTool::Selection::initFromSceneNode( const GafferScene::SceneAlgo::History *history )
 {
+	// Check for SceneNode and return if there isn't one or it is disabled.
+
 	const SceneNode *node = runTimeCast<const SceneNode>( history->scene->node() );
 	if( !node )
 	{
@@ -284,6 +287,22 @@ void TransformTool::Selection::initFromSceneNode( const GafferScene::SceneAlgo::
 	{
 		return;
 	}
+
+	// Check for AimConstraints. These must be accounted for in `sceneToTransformSpace()`.
+
+	if( auto constraint = runTimeCast<const AimConstraint>( node ) )
+	{
+		if(
+			history->scene == constraint->outPlug() &&
+			( filterResult( constraint->filterPlug(), constraint->inPlug() ) & PathMatcher::ExactMatch )
+		)
+		{
+			m_aimConstraint = true;
+			return;
+		}
+	}
+
+	// Determine if node edits the transform at this location.
 
 	TransformPlug *transformPlug = nullptr;
 	if( const ObjectSource *objectSource = runTimeCast<const ObjectSource>( node ) )
@@ -587,23 +606,50 @@ Imath::M44f TransformTool::Selection::sceneToTransformSpace() const
 	//   to the parent location.
 	//
 	// To account for this we align the two scenes by finding the
-	// difference between the parent transforms of `path()` and
+	// difference between the full transforms of `path()` and
 	// `upstreamPath()`.
+	//
+	// This is made trickier by the presence of AimConstraints
+	// between `upstreamScene()` and `scene()`. These overwrite
+	// the local rotation from upstream, meaning we no longer have
+	// a direct correspondence between the upstream and downstream
+	// spaces. In this event we omit the local matrix entirely by
+	// aligning the parent transforms instead.
+	//
+	// \todo I suspect the truth of the situation is more nuanced
+	// than this and the correct approach depends on the type of
+	// constraint and its order of composition with the edits we make
+	// to the upstream transform. We are fortunate to be able to ignore
+	// PointConstraints for now because we only transform direction vectors
+	// with `sceneToTransformSpace()`. I'm sure there is a more principled
+	// approach than the blunt instrument used here.
 
 	M44f downstreamMatrix;
-	if( path().size() )
 	{
-		ScenePlug::ScenePath parentPath = path(); parentPath.pop_back();
 		Context::Scope scopedContext( context() );
-		downstreamMatrix = scene()->fullTransform( parentPath );
+		if( !m_aimConstraint )
+		{
+			downstreamMatrix = scene()->fullTransform( path() );
+		}
+		else if( path().size() )
+		{
+			ScenePlug::ScenePath parentPath = path(); parentPath.pop_back();
+			downstreamMatrix = scene()->fullTransform( parentPath );
+		}
 	}
 
 	M44f upstreamMatrix;
-	if( upstreamPath().size() )
 	{
-		ScenePlug::ScenePath upstreamParentPath = upstreamPath(); upstreamParentPath.pop_back();
 		Context::Scope scopedContext( upstreamContext() );
-		upstreamMatrix = upstreamScene()->fullTransform( upstreamParentPath );
+		if( !m_aimConstraint )
+		{
+			upstreamMatrix = upstreamScene()->fullTransform( upstreamPath() );
+		}
+		else if( upstreamPath().size() )
+		{
+			ScenePlug::ScenePath upstreamParentPath = upstreamPath(); upstreamParentPath.pop_back();
+			upstreamMatrix = upstreamScene()->fullTransform( upstreamParentPath );
+		}
 	}
 
 	return downstreamMatrix.inverse() * upstreamMatrix * transformSpace().inverse();

python/GafferSceneUITest/RotateToolTest.py
@@ -403,5 +403,8 @@ class RotateToolTest( GafferUITest.TestCase ) :
 		tool["orientation"].setValue( tool.Orientation.Local )
 		self.assertEqual( tool.handlesTransform(), script["constraint"]["out"].transform( "/sphere" ) )
 
+		tool.rotate( imath.Eulerf( 0, 90, 0 ) )
+		self.assertEqual( script["sphere"]["transform"]["rotate"].getValue(), imath.V3f( 0, 90, 0 ) )
+
 if __name__ == "__main__":
 	unittest.main()

python/GafferSceneUITest/TranslateToolTest.py
@@ -1209,5 +1209,14 @@ class TranslateToolTest( GafferUITest.TestCase ) :
 			)
 		)
 
+		# Check translation operation
+
+		tool["orientation"].setValue( tool.Orientation.Local )
+		tool.translate( imath.V3f( 1, 2, 3 ) )
+		self.assertEqual(
+			script["cube"]["transform"]["translate"].getValue(),
+			imath.V3f( 1, 2, 3 )
+		)
+
 if __name__ == "__main__":
 	unittest.main()