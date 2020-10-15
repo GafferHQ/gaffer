GafferHQ/gaffer

commit 1c55a780d5af0a4aff32722a1b0e1d777b63ea84

ImageGadget : Refactor with new private TileShader class
This is just shuffling code around really, but it does tie the LUT texture and associated GLSL shader together in a logical way, and stops us keeping textures alive after we've thrown away the shader they were intended for (when the next shader doesn't need a LUT). This alone probably isn't sufficient to justify the work, but we also need a place to store some additional data in an ABI compatible way for some forthcoming commits.

src/GafferImageUI/ImageGadget.cpp
@@ -106,6 +106,245 @@ void findUsableTextureFormats( GLenum &monochromeFormat, GLenum &colorFormat )
 }
 
 //////////////////////////////////////////////////////////////////////////
+// ImageGadget::TileShader implementation
+//////////////////////////////////////////////////////////////////////////
+
+// Manages an OpenGL shader suitable for rendering tiles,
+// with optional GPU implementation for OCIO transforms.
+class ImageGadget::TileShader : public IECore::RefCounted
+{
+
+	public :
+
+		TileShader( const OpenColorIO::ConstTransformRcPtr &openColorIOTransform )
+			:	m_lut3dTextureID( 0 ), m_useGPUTransform( openColorIOTransform )
+		{
+			// Get GLSL code and LUT for OCIO transform if we have one.
+
+			const int LUT3D_EDGE_SIZE = 128;
+			std::string colorTransformCode;
+			std::vector<float> lut3d;
+			if( openColorIOTransform )
+			{
+				OpenColorIO::ConstConfigRcPtr config = OpenColorIO::GetCurrentConfig();
+				OpenColorIO::ConstProcessorRcPtr processor = config->getProcessor( openColorIOTransform );
+
+				OpenColorIO::GpuShaderDesc shaderDesc;
+				shaderDesc.setLanguage( OpenColorIO::GPU_LANGUAGE_GLSL_1_3 );
+				shaderDesc.setFunctionName( "OCIODisplay" );
+				shaderDesc.setLut3DEdgeLen( LUT3D_EDGE_SIZE );
+
+				lut3d.resize( 3 * LUT3D_EDGE_SIZE * LUT3D_EDGE_SIZE * LUT3D_EDGE_SIZE );
+				processor->getGpuLut3D( &lut3d[0], shaderDesc );
+				colorTransformCode =  processor->getGpuShaderText( shaderDesc );
+			}
+			else
+			{
+				colorTransformCode = "vec4 OCIODisplay(vec4 inPixel, sampler3D lut3d) { return inPixel; }\n";
+			}
+
+			// Build and compile GLSL shader
+
+			std::string combinedFragmentCode;
+			if( glslVersion() >= 330 )
+			{
+				// the __VERSION__ define is a workaround for the fact that cortex's source preprocessing doesn't
+				// define it correctly in the same way as the OpenGL shader preprocessing would.
+				combinedFragmentCode = "#version 330 compatibility\n #define __VERSION__ 330\n\n";
+			}
+			combinedFragmentCode += colorTransformCode + fragmentSource();
+
+			m_shader = ShaderLoader::defaultShaderLoader()->create( vertexSource(), "", combinedFragmentCode );
+
+			// Query shader parameters
+
+			m_channelTextureUnits[0] = m_shader->uniformParameter( "redTexture" )->textureUnit;
+			m_channelTextureUnits[1] = m_shader->uniformParameter( "greenTexture" )->textureUnit;
+			m_channelTextureUnits[2] = m_shader->uniformParameter( "blueTexture" )->textureUnit;
+			m_channelTextureUnits[3] = m_shader->uniformParameter( "alphaTexture" )->textureUnit;
+			m_activeParameterLocation = m_shader->uniformParameter( "activeParam" )->location;
+
+			// If we have a LUT, load it into an OpenGL texture
+
+			if( openColorIOTransform && m_shader->uniformParameter( "lutTexture" ) )
+			{
+				GLenum monochromeTextureFormat, colorTextureFormat;
+				findUsableTextureFormats( monochromeTextureFormat, colorTextureFormat );
+				glGenTextures( 1, &m_lut3dTextureID );
+				glActiveTexture( GL_TEXTURE0 + m_shader->uniformParameter( "lutTexture" )->textureUnit );
+				glBindTexture( GL_TEXTURE_3D, m_lut3dTextureID );
+				glTexImage3D(
+					GL_TEXTURE_3D, 0, colorTextureFormat, LUT3D_EDGE_SIZE, LUT3D_EDGE_SIZE, LUT3D_EDGE_SIZE,
+					0, GL_RGB, GL_FLOAT, &lut3d[0]
+				);
+				glTexParameteri( GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+				glTexParameteri( GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR) ;
+				glTexParameteri( GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+				glTexParameteri( GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+				glTexParameteri( GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE );
+			}
+		}
+
+		~TileShader()
+		{
+			if( m_lut3dTextureID )
+			{
+				glDeleteTextures( 1, &m_lut3dTextureID );
+			}
+		}
+
+		// Binds shader and provides `loadTile()` method to update
+		// parameters for a specific tile.
+		struct ScopedBinding : PushAttrib
+		{
+
+			ScopedBinding( const TileShader &tileShader, bool clipping, float exposure, float gamma )
+				:	PushAttrib( GL_COLOR_BUFFER_BIT ), m_tileShader( tileShader )
+			{
+				glGetIntegerv( GL_CURRENT_PROGRAM, &m_previousProgram );
+				glUseProgram( m_tileShader.m_shader->program() );
+				glEnable( GL_TEXTURE_2D );
+				glEnable( GL_BLEND );
+				glBlendFunc( GL_ONE, GL_ONE_MINUS_SRC_ALPHA );
+
+				if( tileShader.m_useGPUTransform )
+				{
+					glUniform1f( tileShader.m_shader->uniformParameter( "multiply" )->location, pow( 2.0f, exposure ) );
+					glUniform1f( tileShader.m_shader->uniformParameter( "power" )->location, gamma > 0.0 ? 1.0f / gamma : 1.0f );
+					glUniform1f( tileShader.m_shader->uniformParameter( "clipping" )->location, clipping );
+				}
+				else
+				{
+					glUniform1f( tileShader.m_shader->uniformParameter( "multiply" )->location, 1.0f );
+					glUniform1f( tileShader.m_shader->uniformParameter( "power" )->location, 1.0f );
+					glUniform1f( tileShader.m_shader->uniformParameter( "clipping" )->location, false );
+				}
+
+				glUniform1i( tileShader.m_shader->uniformParameter( "redTexture" )->location, tileShader.m_channelTextureUnits[0] );
+				glUniform1i( tileShader.m_shader->uniformParameter( "greenTexture" )->location, tileShader.m_channelTextureUnits[1] );
+				glUniform1i( tileShader.m_shader->uniformParameter( "blueTexture" )->location, tileShader.m_channelTextureUnits[2] );
+				glUniform1i( tileShader.m_shader->uniformParameter( "alphaTexture" )->location, tileShader.m_channelTextureUnits[3] );
+
+				if( tileShader.m_shader->uniformParameter( "lutTexture" ) )
+				{
+					const GLuint lutTextureUnit = tileShader.m_shader->uniformParameter( "lutTexture" )->textureUnit;
+					glUniform1i( tileShader.m_shader->uniformParameter( "lutTexture" )->location, lutTextureUnit );
+					glActiveTexture( GL_TEXTURE0 + lutTextureUnit );
+					glBindTexture( GL_TEXTURE_3D, tileShader.m_lut3dTextureID );
+				}
+			}
+
+			~ScopedBinding()
+			{
+				glUseProgram( m_previousProgram );
+			}
+
+			void loadTile( const IECoreGL::Texture *channelTextures[4], bool active )
+			{
+				for( int i = 0; i < 4; ++i )
+				{
+					glActiveTexture( GL_TEXTURE0 + m_tileShader.m_channelTextureUnits[i] );
+					channelTextures[i]->bind();
+				}
+				glUniform1i( m_tileShader.m_activeParameterLocation, active );
+			}
+
+			private :
+
+				const TileShader &m_tileShader;
+				GLint m_previousProgram;
+
+		};
+
+	private :
+
+		GLuint m_lut3dTextureID;
+		IECoreGL::ShaderPtr m_shader;
+		GLuint m_channelTextureUnits[4];
+		GLint m_activeParameterLocation;
+		bool m_useGPUTransform;
+
+		static const char *vertexSource()
+		{
+			static const char *g_vertexSource =
+			"void main()"
+			"{"
+			"	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;"
+			"	gl_TexCoord[0] = gl_MultiTexCoord0;"
+			"}";
+
+			return g_vertexSource;
+		}
+
+		static const std::string &fragmentSource()
+		{
+			static std::string g_fragmentSource;
+			if( g_fragmentSource.empty() )
+			{
+				g_fragmentSource =
+
+				"uniform sampler2D redTexture;\n"
+				"uniform sampler2D greenTexture;\n"
+				"uniform sampler2D blueTexture;\n"
+				"uniform sampler2D alphaTexture;\n"
+
+				"uniform sampler3D lutTexture;\n"
+
+				"uniform bool activeParam;\n"
+				"uniform float multiply;\n"
+				"uniform float power;\n"
+				"uniform bool clipping;\n"
+
+				"#if __VERSION__ >= 330\n"
+
+				"layout( location=0 ) out vec4 outColor;\n"
+				"#define OUTCOLOR outColor\n"
+
+				"#else\n"
+
+				"#define OUTCOLOR gl_FragColor\n"
+
+				"#endif\n"
+
+				"#define ACTIVE_CORNER_RADIUS 0.3\n"
+
+				"void main()"
+				"{"
+				"	OUTCOLOR = vec4(\n"
+				"		texture2D( redTexture, gl_TexCoord[0].xy ).r,\n"
+				"		texture2D( greenTexture, gl_TexCoord[0].xy ).r,\n"
+				"		texture2D( blueTexture, gl_TexCoord[0].xy ).r,\n"
+				"		texture2D( alphaTexture, gl_TexCoord[0].xy ).r\n"
+				"	);\n"
+				"	if( clipping )\n"
+				"	{\n"
+				"		OUTCOLOR = vec4(\n"
+				"			OUTCOLOR.r < 0.0 ? 1.0 : ( OUTCOLOR.r > 1.0 ? 0.0 : OUTCOLOR.r ),\n"
+				"			OUTCOLOR.g < 0.0 ? 1.0 : ( OUTCOLOR.g > 1.0 ? 0.0 : OUTCOLOR.g ),\n"
+				"			OUTCOLOR.b < 0.0 ? 1.0 : ( OUTCOLOR.b > 1.0 ? 0.0 : OUTCOLOR.b ),\n"
+				"			OUTCOLOR.a\n"
+				"		);\n"
+				"	}\n"
+				"	OUTCOLOR = vec4( pow( OUTCOLOR.rgb * multiply, vec3( power ) ), OUTCOLOR.a );\n"
+				"	OUTCOLOR = OCIODisplay( OUTCOLOR, lutTexture );\n"
+				"	if( activeParam )\n"
+				"	{\n"
+				"		vec2 pixelWidth = vec2( dFdx( gl_TexCoord[0].x ), dFdy( gl_TexCoord[0].y ) );\n"
+				"		float aspect = pixelWidth.x / pixelWidth.y;\n"
+				"		vec2 p = abs( gl_TexCoord[0].xy - vec2( 0.5 ) );\n"
+				"		float eX = step( 0.5 - pixelWidth.x, p.x ) * step( 0.5 - ACTIVE_CORNER_RADIUS, p.y );\n"
+				"		float eY = step( 0.5 - pixelWidth.y, p.y ) * step( 0.5 - ACTIVE_CORNER_RADIUS * aspect, p.x );\n"
+				"		float e = eX + eY - eX * eY;\n"
+				"		OUTCOLOR += vec4( 0.15 ) * e;\n"
+				"	}\n"
+				"}";
+			}
+			return g_fragmentSource;
+		}
+
+};
+
+//////////////////////////////////////////////////////////////////////////
 // ImageGadget implementation
 //////////////////////////////////////////////////////////////////////////
 
@@ -147,8 +386,6 @@ ImageGadget::ImageGadget()
 	m_gradeNode = new Grade;
 	m_gradeNode->inPlug()->setInput( m_clampNode->outPlug() );
 	m_gradeNode->channelsPlug()->setValue( "*" );
-
-	m_lut3dTextureID = 0;
 }
 
 ImageGadget::~ImageGadget()
@@ -156,11 +393,6 @@ ImageGadget::~ImageGadget()
 	// Make sure background task completes before anything
 	// it relies on is destroyed.
 	m_tilesTask.reset();
-
-	if( m_lut3dTextureID != 0 )
-	{
-		glDeleteTextures( 1, &m_lut3dTextureID );
-	}
 }
 
 void ImageGadget::setImage( GafferImage::ImagePlugPtr image )
@@ -838,90 +1070,6 @@ void ImageGadget::removeOutOfBoundsTiles() const
 // Rendering
 //////////////////////////////////////////////////////////////////////////
 
-namespace
-{
-
-const char *vertexSource()
-{
-	static const char *g_vertexSource =
-	"void main()"
-	"{"
-	"	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;"
-	"	gl_TexCoord[0] = gl_MultiTexCoord0;"
-	"}";
-
-	return g_vertexSource;
-}
-
-const std::string &fragmentSource()
-{
-	static std::string g_fragmentSource;
-	if( g_fragmentSource.empty() )
-	{
-		g_fragmentSource =
-
-		"uniform sampler2D redTexture;\n"
-		"uniform sampler2D greenTexture;\n"
-		"uniform sampler2D blueTexture;\n"
-		"uniform sampler2D alphaTexture;\n"
-
-		"uniform sampler3D lutTexture;\n"
-
-		"uniform bool activeParam;\n"
-		"uniform float multiply;\n"
-		"uniform float power;\n"
-		"uniform bool clipping;\n"
-
-		"#if __VERSION__ >= 330\n"
-
-		"layout( location=0 ) out vec4 outColor;\n"
-		"#define OUTCOLOR outColor\n"
-
-		"#else\n"
-
-		"#define OUTCOLOR gl_FragColor\n"
-
-		"#endif\n"
-
-		"#define ACTIVE_CORNER_RADIUS 0.3\n"
-
-		"void main()"
-		"{"
-		"	OUTCOLOR = vec4(\n"
-		"		texture2D( redTexture, gl_TexCoord[0].xy ).r,\n"
-		"		texture2D( greenTexture, gl_TexCoord[0].xy ).r,\n"
-		"		texture2D( blueTexture, gl_TexCoord[0].xy ).r,\n"
-		"		texture2D( alphaTexture, gl_TexCoord[0].xy ).r\n"
-		"	);\n"
-		"	if( clipping )\n"
-		"	{\n"
-		"		OUTCOLOR = vec4(\n"
-		"			OUTCOLOR.r < 0.0 ? 1.0 : ( OUTCOLOR.r > 1.0 ? 0.0 : OUTCOLOR.r ),\n"
-		"			OUTCOLOR.g < 0.0 ? 1.0 : ( OUTCOLOR.g > 1.0 ? 0.0 : OUTCOLOR.g ),\n"
-		"			OUTCOLOR.b < 0.0 ? 1.0 : ( OUTCOLOR.b > 1.0 ? 0.0 : OUTCOLOR.b ),\n"
-		"			OUTCOLOR.a\n"
-		"		);\n"
-		"	}\n"
-		"	OUTCOLOR = vec4( pow( OUTCOLOR.rgb * multiply, vec3( power ) ), OUTCOLOR.a );\n"
-		"	OUTCOLOR = OCIODisplay( OUTCOLOR, lutTexture );\n"
-		"	if( activeParam )\n"
-		"	{\n"
-		"		vec2 pixelWidth = vec2( dFdx( gl_TexCoord[0].x ), dFdy( gl_TexCoord[0].y ) );\n"
-		"		float aspect = pixelWidth.x / pixelWidth.y;\n"
-		"		vec2 p = abs( gl_TexCoord[0].xy - vec2( 0.5 ) );\n"
-		"		float eX = step( 0.5 - pixelWidth.x, p.x ) * step( 0.5 - ACTIVE_CORNER_RADIUS, p.y );\n"
-		"		float eY = step( 0.5 - pixelWidth.y, p.y ) * step( 0.5 - ACTIVE_CORNER_RADIUS * aspect, p.x );\n"
-		"		float e = eX + eY - eX * eY;\n"
-		"		OUTCOLOR += vec4( 0.15 ) * e;\n"
-		"	}\n"
-		"}";
-	}
-	return g_fragmentSource;
-}
-
-
-} // namespace
-
 void ImageGadget::visibilityChanged()
 {
 	if( !visible() )
@@ -930,128 +1078,21 @@ void ImageGadget::visibilityChanged()
 	}
 }
 
-IECoreGL::Shader *ImageGadget::shader() const
+ImageGadget::TileShader *ImageGadget::shader() const
 {
 	if( !m_shaderDirty )
 	{
 		return m_shader.get();
 	}
 
-	const int LUT3D_EDGE_SIZE = 128;
-	if( m_lut3dTextureID == 0 )
-	{
-		glGenTextures( 1, &m_lut3dTextureID );
-	}
-
-	std::string colorTransformCode;
-	std::vector<float> lut3d;
-	if( m_gpuOcioTransform )
-	{
-		OpenColorIO::ConstConfigRcPtr config = OpenColorIO::GetCurrentConfig();
-
-		OpenColorIO::ConstProcessorRcPtr processor = config->getProcessor( m_gpuOcioTransform );
-
-		OpenColorIO::GpuShaderDesc shaderDesc;
-		shaderDesc.setLanguage( OpenColorIO::GPU_LANGUAGE_GLSL_1_3 );
-		shaderDesc.setFunctionName( "OCIODisplay" );
-		shaderDesc.setLut3DEdgeLen( LUT3D_EDGE_SIZE );
-
-		int num3Dentries = 3*LUT3D_EDGE_SIZE*LUT3D_EDGE_SIZE*LUT3D_EDGE_SIZE;
-		lut3d.resize( num3Dentries );
-		processor->getGpuLut3D( &lut3d[0], shaderDesc );
-		colorTransformCode =  processor->getGpuShaderText( shaderDesc );
-	}
-	else
-	{
-		colorTransformCode = "vec4 OCIODisplay(vec4 inPixel, sampler3D lut3d) { return inPixel; }\n";
-	}
-
-	std::string combinedFragmentCode;
-	if( glslVersion() >= 330 )
-	{
-		// the __VERSION__ define is a workaround for the fact that cortex's source preprocessing doesn't
-		// define it correctly in the same way as the OpenGL shader preprocessing would.
-		combinedFragmentCode = "#version 330 compatibility\n #define __VERSION__ 330\n\n";
-	}
-	combinedFragmentCode += colorTransformCode + fragmentSource();
-
-	m_shader = ShaderLoader::defaultShaderLoader()->create( vertexSource(), "", combinedFragmentCode );
-
-	if( m_gpuOcioTransform && m_shader->uniformParameter( "lutTexture" ) )
-	{
-		GLenum monochromeTextureFormat, colorTextureFormat;
-		findUsableTextureFormats( monochromeTextureFormat, colorTextureFormat );
-
-		// Load the data into an OpenGL 3D Texture
-		glActiveTexture( GL_TEXTURE0 + m_shader->uniformParameter( "lutTexture" )->textureUnit );
-		glBindTexture( GL_TEXTURE_3D, m_lut3dTextureID );
-		glTexImage3D(
-			GL_TEXTURE_3D, 0, colorTextureFormat, LUT3D_EDGE_SIZE, LUT3D_EDGE_SIZE, LUT3D_EDGE_SIZE,
-			0, GL_RGB, GL_FLOAT, &lut3d[0]
-		);
-	}
-
+	m_shader = new TileShader( m_gpuOcioTransform );
 	m_shaderDirty = false;
 	return m_shader.get();
 }
 
 void ImageGadget::renderTiles() const
 {
-	GLint previousProgram;
-	glGetIntegerv( GL_CURRENT_PROGRAM, &previousProgram );
-
-	PushAttrib pushAttrib( GL_COLOR_BUFFER_BIT );
-
-	Shader *shader = this->shader( );
-	glUseProgram( shader->program() );
-
-	glEnable( GL_TEXTURE_2D );
-
-	glEnable( GL_BLEND );
-	glBlendFunc( GL_ONE, GL_ONE_MINUS_SRC_ALPHA );
-
-	std::vector<std::string> uniformNames;
-	shader->uniformParameterNames( uniformNames );
-
-	GLuint textureUnits[4];
-	textureUnits[0] = shader->uniformParameter( "redTexture" )->textureUnit;
-	textureUnits[1] = shader->uniformParameter( "greenTexture" )->textureUnit;
-	textureUnits[2] = shader->uniformParameter( "blueTexture" )->textureUnit;
-	textureUnits[3] = shader->uniformParameter( "alphaTexture" )->textureUnit;
-
-	glUniform1i( shader->uniformParameter( "redTexture" )->location, textureUnits[0] );
-	glUniform1i( shader->uniformParameter( "greenTexture" )->location, textureUnits[1] );
-	glUniform1i( shader->uniformParameter( "blueTexture" )->location, textureUnits[2] );
-	glUniform1i( shader->uniformParameter( "alphaTexture" )->location, textureUnits[3] );
-
-	if( shader->uniformParameter( "lutTexture" ) )
-	{
-		GLuint lutTextureUnit = shader->uniformParameter( "lutTexture" )->textureUnit;
-		glUniform1i( shader->uniformParameter( "lutTexture" )->location, lutTextureUnit );
-		glActiveTexture( GL_TEXTURE0 + lutTextureUnit );
-		glBindTexture( GL_TEXTURE_3D, m_lut3dTextureID );
-
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	}
-
-	if( m_gpuOcioTransform || !m_displayTransform )
-	{
-		glUniform1f( shader->uniformParameter( "multiply" )->location, pow( 2.0f, m_exposure ) );
-		glUniform1f( shader->uniformParameter( "power" )->location, m_gamma > 0.0 ? 1.0f / m_gamma : 1.0f );
-		glUniform1f( shader->uniformParameter( "clipping" )->location, m_clipping );
-	}
-	else
-	{
-		glUniform1f( shader->uniformParameter( "multiply" )->location, 1.0f );
-		glUniform1f( shader->uniformParameter( "power" )->location, 1.0f );
-		glUniform1f( shader->uniformParameter( "clipping" )->location, false );
-	}
-
-	GLint activeParameterLocation = shader->uniformParameter( "activeParam" )->location;
+	TileShader::ScopedBinding shaderBinding( *shader(), m_clipping, m_exposure, m_gamma );
 
 	const Box2i dataWindow = this->dataWindow();
 	const float pixelAspect = this->format().getPixelAspect();
@@ -1062,22 +1103,21 @@ void ImageGadget::renderTiles() const
 		for( tileOrigin.x = ImagePlug::tileOrigin( dataWindow.min ).x; tileOrigin.x < dataWindow.max.x; tileOrigin.x += ImagePlug::tileSize() )
 		{
 			bool active = false;
+			const IECoreGL::Texture *channelTextures[4];
 			for( int i = 0; i < 4; ++i )
 			{
-				glActiveTexture( GL_TEXTURE0 + textureUnits[i] );
 				const InternedString channelName = m_soloChannel == -1 ? m_rgbaChannels[i] : m_rgbaChannels[m_soloChannel];
 				Tiles::const_iterator it = m_tiles.find( TileIndex( tileOrigin, channelName ) );
 				if( it != m_tiles.end() )
 				{
-					it->second.texture( active )->bind();
+					channelTextures[i] = it->second.texture( active );
 				}
 				else
 				{
-					blackTexture()->bind();
+					channelTextures[i] = blackTexture();
 				}
 			}
-
-			glUniform1i( activeParameterLocation, active );
+			shaderBinding.loadTile( channelTextures, active );
 
 			const Box2i tileBound( tileOrigin, tileOrigin + V2i( ImagePlug::tileSize() ) );
 			const Box2i validBound = BufferAlgo::intersection( tileBound, dataWindow );
@@ -1110,8 +1150,6 @@ void ImageGadget::renderTiles() const
 
 		}
 	}
-
-	glUseProgram( previousProgram );
 }
 
 void ImageGadget::renderText( const std::string &text, const Imath::V2f &position, const Imath::V2f &alignment, const GafferUI::Style *style ) const

include/GafferImageUI/ImageGadget.h
@@ -193,10 +193,11 @@ class GAFFERIMAGEUI_API ImageGadget : public GafferUI::Gadget
 		GafferImage::ImageProcessorPtr m_displayTransform;
 		OpenColorIO::ConstTransformRcPtr m_gpuOcioTransform;
 
-		IECoreGL::Shader *shader() const;
-		mutable GLuint m_lut3dTextureID;
-		mutable IECoreGL::ShaderPtr m_shader;
-		mutable bool m_shaderDirty;
+		IE_CORE_FORWARDDECLARE( TileShader ); /// \todo Move to rendering section below
+		TileShader *shader() const;  /// \todo Move to rendering section below
+		GLuint m_unused; /// \todo Remove
+		mutable TileShaderPtr m_shader; /// \todo Move to rendering section below
+		mutable bool m_shaderDirty; /// \todo Move to rendering section below
 
 		bool m_useGPU;
 		bool m_labelsVisible;

commit 563cdbbc0d1d3db09e88415546be7de2dc51abe0

ImageGadget : Fix crash viewing monochrome or soloed channels
In this case, we call `Tile::texture()` on the _same_ tile more than once, before calling `loadTile()` with all the textures we've gathered. If we're unlucky, in between calls, new data can arrive in `Tile::m_channelDataToConvert`, so that the next call to `texture()` will discard the previous texture. By owning a reference to the textures we prevent them from being deleted before `loadTile()` has completed and we have drawn the tile.

This fixes a regression introduced in 1c55a780d5af0a4aff32722a1b0e1d777b63ea84. Prior to that, we would call `Texture::bind()` _before_ the texture was destroyed, avoiding the crash we're fixing here. But the `Texture` could still have been deleted between the `bind()` and the drawing of the tile. It's not clear to me how we were getting away with that, but _maybe_ it could explain a rare drawing glitch we've seen during IPR?

src/GafferImageUI/ImageGadget.cpp
@@ -255,7 +255,7 @@ class ImageGadget::TileShader : public IECore::RefCounted
 				glUseProgram( m_previousProgram );
 			}
 
-			void loadTile( const IECoreGL::Texture *channelTextures[4], bool active )
+			void loadTile( IECoreGL::ConstTexturePtr channelTextures[4], bool active )
 			{
 				for( int i = 0; i < 4; ++i )
 				{
@@ -1168,7 +1168,7 @@ void ImageGadget::renderTiles() const
 		for( tileOrigin.x = ImagePlug::tileOrigin( dataWindow.min ).x; tileOrigin.x < dataWindow.max.x; tileOrigin.x += ImagePlug::tileSize() )
 		{
 			bool active = false;
-			const IECoreGL::Texture *channelTextures[4];
+			IECoreGL::ConstTexturePtr channelTextures[4];
 			for( int i = 0; i < 4; ++i )
 			{
 				const InternedString channelName = m_soloChannel == -1 ? m_rgbaChannels[i] : m_rgbaChannels[m_soloChannel];