GafferHQ/gaffer

commit 538b860473f1a07d4f8d9c6458f501e4611a6c02

IECoreArnoldPreview::ShaderNetworkAlgo : Fix for OSL 1.11+
For Arnold, we don't seem to need the same fix as in `GafferOSL::ShadingEngine`. Perhaps the original todo was valid in this case because Arnold is doing the work for us.

Note I'm not entirely positive this is correct. All GafferArnoldTests are paassing, but I've yet to use this interactively and actually see it shading correctly. Regarding the changes I've made to `GafferArnoldTest.IECoreArnoldPreviewTest.RendererTest.testOSLShaderComponentConnections`, the shaders in the .ass file are as follows:

```
osl
{
 name shader:38e598c1eeef2402dd8ef9b497d1d21d
 shadername "Maths/MixColor"
 param_a.r shader:38e598c1eeef2402dd8ef9b497d1d21d:source:out.b
 param_a.g shader:38e598c1eeef2402dd8ef9b497d1d21d:source:out.r
 param_a.b shader:38e598c1eeef2402dd8ef9b497d1d21d:source:out.g
}

osl
{
 name shader:38e598c1eeef2402dd8ef9b497d1d21d:source:out
 declare output constant STRING
 output "out"
 shadername "Maths/MixColor"
}
```

src/GafferArnold/IECoreArnoldPreview/ShaderNetworkAlgo.cpp
@@ -336,14 +336,6 @@ namespace ShaderNetworkAlgo
 
 std::vector<AtNode *> convert( const IECoreScene::ShaderNetwork *shaderNetwork, const std::string &name, const AtNode *parentNode )
 {
-	ShaderNetworkPtr networkCopy;
-	if( true ) // todo : make conditional on OSL < 1.10
-	{
-		networkCopy = shaderNetwork->copy();
-		IECoreScene::ShaderNetworkAlgo::convertOSLComponentConnections( networkCopy.get() );
-		shaderNetwork = networkCopy.get();
-	}
-
 	ShaderMap converted;
 	vector<AtNode *> result;
 	const InternedString output = shaderNetwork->getOutput().shader;

python/GafferArnoldTest/ArnoldRenderTest.py
@@ -1090,17 +1090,22 @@ class ArnoldRenderTest( GafferSceneTest.SceneTestCase ) :
 
 	def testOSLShaders( self ) :
 
-		swizzle = GafferOSL.OSLShader()
-		swizzle.loadShader( "MaterialX/mx_swizzle_color_float" )
-		swizzle["parameters"]["in"].setValue( imath.Color3f( 0, 0, 1 ) )
-		swizzle["parameters"]["channels"].setValue( "b" )
+		blue = GafferOSL.OSLShader()
+		blue.loadShader( "Maths/MixColor" )
+		blue["parameters"]["a"].setValue( imath.Color3f( 0, 0, 1 ) )
 
-		pack = GafferOSL.OSLShader()
-		pack.loadShader( "MaterialX/mx_pack_color" )
-		pack["parameters"]["in1"].setInput( swizzle["out"]["out"] )
+		green = GafferOSL.OSLShader()
+		green.loadShader( "Maths/MixColor" )
+		green["parameters"]["a"].setValue( imath.Color3f( 0, 1, 0 ) )
+
+		mix = GafferOSL.OSLShader()
+		mix.loadShader( "Maths/MixColor" )
+		mix["parameters"]["a"].setInput( blue["out"]["out"] )
+		mix["parameters"]["b"].setInput( green["out"]["out"] )
+		mix["parameters"]["m"].setValue( 0.5 )
 
 		ball = GafferArnold.ArnoldShaderBall()
-		ball["shader"].setInput( pack["out"] )
+		ball["shader"].setInput( mix["out"] )
 
 		catalogue = GafferImage.Catalogue()
 
@@ -1129,7 +1134,7 @@ class ArnoldRenderTest( GafferSceneTest.SceneTestCase ) :
 
 			handler.waitFor( 0.1 ) #Just need to let the catalogue update
 
-			self.assertEqual( self.__color4fAtUV( catalogue, imath.V2f( 0.5 ) ), imath.Color4f( 1, 0, 0, 1 ) )
+			self.assertEqual( self.__color4fAtUV( catalogue, imath.V2f( 0.5 ) ), imath.Color4f( 0, 0.5, 0.5, 1 ) )
 
 	def testDefaultLightsMistakesDontForceLinking( self ) :
 

python/GafferArnoldTest/IECoreArnoldPreviewTest/RendererTest.py
@@ -412,33 +412,21 @@ class RendererTest( GafferTest.TestCase ) :
 
 			outputNode = arnold.AiNodeGetPtr( arnold.AiNodeLookUpByName( "test" ), "shader" )
 
-			pack = arnold.AiNodeGetLink( outputNode, "param_a" )
-			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( pack ) ), "osl" )
-			self.assertEqual( arnold.AiNodeGetStr( pack, "shadername" ), "MaterialX/mx_pack_color" )
+			# OSL 1.10+ allows component level connections, in which case we will
+			# get a link to each channel directly.
+			component = arnold.c_int()
+			rSource = arnold.AiNodeGetLink( outputNode, "param_a.r", component )
+			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( rSource ) ), "osl" )
+			self.assertEqual( arnold.AiNodeGetStr( rSource, "shadername" ), "Maths/MixColor" )
+			self.assertEqual( component.value, 2 ) # r is driven by b
 
-			swizzleR = arnold.AiNodeGetLink( pack, "param_in1" )
-			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleR ) ), "osl" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleR, "shadername" ), "MaterialX/mx_swizzle_color_float" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleR, "param_channels" ), "b" )
-			swizzleRSource = arnold.AiNodeGetLink( swizzleR, "param_in" )
+			gSource = arnold.AiNodeGetLink( outputNode, "param_a.g", component )
+			self.assertReferSameNode( rSource, gSource )
+			self.assertEqual( component.value, 0 ) # g is driven by r
 
-			swizzleG = arnold.AiNodeGetLink( pack, "param_in2" )
-			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleG ) ), "osl" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleG, "shadername" ), "MaterialX/mx_swizzle_color_float" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleG, "param_channels" ), "r" )
-			swizzleGSource = arnold.AiNodeGetLink( swizzleG, "param_in" )
-
-			swizzleB = arnold.AiNodeGetLink( pack, "param_in3" )
-			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleB ) ), "osl" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleB, "shadername" ), "MaterialX/mx_swizzle_color_float" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleB, "param_channels" ), "g" )
-			swizzleBSource = arnold.AiNodeGetLink( swizzleG, "param_in" )
-
-			self.assertReferSameNode( swizzleRSource, swizzleGSource )
-			self.assertReferSameNode( swizzleRSource, swizzleBSource )
-
-			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleRSource ) ), "osl" )
-			self.assertEqual( arnold.AiNodeGetStr( swizzleRSource, "shadername" ), "Maths/MixColor" )
+			bSource = arnold.AiNodeGetLink( outputNode, "param_a.b", component )
+			self.assertReferSameNode( rSource, bSource )
+			self.assertEqual( component.value, 1 ) # b is driven by g
 
 	def testLightNames( self ) :
 

commit 6ba124d3742ee87cf8c788a00f27c8dbfce195d2

Arnold ShaderNetworkAlgo : Fix OSL component connections
These were broken in 538b860473f1a07d4f8d9c6458f501e4611a6c02, where we assumed that Arnold handles component connections correctly internally.

The change to `ArnoldRenderTest.testOSLShaders` exposes a bug in Arnold (which results in a 0,0.5,0.25 pixel rather than the expected 0,0.5,0.5 pixel).

We're reverting to our old behaviour of translating component connections ourselves via MaterilaX swizzle and pack shaders until Arnold can provide a fix.

src/GafferArnold/IECoreArnoldPreview/ShaderNetworkAlgo.cpp
@@ -336,6 +336,11 @@ namespace ShaderNetworkAlgo
 
 std::vector<AtNode *> convert( const IECoreScene::ShaderNetwork *shaderNetwork, const std::string &name, const AtNode *parentNode )
 {
+	// \todo: remove this conversion once Arnold supports it natively
+	ShaderNetworkPtr networkCopy = shaderNetwork->copy();
+	IECoreScene::ShaderNetworkAlgo::convertOSLComponentConnections( networkCopy.get() );
+	shaderNetwork = networkCopy.get();
+
 	ShaderMap converted;
 	vector<AtNode *> result;
 	const InternedString output = shaderNetwork->getOutput().shader;

python/GafferArnoldTest/ArnoldRenderTest.py
@@ -1090,9 +1090,9 @@ class ArnoldRenderTest( GafferSceneTest.SceneTestCase ) :
 
 	def testOSLShaders( self ) :
 
-		blue = GafferOSL.OSLShader()
-		blue.loadShader( "Maths/MixColor" )
-		blue["parameters"]["a"].setValue( imath.Color3f( 0, 0, 1 ) )
+		purple = GafferOSL.OSLShader()
+		purple.loadShader( "Maths/MixColor" )
+		purple["parameters"]["a"].setValue( imath.Color3f( 0.5, 0, 1 ) )
 
 		green = GafferOSL.OSLShader()
 		green.loadShader( "Maths/MixColor" )
@@ -1100,7 +1100,9 @@ class ArnoldRenderTest( GafferSceneTest.SceneTestCase ) :
 
 		mix = GafferOSL.OSLShader()
 		mix.loadShader( "Maths/MixColor" )
-		mix["parameters"]["a"].setInput( blue["out"]["out"] )
+		# test component connections
+		mix["parameters"]["a"][2].setInput( purple["out"]["out"][2] )
+		# test color connections
 		mix["parameters"]["b"].setInput( green["out"]["out"] )
 		mix["parameters"]["m"].setValue( 0.5 )
 

python/GafferArnoldTest/IECoreArnoldPreviewTest/RendererTest.py
@@ -412,21 +412,33 @@ class RendererTest( GafferTest.TestCase ) :
 
 			outputNode = arnold.AiNodeGetPtr( arnold.AiNodeLookUpByName( "test" ), "shader" )
 
-			# OSL 1.10+ allows component level connections, in which case we will
-			# get a link to each channel directly.
-			component = arnold.c_int()
-			rSource = arnold.AiNodeGetLink( outputNode, "param_a.r", component )
-			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( rSource ) ), "osl" )
-			self.assertEqual( arnold.AiNodeGetStr( rSource, "shadername" ), "Maths/MixColor" )
-			self.assertEqual( component.value, 2 ) # r is driven by b
+			pack = arnold.AiNodeGetLink( outputNode, "param_a" )
+			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( pack ) ), "osl" )
+			self.assertEqual( arnold.AiNodeGetStr( pack, "shadername" ), "MaterialX/mx_pack_color" )
 
-			gSource = arnold.AiNodeGetLink( outputNode, "param_a.g", component )
-			self.assertReferSameNode( rSource, gSource )
-			self.assertEqual( component.value, 0 ) # g is driven by r
+			swizzleR = arnold.AiNodeGetLink( pack, "param_in1" )
+			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleR ) ), "osl" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleR, "shadername" ), "MaterialX/mx_swizzle_color_float" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleR, "param_channels" ), "b" )
+			swizzleRSource = arnold.AiNodeGetLink( swizzleR, "param_in" )
 
-			bSource = arnold.AiNodeGetLink( outputNode, "param_a.b", component )
-			self.assertReferSameNode( rSource, bSource )
-			self.assertEqual( component.value, 1 ) # b is driven by g
+			swizzleG = arnold.AiNodeGetLink( pack, "param_in2" )
+			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleG ) ), "osl" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleG, "shadername" ), "MaterialX/mx_swizzle_color_float" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleG, "param_channels" ), "r" )
+			swizzleGSource = arnold.AiNodeGetLink( swizzleG, "param_in" )
+
+			swizzleB = arnold.AiNodeGetLink( pack, "param_in3" )
+			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleB ) ), "osl" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleB, "shadername" ), "MaterialX/mx_swizzle_color_float" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleB, "param_channels" ), "g" )
+			swizzleBSource = arnold.AiNodeGetLink( swizzleG, "param_in" )
+
+			self.assertReferSameNode( swizzleRSource, swizzleGSource )
+			self.assertReferSameNode( swizzleRSource, swizzleBSource )
+
+			self.assertEqual( arnold.AiNodeEntryGetName( arnold.AiNodeGetNodeEntry( swizzleRSource ) ), "osl" )
+			self.assertEqual( arnold.AiNodeGetStr( swizzleRSource, "shadername" ), "Maths/MixColor" )
 
 	def testLightNames( self ) :
 