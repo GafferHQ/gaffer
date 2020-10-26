GafferHQ/gaffer

commit ce15fd6ac484a35f0103315433749b124c2c8005

NumericWidget : Support basic math operators
This allows a field's existing value to be modified rather than replaced.
Basic value/operator/value expressions are supported using `+`, `-`,
`/` and `*`. eg: `1+0.5`, `256 / 2` etc.

python/GafferUI/NumericWidget.py
@@ -36,6 +36,10 @@
 ##########################################################################
 
 import math
+import re
+import operator
+
+import six
 
 import IECore
 
@@ -75,7 +79,10 @@ class NumericWidget( GafferUI.TextWidget ) :
 
 	def getValue( self ) :
 
-		return self.__numericType( self.getText() )
+		# Call `fixup()` so that we can always return a valid value,
+		# even if the user is part way through entering an expression.
+		value = self._qtWidget().validator().fixup( self.getText() )
+		return self.__numericType( value )
 
 	## A signal emitted whenever the value has been changed and the user would expect
 	# to see that change reflected in whatever the field controls. Slots should have
@@ -273,15 +280,7 @@ class NumericWidget( GafferUI.TextWidget ) :
 		if self.__numericType is not numericType :
 
 			self.__numericType = numericType
-
-			if self.__numericType is int :
-				validator = QtGui.QIntValidator( self._qtWidget() )
-			else :
-				validator = QtGui.QDoubleValidator( self._qtWidget() )
-				validator.setDecimals( 4 )
-				validator.setNotation( QtGui.QDoubleValidator.StandardNotation )
-
-			self._qtWidget().setValidator( validator )
+			self._qtWidget().setValidator( _ExpressionValidator( self._qtWidget(), self.__numericType ) )
 
 		# update our textual value
 		text = self.__valueToString( value )
@@ -304,3 +303,94 @@ class NumericWidget( GafferUI.TextWidget ) :
 			return
 
 		signal( self, reason )
+
+# A basic validator/evaluator that supports simple maths
+# operators [+-/*] along with standard number validation, eg:
+#   2 + 3
+#   4.4 / 2
+# Qt will call validate/fixup as part of the edit cycle.
+class _ExpressionValidator( QtGui.QValidator ) :
+
+	def __init__( self, parent, numericType ) :
+
+		QtGui.QValidator.__init__( self, parent )
+
+		self.__numericType = numericType
+		if self.__numericType is int :
+			operand = r"-?[0-9]*"
+		else :
+			operand = r"-?[0-9]*\.?[0-9]{0,4}"
+
+		# Captures `( operand1, operator, operand2 )``, with the latter
+		# two being optional. Note that the regex for the operands is
+		# deliberately loose and accepts incomplete text such as "", "-"
+		# and ".". This is dealt with in `validate()` and `fixup()`.
+		self.__expression = re.compile(
+			r"^\s*({operand})\s*(?:([-+*/%])\s*({operand}))?\s*$".format(
+				operand = operand
+			)
+		)
+
+	def fixup( self, text ) :
+
+		match = re.match( self.__expression, text )
+		try :
+			operand1 = self.__numericType( match.group( 1 ) )
+		except :
+			return "0"
+
+		try :
+			operand2 = self.__numericType( match.group( 3 ) )
+		except :
+			# Incomplete expression, return first number
+			return match.group( 1 )
+
+		# Evaluate expression
+
+		op = {
+			"/" : operator.floordiv if self.__numericType is int else operator.truediv,
+			"*" : operator.mul,
+			"+" : operator.add,
+			"-" : operator.sub,
+			"%" : operator.mod
+		}[match.group(2)]
+
+		try :
+			return str( op( operand1, operand2 ) )
+		except ZeroDivisionError :
+			return match.group( 1 )
+
+	def validate( self, text, pos ) :
+
+		# Work around https://bugreports.qt.io/browse/PYSIDE-106. Because
+		# that prevents `fixup()` from working, it stops Qt automatically
+		# fixing `Intermediate` input when editing finishes, which in turn stops
+		# `QLineEdit.editingFinished` from being emitted. We return `Acceptable`
+		# instead of `Intermediate` and then apply `fixup()` ourselves via
+		# `__editingFinished()` and `getValue()`.
+		## \todo Remove once we've moved to GafferHQ/dependencies 2.0.0, which
+		# contains the relevant PySide fix. Include a grace period for folks
+		# building with their own older dependencies.
+		Intermediate = QtGui.QValidator.Acceptable
+
+		match = re.match( self.__expression, text )
+		if match is None :
+			return QtGui.QValidator.Invalid, text, pos
+
+		# Check first operand
+		try :
+			self.__numericType( match.group( 1 ) )
+		except :
+			return (
+				Intermediate if not match.group( 2 ) else QtGui.QValidator.Invalid,
+				text, pos
+			)
+
+		# If operator is present, check second operand
+		if match.group( 2 ) :
+			try :
+				self.__numericType( match.group( 3 ) )
+			except :
+				return Intermediate, text, pos
+
+		return QtGui.QValidator.Acceptable, text, pos

python/GafferUITest/NumericWidgetTest.py
@@ -1,0 +1,123 @@
+##########################################################################
+#
+#  Copyright (c) 2020, Cinesite VFX Ltd. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#      * Redistributions of source code must retain the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided with
+#        the distribution.
+#
+#      * Neither the name of Cinesite VFX Ltd. nor the names of
+#        any other contributors to this software may be used to endorse or
+#        promote products derived from this software without specific prior
+#        written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##########################################################################
+
+import unittest
+import weakref
+
+import GafferTest
+import GafferUI
+import GafferUITest
+
+class NumericWidgetTest( GafferUITest.TestCase ) :
+
+	def testLifespan( self ) :
+
+		w = GafferUI.NumericWidget( 0 )
+		r = weakref.ref( w )
+
+		self.assertTrue( r() is w )
+
+		del w
+
+		self.assertTrue( r() is None )
+
+	def testValueChangedSignal( self ) :
+
+		w = GafferUI.NumericWidget( 1 )
+		signals = GafferTest.CapturingSlot( w.valueChangedSignal() )
+
+		w.setValue( 10 )
+		self.assertEqual( w.getValue(), 10 )
+
+		self.assertEqual( [ s[1] for s in signals ], [ GafferUI.NumericWidget.ValueChangedReason.SetValue ] )
+
+	def testType( self ) :
+
+		w = GafferUI.NumericWidget( 1.0 )
+		w.setText( "2" )
+		self.assertEqual( w.getValue(), 2.0 )
+		self.assertIsInstance( w.getValue(), float )
+
+		w = GafferUI.NumericWidget( 1 )
+		w.setText( "2" )
+		self.assertEqual( w.getValue(), 2 )
+		self.assertIsInstance( w.getValue(), int )
+
+	def testMaths( self ) :
+
+		for text, expected, type_ in (
+			( "3", 3, float ),
+			( " 3", 3, int ),
+			( "-3.1", -3.1, float ),
+			( "-3", -3, int ),
+			( "1+1", 2, int ),
+			( " 1 +1", 2, int ),
+			( "1+ 1", 2, int ),
+			( "1 + 1", 2, int ),
+			( " 1 + 1", 2, int ),
+			( "1 + 1 ", 2, int ),
+			( " 1 + 1 ", 2, int ),
+			( "-1 +2", 1, int ),
+			( "1-4", -3, int ),
+			( "-4+ 2", -2, int ),
+			( "-4+-2", -6, int ),
+			( "1+-2", -1, int ),
+			( "1--1", 2, int ),
+			( " 1 / 2", 0, int ),
+			( "1/2", 0.5, float ),
+			( " 2*3", 6, int ),
+			( "-2 * 3.1", -6.2, float ),
+			( " -2 *3.1 ", -6.2, float ),
+			( "3%2", 1, int ),
+			( "2.5 % 2", 0.5, float ),
+			( "1/0", 1, int ),
+			( "5/0", 5.0, float ),
+			( "03/1", 3.0, float ),
+			( "03/1", 3, int ),
+			( "3/01", 3.0, float ),
+			( "3/01", 3, int ),
+			( "1 / ", 1, int ),
+			( "2 +", 2, int ),
+			( "3.0*", 3.0, float ),
+			( ".", 0.0, float ),
+		) :
+			w = GafferUI.NumericWidget( type_( 0 ) )
+			w._qtWidget().setText( text )
+			self.assertEqual( w.getValue(), expected )
+			self.assertIsInstance( w.getValue(), type_ )
+
+if __name__ == "__main__":
+	unittest.main()

python/GafferUITest/__init__.py
@@ -120,6 +120,7 @@ from .NodeSetEditorTest import NodeSetEditorTest
 from .BackdropNodeGadgetTest import BackdropNodeGadgetTest
 from .StringPlugValueWidgetTest import StringPlugValueWidgetTest
 from .BoolPlugValueWidgetTest import BoolPlugValueWidgetTest
+from .NumericWidgetTest import NumericWidgetTest
 
 if __name__ == "__main__":
 	unittest.main()

commit 7db2a82d3c4c0a0f1c833fb336f4708275238e2d

NumericPlugValueWidget : Don't clobber value with invalid entry
ce15fd6 inadvertently changed `NumericWidget` validation, such that `0` was
returned for invalid input. This resulted in plugs being overwritten with
any time editing finished with invalid text in the widget. This also
had the side-effect that a `NumericPlugValueWidget` representing multiple
plugs with different values would overwrite all plugs to `0` if editing
finished without a change as the default presentation of mixed values is
as an empty string + placeholder text.

This commit reverts user-facing behaviour to that prior to `0.58.2.0`, with
the added benefit that the text field will now be updated to contain the
plug value when editing is finished, rather than the next time the widget
is updated.

python/GafferUI/NumericPlugValueWidget.py
@@ -154,6 +154,10 @@ class NumericPlugValueWidget( GafferUI.PlugValueWidget ) :
 
 	def __valueChanged( self, widget, reason ) :
 
+		if reason == GafferUI.NumericWidget.ValueChangedReason.InvalidEdit :
+			self._updateFromPlugs()
+			return
+
 		if self._editable( canEditAnimation = True ) :
 
 			if not widget.changesShouldBeMerged( self.__lastChangedReason, reason ) :

python/GafferUI/NumericWidget.py
@@ -51,7 +51,7 @@ from Qt import QtGui
 ## \todo Fix bug when pressing up arrow with cursor to left of minus sign
 class NumericWidget( GafferUI.TextWidget ) :
 
-	ValueChangedReason = IECore.Enum.create( "Invalid", "SetValue", "DragBegin", "DragMove", "DragEnd", "Increment", "Edit" )
+	ValueChangedReason = IECore.Enum.create( "Invalid", "SetValue", "DragBegin", "DragMove", "DragEnd", "Increment", "Edit", "InvalidEdit" )
 
 	def __init__( self, value, **kw ) :
 
@@ -264,13 +264,18 @@ class NumericWidget( GafferUI.TextWidget ) :
 
 		assert( widget is self )
 
+		reason = self.ValueChangedReason.Edit
+
 		# In __incrementIndex we temporarily pad with leading
 		# zeroes in order to achieve consistent editing. Revert
 		# back to our standard form now so we don't leave it in
 		# this state.
-		self.setText( self.__valueToString( self.getValue() ) )
+		try :
+			self.setText( self.__valueToString( self.getValue() ) )
+		except ValueError as e :
+			reason = self.ValueChangedReason.InvalidEdit
 
-		self.__emitValueChanged( self.ValueChangedReason.Edit )
+		self.__emitValueChanged( reason )
 
 	def __setValueInternal( self, value, reason ) :
 
@@ -337,7 +342,7 @@ class _ExpressionValidator( QtGui.QValidator ) :
 		try :
 			operand1 = self.__numericType( match.group( 1 ) )
 		except :
-			return "0"
+			return text
 
 		try :
 			operand2 = self.__numericType( match.group( 3 ) )

python/GafferUITest/NumericPlugValueWidgetTest.py
@@ -90,6 +90,30 @@ class NumericPlugValueWidgetTest( GafferUITest.TestCase ) :
 		Gaffer.MetadataAlgo.setReadOnly( n["user"]["i1"], True )
 		self.assertFalse( w.numericWidget().getEditable() )
 
+	def testMixedOrInvalidValuesPreservesExisting( self ) :
+
+		n = Gaffer.Node()
+		n["user"]["i1"] = Gaffer.IntPlug()
+		n["user"]["i2"] = Gaffer.IntPlug()
+		n["user"]["i1"].setValue( 1 )
+		n["user"]["i2"].setValue( 2 )
+
+		w = GafferUI.NumericPlugValueWidget( n["user"]["i1"] )
+		self.assertEqual( w.numericWidget().getValue(), 1 )
+
+		w.numericWidget().setText( "" )
+		w.numericWidget()._qtWidget().editingFinished.emit()
+
+		self.assertEqual( n["user"]["i1"].getValue(), 1 )
+
+		w = GafferUI.NumericPlugValueWidget( n["user"].children() )
+		self.assertEqual( w.numericWidget().getText(), "" )
+
+		w.numericWidget()._qtWidget().editingFinished.emit()
+
+		self.assertEqual( n["user"]["i1"].getValue(), 1 )
+		self.assertEqual( n["user"]["i2"].getValue(), 2 )
+
 	def testFixedCharacterWidth( self ) :
 
 		n = Gaffer.Node()

python/GafferUITest/NumericWidgetTest.py
@@ -112,7 +112,6 @@ class NumericWidgetTest( GafferUITest.TestCase ) :
 			( "1 / ", 1, int ),
 			( "2 +", 2, int ),
 			( "3.0*", 3.0, float ),
-			( ".", 0.0, float ),
 		) :
 			w = GafferUI.NumericWidget( type_( 0 ) )
 			w._qtWidget().setText( text )