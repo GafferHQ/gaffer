88565c28fdd46e253392ad2ba1571a5ab841b1a9
Daniel Dresser <dresserd@gmail.com>
2020-11-10 19:17:19 -0800

    Omit copy from tiles()

--- python/GafferImageTest/MergeTest.py
+++ python/GafferImageTest/MergeTest.py
@@ -718,11 +718,11 @@ class MergeTest( GafferImageTest.ImageTestCase ) :
 		merge["in"][1].setInput( transform["out"] )
 
 		# Precache upstream network, we're only interested in the performance of Merge
-		GafferImage.ImageAlgo.tiles( alphaShuffle["out"] )
-		GafferImage.ImageAlgo.tiles( transform["out"] )
+		GafferImage.ImageAlgo.tiles( alphaShuffle["out"], _copy = False )
+		GafferImage.ImageAlgo.tiles( transform["out"], _copy = False )
 
 		with GafferTest.TestRunner.PerformanceScope() :
-			GafferImage.ImageAlgo.tiles( merge["out"] )
+			GafferImage.ImageAlgo.tiles( merge["out"], _copy = False )
 
 	@unittest.skipIf( GafferTest.inCI(), "Performance not relevant on CI platform" )
 	@GafferTest.TestRunner.PerformanceTestMethod( repeat = 5)
