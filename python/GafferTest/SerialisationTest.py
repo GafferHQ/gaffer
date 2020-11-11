GafferHQ/gaffer

commit a5b6d38c2b78a9b471ebf4453d14bdd089cce10f

Plug : Use `unordered_map` for dirty plugs container
This shaves 6% of runtime off destruction time for a large script.

src/Gaffer/Plug.cpp
@@ -820,7 +820,7 @@ class Plug::DirtyPlugs
 		typedef Graph::vertex_descriptor VertexDescriptor;
 		typedef Graph::edge_descriptor EdgeDescriptor;
 
-		typedef std::map<const Plug *, VertexDescriptor> PlugMap;
+		typedef std::unordered_map<const Plug *, VertexDescriptor> PlugMap;
 
 		// Equivalent to the return type for map::insert - the first
 		// field is the vertex descriptor, and the second field is

commit 6631001410014cb6aafeae9c90080bb7185044c4

Plug : Fix dirty propagation performance regression
This could be triggered by many repeated emissions with few or no dirty plugs. Unfortunately those can be triggered during serialisation by the creation of unnecessary temporary plugs, and also by destruction of many Gadgets because `~GraphComponent` contains a DirtyPropagationScope. We should reconsider those anyway, but this fix gets us back on par or better than 0.57 performance in the new test. The regression was originally introduced in a5b6d38c2b78a9b471ebf4453d14bdd089cce10f which was first released in 0.58.0.0.

src/Gaffer/Plug.cpp
@@ -927,7 +927,24 @@ class Plug::DirtyPlugs
 			}
 
 			m_graph.clear();
-			m_plugs.clear();
+			// Using swap instead of `clear()` because libstdc++'s `clear()`
+			// method is linear in the number of buckets, _not_ in `size()` as
+			// required by the standard :
+			//
+			// - https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67922
+			// - https://cplusplus.github.io/LWG/issue2550
+			//
+			// This caused severe performance regressions when making many
+			// small calls to `emit()` after previously growing `m_plugs` to
+			// a large size.
+			//
+			/// \todo Consider `m_plugs.erase( m_plugs.begin(), m_plugs.end() )`
+			/// as an alternative. This appears to be slightly slower for the
+			/// many-small-emits case, but may provide benefits through reduced
+			/// allocation for several large emissions. Also investigate the
+			/// root causes of the many-small-emits cases.
+			PlugMap emptyPlugs;
+			m_plugs.swap( emptyPlugs );
 		}
 
 		Graph m_graph;

src/GafferModule/ContextProcessorBinding.cpp
@@ -107,6 +107,7 @@ class SetupBasedNodeSerialiser : public NodeSerialiser
 
 		// Add a call to `setup()` to recreate the plugs.
 
+		/// \todo Avoid creating a temporary plug.
 		PlugPtr plug = inPlug->createCounterpart( g_inPlugName, Plug::In );
 		plug->setFlags( Plug::Dynamic, false );
 

src/GafferModule/DotBinding.cpp
@@ -97,6 +97,7 @@ class DotSerialiser : public NodeSerialiser
 
 		// Add a call to `setup()` to recreate the plugs.
 
+		/// \todo Avoid creating a temporary plug.
 		PlugPtr plug = dot->inPlug()->createCounterpart( "in", Plug::In );
 		plug->setFlags( Plug::Dynamic, false );
 

src/GafferModule/SubGraphBinding.cpp
@@ -129,6 +129,7 @@ class BoxIOSerialiser : public NodeSerialiser
 
 		// Add a call to `setup()` to recreate the plugs.
 
+		/// \todo Avoid creating a temporary plug.
 		PlugPtr plug = boxIO->plug()->createCounterpart( boxIO->plug()->getName(), Plug::In );
 		plug->setFlags( Plug::Dynamic, false );
 

src/GafferModule/SwitchBinding.cpp
@@ -100,6 +100,7 @@ class SwitchSerialiser : public NodeSerialiser
 
 		// Add a call to `setup()` to recreate the plugs.
 
+		/// \todo Avoid creating a temporary plug.
 		PlugPtr plug = sw->inPlugs()->getChild<Plug>( 0 )->createCounterpart( "in", Plug::In );
 		if( IECore::runTimeCast<const NameSwitch>( sw ) )
 		{

python/GafferTest/SerialisationTest.py
@@ -35,6 +35,7 @@
 ##########################################################################
 
 import imath
+import unittest
 import six
 
 import IECore
@@ -273,6 +274,31 @@ class SerialisationTest( GafferTest.TestCase ) :
 			Gaffer.Serialisation.objectFromBase64( b ),
 			o
 		)
+
+	@unittest.skipIf( GafferTest.inCI(), "Performance not relevant on CI platform" )
+	@GafferTest.TestRunner.PerformanceTestMethod( repeat = 1 )
+	def testSwitchPerformance( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		def build( maxDepth, upstreamNode = None, depth = 0 ) :
+
+			node = Gaffer.Switch()
+			node.setup( Gaffer.V3iPlug() )
+			if upstreamNode is not None :
+				node["in"][0].setInput( upstreamNode["out"] )
+
+			script.addChild( node )
+
+			if depth < maxDepth :
+				build( maxDepth, node, depth + 1 )
+				build( maxDepth, node, depth + 1 )
+
+		with Gaffer.DirtyPropagationScope() :
+			build( 13 )
+
+		with GafferTest.TestRunner.PerformanceScope() :
+			script.serialise()
 
 if __name__ == "__main__":
 	unittest.main()