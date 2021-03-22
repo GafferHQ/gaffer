edd20daead9f90781294e6a9e131f0cf96c8c856
John Haddon <thehaddonyoof@gmail.com>
2021-03-22 12:17:34 +0000

    installDependencies.sh : Defer to CI dependencies install script
    The CI script is the source of truth for which dependencies we make the
    official builds with, and we don't want to duplicate that information
    elsewhere.

--- config/installDependencies.sh
+++ config/installDependencies.sh
@@ -55,14 +55,4 @@ buildDir=${1:-"build/gaffer-$gafferMilestoneVersion.$gafferMajorVersion.$gafferM
 
 # Get the prebuilt dependencies package and unpack it into the build directory
 
-dependenciesVersion="3.0.0"
-dependenciesVersionSuffix="a2"
-dependenciesPythonVersion="2"
-dependenciesFileName="gafferDependencies-$dependenciesVersion-Python$dependenciesPythonVersion-$platform.tar.gz"
-downloadURL="https://github.com/GafferHQ/dependencies/releases/download/$dependenciesVersion$dependenciesVersionSuffix/$dependenciesFileName"
-
-echo "Downloading dependencies \"$downloadURL\""
-curl -L $downloadURL > $dependenciesFileName
-
-mkdir -p $buildDir
-tar xf $dependenciesFileName -C $buildDir --strip-components=1
+.github/workflows/main/installDependencies.py --dependenciesDir "$buildDir"
