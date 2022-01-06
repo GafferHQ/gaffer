1d7f68b97d48fec4e7f940037bf5cd086afadc0e
John Haddon <thehaddonyoof@gmail.com>
2022-01-06 15:05:49 +0000

    RampPlugValueWidget : Use vertical layout for color ramps
    This makes better use of space with the new embedded colour chooser filling all the available width.

--- python/GafferUI/RampPlugValueWidget.py
+++ python/GafferUI/RampPlugValueWidget.py
@@ -78,7 +78,10 @@ class RampPlugValueWidget( GafferUI.PlugValueWidget ) :
 			self.__lastPositionChangedReason = None
 			self.__positionsMergeGroupId = 0
 
-			with GafferUI.ListContainer( GafferUI.ListContainer.Orientation.Horizontal, spacing = 4 ) :
+			with GafferUI.ListContainer(
+				orientation = GafferUI.ListContainer.Orientation.Horizontal if isinstance( plug.pointYPlug( 0 ), Gaffer.FloatPlug ) else GafferUI.ListContainer.Orientation.Vertical,
+				spacing = 4
+			) :
 
 				self.__positionLabel = GafferUI.LabelPlugValueWidget(
 					plug.pointXPlug( 0 ),
