96204b15c1d33ec19c5ade6536974d11ddf99d24
John Haddon <thehaddonyoof@gmail.com>
2022-01-31 15:00:08 +0000

    Random : Remove inaccurate comment

--- include/Gaffer/Random.h
+++ include/Gaffer/Random.h
@@ -46,7 +46,6 @@ namespace Gaffer
 
 IE_CORE_FORWARDDECLARE( StringPlug )
 
-/// Base class for nodes which generate random values based on Context values.
 class GAFFER_API Random : public ComputeNode
 {
 
