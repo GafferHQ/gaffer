b4b31097de7c5771d08d009ab6a129cca0bc8a17
John Haddon <thehaddonyoof@gmail.com>
2022-02-08 10:22:46 +0000

    CI : Replace Arnold 7.0.0.0 with Arnold 7.0.0.2
    This allows us to re-enable some previously disabled tests.

--- python/IECoreArnoldTest/RendererTest.py
+++ python/IECoreArnoldTest/RendererTest.py
@@ -58,12 +58,6 @@ class RendererTest( GafferTest.TestCase ) :
 
 		GafferTest.TestCase.setUp( self )
 
-		if GafferTest.inCI() and [ int( v ) for v in arnold.AiGetVersion() ] == [ 7, 0, 0, 0 ] :
-			# Skipping due to multiple-render-session bugs in Arnold 7.0.0.0. We have
-			# reported the bug with a small repro and it is confirmed as fixed for the
-			# upcoming 7.0.0.1 release.
-			self.skipTest( "Exposes Arnold bug" )
-
 	def assertReferSameNode( self, a, b ):
 		self.assertEqual( arnold.addressof( a.contents ), arnold.addressof( b.contents ) )
 
