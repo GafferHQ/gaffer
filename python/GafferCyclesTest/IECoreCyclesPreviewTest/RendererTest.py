GafferHQ/gaffer

commit 9f503aae2b0ce10a0d1838436c8ea97d57ff948a

IECoreCycles : Fix primitive variable interpolation bugs
The rules for converting from `PrimitiveVariable::Interpolation` to `ccl::AttributeElement` are subtle and specific to each type of geometry, so we make that the responsibility of the individual type-specific converters rather than trying to deal with it in AttributeAlgo.

Among other things, this fixes the handling of `vertex Cd` in #4849. In fact, that issue was using `Varying` interpolation, which while it should have been equivalent to `Vertex` was instead being treated as equivalent to `FaceVarying`.

src/GafferCycles/IECoreCyclesPreview/MeshAlgo.cpp
@@ -612,9 +612,26 @@ ccl::Mesh *convertCommon( const IECoreScene::MeshPrimitive *mesh )
 	}
 
 	// Finally, do a generic conversion of anything that remains.
-	for( PrimitiveVariableMap::iterator it = variablesToConvert.begin(), eIt = variablesToConvert.end(); it != eIt; ++it )
+	for( const auto &[name, variable] : variablesToConvert )
 	{
-		AttributeAlgo::convertPrimitiveVariable( it->first, it->second, attributes );
+		switch( variable.interpolation )
+		{
+			case PrimitiveVariable::Constant :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_MESH );
+				break;
+			case PrimitiveVariable::Uniform :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_FACE );
+				break;
+			case PrimitiveVariable::Vertex :
+			case PrimitiveVariable::Varying :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_VERTEX );
+				break;
+			case PrimitiveVariable::FaceVarying :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_CORNER );
+				break;
+			default :
+				break;
+		}
 	}
 	return cmesh;
 }

python/GafferCyclesTest/IECoreCyclesPreviewTest/RendererTest.py
@@ -600,10 +600,286 @@ class RendererTest( GafferTest.TestCase ) :
 					"Primitive variable \"test\" has unsupported type \"{}\"".format( data.typeName() )
 				)
 
+	def __testPrimitiveVariableInterpolation( self, primitive, primitiveVariable, expectedPixels, maxDifference = 0.0 ) :
+
+		renderer = GafferScene.Private.IECoreScenePreview.Renderer.create(
+			"Cycles",
+			GafferScene.Private.IECoreScenePreview.Renderer.RenderType.Batch,
+		)
+
+		# Frame the primitive so it fills the entire image.
+
+		renderer.camera(
+			"testCamera",
+			IECoreScene.Camera(
+				parameters = {
+					"resolution" : imath.V2i( 100, 100 ),
+					"projection" : "orthographic",
+					"screenWindow" : imath.Box2f( imath.V2f( -0.5 ), imath.V2f( 0.5 ) )
+				}
+			),
+			renderer.attributes( IECore.CompoundObject() )
+		)
+		renderer.option( "camera", IECore.StringData( "testCamera" ) )
+
+		fileName = os.path.join( self.temporaryDirectory(), "test.exr" )
+		renderer.output(
+			"testOutput",
+			IECoreScene.Output(
+				fileName,
+				"exr",
+				"rgba",
+				{}
+			)
+		)
+
+		# Render with a constant shader showing the primitive variable
+
+		shader = IECoreScene.ShaderNetwork(
+			shaders = {
+				"output" : IECoreScene.Shader( "principled_bsdf", "cycles:surface", { "base_color" : imath.Color3f( 0 ) } ),
+				"attribute" : IECoreScene.Shader( "attribute", "cycles:shader", { "attribute" : primitiveVariable } ),
+			},
+			connections = [
+				( ( "attribute", "color" ), ( "output", "emission" ) ),
+			],
+			output = "output",
+		)
+
+		primitiveHandle = renderer.object( "primitive", primitive, renderer.attributes( IECore.CompoundObject( { "cycles:surface" : shader } ) ) )
+		primitiveHandle.transform( imath.M44f().translate( imath.V3f( 0, 0, 1 ) ) )
+
+		renderer.render()
+
+		# Check we got what we expected
+
+		image = IECore.Reader.create( fileName ).read()
+
+		for uv, expectedColor in expectedPixels.items() :
+
+			if isinstance( expectedColor, imath.V3f ) :
+				expectedColor = imath.Color4f( expectedColor[0], expectedColor[1], expectedColor[2], 1 )
+
+			color = self.__colorAtUV( image, uv )
+			if maxDifference :
+				for i in range( 0, 4 ) :
+					self.assertAlmostEqual( color[i], expectedColor[i], delta = maxDifference )
+			else :
+				self.assertEqual( color, expectedColor )
+
+	def testMeshPrimitiveVariableInterpolation( self ) :
+
+		# Plane with 3x3 faces and primitive variables with various interpolations.
+
+		plane = IECoreScene.MeshPrimitive.createPlane(
+			imath.Box2f( imath.V2f( -0.5 ), imath.V2f( 0.5 ) ),
+			imath.V2i( 3 )
+		)
+
+		plane["uniformColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Uniform,
+			IECore.Color3fVectorData( [
+				imath.Color3f( i, i, i )
+				for i in range( 0, plane.variableSize( IECoreScene.PrimitiveVariable.Interpolation.Uniform ) )
+			] )
+		)
+
+		plane["vertexColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Vertex,
+			IECore.Color3fVectorData( [
+				imath.Color3f( uv.x, uv.y, 0 ) for uv in plane["uv"].data
+			] )
+		)
+
+		plane["varyingColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Varying,
+			plane["vertexColor"].data
+		)
+
+		# Image UV coordinates of the center of particular faces.
+
+		topLeft = imath.V2f( 1/6.0, 1/6.0 )
+		topCenter = imath.V2f( 0.5, 1/6.0 )
+		center = imath.V2f( 0.5, 0.5 )
+		bottomRight = imath.V2f( 5/6.0 )
+
+		# Tests for each of the primitive variables.
+
+		self.__testPrimitiveVariableInterpolation(
+			plane, "uniformColor",
+			{
+				topLeft : imath.Color4f( 0, 0, 0, 1 ),
+				topCenter : imath.Color4f( 1, 1, 1, 1 ),
+				center : imath.Color4f( 4, 4, 4, 1 ),
+				bottomRight : imath.Color4f( 8, 8, 8, 1 ),
+			}
+		)
+
+		self.__testPrimitiveVariableInterpolation(
+			plane, "vertexColor",
+			{
+				topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
+				topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
+				center : imath.Color4f( center.x, center.y, 0, 1 ),
+				bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
+			},
+			maxDifference = 0.01
+		)
+
+		self.__testPrimitiveVariableInterpolation(
+			plane, "varyingColor",
+			{
+				topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
+				topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
+				center : imath.Color4f( center.x, center.y, 0, 1 ),
+				bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
+			},
+			maxDifference = 0.01
+		)
+
+	def testPointsPrimitiveVariableInterpolation( self ) :
+
+		# 3 points diagonally
+
+		points = IECoreScene.PointsPrimitive(
+			IECore.V3fVectorData( [ imath.V3f( -0.5, 0.5, 0 ), imath.V3f( 0 ), imath.V3f( 0.5, -0.5, 0 ) ] )
+		)
+		points["width"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Constant,
+			IECore.FloatData( 0.1 )
+		)
+
+		points["uniformColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Uniform,
+			IECore.Color3fVectorData( [ imath.Color3f( 1, 0.5, 0.25 ) ] )
+		)
+
+		points["vertexColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Vertex,
+			IECore.Color3fVectorData( [
+				imath.Color3f( p.x + 0.5, p.y + 0.5, 0 ) for p in points["P"].data
+			] )
+		)
+
+		points["varyingColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Varying,
+			points["vertexColor"].data
+		)
+
+		# Image UV coordinates of each point
+
+		bottomLeft = imath.V2f( 0, 1 )
+		center = imath.V2f( 0.5 )
+		topRight = imath.V2f( 1, 0 )
+
+		# Tests for each of the primitive variables.
+
+		## \todo Fix and enable. As far as I can tell, we're passing the data
+		# to Cycles correctly, but it just doesn't make it through to the shader.
+		if False :
+			self.__testPrimitiveVariableInterpolation(
+				points, "uniformColor",
+				{
+					bottomLeft : points["uniformColor"].data[0],
+					center : points["uniformColor"].data[0],
+					topRight : points["uniformColor"].data[0],
+				}
+			)
+
+		self.__testPrimitiveVariableInterpolation(
+			points, "vertexColor",
+			{
+				bottomLeft : imath.Color4f( 0, 1, 0, 1 ),
+				center : imath.Color4f( 0.5, 0.5, 0, 1 ),
+				topRight : imath.Color4f( 1, 0, 0, 1 ),
+			}
+		)
+
+		self.__testPrimitiveVariableInterpolation(
+			points, "varyingColor",
+			{
+				bottomLeft : imath.Color4f( 0, 1, 0, 1 ),
+				center : imath.Color4f( 0.5, 0.5, 0, 1 ),
+				topRight : imath.Color4f( 1, 0, 0, 1 ),
+			}
+		)
+
+	def testCurvesPrimitiveVariableInterpolation( self ) :
+
+		# 3 vertical curves
+
+		curves = IECoreScene.CurvesPrimitive(
+			verticesPerCurve = IECore.IntVectorData( [ 2, 2, 2 ] ),
+			p = IECore.V3fVectorData( [
+				imath.V3f( -0.5, 0.5, 0 ), imath.V3f( -0.5, -0.5, 0 ),
+				imath.V3f( 0, 0.5, 0 ), imath.V3f( 0, -0.5, 0 ),
+				imath.V3f( 0.5, 0.5, 0 ), imath.V3f( 0.5, -0.5, 0 ),
+			] )
+		)
+		curves["width"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Constant,
+			IECore.FloatData( 0.1 )
+		)
+
+		curves["uniformColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Uniform,
+			IECore.Color3fVectorData( [ imath.Color3f( i + 1 ) for i in range( 0, 3 ) ] )
+		)
+
+		curves["vertexColor"] = IECoreScene.PrimitiveVariable(
+			IECoreScene.PrimitiveVariable.Interpolation.Vertex,
+			IECore.Color3fVectorData( [
+				imath.Color3f( p.x + 0.5, p.y + 0.5, 0 ) for p in curves["P"].data
+			] )
+		)
+
+		# Image UV coordinates of top and bottom of each curve
+
+		leftTop = imath.V2f( 0, 0 )
+		leftBottom = imath.V2f( 0, 1 )
+		centerTop = imath.V2f( 0.5, 0 )
+		centerBottom = imath.V2f( 0.5, 1 )
+		rightTop = imath.V2f(1, 0 )
+		rightBottom = imath.V2f( 1, 1 )
+
+		# Tests for each of the primitive variables.
+
+		self.__testPrimitiveVariableInterpolation(
+			curves, "uniformColor",
+			{
+				leftTop : curves["uniformColor"].data[0],
+				leftBottom : curves["uniformColor"].data[0],
+				centerTop : curves["uniformColor"].data[1],
+				centerBottom : curves["uniformColor"].data[1],
+				rightTop : curves["uniformColor"].data[2],
+				rightBottom : curves["uniformColor"].data[2],
+			}
+		)
+
+		self.__testPrimitiveVariableInterpolation(
+			curves, "vertexColor",
+			{
+				## \todo I would expect the top of each curve to
+				# have the highest Y value (and therefore a
+				# green value of 1). But it's the other way around.
+				# Investigate possible camera inversion bug.
+				leftTop : imath.Color4f( 0, 0, 0, 1 ),
+				leftBottom : imath.Color4f( 0, 1, 0, 1 ),
+				centerTop : imath.Color4f( 0.5, 0, 0, 1 ),
+				centerBottom : imath.Color4f( 0.5, 1, 0, 1 ),
+				rightTop : imath.Color4f( 1, 0, 0, 1 ),
+				rightBottom : imath.Color4f( 1, 1, 0, 1 ),
+			},
+			maxDifference = 0.01
+		)
+
 	def __colorAtUV( self, image, uv ) :
 
 		dimensions = image.dataWindow.size() + imath.V2i( 1 )
-		i = dimensions.x * int( dimensions.y * uv.y ) + int( dimensions.x * uv.x )
+
+		ix = int( uv.x * ( dimensions.x - 1 ) )
+		iy = int( uv.y * ( dimensions.y - 1 ) )
+		i = iy * dimensions.x + ix
 
 		return imath.Color4f( image["R"][i], image["G"][i], image["B"][i], image["A"][i] )
 

include/GafferCycles/IECoreCyclesPreview/AttributeAlgo.h
@@ -52,7 +52,7 @@ namespace AttributeAlgo
 
 /// Converts a primitive variable to a ccl::Attribute inside of a ccl::AttributeSet
 /// \todo Move to GeometryAlgo.
-IECORECYCLES_API void convertPrimitiveVariable( const std::string &name, const IECoreScene::PrimitiveVariable &primitiveVariable, ccl::AttributeSet &attributes );
+IECORECYCLES_API void convertPrimitiveVariable( const std::string &name, const IECoreScene::PrimitiveVariable &primitiveVariable, ccl::AttributeSet &attributes, ccl::AttributeElement attributeElement );
 
 } // namespace AttributeAlgo
 

src/GafferCycles/IECoreCyclesPreview/AttributeAlgo.cpp
@@ -112,7 +112,7 @@ namespace IECoreCycles
 namespace AttributeAlgo
 {
 
-void convertPrimitiveVariable( const std::string &name, const IECoreScene::PrimitiveVariable &primitiveVariable, ccl::AttributeSet &attributes )
+void convertPrimitiveVariable( const std::string &name, const IECoreScene::PrimitiveVariable &primitiveVariable, ccl::AttributeSet &attributes, ccl::AttributeElement attributeElement )
 {
 	// Create attribute.
 
@@ -123,34 +123,7 @@ void convertPrimitiveVariable( const std::string &name, const IECoreScene::Primi
 		return;
 	}
 
-	ccl::AttributeElement celem = ccl::ATTR_ELEMENT_NONE;
-	switch( primitiveVariable.interpolation )
-	{
-		case PrimitiveVariable::Constant :
-			celem = ccl::ATTR_ELEMENT_MESH;
-			break;
-		case PrimitiveVariable::Vertex :
-			if( attributes.geometry->geometry_type == ccl::Geometry::HAIR )
-			{
-				celem = ccl::ATTR_ELEMENT_CURVE_KEY;
-			}
-			else
-			{
-				celem = ccl::ATTR_ELEMENT_VERTEX;
-			}
-			break;
-		case PrimitiveVariable::Varying :
-		case PrimitiveVariable::FaceVarying :
-			celem = ccl::ATTR_ELEMENT_CORNER;
-			break;
-		case PrimitiveVariable::Uniform :
-			celem = ccl::ATTR_ELEMENT_FACE;
-			break;
-		default :
-			break;
-	}
-
-	ccl::Attribute *attr = attributes.add( ccl::ustring( name.c_str() ), ctype, celem );
+	ccl::Attribute *attr = attributes.add( ccl::ustring( name.c_str() ), ctype, attributeElement );
 
 	// Tag as a standard attribute if possible. Note that we don't use `AttributeSet::add( AttributeStandard )`
 	// because that crashes for certain combinations of geometry type and attribute. But some of those "crashy"

src/GafferCycles/IECoreCyclesPreview/CurvesAlgo.cpp
@@ -110,9 +110,24 @@ ccl::Hair *convertCommon( const IECoreScene::CurvesPrimitive *curve )
 	variablesToConvert.erase( "P" );
 	variablesToConvert.erase( "width" );
 
-	for( PrimitiveVariableMap::iterator it = variablesToConvert.begin(), eIt = variablesToConvert.end(); it != eIt; ++it )
+	for( const auto &[name, variable] : variablesToConvert )
 	{
-		AttributeAlgo::convertPrimitiveVariable( it->first, it->second, hair->attributes );
+		switch( variable.interpolation )
+		{
+			case PrimitiveVariable::Constant :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, hair->attributes, ccl::ATTR_ELEMENT_OBJECT );
+				break;
+			case PrimitiveVariable::Uniform :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, hair->attributes, ccl::ATTR_ELEMENT_CURVE );
+				break;
+			case PrimitiveVariable::Vertex :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, hair->attributes, ccl::ATTR_ELEMENT_CURVE_KEY );
+				break;
+			default :
+				// Varying and FaceVarying define values at the end of each cubic curve
+				// segment. Not supported by Cycles.
+				break;
+		}
 	}
 	return hair;
 }

src/GafferCycles/IECoreCyclesPreview/PointsAlgo.cpp
@@ -113,10 +113,24 @@ ccl::PointCloud *convertCommon( const IECoreScene::PointsPrimitive *points )
 	// Convert primitive variables. P is done, and width/radius if found (removed above).
 	variablesToConvert.erase( "P" );
 
-	for( PrimitiveVariableMap::iterator it = variablesToConvert.begin(), eIt = variablesToConvert.end(); it != eIt; ++it )
+	for( const auto &[name, variable] : variablesToConvert )
 	{
-		AttributeAlgo::convertPrimitiveVariable( it->first, it->second, pointcloud->attributes );
+		switch( variable.interpolation )
+		{
+			case PrimitiveVariable::Constant :
+			case PrimitiveVariable::Uniform :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, pointcloud->attributes, ccl::ATTR_ELEMENT_OBJECT );
+				break;
+			case PrimitiveVariable::Vertex :
+			case PrimitiveVariable::Varying :
+			case PrimitiveVariable::FaceVarying :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, pointcloud->attributes, ccl::ATTR_ELEMENT_VERTEX );
+				break;
+			default :
+				break;
+		}
 	}
+
 	return pointcloud;
 }
 

src/GafferCycles/IECoreCyclesPreview/SphereAlgo.cpp
@@ -83,11 +83,20 @@ ccl::PointCloud *convertCommon( const IECoreScene::SpherePrimitive *sphere )
 	pointcloud->reserve( 1 );
 	pointcloud->add_point( ccl::make_float3( 0.0f, 0.0f, 0.0f ), sphere->radius(), 0);
 
-	PrimitiveVariableMap variablesToConvert = sphere->variables;
-	for( PrimitiveVariableMap::iterator it = variablesToConvert.begin(), eIt = variablesToConvert.end(); it != eIt; ++it )
+	for( const auto &[name, variable] : sphere->variables )
 	{
-		AttributeAlgo::convertPrimitiveVariable( it->first, it->second, pointcloud->attributes );
+		switch( variable.interpolation )
+		{
+			case PrimitiveVariable::Constant :
+			case PrimitiveVariable::Uniform :
+				AttributeAlgo::convertPrimitiveVariable( name, variable, pointcloud->attributes, ccl::ATTR_ELEMENT_OBJECT );
+			default :
+				// Other interpolation types define values for four corners
+				// of a quadric shape. Not supported by Cycle's point cloud.
+				break;
+		}
 	}
+
 	return pointcloud;
 }
 

commit f26ebdd51cbbbba65f4d02139991853f8313d087

MeshAlgo : Fix conversion of primitive variables on subdiv meshes
I broke this in 9f503aae2b0ce10a0d1838436c8ea97d57ff948a, but it went unnoticed until I broke it further by removing the custom UV conversion in d608fc3bb6e66d0cbcdbfcdd505e08a930574b5c.

src/GafferCycles/IECoreCyclesPreview/MeshAlgo.cpp
@@ -165,6 +165,7 @@ ccl::Mesh *convertCommon( const IECoreScene::MeshPrimitive *mesh )
 
 	// Convert primitive variables.
 
+	ccl::AttributeSet &attributes = cmesh->get_subdivision_type() != ccl::Mesh::SUBDIVISION_NONE ? cmesh->subd_attributes : cmesh->attributes;
 	for( const auto &[name, variable] : mesh->variables )
 	{
 		if( name == "P" )
@@ -175,17 +176,17 @@ ccl::Mesh *convertCommon( const IECoreScene::MeshPrimitive *mesh )
 		switch( variable.interpolation )
 		{
 			case PrimitiveVariable::Constant :
-				GeometryAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_MESH );
+				GeometryAlgo::convertPrimitiveVariable( name, variable, attributes, ccl::ATTR_ELEMENT_MESH );
 				break;
 			case PrimitiveVariable::Uniform :
-				GeometryAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_FACE );
+				GeometryAlgo::convertPrimitiveVariable( name, variable, attributes, ccl::ATTR_ELEMENT_FACE );
 				break;
 			case PrimitiveVariable::Vertex :
 			case PrimitiveVariable::Varying :
-				GeometryAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_VERTEX );
+				GeometryAlgo::convertPrimitiveVariable( name, variable, attributes, ccl::ATTR_ELEMENT_VERTEX );
 				break;
 			case PrimitiveVariable::FaceVarying :
-				GeometryAlgo::convertPrimitiveVariable( name, variable, cmesh->attributes, ccl::ATTR_ELEMENT_CORNER );
+				GeometryAlgo::convertPrimitiveVariable( name, variable, attributes, ccl::ATTR_ELEMENT_CORNER );
 				break;
 			default :
 				break;

python/GafferCyclesTest/IECoreCyclesPreviewTest/RendererTest.py
@@ -728,67 +728,71 @@ class RendererTest( GafferTest.TestCase ) :
 
 		# Tests for each of the primitive variables.
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "constantColor",
-			{
-				topLeft : plane["constantColor"].data.value,
-				topCenter : plane["constantColor"].data.value,
-				center : plane["constantColor"].data.value,
-				bottomRight : plane["constantColor"].data.value,
-			}
-		)
+		for interpolation in [ "linear", "catmullClark" ] :
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "constantInt",
-			{
-				topLeft : plane["constantInt"].data.value,
-				topCenter : plane["constantInt"].data.value,
-				center : plane["constantInt"].data.value,
-				bottomRight : plane["constantInt"].data.value,
-			}
-		)
+			plane.setInterpolation( interpolation )
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "uniformColor",
-			{
-				topLeft : imath.Color4f( 0, 0, 0, 1 ),
-				topCenter : imath.Color4f( 1, 1, 1, 1 ),
-				center : imath.Color4f( 4, 4, 4, 1 ),
-				bottomRight : imath.Color4f( 8, 8, 8, 1 ),
-			}
-		)
+			self.__testPrimitiveVariableInterpolation(
+				plane, "constantColor",
+				{
+					topLeft : plane["constantColor"].data.value,
+					topCenter : plane["constantColor"].data.value,
+					center : plane["constantColor"].data.value,
+					bottomRight : plane["constantColor"].data.value,
+				}
+			)
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "uniformIndexedColor",
-			{
-				topLeft : imath.Color4f( 1, 0, 0, 1 ),
-				topCenter : imath.Color4f( 0, 1, 0, 1 ),
-				center : imath.Color4f( 1, 0, 0, 1 ),
-				bottomRight : imath.Color4f( 1, 0, 0, 1 ),
-			}
-		)
+			self.__testPrimitiveVariableInterpolation(
+				plane, "constantInt",
+				{
+					topLeft : plane["constantInt"].data.value,
+					topCenter : plane["constantInt"].data.value,
+					center : plane["constantInt"].data.value,
+					bottomRight : plane["constantInt"].data.value,
+				}
+			)
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "vertexColor",
-			{
-				topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
-				topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
-				center : imath.Color4f( center.x, center.y, 0, 1 ),
-				bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
-			},
-			maxDifference = 0.01
-		)
+			self.__testPrimitiveVariableInterpolation(
+				plane, "uniformColor",
+				{
+					topLeft : imath.Color4f( 0, 0, 0, 1 ),
+					topCenter : imath.Color4f( 1, 1, 1, 1 ),
+					center : imath.Color4f( 4, 4, 4, 1 ),
+					bottomRight : imath.Color4f( 8, 8, 8, 1 ),
+				}
+			)
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "varyingColor",
-			{
-				topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
-				topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
-				center : imath.Color4f( center.x, center.y, 0, 1 ),
-				bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
-			},
-			maxDifference = 0.01
-		)
+			self.__testPrimitiveVariableInterpolation(
+				plane, "uniformIndexedColor",
+				{
+					topLeft : imath.Color4f( 1, 0, 0, 1 ),
+					topCenter : imath.Color4f( 0, 1, 0, 1 ),
+					center : imath.Color4f( 1, 0, 0, 1 ),
+					bottomRight : imath.Color4f( 1, 0, 0, 1 ),
+				}
+			)
+
+			self.__testPrimitiveVariableInterpolation(
+				plane, "vertexColor",
+				{
+					topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
+					topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
+					center : imath.Color4f( center.x, center.y, 0, 1 ),
+					bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
+				},
+				maxDifference = 0.01
+			)
+
+			self.__testPrimitiveVariableInterpolation(
+				plane, "varyingColor",
+				{
+					topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
+					topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
+					center : imath.Color4f( center.x, center.y, 0, 1 ),
+					bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
+				},
+				maxDifference = 0.01
+			)
 
 	def testPointsPrimitiveVariableInterpolation( self ) :
 
@@ -997,16 +1001,19 @@ class RendererTest( GafferTest.TestCase ) :
 			plane["uv"].interpolation, IECoreScene.PrimitiveVariable.Interpolation.FaceVarying
 		)
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "uv",
-			{
-				topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
-				topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
-				center : imath.Color4f( center.x, center.y, 0, 1 ),
-				bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
-			},
-			maxDifference = 0.01
-		)
+		for interpolation in [ "linear", "catmullClark" ] :
+
+			plane.setInterpolation( interpolation )
+			self.__testPrimitiveVariableInterpolation(
+				plane, "uv",
+				{
+					topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
+					topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
+					center : imath.Color4f( center.x, center.y, 0, 1 ),
+					bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
+				},
+				maxDifference = 0.01
+			)
 
 		# Test Vertex
 
@@ -1020,16 +1027,19 @@ class RendererTest( GafferTest.TestCase ) :
 			plane["uv"].interpolation, IECoreScene.PrimitiveVariable.Interpolation.Vertex
 		)
 
-		self.__testPrimitiveVariableInterpolation(
-			plane, "uv",
-			{
-				topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
-				topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
-				center : imath.Color4f( center.x, center.y, 0, 1 ),
-				bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
-			},
-			maxDifference = 0.01
-		)
+		for interpolation in [ "linear", "catmullClark" ] :
+
+			plane.setInterpolation( interpolation )
+			self.__testPrimitiveVariableInterpolation(
+				plane, "uv",
+				{
+					topLeft : imath.Color4f( topLeft.x, topLeft.y, 0, 1 ),
+					topCenter : imath.Color4f( topCenter.x, topCenter.y, 0, 1 ),
+					center : imath.Color4f( center.x, center.y, 0, 1 ),
+					bottomRight : imath.Color4f( bottomRight.x, bottomRight.y, 0, 1 ),
+				},
+				maxDifference = 0.01
+			)
 
 	def __colorAtUV( self, image, uv ) :
 