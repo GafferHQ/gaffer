cac9d6fe99d912b396c2bbcd069d9f550c82869c
John Haddon <thehaddonyoof@gmail.com>
2022-11-30 16:06:58 +0000

    fixup! TestCase : Return `pathlib.Path` from `temporaryDirectory()`

--- python/GafferTest/HiddenFilePathFilterTest.py
+++ python/GafferTest/HiddenFilePathFilterTest.py
@@ -46,13 +46,13 @@ class HiddenFilePathFilterTest( GafferTest.TestCase ) :
 
 	def test( self ) :
 
-		hiddenFile = Gaffer.FileSystemPath( self.temporaryDirectory().joinpath( ".sneaky.txt" ).as_posix() )
+		hiddenFile = Gaffer.FileSystemPath( ( self.temporaryDirectory() / ".sneaky.txt" ).as_posix() )
 		with open( hiddenFile.nativeString(), "w" ) as f :
 			f.write( "Can't see me" )
 		if os.name == "nt" :
 			subprocess.check_call( [ "attrib", "+H", hiddenFile.nativeString() ] )
 
-		visibleFile = Gaffer.FileSystemPath( self.temporaryDirectory().joinpath( "frank.txt" ).as_posix() )
+		visibleFile = Gaffer.FileSystemPath( ( self.temporaryDirectory() / "frank.txt" ).as_posix() )
 		with open( visibleFile.nativeString(), "w" ) as f :
 			f.write( "Can see me" )
 
