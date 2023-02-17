GafferHQ/gaffer

commit 8454f37c6d39ad1304bb3eb05dfb0feaaaaaf504

SocketAlgo : Remove support for transform types
This had the same bugs as the other types, but after checking the Cycles nodes and the GafferCycles code, I'm fairly confident that we don't need it. Added a warning in case I'm wrong, and also removed the silent no-op for the NODE socket types.

src/GafferCycles/IECoreCyclesPreview/SocketAlgo.cpp
@@ -424,16 +424,6 @@ void setSocket( ccl::Node *node, const ccl::SocketType *socket, const IECore::Da
 		case ccl::SocketType::ENUM:
 			setEnumSocket( node, *socket, value );
 			break;
-		case ccl::SocketType::TRANSFORM:
-			if( const M44fData *data = static_cast<const M44fData *>( value ) )
-			{
-				node->set( *socket, setTransform( data->readable() ) );
-			}
-			else if( const M44dData *data = static_cast<const M44dData *>( value ) )
-			{
-				node->set( *socket, setTransform( data->readable() ) );
-			}
-			break;
 		case ccl::SocketType::BOOLEAN_ARRAY:
 			setArraySocket<bool>( node, *socket, value );
 			break;
@@ -455,38 +445,12 @@ void setSocket( ccl::Node *node, const ccl::SocketType *socket, const IECore::Da
 		case ccl::SocketType::STRING_ARRAY:
 			setArraySocket<ccl::ustring, StringVectorData>( node, *socket, value );
 			break;
-		case ccl::SocketType::TRANSFORM_ARRAY:
-			if( const M44dVectorData *data = static_cast<const M44dVectorData *>( value ) )
-			{
-				const vector<M44d> &matrices = data->readable();
-				auto matricesSize = matrices.size();
-				ccl::array<ccl::Transform> array( matricesSize );
-				ccl::Transform *tdata = array.data();
-				for(size_t i = 0; i < matricesSize; ++i)
-				{
-					auto m = matrices[i];
-					*(tdata++) = setTransform( m );
-				}
-				node->set( *socket, array );
-			}
-			else if( const M44fVectorData *data = static_cast<const M44fVectorData *>( value ) )
-			{
-				const vector<M44f> &matrices = data->readable();
-				auto matricesSize = matrices.size();
-				ccl::array<ccl::Transform> array( matricesSize );
-				ccl::Transform *tdata = array.data();
-				for(size_t i = 0; i < matricesSize; ++i)
-				{
-					auto m = matrices[i];
-					*(tdata++) = setTransform( m );
-				}
-				node->set( *socket, array );
-			}
-			break;
-		case ccl::SocketType::NODE:
-		case ccl::SocketType::NODE_ARRAY:
-			break;
 		default:
+			IECore::msg(
+				IECore::Msg::Warning, "Cycles::SocketAlgo",
+				boost::format( "Unsupported socket type `%1%` for socket `%2%` on node `%3%`." )
+					% ccl::SocketType::type_name( socket->type ) % value->typeName() % socket->name % node->name
+			);
 			break;
 	}
 }

commit cbe938170a82056219965fc2d95d4c40f502603f

Cycles SocketAlgo : Fix warning format arguments
There was one too many, which caused `boost::format()` to throw, subsequently crashing Gaffer.

I broke this in 8454f37c6d39ad1304bb3eb05dfb0feaaaaaf504, where I confidently stated that we didn't need to support transform sockets. But the `texture_coordinate` shader does have such a socket, called `ob_tfm`, which is how this problem surfaced. But I'm still not convinced we need to support it; I'm not sure how a user would use `ob_tfm` - maybe it's more of an implementation detail for Blender?

Fixes #5147.

src/GafferCycles/IECoreCyclesPreview/SocketAlgo.cpp
@@ -449,7 +449,7 @@ void setSocket( ccl::Node *node, const ccl::SocketType *socket, const IECore::Da
 			IECore::msg(
 				IECore::Msg::Warning, "Cycles::SocketAlgo",
 				boost::format( "Unsupported socket type `%1%` for socket `%2%` on node `%3%`." )
-					% ccl::SocketType::type_name( socket->type ) % value->typeName() % socket->name % node->name
+					% ccl::SocketType::type_name( socket->type ) % socket->name % node->name
 			);
 			break;
 	}

python/GafferCyclesTest/IECoreCyclesPreviewTest/RendererTest.py
@@ -1793,5 +1793,37 @@ class RendererTest( GafferTest.TestCase ) :
 				"Invalid enum value \"missing\" for socket `subsurface_method` on node .*"
 			)
 
+	def testUnsupportedShaderParameters( self ) :
+
+		renderer = GafferScene.Private.IECoreScenePreview.Renderer.create(
+			"Cycles",
+			GafferScene.Private.IECoreScenePreview.Renderer.RenderType.Batch,
+		)
+
+		with IECore.CapturingMessageHandler() as mh :
+
+			attributes = renderer.attributes(
+				IECore.CompoundObject( {
+					"cycles:surface" : IECoreScene.ShaderNetwork(
+						shaders = {
+							"output" : IECoreScene.Shader( "principled_bsdf", "cycles:surface" ),
+							"coordinates" : IECoreScene.Shader( "texture_coordinate", "cycles:shader", { "ob_tfm" : imath.M44f() } ),
+						},
+						connections = [
+							( ( "coordinates", "normal" ), ( "output", "normal" ) )
+						],
+						output = "output"
+					)
+				} )
+			)
+
+		self.assertEqual( len( mh.messages ), 1 )
+		self.assertEqual( mh.messages[0].context, "Cycles::SocketAlgo" )
+		self.assertEqual( mh.messages[0].level, IECore.Msg.Level.Warning )
+		six.assertRegex(
+			self, mh.messages[0].message,
+			"Unsupported socket type `transform` for socket `ob_tfm` on node .*"
+		)
+
 if __name__ == "__main__":
 	unittest.main()