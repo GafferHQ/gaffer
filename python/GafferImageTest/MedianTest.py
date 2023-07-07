757335df731d064fb091dd2547be62b12bb9e709
Daniel Dresser <danield@image-engine.com>
2023-07-06 17:00:13 -0700

    RankFilter : Thorough correctness tests using OIIO as reference

--- python/GafferImageTest/MedianTest.py
+++ python/GafferImageTest/MedianTest.py
@@ -38,6 +38,7 @@ import os
 import time
 import unittest
 import imath
+import OpenImageIO
 
 import IECore
 
@@ -172,6 +173,194 @@ class MedianTest( GafferImageTest.ImageTestCase ) :
 		t = time.time()
 		bt.cancelAndWait()
 		self.assertLess( time.time() - t, acceptableCancellationDelay )
+
+	def writeRefMedianFiltered( self, imageBuf, rad, fileName ):
+		# OIIO has a different edge behaviour to us - they reduce the size of the filter region
+		# as they get closer to the edge. In order to get matching behaviour, pad out the image
+		# with black before taking the median
+		roi = imageBuf.roi
+		padded = OpenImageIO.ImageBufAlgo.crop( imageBuf,
+			OpenImageIO.ROI(-rad.x, roi.xend + rad.x, -rad.y, roi.yend + rad.y, 0, 1, 0, roi.chend)
+		)
+		filtered = OpenImageIO.ImageBufAlgo.median_filter( padded, 1 + 2 * rad.x, 1 + 2 * rad.y )
+		cropped = OpenImageIO.ImageBufAlgo.crop( filtered, roi )
+		cropped.write( str( self.temporaryDirectory() / fileName ) )
+
+	def testAgainstOIIO( self ):
+		pureNoise = OpenImageIO.ImageBufAlgo.noise( "gaussian", 0, 1, roi = OpenImageIO.ROI(0, 320, 0, 240, 0, 1, 0, 3) )
+		pureNoise.write( str( self.temporaryDirectory() / "pureNoise.exr" ) )
+		self.writeRefMedianFiltered( pureNoise, imath.V2i( 1 ), "pureNoiseMed1.exr" )
+		self.writeRefMedianFiltered( pureNoise, imath.V2i( 4 ), "pureNoiseMed4.exr" )
+		self.writeRefMedianFiltered( pureNoise, imath.V2i( 3, 7 ), "pureNoiseMed3x7.exr" )
+
+		diagonalGradient = {
+			"topleft" : (1,1,1), "bottomright" : (0,0,0),
+			"bottomleft" : (0.5,0.5,0.5), "topright" : (0.5, 0.5, 0.5)
+		}
+
+		gradWithNoise = OpenImageIO.ImageBufAlgo.fill(
+			roi = OpenImageIO.ROI(0, 320, 0, 240, 0, 1, 0, 3), **diagonalGradient
+		)
+		OpenImageIO.ImageBufAlgo.noise( gradWithNoise, "gaussian", 0, 1 )
+		gradWithNoise.write( str( self.temporaryDirectory() / "gradWithNoise.exr" ) )
+		self.writeRefMedianFiltered( gradWithNoise, imath.V2i( 1 ), "gradWithNoiseMed1.exr" )
+		self.writeRefMedianFiltered( gradWithNoise, imath.V2i( 3 ), "gradWithNoiseMed3.exr" )
+		self.writeRefMedianFiltered( gradWithNoise, imath.V2i( 1, 7 ), "gradWithNoiseMed1x7.exr" )
+		self.writeRefMedianFiltered( gradWithNoise, imath.V2i( 7, 1 ), "gradWithNoiseMed7x1.exr" )
+
+		# Exercise special cases with the fast median filter where you get a scanline that is all high
+		scanlines = OpenImageIO.ImageBufAlgo.noise("gaussian", 0.5, 2, roi = OpenImageIO.ROI(0, 1, 0, 256, 0, 1, 0, 3))
+		scanlines = OpenImageIO.ImageBufAlgo.resize( scanlines, roi = OpenImageIO.ROI(0, 8, 0, 256, 0, 1, 0, 3))
+		scanlines.write( str( self.temporaryDirectory() / "scanlines.exr" ) )
+		self.writeRefMedianFiltered( scanlines, imath.V2i( 3 ), "scanlinesMed3.exr" )
+
+		scanlinesWithNoise = OpenImageIO.ImageBuf()
+		scanlinesWithNoise.copy( scanlines )
+		OpenImageIO.ImageBufAlgo.noise( scanlinesWithNoise, "gaussian", 0.5, 1 )
+		scanlinesWithNoise = OpenImageIO.ImageBufAlgo.clamp( scanlinesWithNoise, [ 0, 0, 0 ], [ 1, 1, 1 ] )
+		scanlinesWithNoise.write( str( self.temporaryDirectory() / "scanlinesWithNoise.exr" ) )
+		self.writeRefMedianFiltered( scanlinesWithNoise, imath.V2i( 3 ), "scanlinesWithNoiseMed3.exr" )
+
+		oneExceptional = OpenImageIO.ImageBufAlgo.fill(
+			roi = OpenImageIO.ROI(0, 8, 0, 8, 0, 1, 0, 3), **diagonalGradient
+		)
+
+		oneExceptional.setpixel( 2, 2, 0, ( -float( "inf" ), float( "nan" ), float( "inf") ) )
+		oneExceptional.write( str( self.temporaryDirectory() / "oneExceptional.exr" ) )
+
+		# OIIO can't deal with NaNs - just use the channel with negative infinity as a replacement.
+		# Gaffer will treat NaN like a regative infinity
+		oneExceptionalNoNan = OpenImageIO.ImageBufAlgo.channels( oneExceptional, ( 0, 0, 2 ), newchannelnames = ("R", "G", "B" ) )
+		self.writeRefMedianFiltered( oneExceptionalNoNan, imath.V2i( 2 ), "oneExceptionalMed2.exr" )
+
+		halfExceptional = OpenImageIO.ImageBufAlgo.fill(
+			roi = OpenImageIO.ROI(0, 8, 0, 8, 0, 1, 0, 3), **diagonalGradient
+		)
+		for y in range( 8 ):
+			for x in range( 8 ):
+				if (x % 2) == (y % 2):
+					halfExceptional.setpixel( x, y, 0, [ -float( "inf" ), float( "nan" ), float( "inf") ] )
+		halfExceptional.write( str( self.temporaryDirectory() / "halfExceptional.exr" ) )
+		halfExceptionalNoNan = OpenImageIO.ImageBufAlgo.channels( halfExceptional, ( 0, 0, 2 ), newchannelnames = ("R", "G", "B" ) )
+		self.writeRefMedianFiltered( halfExceptionalNoNan, imath.V2i( 2 ), "halfExceptionalMed2.exr" )
+
+		mostExceptional = OpenImageIO.ImageBufAlgo.fill(
+			[ -float( "inf" ), float( "nan" ), float( "inf") ],
+			roi = OpenImageIO.ROI(0, 8, 0, 8, 0, 1, 0, 3)
+		)
+		mostExceptional.setpixel( 2, 2, 0, [ 0.1, 0.2, 0.3 ] )
+		mostExceptional.write( str( self.temporaryDirectory() / "mostExceptional.exr" ) )
+		mostExceptionalNoNan = OpenImageIO.ImageBufAlgo.channels( mostExceptional, ( 0, 0, 2 ), newchannelnames = ("R", "G", "B" ) )
+		self.writeRefMedianFiltered( mostExceptionalNoNan, imath.V2i( 2 ), "mostExceptionalMed2.exr" )
+
+		# We can use OIIO to produce some good test images, but it appears that OIIO produces slightly different
+		# results on Windows, so we instead use an image stored in the test images folder
+		#noisyBlobs = OpenImageIO.ImageBufAlgo.noise("gaussian", 0.5, 1, roi = OpenImageIO.ROI(0, 8, 0, 8, 0, 1, 0, 1))
+		#noisyBlobs = OpenImageIO.ImageBufAlgo.resize( noisyBlobs, roi = OpenImageIO.ROI(0, 256, 0, 256, 0, 1, 0, 1))
+		#OpenImageIO.ImageBufAlgo.noise(noisyBlobs, "gaussian", 0, 0.5)
+		#noisyBlobs.write( str( self.temporaryDirectory() / "noisyBlobs.exr" ) )
+		noisyBlobs = OpenImageIO.ImageBuf( str( self.imagesPath() / "noisyBlobs.exr" ) )
+
+		self.writeRefMedianFiltered( noisyBlobs, imath.V2i( 1 ), "noisyBlobsMed1.exr" )
+		self.writeRefMedianFiltered( noisyBlobs, imath.V2i( 4 ), "noisyBlobsMed4.exr" )
+		# We want at least one test for a large radius, but this takes over 20 seconds for OIIO to generate
+		# using its naive algorithm, so instead we store this reference image in the repo
+		# self.writeRefMedianFiltered( noisyBlobs, imath.V2i( 72, 67 ), "noisyBlobsMed72x67.exr" )
+
+		imageReader = GafferImage.ImageReader()
+		median = GafferImage.Median()
+		median["in"].setInput( imageReader["out"] )
+		refReader = GafferImage.ImageReader()
+
+		driverShuffle = GafferImage.Shuffle()
+		driverShuffle["in"].setInput( imageReader["out"] )
+		driverShuffle["channels"].addChild( GafferImage.Shuffle.ChannelPlug( "driver", "R" ) )
+		driverShuffle["channels"].addChild( GafferImage.Shuffle.ChannelPlug( "driven", "R" ) )
+
+		driverDelete = GafferImage.DeleteChannels()
+		driverDelete["in"].setInput( driverShuffle["out"] )
+		driverDelete["mode"].setValue( GafferImage.DeleteChannels.Mode.Keep )
+		driverDelete["channels"].setValue( 'driven driver' )
+
+		driverMedian = GafferImage.Median()
+		driverMedian["in"].setInput( driverDelete["out"] )
+
+		refDriverShuffle = GafferImage.Shuffle()
+		refDriverShuffle["in"].setInput( refReader["out"] )
+		refDriverShuffle["channels"].addChild( GafferImage.Shuffle.ChannelPlug( "driver", "R" ) )
+		refDriverShuffle["channels"].addChild( GafferImage.Shuffle.ChannelPlug( "driven", "R" ) )
+
+		refDriverDelete = GafferImage.DeleteChannels()
+		refDriverDelete["in"].setInput( refDriverShuffle["out"] )
+		refDriverDelete["mode"].setValue( GafferImage.DeleteChannels.Mode.Keep )
+		refDriverDelete["channels"].setValue( 'driven driver' )
+
+		for source, rad, ref in [
+			( "pureNoise.exr", imath.V2i( 1 ), "pureNoiseMed1.exr" ),
+			( "pureNoise.exr", imath.V2i( 4 ), "pureNoiseMed4.exr" ),
+			( "pureNoise.exr", imath.V2i( 3, 7 ), "pureNoiseMed3x7.exr" ),
+			( "gradWithNoise.exr", imath.V2i( 1 ), "gradWithNoiseMed1.exr" ),
+			( "gradWithNoise.exr", imath.V2i( 3 ), "gradWithNoiseMed3.exr" ),
+			( "gradWithNoise.exr", imath.V2i( 1, 7 ), "gradWithNoiseMed1x7.exr" ),
+			( "gradWithNoise.exr", imath.V2i( 7, 1 ), "gradWithNoiseMed7x1.exr" ),
+			( "scanlines.exr", imath.V2i( 3 ), "scanlinesMed3.exr" ),
+			( "scanlinesWithNoise.exr", imath.V2i( 3 ), "scanlinesWithNoiseMed3.exr" ),
+			( "oneExceptional.exr", imath.V2i( 2 ), "oneExceptionalMed2.exr" ),
+			( "halfExceptional.exr", imath.V2i( 2 ), "halfExceptionalMed2.exr" ),
+			( "mostExceptional.exr", imath.V2i( 2 ), "mostExceptionalMed2.exr" ),
+			( "noisyBlobs.exr", imath.V2i( 1 ), "noisyBlobsMed1.exr" ),
+			( "noisyBlobs.exr", imath.V2i( 4 ), "noisyBlobsMed4.exr" )
+		] :
+			if source == "noisyBlobs.exr":
+				imageReader["fileName"].setValue( self.imagesPath() / source )
+			else:
+				imageReader["fileName"].setValue( self.temporaryDirectory() / source )
+			refReader["fileName"].setValue( self.temporaryDirectory() / ref )
+			with self.subTest( refFile = ref ):
+				median["radius"].setValue( rad )
+				self.assertImagesEqual( median["out"], refReader["out"], ignoreMetadata = True )
+
+
+			driverMedian["radius"].setValue( rad )
+			for channelName in imageReader["out"].channelNames():
+				if ( ref == "halfExceptionalMed2.exr" or ref == "mostExceptionalMed2.exr" ) and channelName == "G":
+					# We usually don't pass through NaNs, but when we're using a driver channel, we just
+					# take the channel value from the corresponding position in the input. This doesn't
+					# really correspond to anything OIIO does, and it's a rare corner case. I think
+					# the current behaviour is correct, but we just skip the test
+					continue
+
+				with self.subTest( refFile = ref, driverChannel = channelName ):
+					for channelPlug in [ i["in"] for i in driverShuffle["channels"].children() + refDriverShuffle["channels"].children() ] + [ driverMedian["masterChannel"] ]:
+						channelPlug.setValue( channelName )
+					self.assertImagesEqual( driverMedian["out"], refDriverDelete["out"], ignoreMetadata = True )
+
+		median["radius"].setValue( imath.V2i( 72, 67 ) )
+		refReader["fileName"].setValue( self.imagesPath() / "noisyBlobsMed72x67.exr" )
+		self.assertImagesEqual( median["out"], refReader["out"], ignoreMetadata = True )
+
+		# Shift the data window around to get more coverage of different tile locations,
+		# including negative tile origins
+
+		offset = GafferImage.Offset()
+		offset["in"].setInput( imageReader["out"] )
+
+		median["in"].setInput( offset["out"] )
+
+		reverseOffset = GafferImage.Offset()
+		reverseOffset["in"].setInput( median["out"] )
+
+		offset["offset"].setValue( imath.V2i( 107, 136 ) )
+		reverseOffset["offset"].setValue( imath.V2i( -107, -136 ) )
+		self.assertImagesEqual( reverseOffset["out"], refReader["out"], ignoreMetadata = True )
+
+		offset["offset"].setValue( imath.V2i( -107, -136 ) )
+		reverseOffset["offset"].setValue( imath.V2i( 107, 136 ) )
+		self.assertImagesEqual( reverseOffset["out"], refReader["out"], ignoreMetadata = True )
+
+		offset["offset"].setValue( imath.V2i( -1070, 1360 ) )
+		reverseOffset["offset"].setValue( imath.V2i( 1070, -1360 ) )
+		self.assertImagesEqual( reverseOffset["out"], refReader["out"], ignoreMetadata = True )
 
 	@GafferTest.TestRunner.PerformanceTestMethod( repeat = 1 )
 	def testPerf( self ) :
