GafferHQ/gaffer

commit d67ace35f72dbc12d98156f6cd2f8c54bf8ccc54

Catalogue : Improve recognition of displays from the same render
The change to `testDisplayDriverAOVGrouping` is necessary because it was exploiting a bug in the previous implementation. As soon as the first image was sent the Catalogue was saving the image (because the driver was closed), but then still allowing other drivers to be added to the image (causing another save after that). We now reject new drivers as soon as we start a save, so the test needs to keep the first driver open until the second has been created. The test now matches more closely the sequence of events we'd see in a real render.

src/GafferImage/Catalogue.cpp
@@ -115,7 +115,7 @@ class Catalogue::InternalImage : public ImageNode
 	public :
 
 		InternalImage( const std::string &name = "InternalImage" )
-			:	ImageNode( name ), m_clientPID( -1 ), m_numDriversClosed( 0 )
+			:	ImageNode( name )
 		{
 			storeIndexOfNextChild( g_firstChildIndex );
 
@@ -230,7 +230,7 @@ class Catalogue::InternalImage : public ImageNode
 				m_saver = AsynchronousSaver::create( this );
 			}
 
-			m_clientPID = -2; // Make sure insertDriver() will reject new drivers
+			m_renderID = "invalid"; // Make sure `insertDriver()` will reject new drivers
 		}
 
 		void save( const std::filesystem::path &fileName ) const
@@ -253,49 +253,70 @@ class Catalogue::InternalImage : public ImageNode
 				return false;
 			}
 
-			// If we already represent a render, we can't
-			// accept a different one.
-			const IntData *clientPID = parameters->member<IntData>( "clientPID" );
-			if( m_clientPID != -1 && clientPID && clientPID->readable() != m_clientPID )
+			// If we already represent a render, we can't accept a driver from a
+			// different one. Gaffer provides a `gaffer:renderID` parameter for
+			// this purpose, and if that isn't available we fall back to the
+			// `clientPID` parameter provided by `IECoreImage::ClientDisplayDriver`.
+			const StringData *renderIDData = parameters->member<StringData>( "gaffer:renderID" );
+			const IntData *clientPIDData = parameters->member<IntData>( "clientPID" );
+			const string renderID = renderIDData ? renderIDData->readable() : ( clientPIDData ? to_string( clientPIDData->readable() ) : "unknown" );
+			if( !m_renderID.empty() && m_renderID != renderID )
 			{
 				return false;
 			}
 
-			// The clientPID mechanism isn't foolproof,
-			// so if the channels in the new driver clash
-			// with the existing channels, we must assume
-			// they're from another render.
-			const vector<string> &channels = driver->channelNames();
-			ConstStringVectorDataPtr existingChannelsData = copyChannels()->outPlug()->channelNamesPlug()->getValue();
-			const vector<string> &existingChannels = existingChannelsData->readable();
-			for( vector<string>::const_iterator it = channels.begin(), eIt = channels.end(); it != eIt; ++it )
+			// The DisplayDriver API doesn't allow the crop window or channel
+			// names to be changed on the fly, so we'll always receive a new
+			// driver if they change. In these cases we want to replace the
+			// existing driver rather than insert a new one. We can only find
+			// the driver to replace if we have a unique identifier for the
+			// output, which Gaffer provides via the `gaffer:outputID`
+			// parameter.
+			const StringData *outputIDData = parameters->member<StringData>( "gaffer:outputID" );
+			string outputID;
+			if( outputIDData )
 			{
-				if( find( existingChannels.begin(), existingChannels.end(), *it ) != existingChannels.end() )
+				outputID = outputIDData->readable();
+			}
+			else
+			{
+				// We don't have a foolproof way of knowing when we can replace
+				// an existing driver. So reject any driver that conflicts with
+				// the existing channel names.
+				ConstStringVectorDataPtr existingChannelsData = copyChannels()->outPlug()->channelNamesPlug()->getValue();
+				const vector<string> &existingChannels = existingChannelsData->readable();
+				for( const auto &channel : driver->channelNames() )
 				{
-					return false;
+					if( find( existingChannels.begin(), existingChannels.end(), channel ) != existingChannels.end() )
+					{
+						return false;
+					}
 				}
+				outputID = boost::algorithm::join( driver->channelNames(), ", " );
 			}
 
-			// All is well - insert the display.
-			DisplayPtr display = new Display;
+			// Insert the driver by hosting it in a Display node, reusing an
+			// existing node if we have one.
+
+			DisplayPtr &display = m_displays[outputID];
+			if( !display )
+			{
+				display = new Display;
+				addChild( display );
+				ArrayPlug *a = copyChannels()->inPlugs();
+				size_t nextIndex = a->children().size() - 1;
+				if( nextIndex == 1 && !a->getChild<ImagePlug>( 0 )->getInput() )
+				{
+					// CopyChannels starts with two input plugs, and we must use
+					// the first one to make sure the format etc is passed through.
+					nextIndex = 0;
+				}
+				a->getChild<ImagePlug>( nextIndex )->setInput( display->outPlug() );
+				imageSwitch()->indexPlug()->setValue( 1 );
+			}
 			display->setDriver( driver );
-			addChild( display );
-			ArrayPlug *a = copyChannels()->inPlugs();
-			size_t nextIndex = a->children().size() - 1;
-			if( nextIndex == 1 && !a->getChild<ImagePlug>( 0 )->getInput() )
-			{
-				// CopyChannels starts with two input plugs, and we must use
-				// the first one to make sure the format etc is passed through.
-				nextIndex = 0;
-			}
 
-			a->getChild<ImagePlug>( nextIndex )->setInput( display->outPlug() );
-			imageSwitch()->indexPlug()->setValue( 1 );
-
-			if( clientPID )
-			{
-				m_clientPID = clientPID->readable();
-			}
+			m_renderID = renderID;
 
 			if( auto nameData = parameters->member<StringData>( "catalogue:imageName" ) )
 			{
@@ -324,16 +345,19 @@ class Catalogue::InternalImage : public ImageNode
 
 		void driverClosed()
 		{
-			m_numDriversClosed++;
-			if( m_numDriversClosed != copyChannels()->inPlugs()->children().size() - 1 )
+			for( const auto &[outputID, display] : m_displays )
 			{
-				return;
+				if( !display->driverClosed() )
+				{
+					return;
+				}
 			}
 
 			// All our drivers have been closed, so the render has completed.
 			// Save the image to disk. We do this in the background because
 			// saving large images with many AOVs takes several seconds.
 
+			m_renderID = "invalid";
 			isRendering( false );
 			m_saver = AsynchronousSaver::create( this );
 		}
@@ -387,15 +411,11 @@ class Catalogue::InternalImage : public ImageNode
 
 		void removeDisplays()
 		{
-			vector<Display *> toDelete;
-			for( Display::Iterator it( this ); !it.done(); ++it )
+			for( auto &[outputID, display] : m_displays )
 			{
-				toDelete.push_back( it->get() );
+				removeChild( display );
 			}
-			for( vector<Display *>::const_iterator it = toDelete.begin(), eIt = toDelete.end(); it != eIt; ++it )
-			{
-				removeChild( *it );
-			}
+			m_displays.clear();
 		}
 
 		ImageReader *imageReader()
@@ -624,8 +644,11 @@ class Catalogue::InternalImage : public ImageNode
 
 		};
 
-		int m_clientPID;
-		size_t m_numDriversClosed;
+		string m_renderID;
+
+		using DisplayMap = unordered_map<string, DisplayPtr>;
+		DisplayMap m_displays;
+
 		AsynchronousSaver::Ptr m_saver;
 
 		static size_t g_firstChildIndex;

python/GafferImageTest/CatalogueTest.py
@@ -221,8 +221,9 @@ class CatalogueTest( GafferImageTest.ImageTestCase ) :
 		aov2["color"].setValue( imath.Color4f( 0, 1, 0, 1 ) )
 		aov2["layer"].setValue( "diffuse" )
 
-		self.sendImage( aov1["out"], c )
+		driver = self.sendImage( aov1["out"], c, close = False )
 		self.sendImage( aov2["out"], c )
+		driver.close()
 
 		self.assertEqual( len( c["images"] ), 1 )
 		self.assertEqual(

python/GafferArnoldTest/InteractiveArnoldRenderTest.py
@@ -615,6 +615,129 @@ class InteractiveArnoldRenderTest( GafferSceneTest.InteractiveRenderTest ) :
 		self.assertGreater( litColor.g, 0.1 )
 		self.assertGreater( litColor.b, 0.1 )
 
+	def testEditLightGroups( self ) :
+
+		script = Gaffer.ScriptNode()
+		script["catalogue"] = GafferImage.Catalogue()
+
+		script["sphere"] = GafferScene.Sphere()
+
+		script["light1"] = GafferArnold.ArnoldLight()
+		script["light1"].loadShader( "point_light" )
+		script["light2"] = GafferArnold.ArnoldLight()
+		script["light2"].loadShader( "point_light" )
+
+		script["parent"] = GafferScene.Parent()
+		script["parent"]["parent"].setValue( "/" )
+		script["parent"]["children"][0].setInput( script["sphere"]["out"] )
+		script["parent"]["children"][1].setInput( script["light1"]["out"] )
+		script["parent"]["children"][2].setInput( script["light2"]["out"] )
+
+		script["outputs"] = GafferScene.Outputs()
+		script["outputs"]["in"].setInput( script["parent"]["out"] )
+		script["outputs"].addOutput(
+			"beauty",
+			IECoreScene.Output(
+				"test",
+				"ieDisplay",
+				"rgba",
+				{
+					"driverType" : "ClientDisplayDriver",
+					"displayHost" : "localhost",
+					"displayPort" : str( script["catalogue"].displayDriverServer().portNumber() ),
+					"remoteDisplayType" : "GafferImage::GafferDisplayDriver",
+				}
+			)
+		)
+		script["outputs"].addOutput(
+			"beautyPerLight",
+			IECoreScene.Output(
+				"test",
+				"ieDisplay",
+				"rgba",
+				{
+					"driverType" : "ClientDisplayDriver",
+					"displayHost" : "localhost",
+					"displayPort" : str( script["catalogue"].displayDriverServer().portNumber() ),
+					"remoteDisplayType" : "GafferImage::GafferDisplayDriver",
+					"layerPerLightGroup" : True,
+					"layerName" : "beauty",
+				}
+			)
+		)
+
+		script["renderer"] = self._createInteractiveRender()
+		script["renderer"]["in"].setInput( script["outputs"]["out"] )
+
+		# Start a render, give it time to finish, and check the output.
+		# Because there are no light groups at all, Arnold chooses to
+		# render just `beauty`.
+
+		script["renderer"]["state"].setValue( script["renderer"].State.Running )
+		self.uiThreadCallHandler.waitFor( 2 )
+
+		self.assertEqual( len( script["catalogue"]["images"] ), 1 )
+		self.assertEqual(
+			set( script["catalogue"]["out"].channelNames() ),
+			{ "R", "G", "B", "A" } |
+			{ "beauty.{}".format( c ) for c in "RGBA" }
+		)
+		self.assertEqual( script["catalogue"]["out"].metadata()["gaffer:isRendering"], IECore.BoolData( True ) )
+
+		# Add a light group. We should now get `beauty_groupA` for the
+		# light group we made, and `beauty_default` as a catch-all for
+		# anything else.
+
+		script["light1"]["parameters"]["aov"].setValue( "groupA" )
+		self.uiThreadCallHandler.waitFor( 1 )
+
+		self.assertEqual( len( script["catalogue"]["images"] ), 1 )
+		self.assertEqual( script["catalogue"]["out"].metadata()["gaffer:isRendering"], IECore.BoolData( True ) )
+		self.assertEqual(
+			set( script["catalogue"]["out"].channelNames() ),
+			{ "R", "G", "B", "A" } |
+			{ "beauty_default.{}".format( c ) for c in "RGBA" } |
+			{ "beauty_groupA.{}".format( c ) for c in "RGBA" }
+		)
+
+		# Add another light group and check it appears. Ideally the
+		# `beauty_default` fallback would disappear as well, but Arnold doesn't
+		# do that yet.
+
+		script["light2"]["parameters"]["aov"].setValue( "groupB" )
+		self.uiThreadCallHandler.waitFor( 1 )
+
+		self.assertEqual( len( script["catalogue"]["images"] ), 1 )
+		self.assertEqual( script["catalogue"]["out"].metadata()["gaffer:isRendering"], IECore.BoolData( True ) )
+		self.assertEqual(
+			set( script["catalogue"]["out"].channelNames() ),
+			{ "R", "G", "B", "A" } |
+			{ "beauty_default.{}".format( c ) for c in "RGBA" } |
+			{ "beauty_groupA.{}".format( c ) for c in "RGBA" } |
+			{ "beauty_groupB.{}".format( c ) for c in "RGBA" }
+		)
+
+		# Remove a light group. Ideally we'd assert that the additional image
+		# layers have been removed now, but Arnold doesn't seem to reliably
+		# reopen the driver with fewer layers. So we satisfy ourselves with
+		# checking that at least we haven't made any unnecessary catalogue
+		# images.
+
+		script["light1"]["enabled"].setValue( False )
+		self.uiThreadCallHandler.waitFor( 1 )
+
+		self.assertEqual( len( script["catalogue"]["images"] ), 1 )
+		self.assertEqual( script["catalogue"]["out"].metadata()["gaffer:isRendering"], IECore.BoolData( True ) )
+
+		# Stop the renderer, and check we still have only one image, and
+		# that it is no longer rendering.
+
+		script["renderer"]["state"].setValue( script["renderer"].State.Stopped )
+		self.uiThreadCallHandler.assertCalled() # Wait for saving to complete
+
+		self.assertEqual( len( script["catalogue"]["images"] ), 1 )
+		self.assertNotIn( "gaffer:isRendering", script["catalogue"]["out"].metadata() )
+
 	def _createConstantShader( self ) :
 
 		shader = GafferArnold.ArnoldShader()

src/GafferScene/RendererAlgo.cpp
@@ -1569,9 +1569,15 @@ struct ObjectOutput : public LocationOutput
 namespace
 {
 
-ConstOutputPtr addGafferOutputHeaders( const Output *output, const ScenePlug *scene )
+ConstOutputPtr addGafferOutputParameters( const Output *output, const ScenePlug *scene, const std::string &outputID, const IECoreScenePreview::Renderer *renderer )
 {
 	CompoundDataPtr param = output->parametersData()->copy();
+
+	// Add parameters that provide unique identifiers for the render and the
+	// output. This is used by the Catalogue to know when to start a new image
+	// and when to just update an existing one.
+	param->writable()["gaffer:renderID"] = new StringData( fmt::format( "{}:{}", getpid(), reinterpret_cast<uintptr_t>( renderer ) ) );
+	param->writable()["gaffer:outputID"] = new StringData( outputID );
 
 	const Node *node = scene->node();
 	const ScriptNode *script = node->scriptNode();
@@ -1722,8 +1728,9 @@ void outputOutputs( const ScenePlug *scene, const IECore::CompoundObject *global
 			}
 			if( changedOrAdded )
 			{
-				ConstOutputPtr updatedOutput = addGafferOutputHeaders( output, scene );
-				renderer->output( it->first.string().substr( prefix.size() ), updatedOutput.get() );
+				const string outputID = it->first.string().substr( prefix.size() );
+				ConstOutputPtr updatedOutput = addGafferOutputParameters( output, scene, outputID, renderer );
+				renderer->output( outputID, updatedOutput.get() );
 			}
 		}
 		else

commit 8cb2099145a6d6d996251a270326f2c5b4a8bca2

Catalogue : Fix duplicate save processes when IPR ends
This bug was introduced in d67ace35f72dbc12d98156f6cd2f8c54bf8ccc54. What I'd failed to realise at the time was that `Display::driverClosed()` will return `True` _before_ `Display::imageReceivedSignal()` has been emitted on the UI thread, because it's just an accessor to an atomic flag. This means that its the _first_ call to `InternalImage::driverClosed()` that will initiate the saving process, rather than the last call as before. So in subsequent calls we need to check we haven't yet initialised `m_saver` to avoid repeating the save process unnecessarily.

For people with a lot of AOVs, the slowdown from the multiple saves could be quite drastic. What made it even worse was that the assignment of `m_saver = newRedundantAsynchronousSaver` would block, because the destructor for the old saver waits for the background process to finish.

src/GafferImage/Catalogue.cpp
@@ -345,6 +345,11 @@ class Catalogue::InternalImage : public ImageNode
 
 		void driverClosed()
 		{
+			if( m_saver )
+			{
+				return;
+			}
+
 			for( const auto &[outputID, display] : m_displays )
 			{
 				if( !display->driverClosed() )

python/GafferImageTest/CatalogueTest.py
@@ -1003,6 +1003,55 @@ class CatalogueTest( GafferImageTest.ImageTestCase ) :
 			c["catalogue:imageName"] = "output:3"
 			self.assertImagesEqual( notFoundText["out"], catalogue["out"] )
 
+	def testYouOnlySaveOnce( self ) :
+
+		# Send a bunch of AOVs to a Catalogue image, but don't
+		# close the drivers yet.
+
+		script = Gaffer.ScriptNode()
+
+		script["catalogue"] = GafferImage.Catalogue()
+		script["catalogue"]["directory"].setValue( self.temporaryDirectory() / "catalogue" )
+
+		script["constant"] = GafferImage.Constant()
+		script["constant"]["format"].setValue( GafferImage.Format( 100, 100 ) )
+		script["constant"]["color"].setValue( imath.Color4f( 1, 0, 0, 1 ) )
+
+		drivers = []
+		for layer in [ "" ] + [ f"aov{n}" for n in range( 0, 10 ) ] :
+
+			script["constant"]["layer"].setValue( layer )
+			drivers.append( self.sendImage( script["constant"]["out"], script["catalogue"], waitForSave = False, close = False ) )
+
+		self.assertEqual( len( script["catalogue"]["images"] ), 1 )
+
+		# Now close the drivers from a background thread, as they will be by a
+		# real renderer, and check that the image is saved to file appropriately.
+
+		with GafferTest.ParallelAlgoTest.UIThreadCallHandler() as handler :
+			closingThread = threading.Thread( target = lambda : [ d.close( withCallHandler = False ) for d in drivers ] )
+			closingThread.start()
+			# In practice, renderers close drivers fast enough that all drivers
+			# are closed before the first `Display::imageReceivedSignal()` is
+			# emitted on the UI thread. Emulate this by waiting for the thread to
+			# finish before calling `assertCalled()`
+			closingThread.join()
+			# We now have a bunch of pending UI thread calls, one for each driver,
+			# so handle those. We expect the first call to start the saving process,
+			# but that process happens in the background and won't be visible yet.
+			for driver in drivers :
+				handler.assertCalled()
+				self.assertEqual( script["catalogue"]["images"][0]["fileName"].getValue(), "" )
+			# The saving process will make one more UI thread call, to make the
+			# result of the save visible. Handle that and check we now have an image
+			# file.
+			handler.assertCalled()
+			self.assertNotEqual( script["catalogue"]["images"][0]["fileName"].getValue(), "" )
+			self.assertTrue( pathlib.Path( script["catalogue"]["images"][0]["fileName"].getValue() ).is_file() )
+			# That should be it. There should definitely not be a whole load more
+			# attempts to save the image, so assert that no more UI thread calls are
+			# made.
+			handler.assertDone()
 
 if __name__ == "__main__":
 	unittest.main()

python/GafferImageTest/DisplayTest.py
@@ -105,7 +105,11 @@ class DisplayTest( GafferImageTest.ImageTestCase ) :
 				h.assertCalled()
 				h.assertDone()
 
-		def close( self ) :
+		def close( self, withCallHandler = True ) :
+
+			if not withCallHandler :
+				self.__driver.imageClose()
+				return
 
 			with GafferTest.ParallelAlgoTest.UIThreadCallHandler() as h :
 				self.__driver.imageClose()