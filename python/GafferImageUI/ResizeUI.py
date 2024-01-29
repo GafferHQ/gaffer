83f7027c8cf6a32f33e21c94c28acdc8c8aa6fbd
John Haddon <thehaddonyoof@gmail.com>
2024-01-29 10:57:23 +0000

    ResizeUI : Add warning when `filterDeep` is on

--- python/GafferImageUI/ResizeUI.py
+++ python/GafferImageUI/ResizeUI.py
@@ -34,11 +34,13 @@
 #
 ##########################################################################
 
+import inspect
 import itertools
 
 import Gaffer
 import GafferUI
 import GafferImage
+import GafferImageUI
 
 Gaffer.Metadata.registerNode(
 
@@ -49,6 +51,11 @@ Gaffer.Metadata.registerNode(
 	Resizes the image to a new resolution, scaling the
 	contents to fit the new size.
 	""",
+
+	"layout:customWidget:filterDeepWarning:widgetType", "GafferImageUI.ResampleUI._FilterDeepWarningWidget",
+	"layout:customWidget:filterDeepWarning:section", "Settings",
+	"layout:customWidget:filterDeepWarning:accessory", True,
+	"layout:customWidget:filterDeepWarning:visibilityActivator", lambda node : node["filterDeep"].getValue(),
 
 	plugs = {
 
@@ -136,16 +143,21 @@ Gaffer.Metadata.registerNode(
 		"filterDeep" : [
 
 			"description",
-			"""
-			By default, we use a nearest filter for deep images, because
-			accurately filtering deeps can be extremely expensive, and
-			produces images that are too large to store on disk. If you
-			know what you're doing, turn this on to do accurate filtering
-			( the only manageable use case is probably if you immediately
-			do a merge or holdout after resampling, so the a minimum
-			amount of processing needs to happen to the huge data of the
-			resampled deep ).
-			""",
+			inspect.cleandoc(
+				"""
+				When on, deep images are resized accurately using the same filter
+				as flat images. When off, deep images are resized using the Nearest
+				filter.
+
+				Filters with negative lobes ( such as Lanczos3 which is the Default
+				for downscaling ) cannot be represented at all depths with perfect
+				accuracy, because deep alpha must be between 0 and 1, and must be
+				non-decreasing. In extreme cases, involving bright segments
+				with very low alpha, it may be preferable to choose a softer filter
+				without negative lobes ( like Blackman-Harris ).
+
+				"""
+			) + GafferImageUI.ResampleUI._filterDeepWarning,
 
 		],
 
