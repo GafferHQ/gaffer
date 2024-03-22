GafferHQ/gaffer

commit 9d15bd21c4049d89118faf3ac27d01c5799b8264

ScriptNode : Remove AST code
The API we were using to do this is no longer available in Python 3.10, and wasn't available on Windows or MacOS anyway. This does lead to around a 7% performance regression when loading a large template from `gaffer-examples`, but there's little we can do about it without changing approach completely.

Alternatives tried at this point are :

- Continuing to use the AST, accessing it via the Python `ast` module rather than the C API. This led to a 80% regression.
- Doing our own parsing for the very long `__children["child"]["child"]["child"]` prefixes present on most lines of the serialisation. This was no better, because we still need to use `__getitem__` at each step of the way, and it seems that is where much of the overhead is. We can't stop calling `__getitem__`
because our monkey-patching compatibility configs rely on it. If we could, then we could move a great deal of the execution to pure C++, which I think might be a big win.

src/GafferModule/ScriptNodeBinding.cpp
@@ -70,45 +70,6 @@ using namespace boost;
 using namespace Gaffer;
 using namespace GafferBindings;
 
-#if !defined( _MSC_VER ) && PY_VERSION_HEX < 0x03080000
-
-//////////////////////////////////////////////////////////////////////////
-// Access to Python AST
-//////////////////////////////////////////////////////////////////////////
-
-extern "C"
-{
-
-// Essential to include this last, since it defines macros which
-// clash with other headers.
-#include "Python-ast.h"
-
-/// \todo The already esoteric AST API appears to be even more obscure
-/// in Python 3, and it has never been available on Windows. We would do
-/// well to avoid it entirely. One simple alternative is implemented in
-/// https://github.com/johnhaddon/gaffer/tree/simpleTolerantExec, but
-/// initial benchmarking suggested that performance was worse.
-#include "asdl.h"
-#undef arg
-#define asdl_seq_new _Py_asdl_seq_new
-
-};
-
-namespace boost {
-namespace python {
-
-// Specialisation to allow use of handle<PyCodeObject>
-template<>
-struct base_type_traits<PyCodeObject>
-{
-	using type = PyObject;
-};
-
-} // namespace python
-} // namespace boost
-
-#endif
-
 //////////////////////////////////////////////////////////////////////////
 // Serialisation
 //////////////////////////////////////////////////////////////////////////
@@ -124,81 +85,6 @@ const std::string formattedErrorContext( int lineNumber, const std::string &cont
 	);
 }
 
-#if !defined( _MSC_VER ) && PY_VERSION_HEX < 0x03080000
-// Execute the script one top level statement at a time,
-// reporting errors that occur, but otherwise continuing
-// with execution.
-bool tolerantExec( const std::string &pythonScript, boost::python::object globals, boost::python::object locals, const std::string &context )
-{
-	// The python parsing framework uses an arena to simplify memory allocation,
-	// which is handy for us, since we're going to manipulate the AST a little.
-	std::unique_ptr<PyArena, decltype( &PyArena_Free )> arena( PyArena_New(), PyArena_Free );
-
-	// Parse the whole script, getting an abstract syntax tree for a
-	// module which would execute everything.
-	mod_ty mod = PyParser_ASTFromString(
-		pythonScript.c_str(),
-		"<string>",
-		Py_file_input,
-		nullptr,
-		arena.get()
-	);
-
-	if( !mod )
-	{
-		int lineNumber = 0;
-		std::string message = IECorePython::ExceptionAlgo::formatPythonException( /* withTraceback = */ false, &lineNumber );
-		IECore::msg( IECore::Msg::Error, formattedErrorContext( lineNumber, context ), message );
-		return false;
-	}
-
-	const IECore::Canceller *canceller = Context::current()->canceller();
-	IECore::Canceller::check( canceller );
-
-	assert( mod->kind == Module_kind );
-
-	// Loop over the top-level statements in the module body,
-	// executing one at a time.
-	bool result = false;
-	int numStatements = asdl_seq_LEN( mod->v.Module.body );
-	for( int i=0; i<numStatements; ++i )
-	{
-		IECore::Canceller::check( canceller );
-
-		// Make a new module containing just this one statement.
-		asdl_seq *newBody = asdl_seq_new( 1, arena.get() );
-		asdl_seq_SET( newBody, 0, asdl_seq_GET( mod->v.Module.body, i ) );
-		mod_ty newModule = Module(
-			newBody,
-			arena.get()
-		);
-
-		// Compile it.
-		boost::python::handle<PyCodeObject> code( PyAST_Compile( newModule, "<string>", nullptr, arena.get() ) );
-
-		// And execute it.
-		boost::python::handle<> v( boost::python::allow_null(
-			PyEval_EvalCode(
-				(PyObject *)code.get(),
-				globals.ptr(),
-				locals.ptr()
-			)
-		) );
-
-		// Report any errors.
-		if( v == nullptr)
-		{
-			int lineNumber = 0;
-			std::string message = IECorePython::ExceptionAlgo::formatPythonException( /* withTraceback = */ false, &lineNumber );
-			IECore::msg( IECore::Msg::Error, formattedErrorContext( lineNumber, context ), message );
-			result = true;
-		}
-	}
-
-	return result;
-}
-
-#else
 // Execute the script one line at a time, reporting errors that occur,
 // but otherwise continuing with execution.
 bool tolerantExec( const std::string &pythonScript, boost::python::object globals, boost::python::object locals, const std::string &context )
@@ -229,8 +115,6 @@ bool tolerantExec( const std::string &pythonScript, boost::python::object global
 
 	return result;
 }
-
-#endif
 
 // The dict returned will form both the locals and the globals for
 // the execute() methods. It's not possible to have a separate locals

commit bb66f1f564e389742926a8a4aeacf51cc5571701

ScriptNode : Support multi-line `if` in `tolerantExec()`
Although we don't make use of any compound statements in our own serialisations, third parties have done so using custom serialisers. Parsing for those was broken by 9d15bd21c4049d89118faf3ac27d01c5799b8264 when we
stopped using an AST for parsing. Here we do the absolute bare minimum necessary to get these legacy files loading. As noted in the comments, we do not intend to do more : rather our future intention is likely to be constrain the serialisation format further for improved performance.

src/GafferModule/ScriptNodeBinding.cpp
@@ -65,6 +65,7 @@
 #include "fmt/format.h"
 
 #include <memory>
+#include <regex>
 
 using namespace boost;
 using namespace Gaffer;
@@ -85,12 +86,15 @@ const std::string formattedErrorContext( int lineNumber, const std::string &cont
 	);
 }
 
+const std::regex g_blockStartRegex( R"(^if[ \t(])" );
+const std::regex g_blockContinuationRegex( R"(^[ \t]+)" );
+
 // Execute the script one line at a time, reporting errors that occur,
 // but otherwise continuing with execution.
 bool tolerantExec( const std::string &pythonScript, boost::python::object globals, boost::python::object locals, const std::string &context )
 {
 	bool result = false;
-	int lineNumber = 1;
+	int lineNumber = 0;
 
 	const IECore::Canceller *canceller = Context::current()->canceller();
 
@@ -99,10 +103,49 @@ bool tolerantExec( const std::string &pythonScript, boost::python::object global
 	{
 		IECore::Canceller::check( canceller );
 
-		const std::string line( it->begin(), it->end() );
+		std::string toExecute( it->begin(), it->end() );
+		++it; ++lineNumber;
+
+		// Our serialisations have always been in a form that can be executed
+		// line-by-line. But certain third parties have used custom serialisers
+		// that output multi-line `if` statements that must be executed in a
+		// single call. Here we detect them and group them together.
+		//
+		// Notes :
+		//
+		// - Historically, we used a Python C AST API to support _any_ compound
+		//   statements here. But that is [no longer available](9d15bd21c4049d89118faf3ac27d01c5799b8264),
+		//   and using the `ast` module instead would be a significant performance
+		//   regression.
+		// - While using Python as our serialisation format is great from an
+		//   educational and reuse perspective, it has never been good from a
+		//   performance perspective. A likely future direction is to constrain
+		//   the serialisation syntax further such that it is still valid
+		//   Python, but we can parse and execute the majority of it directly in C++
+		//   for improvement performance.
+		// - We are therefore deliberately supporting only the absolute minimum of syntax
+		//   needed for the legacy third-party serialisations here, to give us
+		//   more flexibility in optimising the parsing in future.
+		if( std::regex_search( toExecute, g_blockStartRegex )  )
+		{
+			while( it != split_iterator<std::string::const_iterator>() )
+			{
+				const std::string line( it->begin(), it->end() );
+				if( std::regex_search( line, g_blockContinuationRegex ) )
+				{
+					toExecute += "\n" + line;
+					++it; ++lineNumber;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
 		try
 		{
-			exec( line.c_str(), globals, locals );
+			exec( toExecute.c_str(), globals, locals );
 		}
 		catch( const boost::python::error_already_set & )
 		{
@@ -110,7 +153,6 @@ bool tolerantExec( const std::string &pythonScript, boost::python::object global
 			IECore::msg( IECore::Msg::Error, formattedErrorContext( lineNumber, context ), message );
 			result = true;
 		}
-		++it; ++lineNumber;
 	}
 
 	return result;

python/GafferTest/ScriptNodeTest.py
@@ -1763,5 +1763,79 @@ class ScriptNodeTest( GafferTest.TestCase ) :
 		self.assertEqual( memberRemovals, [ ( s.focusSet(), n ) ] )
 		self.assertEqual( memberAdditions, [] )
 
+	def testExecuteWithConditionals( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		script.execute(
+			inspect.cleandoc(
+				"""
+				if True :
+					script.addChild( Gaffer.Node( "True1" ) )
+				script.addChild( Gaffer.Node( "Unconditional1" ) )
+
+				if False :
+					script.addChild( Gaffer.Node( "False" ) )
+
+				if True :
+					script.addChild( Gaffer.Node( "True2" ) )
+					script.addChild( Gaffer.Node( "True3" ) )
+				script.addChild( Gaffer.Node( "Unconditional2" ) )
+
+				if(True):
+					script.addChild( Gaffer.Node( "True4" ) )
+
+				script.addChild( Gaffer.Node( "Unconditional3" ) )
+
+				if(True) :
+					script.addChild( Gaffer.Node( "True5" ) )
+
+				if (True):
+					script.addChild( Gaffer.Node( "True6" ) )
+
+				if True : script.addChild( Gaffer.Node( "True7" ) )
+				if False : script.addChild( Gaffer.Node( "False" ) )
+
+				"""
+			),
+			continueOnError = True
+		)
+
+		self.assertIn( "True1", script )
+		self.assertIn( "True2", script )
+		self.assertIn( "True3", script )
+		self.assertIn( "True4", script )
+		self.assertIn( "True5", script )
+		self.assertIn( "True6", script )
+		self.assertIn( "True7", script )
+		self.assertIn( "Unconditional1", script )
+		self.assertIn( "Unconditional2", script )
+		self.assertIn( "Unconditional3", script )
+		self.assertNotIn( "False", script )
+		self.assertNotIn( "True8", script )
+
+	def testLineNumbersAfterConditionalExecution( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		with IECore.CapturingMessageHandler() as mh :
+			script.execute(
+				inspect.cleandoc(
+					"""
+					if True :
+						script.addChild( Gaffer.Node( "True" ) )
+					a = 10 * b
+					"""
+				),
+				continueOnError = True
+			)
+
+		self.assertIn( "True", script )
+
+		self.assertEqual( len( mh.messages ), 1 )
+		self.assertEqual( mh.messages[0].level, IECore.Msg.Level.Error )
+		self.assertTrue( "Line 3" in mh.messages[0].context )
+		self.assertTrue( "name 'b' is not defined" in mh.messages[0].message )
+
 if __name__ == "__main__":
 	unittest.main()