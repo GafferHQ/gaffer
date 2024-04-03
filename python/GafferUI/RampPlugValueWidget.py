755ea10e9866bcce76738add14123fa607930a42
John Haddon <thehaddonyoof@gmail.com>
2024-04-03 12:35:45 +0100

    fixup! RampPlugValueWidget : hide value changed indicator
    We were making serialisable per-instance metadata when a static registration could do the same job.

--- python/GafferUI/RampPlugValueWidget.py
+++ python/GafferUI/RampPlugValueWidget.py
@@ -83,7 +83,6 @@ class RampPlugValueWidget( GafferUI.PlugValueWidget ) :
 				spacing = 4
 			) :
 
-				Gaffer.Metadata.registerValue( plug.pointXPlug( 0 ), "showValueChangedIndicator", False )
 				self.__positionLabel = GafferUI.LabelPlugValueWidget(
 					plug.pointXPlug( 0 ),
 					parenting = { "verticalAlignment" : GafferUI.VerticalAlignment.Top }
@@ -93,7 +92,6 @@ class RampPlugValueWidget( GafferUI.PlugValueWidget ) :
 					parenting = { "verticalAlignment" : GafferUI.VerticalAlignment.Top }
 				)
 
-				Gaffer.Metadata.registerValue( plug.pointYPlug( 0 ), "showValueChangedIndicator", False )
 				self.__valueLabel = GafferUI.LabelPlugValueWidget(
 					plug.pointYPlug( 0 ),
 					parenting = { "verticalAlignment" : GafferUI.VerticalAlignment.Top }
