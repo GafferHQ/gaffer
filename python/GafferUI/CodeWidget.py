GafferHQ/gaffer

commit 77a679aaa95811baa01fac038a93d421240306b4

Replace `IECore.Enum` with standard types from `enum` module
Now we're Python 3 only we can use the standard `enum` module, which provides a more comprehensive framework than `IECore.Enum`. For the most part its a drop-in replacement, but there are a few gotchas to be aware of, which I have hopefully dealt with everywhere necessary :

- `str( e )` returns only `<name>` for `IECore.Enum` but returns `<className>.<name>` for `enum.Enum`.
- `int( e )` is provided by default for `IECore.Enum`, but not by `enum.Enum`, so we must use `enum.IntEnum` when that is needed.
- `IECore.Enum` values start at 0, but `enum.Enum` values start at 1 by default. Where this is important (see Wedge), we must specify `start = 0`.

python/GafferUI/CodeWidget.py
@@ -36,6 +36,7 @@
 
 import os
 import re
+import enum
 import functools
 import token
 import keyword
@@ -285,10 +286,13 @@ class CodeWidget( GafferUI.MultiLineTextWidget ) :
 
 class Highlighter( object ) :
 
-	Type = IECore.Enum.create(
-		"SingleQuotedString", "DoubleQuotedString", "Number",
-		"Keyword", "ControlFlow", "Braces", "Operator", "Call",
-		"Comment", "ReservedWord", "Preprocessor"
+	Type = enum.Enum(
+		"Type",
+		[
+			"SingleQuotedString", "DoubleQuotedString", "Number",
+			"Keyword", "ControlFlow", "Braces", "Operator", "Call",
+			"Comment", "ReservedWord", "Preprocessor"
+		]
 	)
 
 	# Specifies a highlight type to be used for the characters

python/GafferCortexUI/OpDialogue.py
@@ -36,6 +36,7 @@
 ##########################################################################
 
 import contextlib
+import enum
 import sys
 import threading
 import traceback
@@ -67,7 +68,7 @@ class OpDialogue( GafferUI.Dialogue ) :
 	#
 	# NoneByDefault : deprecated - the same as DisplayResult
 	# CloseByDefault : deprecated - the same as DisplayResult
-	PostExecuteBehaviour = IECore.Enum.create( "FromUserData", "None_", "Close", "DisplayResult", "DisplayResultAndClose", "NoneByDefault", "CloseByDefault" )
+	PostExecuteBehaviour = enum.Enum( "PostExecuteBehaviour", [ "FromUserData", "None_", "Close", "DisplayResult", "DisplayResultAndClose", "NoneByDefault", "CloseByDefault" ] )
 
 	## Defines which button has the focus when the op is displayed for editing.
 	#
@@ -80,7 +81,7 @@ class OpDialogue( GafferUI.Dialogue ) :
 	# OK : The OK button has the focus.
 	#
 	# Cancel : The cancel button has the focus.
-	DefaultButton = IECore.Enum.create( "FromUserData", "None_", "OK", "Cancel" )
+	DefaultButton = enum.Enum( "DefaultButton", [ "FromUserData", "None_", "OK", "Cancel" ] )
 
 	# If executeInBackground is True, then the Op will be executed on another
 	# thread, allowing the UI to remain responsive during execution. This is
@@ -255,7 +256,7 @@ class OpDialogue( GafferUI.Dialogue ) :
 		# the op is running in the background.
 		return self.__state != self.__State.Execution
 
-	__State = IECore.Enum.create( "ParameterEditing", "Execution", "ErrorDisplay", "ResultDisplay" )
+	__State = enum.Enum( "__State", [ "ParameterEditing", "Execution", "ErrorDisplay", "ResultDisplay" ] )
 
 	def __initiateParameterEditing( self, *unused ) :
 

python/GafferDispatch/LocalDispatcher.py
@@ -34,6 +34,7 @@
 #
 ##########################################################################
 
+import enum
 import os
 import errno
 import signal
@@ -62,7 +63,7 @@ class LocalDispatcher( GafferDispatch.Dispatcher ) :
 
 	class Job( object ) :
 
-		Status = IECore.Enum.create( "Waiting", "Running", "Complete", "Failed", "Killed" )
+		Status = enum.IntEnum( "Status", [ "Waiting", "Running", "Complete", "Failed", "Killed" ] )
 
 		def __init__( self, batch, dispatcher ) :
 

python/GafferDispatch/Wedge.py
@@ -34,7 +34,7 @@
 #
 ##########################################################################
 
-import math
+import enum
 import imath
 
 import IECore
@@ -44,7 +44,7 @@ import GafferDispatch
 
 class Wedge( GafferDispatch.TaskContextProcessor ) :
 
-	Mode = IECore.Enum.create( "FloatRange", "IntRange", "ColorRange", "FloatList", "IntList", "StringList" )
+	Mode = enum.IntEnum( "Mode", [ "FloatRange", "IntRange", "ColorRange", "FloatList", "IntList", "StringList" ], start = 0 )
 
 	def __init__( self, name = "Wedge" ) :
 

python/GafferDispatchUI/DispatchDialogue.py
@@ -34,6 +34,7 @@
 #
 ##########################################################################
 
+import enum
 import functools
 import sys
 import threading
@@ -54,7 +55,7 @@ class DispatchDialogue( GafferUI.Dialogue ) :
 	# Close : The dialogue is closed immediately.
 	#
 	# Confirm : The dialogue remains open confirming success, with a button for returning to the editing state.
-	PostDispatchBehaviour = IECore.Enum.create( "Close", "Confirm" )
+	PostDispatchBehaviour = enum.Enum( "PostDispatchBehaviour", [ "Close", "Confirm" ] )
 
 	__dispatchDialogueMenuDefinition = None
 

python/GafferSceneTest/SceneNodeTest.py
@@ -35,6 +35,7 @@
 #
 ##########################################################################
 
+import enum
 import inspect
 import unittest
 import time
@@ -218,7 +219,7 @@ class SceneNodeTest( GafferSceneTest.SceneTestCase ) :
 
 		class SphereOrCube( GafferScene.SceneNode ) :
 
-			Type = IECore.Enum.create( "Sphere", "Cube" )
+			Type = enum.IntEnum( "Type", [ "Sphere", "Cube" ], start = 0 )
 
 			def __init__( self, name = "SphereOrCube" ) :
 

python/GafferSceneUI/CryptomatteUI.py
@@ -35,6 +35,7 @@
 ##########################################################################
 
 import contextlib
+import enum
 import functools
 import imath
 import re
@@ -294,7 +295,7 @@ GafferUI.Pointer.registerPointer( "addNames", GafferUI.Pointer( "addObjects.png"
 GafferUI.Pointer.registerPointer( "removeNames", GafferUI.Pointer( "removeObjects.png", imath.V2i( 36, 18 ) ) )
 GafferUI.Pointer.registerPointer( "replaceNames", GafferUI.Pointer( "replaceObjects.png", imath.V2i( 36, 18 ) ) )
 
-__DropMode = IECore.Enum.create( "None_", "Add", "Remove", "Replace" )
+__DropMode = enum.Enum( "__DropMode", [ "None_", "Add", "Remove", "Replace" ] )
 
 __originalDragPointer = None
 

python/GafferSceneUI/PathFilterUI.py
@@ -34,7 +34,7 @@
 #
 ##########################################################################
 
-import types
+import enum
 import imath
 import functools
 from collections import deque
@@ -236,7 +236,7 @@ GafferUI.Pointer.registerPointer( "addObjects", GafferUI.Pointer( "addObjects.pn
 GafferUI.Pointer.registerPointer( "removeObjects", GafferUI.Pointer( "removeObjects.png", imath.V2i( 53, 14 ) ) )
 GafferUI.Pointer.registerPointer( "replaceObjects", GafferUI.Pointer( "replaceObjects.png", imath.V2i( 53, 14 ) ) )
 
-__DropMode = IECore.Enum.create( "None_", "Add", "Remove", "Replace" )
+__DropMode = enum.Enum( "__DropMode", [ "None_", "Add", "Remove", "Replace" ] )
 
 __originalDragPointer = None
 

commit ba596d5bc97c4fc352644ffb1e6fa0df788d3342

CodeWidget : Fix errors handling multi-line highlights
The was broken by 77a679aaa95811baa01fac038a93d421240306b4, causing the following error :

```
Traceback (most recent call last):
  File "/home/john/dev/build/gaffer-1.4/python/GafferUI/CodeWidget.py", line 481, in highlightBlock
    self.setCurrentBlockState( int( h.type ) )
TypeError: int() argument must be a string, a bytes-like object or a real number, not 'Type'
Traceback (most recent call last):
  File "/home/john/dev/build/gaffer-1.4/python/GafferUI/CodeWidget.py", line 481, in highlightBlock
    self.setCurrentBlockState( int( h.type ) )
TypeError: int() argument must be a string, a bytes-like object or a real number, not 'Type'
```

python/GafferUI/CodeWidget.py
@@ -314,7 +314,7 @@ class CodeWidget( GafferUI.MultiLineTextWidget ) :
 
 class Highlighter( object ) :
 
-	Type = enum.Enum(
+	Type = enum.IntEnum(
 		"Type",
 		[
 			"SingleQuotedString", "DoubleQuotedString", "Number",