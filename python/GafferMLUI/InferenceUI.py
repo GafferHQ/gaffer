331183eb8f9270cb95d22a25db4becbafacc77ad
John Haddon <thehaddonyoof@gmail.com>
2024-09-24 09:40:08 +0100

    inference model plug

--- python/GafferMLUI/InferenceUI.py
+++ python/GafferMLUI/InferenceUI.py
@@ -34,7 +34,25 @@
 #
 ##########################################################################
 
-from . import TensorToImageUI
-from . import InferenceUI
+import Gaffer
+import GafferML
 
-__import__( "IECore" ).loadConfig( "GAFFER_STARTUP_PATHS", subdirectory = "GafferMLUI" )
+Gaffer.Metadata.registerNode(
+
+	GafferML.Inference,
+
+	plugs = {
+
+		"model" : [
+
+			"nodule:type", "",
+			"plugValueWidget:type", "GafferUI.FileSystemPathPlugValueWidget",
+			"path:leaf", True,
+			"path:valid", True,
+			"path:bookmarks", "onnx",
+			"fileSystemPath:extensions", "onnx",
+
+		],
+
+	}
+)
