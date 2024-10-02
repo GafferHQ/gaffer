GafferHQ/gaffer

commit cfc3857ce0da4c8f77cfc546028f5c7e64aaf585

GL Renderer : Support `Visualiser::ColorSpace`

src/GafferScene/IECoreGLPreview/Renderer.cpp
@@ -119,13 +119,13 @@ class ScopedTransform
 };
 
 template <class... Vs>
-bool haveMatchingVisualisations( Visualisation::Scale scale, Visualisation::Category category, const Vs & ... visualisations )
+bool haveMatchingVisualisations( Visualisation::ColorSpace colorSpace, Visualisation::Scale scale, Visualisation::Category category, const Vs & ... visualisations )
 {
 	for( auto vs : { visualisations... } )
 	{
 		for( auto v : vs )
 		{
-			if( v.scale == scale && v.category & category )
+			if( v.colorSpace == colorSpace && v.scale == scale && v.category & category )
 			{
 				return true;
 			}
@@ -135,13 +135,13 @@ bool haveMatchingVisualisations( Visualisation::Scale scale, Visualisation::Cate
 }
 
 template <class... Vs>
-void renderMatchingVisualisations( Visualisation::Scale scale, Visualisation::Category category, IECoreGL::State *state, const Vs & ... visualisations )
+void renderMatchingVisualisations( Visualisation::ColorSpace colorSpace, Visualisation::Scale scale, Visualisation::Category category, IECoreGL::State *state, const Vs & ... visualisations )
 {
 	for( auto vs : { visualisations... } )
 	{
 		for( auto v : vs )
 		{
-			if( v.scale == scale && v.category & category )
+			if( v.colorSpace == colorSpace && v.scale == scale && v.category & category )
 			{
 				v.renderable->render( state );
 			}
@@ -223,6 +223,8 @@ const IECoreGL::State &selectionState()
 	if( !s )
 	{
 		s = new IECoreGL::State( false );
+		s->add( new IECoreGL::Primitive::DrawPoints( false ), /* override = */ true );
+		s->add( new IECoreGL::Primitive::DrawSolid( false ), /* override = */ true );
 		s->add( new IECoreGL::Primitive::DrawWireframe( true ), /* override = */ true );
 		s->add( new IECoreGL::WireframeColorStateComponent( Color4f( 0.466f, 0.612f, 0.741f, 1.0f ) ), /* override = */ true );
 	}
@@ -294,7 +296,7 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 
 			if( visualisationState || lightVisualisationState || lightFilterVisualisationState )
 			{
-				StatePtr combinedState = new State( *m_state );
+				StatePtr combinedState = new State( /* complete = */ false );
 
 				if( visualisationState )
 				{
@@ -311,13 +313,18 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 					combinedState->add( const_cast<State *>( lightFilterVisualisationState.get() ) );
 				}
 
-				m_state = combinedState;
+				m_visualisationState = combinedState;
 			}
 		}
 
 		const State *state() const
 		{
 			return m_state.get();
+		}
+
+		const State *visualisationState() const
+		{
+			return m_visualisationState.get();
 		}
 
 		const IECoreGLPreview::Visualisations &visualisations() const
@@ -356,6 +363,7 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 	private :
 
 		ConstStatePtr m_state;
+		ConstStatePtr m_visualisationState;
 		Visualisations m_visualisations;
 		Visualisations m_lightVisualisations;
 		Visualisations m_lightFilterVisualisations;
@@ -495,7 +503,7 @@ class OpenGLObject : public IECoreScenePreview::Renderer::ObjectInterface
 			return selection.match( m_name ) & ( PathMatcher::AncestorMatch | PathMatcher::ExactMatch );
 		}
 
-		void render( IECoreGL::State *currentState, const IECore::PathMatcher &selection ) const
+		void render( IECoreGL::State *currentState, const IECore::PathMatcher &selection, Visualisation::ColorSpace colorSpace ) const
 		{
 			const Visualisations &attrVis = visualisations( *m_attributes );
 			const bool haveVisualisations = attrVis.size() > 0 || m_objectVisualisations.size() > 0;
@@ -507,16 +515,16 @@ class OpenGLObject : public IECoreScenePreview::Renderer::ObjectInterface
 
 			const bool isSelected = selected( selection );
 
-			IECoreGL::State::ScopedBinding scope( *m_attributes->state(), *currentState );
-			IECoreGL::State::ScopedBinding selectionScope( selectionState(), *currentState, isSelected );
-
 			// In order to minimize z-fighting, we draw non-geometric visualisations
 			// first and real geometry last, so that they sit on top. This is
 			// still prone to flicker, but seems to provide the best results.
 
-
 			if( haveVisualisations )
 			{
+				IECoreGL::State::ScopedBinding selectionScope(
+					selectionState(), *currentState, isSelected && colorSpace == Visualisation::ColorSpace::Display
+				);
+
 				Visualisation::Category categories = Visualisation::Category::Generic;
 				if( m_attributes->drawFrustum( isSelected ) )
 				{
@@ -525,36 +533,56 @@ class OpenGLObject : public IECoreScenePreview::Renderer::ObjectInterface
 
 				if( m_attributes->visualiserScale() > 0.0f )
 				{
-					if( haveMatchingVisualisations( Visualisation::Scale::Visualiser, categories, attrVis, m_objectVisualisations ) )
+					if( haveMatchingVisualisations( colorSpace, Visualisation::Scale::Visualiser, categories, attrVis, m_objectVisualisations ) )
 					{
 						ScopedTransform v( visualiserTransform( false ) );
-						renderMatchingVisualisations( Visualisation::Scale::Visualiser, categories, currentState, attrVis, m_objectVisualisations );
+						renderMatchingVisualisations( colorSpace, Visualisation::Scale::Visualiser, categories, currentState, attrVis, m_objectVisualisations );
 					}
 
-					if( haveMatchingVisualisations( Visualisation::Scale::LocalAndVisualiser, categories, attrVis, m_objectVisualisations ) )
+					if( haveMatchingVisualisations( colorSpace, Visualisation::Scale::LocalAndVisualiser, categories, attrVis, m_objectVisualisations ) )
 					{
 						ScopedTransform c( visualiserTransform( true ) );
-						renderMatchingVisualisations( Visualisation::Scale::LocalAndVisualiser, categories, currentState, attrVis, m_objectVisualisations );
+						renderMatchingVisualisations( colorSpace, Visualisation::Scale::LocalAndVisualiser, categories, currentState, attrVis, m_objectVisualisations );
 					}
 				}
 
-				if( haveMatchingVisualisations( Visualisation::Scale::None, categories, attrVis, m_objectVisualisations ) )
+				if( haveMatchingVisualisations( colorSpace, Visualisation::Scale::None, categories, attrVis, m_objectVisualisations ) )
 				{
 					ScopedTransform l( m_transformSansScale );
-					renderMatchingVisualisations( Visualisation::Scale::None, categories, currentState, attrVis, m_objectVisualisations );
+					renderMatchingVisualisations( colorSpace, Visualisation::Scale::None, categories, currentState, attrVis, m_objectVisualisations );
 				}
 
-				if( m_renderable || haveMatchingVisualisations( Visualisation::Scale::Local, categories, attrVis, m_objectVisualisations ) )
+				if( haveMatchingVisualisations( colorSpace, Visualisation::Scale::Local, categories, attrVis, m_objectVisualisations ) )
 				{
 					ScopedTransform l( m_transform );
-
-					renderMatchingVisualisations( Visualisation::Scale::Local, categories, currentState, attrVis, m_objectVisualisations );
-					if( m_renderable ) { m_renderable->render( currentState ); }
+					renderMatchingVisualisations( colorSpace, Visualisation::Scale::Local, categories, currentState, attrVis, m_objectVisualisations );
 				}
-
 			}
-			else if( m_renderable )
+
+			// Objects are rendered into `ColorSpace::Scene`, with the caveat that selection
+			// overlays and additional visualisations are drawn into `ColorSpace::Display`.
+
+			const IECoreGL::State *visualisationState = m_attributes->visualisationState();
+			if( m_renderable && ( colorSpace == Visualisation::ColorSpace::Scene || isSelected || visualisationState ) )
 			{
+				IECoreGL::State::ScopedBinding stateScope( *m_attributes->state(), *currentState );
+				// We assume that any additional state provided by visualisers
+				// is intended to be drawn in `ColorSpace::Display`. Currently
+				// the only such state is the yellow outline added by the mesh
+				// light visualiser, so it would be premature to extend the API
+				// to allow visualiser state to also be specified for
+				// `ColorSpace::Scene`. In fact, the potential uses for
+				// visualiser state seem very limited.
+				std::optional<IECoreGL::State::ScopedBinding> visualisationStateScope;
+				if( visualisationState && colorSpace == Visualisation::ColorSpace::Display )
+				{
+					visualisationStateScope.emplace( *visualisationState, *currentState );
+				}
+				IECoreGL::State::ScopedBinding selectionScope(
+					selectionState(),
+					*currentState, isSelected && colorSpace == Visualisation::ColorSpace::Display
+				);
+
 				ScopedTransform l( m_transform );
 				m_renderable->render( currentState );
 			}
@@ -909,7 +937,7 @@ class OpenGLRenderer final : public IECoreScenePreview::Renderer
 			}
 			else if( name == "gl:renderToCurrentContext" )
 			{
-				renderToCurrentContext();
+				renderToCurrentContext( parameters );
 				return nullptr;
 			}
 			else if( boost::starts_with( name.string(), "gl:" ) || name.string().find( ":" ) == string::npos )
@@ -922,8 +950,11 @@ class OpenGLRenderer final : public IECoreScenePreview::Renderer
 
 	private :
 
-		void renderToCurrentContext()
+		void renderToCurrentContext( const IECore::CompoundDataMap &parameters )
 		{
+			const string colorSpaceString = parameter<string>( parameters, "colorSpace", "scene" );
+			const Visualisation::ColorSpace colorSpace = colorSpaceString == "scene" ? Visualisation::ColorSpace::Scene : Visualisation::ColorSpace::Display;
+
 			processQueue();
 			removeDeletedObjects();
 			CachedConverter::defaultCachedConverter()->clearUnused();
@@ -962,11 +993,11 @@ class OpenGLRenderer final : public IECoreScenePreview::Renderer
 					IECoreGL::State::ScopedBinding shapeStateBinding(
 						*shapeState, const_cast<IECoreGL::State &>( *state )
 					);
-					renderObjects( state );
+					renderObjects( state, colorSpace );
 				}
 				else
 				{
-					renderObjects( state );
+					renderObjects( state, colorSpace );
 				}
 
 			glPopAttrib();
@@ -1023,7 +1054,11 @@ class OpenGLRenderer final : public IECoreScenePreview::Renderer
 
 				camera->camera()->render( state );
 
-				renderObjects( state );
+				/// \todo Should we separate these into different AOVs rendered
+				/// to different files? Or provide a mechanism for registering
+				/// transforms that we apply ourselves?
+				renderObjects( state, Visualisation::ColorSpace::Scene );
+				renderObjects( state, Visualisation::ColorSpace::Display );
 
 				writeOutputs( frameBuffer.get() );
 
@@ -1081,8 +1116,12 @@ class OpenGLRenderer final : public IECoreScenePreview::Renderer
 			);
 		}
 
-		void renderObjects( IECoreGL::State *currentState )
+		void renderObjects( IECoreGL::State *currentState, Visualisation::ColorSpace colorSpace )
 		{
+			// Stop selection overlays from `ColorSpace::Display` being
+			// depth culled by wireframes in `ColorSpace::Scene.`
+			glDepthFunc( colorSpace == Visualisation::ColorSpace::Display ? GL_LEQUAL : GL_LESS );
+
 			IECoreGL::Selector *selector = IECoreGL::Selector::currentSelector();
 
 			GLuint i = 1;
@@ -1092,7 +1131,7 @@ class OpenGLRenderer final : public IECoreScenePreview::Renderer
 				{
 					selector->loadName( i++ );
 				}
-				o->render( currentState, m_selection );
+				o->render( currentState, m_selection, colorSpace );
 			}
 		}
 

commit f2cc6382a6ea6ffda8be2a2d8b617c4d2541962b

GL Renderer : Fix custom mesh light texture visualisations
These were broken by cfc3857ce0da4c8f77cfc546028f5c7e64aaf585, where we assumed that such visualisations should be rendered with the Display color space. This led to them being rendered after the main Scene color space, meaning that the depth test prevented the texture visualisation from being seen. We now use a heuristic that means such visualisations are rendered first in Scene color space instead.

In theory we could extend the Visualiser API to allow two states to be specified - one for Display space and one for Scene space. But that would require an ABI break, and we want this fix to go into 1.3. We also have so few uses for the visualisation state that it seems like overkill at this point.

Fixes #6002.

src/GafferScene/IECoreGLPreview/Renderer.cpp
@@ -320,7 +320,7 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 	public :
 
 		OpenGLAttributes( const IECore::CompoundObject *attributes )
-			: m_frustumMode( FrustumMode::WhenSelected )
+			:	m_frustumMode( FrustumMode::WhenSelected ), m_visualisationStateColorSpace( Visualisation::ColorSpace::Display )
 		{
 			const FloatData *visualiserScaleData = attributes->member<FloatData>( "gl:visualiser:scale" );
 			m_visualiserScale = visualiserScaleData ? visualiserScaleData->readable() : 1.0;
@@ -388,6 +388,19 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 				}
 
 				m_visualisationState = combinedState;
+				auto solidState = m_visualisationState->get<IECoreGL::Primitive::DrawSolid>();
+				// The Visualiser API doesn't currently allow a colour space to
+				// be associated with the visualisation state. So we use a
+				// heuristic : if the state includes solid drawing then we
+				// assume Scene space. This allows custom mesh light texture
+				// visualisers to be shown with an appropriate colour transform.
+				// Otherwise we assume Display space, which gives us what we
+				// want for the coloured outline from our own mesh light
+				// visualiser.
+				if( !solidState || solidState->value() )
+				{
+					m_visualisationStateColorSpace = Visualisation::ColorSpace::Scene;
+				}
 			}
 		}
 
@@ -396,9 +409,9 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 			return m_state.get();
 		}
 
-		const State *visualisationState() const
+		const State *visualisationState( Visualisation::ColorSpace colorSpace ) const
 		{
-			return m_visualisationState.get();
+			return colorSpace == m_visualisationStateColorSpace ? m_visualisationState.get() : nullptr;
 		}
 
 		const IECoreGLPreview::Visualisations &visualisations() const
@@ -451,6 +464,7 @@ class OpenGLAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 		FrustumMode m_frustumMode;
 
 		float m_visualiserScale = 1.0f;
+		Visualisation::ColorSpace m_visualisationStateColorSpace;
 };
 
 IE_CORE_DECLAREPTR( OpenGLAttributes )
@@ -636,19 +650,12 @@ class OpenGLObject : public IECoreScenePreview::Renderer::ObjectInterface
 			// Objects are rendered into `ColorSpace::Scene`, with the caveat that selection
 			// overlays and additional visualisations are drawn into `ColorSpace::Display`.
 
-			const IECoreGL::State *visualisationState = m_attributes->visualisationState();
+			const IECoreGL::State *visualisationState = m_attributes->visualisationState( colorSpace );
 			if( m_renderable && ( colorSpace == Visualisation::ColorSpace::Scene || isSelected || visualisationState ) )
 			{
 				IECoreGL::State::ScopedBinding stateScope( *m_attributes->state(), *currentState );
-				// We assume that any additional state provided by visualisers
-				// is intended to be drawn in `ColorSpace::Display`. Currently
-				// the only such state is the yellow outline added by the mesh
-				// light visualiser, so it would be premature to extend the API
-				// to allow visualiser state to also be specified for
-				// `ColorSpace::Scene`. In fact, the potential uses for
-				// visualiser state seem very limited.
 				std::optional<IECoreGL::State::ScopedBinding> visualisationStateScope;
-				if( visualisationState && colorSpace == Visualisation::ColorSpace::Display )
+				if( visualisationState )
 				{
 					visualisationStateScope.emplace( *visualisationState, *currentState );
 				}