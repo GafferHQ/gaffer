77420aab8bcc1b041b55c7b36ba40934f421c5a5
Kyle Scott <kyle@iconcreativestudio.com>
2024-10-02 20:14:18 -0700

    minimizing calls to writable for efficiency

--- src/Gaffer/PlugAlgo.cpp
+++ src/Gaffer/PlugAlgo.cpp
@@ -564,90 +564,99 @@ IECore::DataPtr getArrayAsVectorData( const ArrayPlug *plug )
 		case FloatPlugTypeId :
 		{
 			FloatVectorDataPtr data = new FloatVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const FloatPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const FloatPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case IntPlugTypeId :
 		{
 			IntVectorDataPtr data = new IntVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const IntPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const IntPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case Color3fPlugTypeId :
 		{
 			Color3fVectorDataPtr data = new Color3fVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const Color3fPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const Color3fPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case Color4fPlugTypeId :
 		{
 			Color4fVectorDataPtr data = new Color4fVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const Color4fPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const Color4fPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case StringPlugTypeId :
 		{
 			StringVectorDataPtr data = new StringVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const StringPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const StringPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case BoolPlugTypeId :
 		{
 			BoolVectorDataPtr data = new BoolVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const BoolPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const BoolPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case V2fPlugTypeId :
 		{
 			V2fVectorDataPtr data = new V2fVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const V2fPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const V2fPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case V3fPlugTypeId :
 		{
 			V3fVectorDataPtr data = new V3fVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const V3fPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const V3fPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
 		case M44fPlugTypeId :
 		{
 			M44fVectorDataPtr data = new M44fVectorData();
-			data->writable().resize( size );
+			auto &writable = data->writable();
+			writable.resize( size );
 			for( Plug::InputIterator it( plug ); !it.done(); ++it, ++i )
 			{
-				data->writable()[i] = static_cast<const M44fPlug *>( it->get() )->getValue();
+				writable[i] = static_cast<const M44fPlug *>( it->get() )->getValue();
 			}
 			return data;
 		}
