GafferHQ/gaffer

commit 5019e9f17c74a2834e7a21c773647fc5dbc71bc9

View/BackgroundTask : Improve ScriptNode discovery hack
We now use the same method we use for recovering a ScriptNode from a `View.Settings` node (one hack is better than two, right?). This fixes the following warning when deleting the node being viewed in the Viewer :

```
WARNING : BackgroundTask : Unable to find ScriptNode for SceneView.__preprocessor.out
```

In this case, the input to the `SceneView.in` had been removed by the deletion of the node, and therefore there was no path from `SceneView.__preprocessor.out` to the ScriptNode.

src/Gaffer/BackgroundTask.cpp
@@ -97,14 +97,7 @@ const ScriptNode *scriptNode( const GraphComponent *subject )
 	// the ScriptNode from such classes.
 	while( subject )
 	{
-		if( subject->isInstanceOf( "GafferUI::View" ) )
-		{
-			if( auto inPlug = subject->getChild<Plug>( "in" ) )
-			{
-				return scriptNode( inPlug->getInput() );
-			}
-		}
-		else if( subject->isInstanceOf( "GafferUI::Editor::Settings" ) )
+		if( subject->isInstanceOf( "GafferUI::View" ) || subject->isInstanceOf( "GafferUI::Editor::Settings" ) )
 		{
 			if( auto scriptPlug = subject->getChild<Plug>( "__scriptNode" ) )
 			{

python/GafferSceneUITest/LightToolTest.py
@@ -136,13 +136,8 @@ class LightToolTest( GafferUITest.TestCase ) :
 		# we're pretty happy if it doesn't.
 
 		del preRenderSlot[:]
-		with IECore.CapturingMessageHandler() as mh :
-			while not len( preRenderSlot ) :
-				self.waitForIdle( 1000 )
-
-		# Ignore unrelated message from BackgroundTask. This needs a separate fix.
-		self.assertEqual( len( mh.messages ), 1 )
-		self.assertEqual( mh.messages[0].message, "Unable to find ScriptNode for SceneView.__preprocessor.out" )
+		while not len( preRenderSlot ) :
+			self.waitForIdle( 1000 )
 
 if __name__ == "__main__" :
 	unittest.main()

src/GafferUI/View.cpp
@@ -97,6 +97,13 @@ View::View( const std::string &name, Gaffer::ScriptNodePtr scriptNode, Gaffer::P
 	addChild( new Plug( g_editScopeName ) );
 	addChild( new ToolContainer( g_toolsName ) );
 
+	// Hack to allow BackgroundTask to recover ScriptNode for
+	// cancellation support - see `BackgroundTask.cpp` and
+	// `Editor.Settings`.
+	PlugPtr scriptNodePlug = new Plug( "__scriptNode" );
+	addChild( scriptNodePlug );
+	scriptNodePlug->setInput( scriptNode->fileNamePlug() );
+
 	m_context = m_contextTracker->context( this );
 	m_contextTracker->changedSignal( this ).connect( boost::bind( &View::contextTrackerChanged, this ) );
 	tools()->childAddedSignal().connect( boost::bind( &View::toolsChildAdded, this, ::_2 ) );

commit f631c20695face680faf1470d8c9db79ea423056

BackgroundTask : Fix hang when creating Views
This could be triggered randomly when focussing a node for the first time in a Gaffer session. It was caused by 5019e9f17c74a2834e7a21c773647fc5dbc71bc9, which meant that we could get a ScriptNode from a View that was still undergoing construction, whereas before we wouldn't get to the script until it was fully constructed.

Because we could get the ScriptNode during construction, we were also requesting cancellation during construction. And because the View constructors in Python don't release the GIL, any background tasks need the GIL in order to be cancelled will cause deadlock. We fix this by simply not triggering cancellation from a View until it has finished construction, as determined by `refCount()`. This puts us back in the same situation we were in before.

An alternative fix would have been to release the GIL in the bindings for all View constructors. This is a bit fiddly for constructors, but would have been completely doable. I went for the simpler solution because it seems worthwhile to avoid unnecessary cancellation.

src/Gaffer/BackgroundTask.cpp
@@ -99,6 +99,12 @@ const ScriptNode *scriptNode( const GraphComponent *subject )
 	{
 		if( subject->isInstanceOf( "GafferUI::View" ) || subject->isInstanceOf( "GafferUI::Editor::Settings" ) )
 		{
+			if( !subject->refCount() )
+			{
+				// View or Settings still in construction, and therefore can't
+				// be accessed by any background tasks. No need to cancel anything.
+				return nullptr;
+			}
 			if( auto scriptPlug = subject->getChild<Plug>( "__scriptNode" ) )
 			{
 				return scriptNode( scriptPlug->getInput() );

python/GafferSceneUITest/SceneViewTest.py
@@ -35,6 +35,7 @@
 ##########################################################################
 
 import os
+import time
 import unittest
 
 import imath
@@ -482,6 +483,20 @@ class SceneViewTest( GafferUITest.TestCase ) :
 		view.viewportGadget().preRenderSignal()( view.viewportGadget() ) # Force update
 		self.assertEqual( view.viewportGadget().getCamera().getClippingPlanes(), expectedClippingPlanes )
 		self.assertEqual( view["camera"]["clippingPlanes"].getValue(), expectedClippingPlanes )
+
+	def testConstructWhileBackgroundTaskRuns( self ) :
+
+		script = Gaffer.ScriptNode()
+
+		def task( canceller ) :
+
+			while True :
+				IECore.Canceller.check( canceller )
+				time.sleep( 0.01 )
+
+		backgroundTask = Gaffer.BackgroundTask( script["fileName"], task )
+		GafferSceneUI.SceneView( script )
+		backgroundTask.cancelAndWait()
 
 if __name__ == "__main__":
 	unittest.main()