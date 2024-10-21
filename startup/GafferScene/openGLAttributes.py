de6be9fa13052b485b57b34868a6639687e35563
Murray Stevenson <50844517+murraystevenson@users.noreply.github.com>
2024-10-21 16:58:25 +1100

    openGLAttributes : Register attribute metadata
    This completes the registrations to include all attributes editable from OpenGLAttributes.

--- startup/GafferScene/openGLAttributes.py
+++ startup/GafferScene/openGLAttributes.py
@@ -34,8 +34,212 @@
 #
 ##########################################################################
 
+import imath
+
 import IECore
+
 import Gaffer
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:solid", "label", "Shaded" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:solid", "defaultValue", IECore.BoolData( True ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:solid",
+	"description",
+	"""
+	Whether or not the object is rendered solid, in which
+	case the assigned GLSL shader will be used to perform
+	the shading.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:wireframe", "label", "Wireframe" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:wireframe", "defaultValue", IECore.BoolData( False ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:wireframe",
+	"description",
+	"""
+	Whether or not the object is rendered as a wireframe.
+	Use the `gl:primitive:wireframeColor` and `gl:primitive:wireframeWidth`
+	attributes for finer control of the wireframe appearance.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:wireframeColor", "label", "Wireframe Color" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:wireframeColor", "defaultValue", IECore.Color4fData( imath.Color4f( 0.2, 0.2, 0.2, 1.0 ) ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:wireframeColor",
+	"description",
+	"""
+	The colour to use for the wireframe rendering. Only
+	meaningful if wireframe rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:wireframeWidth", "label", "Wireframe Width" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:wireframeWidth", "defaultValue", IECore.FloatData( 1 ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:wireframeWidth",
+	"description",
+	"""
+	The width in pixels of the wireframe rendering. Only
+	meaningful if wireframe rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:outline", "label", "Outline" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:outline", "defaultValue", IECore.BoolData( False ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:outline",
+	"description",
+	"""
+	Whether or not an outline is drawn around the object.
+	Use the `gl:primitive:outlineColor` and `gl:primitive:outlineWidth`
+	attributes for finer control of the outline.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:outlineColor", "label", "Outline Color" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:outlineColor", "defaultValue", IECore.Color4fData( imath.Color4f( 0.85, 0.75, 0.45, 1.0 ) ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:outlineColor",
+	"description",
+	"""
+	The colour to use for the outline. Only
+	meaningful if outline rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:outlineWidth", "label", "Outline Width" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:outlineWidth", "defaultValue", IECore.FloatData( 1 ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:outlineWidth",
+	"description",
+	"""
+	The width in pixels of the outline. Only
+	meaningful if outline rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:points", "label", "Points" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:points", "defaultValue", IECore.BoolData( False ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:points",
+	"description",
+	"""
+	Whether or not the individual points (vertices) of the
+	object are drawn. Use the `gl:primitive:pointColor` and
+	`gl:primitive:pointWidth` attributes for finer control
+	of the point rendering.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:pointColor", "label", "Point Color" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:pointColor", "defaultValue", IECore.Color4fData( imath.Color4f( 1 ) ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:pointColor",
+	"description",
+	"""
+	The colour to use for the point rendering. Only
+	meaningful if point rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:pointWidth", "label", "Point Width" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:pointWidth", "defaultValue", IECore.FloatData( 1 ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:pointWidth",
+	"description",
+	"""
+	The width in pixels of the points. Only
+	meaningful if point rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:bound", "label", "Bound" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:bound", "defaultValue", IECore.BoolData( False ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:bound",
+	"description",
+	"""
+	Whether or not the bounding box of the object is drawn.
+	This is in addition to any drawing of unexpanded bounding
+	boxes that the viewer performs. Use the `gl:primitive:boundColor`
+	attribute to change the colour of the bounding box.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:boundColor", "label", "Bound Color" )
+Gaffer.Metadata.registerValue( "attribute:gl:primitive:boundColor", "defaultValue", IECore.Color4fData( imath.Color4f( 0.36, 0.8, 0.85, 1.0 ) ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:primitive:boundColor",
+	"description",
+	"""
+	The colour to use for the bounding box rendering. Only
+	meaningful if bounding box rendering is turned on.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:pointsPrimitive:useGLPoints", "label", "Use GL Points" )
+Gaffer.Metadata.registerValue( "attribute:gl:pointsPrimitive:useGLPoints", "defaultValue", IECore.StringData( "forGLPoints" ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:pointsPrimitive:useGLPoints",
+	"description",
+	"""
+	Points primitives have a render type (set by the PointsType
+	node) which allows them to be rendered as particles, disks,
+	spheres etc. This attribute overrides that type for OpenGL
+	only, allowing a much faster rendering as raw OpenGL points.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:pointsPrimitive:glPointWidth", "label", "GL Point Width" )
+Gaffer.Metadata.registerValue( "attribute:gl:pointsPrimitive:glPointWidth", "defaultValue", IECore.FloatData( 1 ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:pointsPrimitive:glPointWidth",
+	"description",
+	"""
+	The width in pixels of the GL points rendered when
+	the `gl:pointsPrimitive:useGLPoints` attribute has
+	overridden the point type.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:curvesPrimitive:useGLLines", "label", "Use GL Lines" )
+Gaffer.Metadata.registerValue( "attribute:gl:curvesPrimitive:useGLLines", "defaultValue", IECore.BoolData( False ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:curvesPrimitive:useGLLines",
+	"description",
+	"""
+	Curves primitives are typically rendered as ribbons
+	and as such have an associated width in object space.
+	This attribute overrides that for OpenGL only, allowing
+	a much faster rendering as raw OpenGL lines.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:curvesPrimitive:glLineWidth", "label", "GL Line Width" )
+Gaffer.Metadata.registerValue( "attribute:gl:curvesPrimitive:glLineWidth", "defaultValue", IECore.FloatData( 1 ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:curvesPrimitive:glLineWidth",
+	"description",
+	"""
+	The width in pixels of the GL lines rendered when
+	the `gl:pointsPrimitive:useGLLines` attribute has
+	overridden the drawing to use lines.
+	"""
+)
+
+Gaffer.Metadata.registerValue( "attribute:gl:curvesPrimitive:ignoreBasis", "label", "Ignore Basis" )
+Gaffer.Metadata.registerValue( "attribute:gl:curvesPrimitive:ignoreBasis", "defaultValue", IECore.BoolData( False ) )
+Gaffer.Metadata.registerValue(
+	"attribute:gl:curvesPrimitive:ignoreBasis",
+	"description",
+	"""
+	Turns off interpolation for cubic curves, just
+	rendering straight lines between the vertices
+	instead.
+	"""
+)
 
 Gaffer.Metadata.registerValue( "attribute:gl:light:drawingMode", "label", "Light Drawing Mode" )
 Gaffer.Metadata.registerValue( "attribute:gl:light:drawingMode", "defaultValue", IECore.StringData( "texture" ) )
@@ -48,7 +252,7 @@ Gaffer.Metadata.registerValue(
 )
 
 Gaffer.Metadata.registerValue( "attribute:gl:light:frustumScale", "label", "Light Frustum Scale" )
-Gaffer.Metadata.registerValue( "attribute:gl:light:frustumScale", "defaultValue", IECore.FloatData( 1.0 ) )
+Gaffer.Metadata.registerValue( "attribute:gl:light:frustumScale", "defaultValue", IECore.FloatData( 1 ) )
 Gaffer.Metadata.registerValue(
 	"attribute:gl:light:frustumScale",
 	"description",
@@ -80,7 +284,7 @@ Gaffer.Metadata.registerValue(
 )
 
 Gaffer.Metadata.registerValue( "attribute:gl:visualiser:scale", "label", "Scale" )
-Gaffer.Metadata.registerValue( "attribute:gl:visualiser:scale", "defaultValue", IECore.FloatData( 1.0 ) )
+Gaffer.Metadata.registerValue( "attribute:gl:visualiser:scale", "defaultValue", IECore.FloatData( 1 ) )
 Gaffer.Metadata.registerValue(
 	"attribute:gl:visualiser:scale",
 	"description",
