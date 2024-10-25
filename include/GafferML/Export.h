fd8fe3bf2737d780670855eb18bbe627b71aeae1
John Haddon <thehaddonyoof@gmail.com>
2024-10-25 15:30:28 +0100

    sort out copyright

--- include/GafferML/Export.h
+++ include/GafferML/Export.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2018, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2024, Cinesite VFX Ltd. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
