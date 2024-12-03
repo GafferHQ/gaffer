GafferHQ/gaffer

commit 9e052380bc2c272067827d6d0144b00cb3d41fbb

Signal : Deprecate default value for `scoped` argument
Instead, require that either `scoped = True` or `scoped = False` is passed explicitly. There's a bit of a long back story to this one...

When I first made the signal bindings for Python way back in the mists of time, I made the mistake of _not_ mirroring the C++ API directly. In C++, `connect()` returns a `Connection` object which can be used to manage the connection. In Python I returned a `ScopedConnection` instead, which is like `Connection` but _also_ disconnects the slot automatically when the `ScopedConnection` is destroyed. This naturally lead to confusion :

```
# The slot will never be called, because the returned ScopedConnection is
# destroyed immediately.
signal.connect( mySlot )
# The slot will be called as expected, but finding somewhere to store
# `connection` can be inconvenient, particularly in config files.
connection = signal.connect( mySlot )
```

In `0.14.0.0` we added a `scoped` argument, which can be used to request the same behaviour as the C++ default, with a regular `Connection` object being returned from `connect()`.

```
# Works!
signal.connect( mySlot, scoped = False )
# Still doesn't work - the default is `scoped = True`
signal.connect( mySlot )
```

In `0.54.0.0` we went further, and introduced a new Trackable Python class that negated one of the major reasons for wanting to use `scoped = True` in Python (#3179). By deriving from Trackable, classes get automatic disconnection of their slots on class destruction, regardless of what type of connection they made. This closely matches the typical C++ usage, which is what we are striving for in the Python bindings.

python/GafferUI/GadgetWidget.py
@@ -108,7 +108,7 @@ class GadgetWidget( GafferUI.GLWidget ) :
 			self.__viewportGadget.setVisible( False )
 
 		self.__viewportGadget = viewportGadget
-		self.__renderRequestConnection = self.__viewportGadget.renderRequestSignal().connect( Gaffer.WeakMethod( self.__renderRequest ) )
+		self.__viewportGadget.renderRequestSignal().connect( Gaffer.WeakMethod( self.__renderRequest ), scoped = False )
 		size = self.size()
 		if size.x and size.y :
 			self.__viewportGadget.setViewport( size )

commit 9fab2947fa012f3b93cddd06a071b7495674ab92

GadgetWidget : Fix signal handling bug
This was broken in 9e052380bc2c272067827d6d0144b00cb3d41fbb, which switched from using a scoped to an unscoped connection. We need a scoped connection so that we automatically remove the connection to the previous viewport when making the connection to the new one.

python/GafferUI/GadgetWidget.py
@@ -106,7 +106,7 @@ class GadgetWidget( GafferUI.GLWidget ) :
 			self.__viewportGadget.setVisible( False )
 
 		self.__viewportGadget = viewportGadget
-		self.__viewportGadget.renderRequestSignal().connect( Gaffer.WeakMethod( self.__renderRequest ), scoped = False )
+		self.__renderRequestConnection = self.__viewportGadget.renderRequestSignal().connect( Gaffer.WeakMethod( self.__renderRequest ), scoped = True )
 		size = self.size()
 		if size.x and size.y :
 			self.__viewportGadget.setViewport( size )

python/GafferUITest/GadgetWidgetTest.py
@@ -66,5 +66,18 @@ class GadgetWidgetTest( GafferUITest.TestCase ) :
 		self.assertFalse( vg1.visible() )
 		self.assertFalse( vg2.visible() )
 
+	def testConnectionLifetime( self ) :
+
+		gadgetWidget = GafferUI.GadgetWidget()
+		viewportGadget1 = gadgetWidget.getViewportGadget()
+		self.assertEqual( viewportGadget1.renderRequestSignal().numSlots(), 1 )
+
+		viewportGadget2 = GafferUI.ViewportGadget()
+		self.assertEqual( viewportGadget2.renderRequestSignal().numSlots(), 0 )
+
+		gadgetWidget.setViewportGadget( viewportGadget2 )
+		self.assertEqual( viewportGadget1.renderRequestSignal().numSlots(), 0 )
+		self.assertEqual( viewportGadget2.renderRequestSignal().numSlots(), 1 )
+
 if __name__ == "__main__":
 	unittest.main()