0ca4028473837f525652cb91560032263a170913
Alex Fuller <boberfly@gmail.com>
2025-01-17 18:19:23 +1100

    Python 3.11 support, Py_TYPE(obj) = type has been deprecated in-favour of Py_SET_TYPE(obj, type) https://docs.python.org/3/whatsnew/3.11.html

--- src/GafferBindings/DependencyNodeBinding.cpp
+++ src/GafferBindings/DependencyNodeBinding.cpp
@@ -74,7 +74,7 @@ PyTypeObject *GafferBindings::Detail::dependencyNodeMetaclass()
 		// because it has functionality critical to making the Boost bindings
 		// work. The only thing we're doing is adding `dependencyNodeMetaclassCall`
 		// as the implementation of the `__call__` method.
-		Py_TYPE( &g_dependencyNodeMetaclass ) = &PyType_Type;
+		Py_SET_TYPE( &g_dependencyNodeMetaclass, &PyType_Type );
 		g_dependencyNodeMetaclass.tp_name = "Gaffer.DependencyNodeMetaclass";
 		g_dependencyNodeMetaclass.tp_basicsize = PyType_Type.tp_basicsize,
 		g_dependencyNodeMetaclass.tp_base = boost::python::objects::class_metatype().get();
