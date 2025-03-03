440c78c071ac9f59a6fcf85fc91d242f2b93cff7
Eric Mehl <eric@thinkhypothetical.com>
2025-03-03 14:48:57 -0500

    ShaderNetworkAlgo : `UsdPreviewSurfaceParameters`

--- python/IECoreRenderManTest/ShaderNetworkAlgoTest.py
+++ python/IECoreRenderManTest/ShaderNetworkAlgoTest.py
@@ -48,6 +48,18 @@ class ShaderNetworkAlgoTest( unittest.TestCase ) :
 
 		parameters = {
 			"diffuseColor" : IECore.Color3fData( imath.Color3f( 0.1, 0.2, 0.3 ) ),
+			"emissiveColor" : IECore.Color3fData( imath.Color3f( 0.4, 0.5, 0.6 ) ),
+			"useSpecularWorkflow" : 1,
+			"specularColor" : IECore.Color3fData( imath.Color3f( 0.7, 0.8, 0.9 ) ),
+			"metallic" : 0.5,
+			"roughness" : 0.375,
+			"clearcoat" : 0.25,
+			"clearcoatRoughness" : 0.75,
+			"opacity" : 0.625,
+			"opacityThreshold" : 0.875,
+			"ior" : 1.25,
+			"normal" : IECore.V3fData( imath.V3f( 0.1, 0.2, 0.3 ) ),
+			"occlusion" : 0.5625,
 		}
 
 		network = IECoreScene.ShaderNetwork(
@@ -59,14 +71,55 @@ class ShaderNetworkAlgoTest( unittest.TestCase ) :
 			output = "previewSurface"
 		)
 
-		convertedNetwork = network.copy()
-		IECoreRenderMan.ShaderNetworkAlgo.convertUSDShaders( convertedNetwork )
+		IECoreRenderMan.ShaderNetworkAlgo.convertUSDShaders( network )
 
-		convertedShader = convertedNetwork.getShader( "previewSurface" )
-		self.assertEqual( convertedShader.name, "PxrSurface" )
-		self.assertEqual( convertedShader.type, "ri:surface" )
+		self.assertEqual( len( network ), 2 )
+
+		convertedShader = network.getShader( "previewSurface" )
+		self.assertEqual( convertedShader.name, "__usd/__UsdPreviewSurfaceParameters" )
+		self.assertEqual( convertedShader.type, "osl:shader" )
 
 		self.assertEqual( convertedShader.parameters["diffuseColor"].value, imath.Color3f( 0.1, 0.2, 0.3 ) )
+		self.assertEqual( convertedShader.parameters["emissiveColor"].value, imath.Color3f( 0.4, 0.5, 0.6 ) )
+		self.assertEqual( convertedShader.parameters["useSpecularWorkflow"].value, 1 )
+		self.assertEqual( convertedShader.parameters["specularColor"].value, imath.Color3f( 0.7, 0.8, 0.9 ) )
+		self.assertEqual( convertedShader.parameters["metallic"].value, 0.5 )
+		self.assertEqual( convertedShader.parameters["roughness"].value, 0.375 )
+		self.assertEqual( convertedShader.parameters["clearcoat"].value, 0.25 )
+		self.assertEqual( convertedShader.parameters["clearcoatRoughness"].value, 0.75 )
+		self.assertEqual( convertedShader.parameters["opacity"].value, 0.625 )
+		self.assertEqual( convertedShader.parameters["opacityThreshold"].value, 0.875 )
+		self.assertEqual( convertedShader.parameters["ior"].value, 1.25 )
+		self.assertNotIn( "normal", convertedShader.parameters )
+		self.assertEqual( convertedShader.parameters["normalIn"].value, imath.V3f( 0.1, 0.2, 0.3 ) )
+		self.assertEqual( convertedShader.parameters["occlusion"].value, 0.5625 )
+
+		convertedSurface = network.getShader( "previewSurfacePxrSurface" )
+		self.assertEqual( convertedSurface.name, "PxrSurface" )
+		self.assertEqual( convertedSurface.type, "ri:surface" )
+
+		for pxrSurfaceIn in [
+			"diffuseGain",
+			"diffuseColor",
+			"specularFaceColor",
+			"specularEdgeColor",
+			"specularRoughness",
+			"specularIor",
+			"clearcoatFaceColor",
+			"clearcoatEdgeColor",
+			"clearcoatRoughness",
+			"glowGain",
+			"glowColor",
+			"bumpNormal",
+			"glassIor",
+			"glassRoughness",
+			"refractionGain",
+			"presence",
+		] :
+			self.assertEqual( network.input( ( "previewSurfacePxrSurface", pxrSurfaceIn ) ), ( "previewSurface", pxrSurfaceIn + "Out" ) )
+
+		self.assertEqual( network.getOutput(), ( "previewSurfacePxrSurface", "" ) )
+
 
 if __name__ == "__main__" :
 	unittest.main()
