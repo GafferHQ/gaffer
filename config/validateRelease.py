GafferHQ/gaffer

commit af40ec4ae0da84ec6530a60e1e47851f523a2840

CI : Validate that all releases contain GafferRenderMan

config/validateRelease.py
@@ -81,6 +81,7 @@ sys.stdout.flush()
 requiredPaths = [
 	os.path.join( "resources", "examples" ),
 	"arnold",
+	"renderMan",
 ]
 
 if args.skipDocs :

.github/workflows/main.yml
@@ -256,7 +256,7 @@ jobs:
     - name: Validate
       run: |
         echo "::add-matcher::./.github/workflows/main/problemMatchers/validateRelease.json"
-        python ./config/validateRelease.py --archive ${{ env.GAFFER_BUILD_NAME }}.${{ env.PACKAGE_EXTENSION }} ${{ env.GAFFER_VALIDATE_EXTRA_FLAGS }} ${{ secrets.ARNOLD_DOWNLOAD_USER == '' && '--skipPaths arnold' || '' }}
+        python ./config/validateRelease.py --archive ${{ env.GAFFER_BUILD_NAME }}.${{ env.PACKAGE_EXTENSION }} ${{ env.GAFFER_VALIDATE_EXTRA_FLAGS }} ${{ secrets.ARNOLD_DOWNLOAD_USER == '' && '--skipPaths arnold' || '' }} ${{ secrets.RENDERMAN_DOWNLOAD_USER == '' && '--skipPaths renderMan' || '' }}
         echo "::remove-matcher owner=validateRelease::"
       if: matrix.publish
 

commit 227de470469e0c484eddc5d63afc5b5b30e77110

CI : Fix error in release validation
I broke this in af40ec4ae0da84ec6530a60e1e47851f523a2840, resulting in this error for PRs made from forks :

```
Validation failed
Error: ERROR: arnold is missing from the archive
```

We are passing `--skipPaths arnold --skipPaths renderMan` in this case, so need to accumulate the arguments rather than let the second one clobber the first.

config/validateRelease.py
@@ -62,7 +62,8 @@ parser.add_argument(
 	"--skipPaths",
 	nargs = '+',
 	default = [],
-	help = "A list of paths to skip the checks for."
+	help = "A list of paths to skip the checks for.",
+	action = "extend",
 )
 
 args = parser.parse_args()