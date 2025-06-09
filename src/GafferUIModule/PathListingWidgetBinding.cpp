c43bef3f5817e32719830952cae04f9b38f9c9d7
Murray Stevenson <50844517+murraystevenson@users.noreply.github.com>
2025-06-09 11:55:05 -0700

    PathListingWidgetBinding : Replace use of deprecated `QVariant::Type`

--- src/GafferUIModule/PathListingWidgetBinding.cpp
+++ src/GafferUIModule/PathListingWidgetBinding.cpp
@@ -111,28 +111,28 @@ bool variantLess( const QVariant &left, const QVariant &right )
 
 	switch( left.userType() )
 	{
-		case QVariant::Invalid :
+		case QMetaType::UnknownType :
 			// Both values are invalid, making them equal.
 			return false;
-		case QVariant::Int :
+		case QMetaType::Int :
 			return left.toInt() < right.toInt();
-		case QVariant::UInt :
+		case QMetaType::UInt :
 			return left.toUInt() < right.toUInt();
-		case QVariant::LongLong:
+		case QMetaType::LongLong :
 			return left.toLongLong() < right.toLongLong();
-		case QVariant::ULongLong:
+		case QMetaType::ULongLong :
 			return left.toULongLong() < right.toULongLong();
-		case QMetaType::Float:
+		case QMetaType::Float :
 			return left.toFloat() < right.toFloat();
-		case QVariant::Double:
+		case QMetaType::Double :
 			return left.toDouble() < right.toDouble();
-		case QVariant::Char:
+		case QMetaType::Char :
 			return left.toChar() < right.toChar();
-		case QVariant::Date:
+		case QMetaType::QDate :
 			return left.toDate() < right.toDate();
-		case QVariant::Time:
+		case QMetaType::QTime :
 			return left.toTime() < right.toTime();
-		case QVariant::DateTime:
+		case QMetaType::QDateTime :
 			return left.toDateTime() < right.toDateTime();
 		default :
 			return left.toString() < right.toString();
