5f7adbb94efe2e678df02c2e3b60c8315fe50ff7
Eric Mehl <eric@thinkhypothetical.com>
2025-06-24 16:09:31 -0400

    VisualiserTool : Bias label vertex positions
    When drawing data labels for uniform variables or face indices, we use
    point positions calculated from the average of the face's points. For
    non-planar faces, this can result in the point being behind a face. This
    biases the point positions slightly towards the camera, in clip space,
    to reduce false-negative visibility tests.

--- src/GafferSceneUI/VisualiserTool.cpp
+++ src/GafferSceneUI/VisualiserTool.cpp
@@ -267,7 +267,10 @@ std::string const g_vertexLabelShaderVertSource
 
 	"void main()\n"
 	"{\n"
-	"   gl_Position = vec4( ps, 1.0 ) * uniforms.o2c;\n"
+	"   vec4 p = vec4( ps, 1.0 ) * uniforms.o2c;\n"
+	"   p.z -= 0.001;\n"
+
+	"   gl_Position = p;\n"
 	"   outputs.vertexId = uint( gl_VertexID );\n"
 	"}\n"
 );
