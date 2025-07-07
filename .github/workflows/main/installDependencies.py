4ee96ab7d4ea54f58bed23493491a990c60ad79f
Murray Stevenson <50844517+murraystevenson@users.noreply.github.com>
2025-07-07 10:39:31 -0700

    CI : Update to Cortex 10.5.15.1

--- .github/workflows/main/installDependencies.py
+++ .github/workflows/main/installDependencies.py
@@ -49,7 +49,7 @@ else :
 
 # Determine default archive URL.
 
-defaultURL = "https://github.com/ImageEngine/cortex/releases/download/10.5.15.0/cortex-10.5.15.0-{platform}{buildEnvironment}.{extension}"
+defaultURL = "https://github.com/ImageEngine/cortex/releases/download/10.5.15.1/cortex-10.5.15.1-{platform}{buildEnvironment}.{extension}"
 
 # Parse command line arguments.
 
