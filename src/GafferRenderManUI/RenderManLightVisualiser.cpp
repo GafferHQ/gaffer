151537f1b849125378490c584de649a39069ea05
Eric Mehl <eric@thinkhypothetical.com>
2025-07-11 15:35:06 -0400

    fixup! RenderManLightVisualiser : `PxrPortalLight`

--- src/GafferRenderManUI/RenderManLightVisualiser.cpp
+++ src/GafferRenderManUI/RenderManLightVisualiser.cpp
@@ -544,8 +544,8 @@ Visualisations RenderManLightVisualiser::visualise( const InternedString &attrib
 
 	else if( lightShader->getName() == "PxrPortalLight" )
 	{
-		result.push_back( Visualisation::createGeometry( LightVisualiserAlgo::quadPortal( V2f( 1.f ), /* hatchingScale = */ 1.f, muted ) ) );
-		LightVisualiserAlgo::addRay( V3f( 0.f ), V3f( 0.f, 0.f, -1.f ), ornamentWireframeVertsPerCurve->writable(), ornamentWireframePoints->writable() );
+		result.push_back( Visualisation::createGeometry( quadPortal( V2f( 1.f ), /* hatchingScale = */ 1.f, muted ) ) );
+		addRay( V3f( 0.f ), V3f( 0.f, 0.f, -1.f ), ornamentWireframeVertsPerCurve->writable(), ornamentWireframePoints->writable() );
 	}
 
 	else if( lightShader->getName() == "PxrRectLight" )
