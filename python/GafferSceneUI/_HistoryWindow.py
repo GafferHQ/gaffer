GafferHQ/gaffer

commit 28274546194a16903920e15ed7e337935b5a9740

HistoryWindow : Use `InspectorColumn.historyPath()`
This removes any special knowledge of specific path types, whereas before we were assuming the path had a `setContext()` method and also that it was appropriate to call that using the context from the ContextTracker. We now just track changes to the path the original InspectorColumn was showing, and get a new `historyPath()` from that each time we need one. Therefore we update every time the underlying Editor updates its path, no matter whether that edit is a context change or something else.

This does require us to add `deferUntilVisible = False` to the lazy update methods on LightEditor, RenderPassEditor and AttributeEditor, so that the history window gets updates even when the original editor is not visible. But since their PathListingWidgets won't actually perform an update until _they_ become visible, this isn't a performance concern.

This all makes it possible to use the HistoryWindow with a new Path subclass that an upcoming revamp of the SceneInspector uses.

python/GafferSceneUI/_HistoryWindow.py
@@ -132,15 +132,16 @@ class _ValueColumn( GafferUI.PathColumn ) :
 
 class _HistoryWindow( GafferUI.Window ) :
 
-	def __init__( self, inspector, inspectionPath, title=None, **kw ) :
+	def __init__( self, inspectorColumn, inspectionRootPath, inspectionPathString, title=None, **kw ) :
 
 		if title is None :
 			title = "History"
 
 		GafferUI.Window.__init__( self, title, **kw )
 
-		self.__inspector = inspector
-		self.__inspectionPath = inspectionPath
+		self.__inspectorColumn = inspectorColumn
+		self.__inspectionRootPath = inspectionRootPath
+		self.__inspectionPathString = inspectionPathString
 
 		with self :
 			self.__pathListingWidget = GafferUI.PathListingWidget(
@@ -166,20 +167,16 @@ class _HistoryWindow( GafferUI.Window ) :
 		self.__pathListingWidget.dragBeginSignal().connectFront( Gaffer.WeakMethod( self.__dragBegin ) )
 		self.__pathListingWidget.updateFinishedSignal().connectFront( Gaffer.WeakMethod( self.__updateFinished ) )
 
-		inspector.dirtiedSignal().connect( Gaffer.WeakMethod( self.__inspectorDirtied ) )
+		self.__inspectorColumn.changedSignal().connect( Gaffer.WeakMethod( self.__inspectorColumnChanged ) )
+		self.__inspectionRootPath.pathChangedSignal().connect( Gaffer.WeakMethod( self.__inspectionRootPathChanged ) )
 
-		## \todo We want to make the inspection framework scene-agnostic. We could add an `Inspector::plug()` method
-		# to provide a scene-agnostic way of querying what is being inspected, and use it here.
-		self.__contextTracker = GafferUI.ContextTracker.acquireForFocus( self.__inspectionPath.getScene() )
-		self.__contextTracker.changedSignal().connect( Gaffer.WeakMethod( self.__contextChanged ) )
 		self.__updatePath()
 
 	def __updatePath( self ) :
 
-		self.__inspectionPath.setContext( self.__contextTracker.context( self.__inspectionPath.getScene() ) )
-		with self.__inspectionPath.inspectionContext() :
-			self.__path = self.__inspector.historyPath()
-
+		inspectionPath = self.__inspectionRootPath.copy()
+		inspectionPath.setFromString( self.__inspectionPathString )
+		self.__path = self.__inspectorColumn.historyPath( inspectionPath )
 		self.__pathListingWidget.setPath( self.__path )
 
 	def __buttonDoubleClick( self, pathListing, event ) :
@@ -263,11 +260,11 @@ class _HistoryWindow( GafferUI.Window ) :
 
 		return None
 
-	def __inspectorDirtied( self, inspector ) :
+	def __inspectorColumnChanged( self, inspectorColumn ) :
 
 		self.__updatePath()
 
-	def __contextChanged( self, contextTracker ) :
+	def __inspectionRootPathChanged( self, contextTracker ) :
 
 		self.__updatePath()
 

python/GafferSceneUI/AttributeEditor.py
@@ -194,7 +194,7 @@ class AttributeEditor( GafferSceneUI.SceneEditor ) :
 		if plug in ( self.settings()["section"], self.settings()["tabGroup"] ) :
 			self.__updateColumns()
 
-	@GafferUI.LazyMethod( deferUntilPlaybackStops = True )
+	@GafferUI.LazyMethod( deferUntilVisible = False, deferUntilPlaybackStops = True )
 	def __lazyUpdateFromContext( self ) :
 
 		self.__pathListing.getPath().setContext( self.context() )

python/GafferSceneUI/LightEditor.py
@@ -234,7 +234,7 @@ class LightEditor( GafferSceneUI.SceneEditor ) :
 		if plug in ( self.settings()["section"], self.settings()["attribute"] ) :
 			self.__updateColumns()
 
-	@GafferUI.LazyMethod( deferUntilPlaybackStops = True )
+	@GafferUI.LazyMethod( deferUntilVisible = False, deferUntilPlaybackStops = True )
 	def __lazyUpdateFromContext( self ) :
 
 		self.__pathListing.getPath().setContext( self.context() )

python/GafferSceneUI/RenderPassEditor.py
@@ -277,7 +277,7 @@ class RenderPassEditor( GafferSceneUI.SceneEditor ) :
 		elif plug in ( self.settings()["in"], self.settings()["editScope"] ) :
 			self.__updateButtonStatus()
 
-	@GafferUI.LazyMethod( deferUntilPlaybackStops = True )
+	@GafferUI.LazyMethod( deferUntilVisible = False, deferUntilPlaybackStops = True )
 	def __lazyUpdateFromContext( self ) :
 
 		self.__pathListing.getPath().setContext( self.context() )

python/GafferSceneUI/_InspectorColumn.py
@@ -309,8 +309,9 @@ def __showHistory( pathListing ) :
 			if inspector is None :
 				continue
 			window = _HistoryWindow(
-				inspector,
-				path,
+				column,
+				pathListing.getPath(),
+				pathString,
 				"History : {} : {}".format( pathString, column.headerData().value )
 			)
 			pathListing.ancestor( GafferUI.Window ).addChildWindow( window, removeOnClose = True )

commit 9d1a17f1cfb3de4ad6bea4be3d5c93897c0bb959

HistoryWindow : Fix editing of source plugs
I broke this in 28274546194a16903920e15ed7e337935b5a9740 when removing the `__inspector` member and replacing it with `__inspectorColumn`.

python/GafferSceneUI/_HistoryWindow.py
@@ -41,6 +41,8 @@ import GafferUI
 import GafferScene
 import GafferSceneUI
 
+from . import _GafferSceneUI
+
 class _OperationIconColumn( GafferUI.PathColumn ) :
 
 	def __init__( self ) :
@@ -207,10 +209,15 @@ class _HistoryWindow( GafferUI.Window ) :
 			)
 		elif (
 			( selectedColumn == self.__valueColumnIndex or selectedColumn == self.__operationColumnIndex ) and
-			not isinstance( self.__inspector, GafferSceneUI.Private.SetMembershipInspector )
+			## \todo This is the same method _InspectorColumn.py uses to identify columns that shouldn't
+			# have popup editors shown for them, but we should be able to come up with a better way.
+			# Maybe SetMembershipInspector shouldn't even implement `source()`, since it doesn't meet the
+			# expectation that the plug could sensibly be shown to the user?
+			not isinstance( self.__inspectorColumn, _GafferSceneUI._LightEditorSetMembershipColumn )
 		) :
 			editPlug = selectedPath.property( "history:source" )
 			if editPlug is not None :
+				## \todo It would be nice to implement direct toggling for boolean values here.
 				self.__popup = GafferUI.PlugPopup(
 					[ editPlug ], warning = selectedPath.property( "history:editWarning" )
 				)