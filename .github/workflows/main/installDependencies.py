ff82f7b37f418c25e79a97d62204a6df10e43f34
John Haddon <thehaddonyoof@gmail.com>
2025-08-28 14:59:28 +0100

    CI : Update to Cortex 10.6.0.1
    And 10.5.15.3 for the `linux-gcc11-platform23` variant.

--- .github/workflows/main/installDependencies.py
+++ .github/workflows/main/installDependencies.py
@@ -49,7 +49,7 @@ else :
 
 # Determine default archive URL.
 
-defaultURL = "https://github.com/GafferHQ/dependencies/releases/download/10.0.0a5/gafferDependencies-10.0.0a5-{platform}{buildEnvironment}.{extension}"
+defaultURL = "https://github.com/ImageEngine/cortex/releases/download/10.6.0.1/cortex-10.6.0.1-{platform}{buildEnvironment}.{extension}"
 
 # Parse command line arguments.
 
