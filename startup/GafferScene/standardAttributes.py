7dee0cf57f07a2b77d6ea422970afb99a85bc6d6
John Haddon <thehaddonyoof@gmail.com>
2025-10-28 14:01:53 +0000

    Standard metadata : Prefer dictionary formatting

--- startup/GafferScene/standardAttributes.py
+++ startup/GafferScene/standardAttributes.py
@@ -40,10 +40,10 @@ import Gaffer
 
 Gaffer.Metadata.registerValues( {
 
-	"attribute:scene:visible" : [
+	"attribute:scene:visible" : {
 
-		"defaultValue", True,
-		"description",
+		"defaultValue" : True,
+		"description" :
 		"""
 		Whether or not the object can be seen - invisible objects are
 		not sent to the renderer at all. Typically more fine
@@ -53,31 +53,31 @@ Gaffer.Metadata.registerValues( {
 		always make all the children invisible too, regardless
 		of their visibility settings.
 		""",
-		"category", "Standard",
-		"label", "Visible",
-		"layout:section", "Attributes",
+		"category" : "Standard",
+		"label" : "Visible",
+		"layout:section" : "Attributes",
 
-	],
+	},
 
-	"attribute:doubleSided" : [
+	"attribute:doubleSided" : {
 
-		"defaultValue", True,
-		"description",
+		"defaultValue" : True,
+		"description" :
 		"""
 		Whether or not the object can be seen from both sides.
 		Single sided objects appear invisible when seen from
 		the back.
 		""",
-		"category", "Standard",
-		"label", "Double Sided",
-		"layout:section", "Attributes",
+		"category" : "Standard",
+		"label" : "Double Sided",
+		"layout:section" : "Attributes",
 
-	],
+	},
 
-	"attribute:render:displayColor" : [
+	"attribute:render:displayColor" : {
 
-		"defaultValue", imath.Color3f( 1 ),
-		"description",
+		"defaultValue" : imath.Color3f( 1 ),
+		"description" :
 		"""
 		The default colour used to display the object in the absence
 		of a specific shader assignment. Commonly used to control
@@ -86,91 +86,91 @@ Gaffer.Metadata.registerValues( {
 		> Tip : For more detailed control of object appearance in the
 		> Viewer, use OpenGL attributes.
 		""",
-		"category", "Standard",
-		"label", "Display Color",
-		"layout:section", "Attributes",
+		"category" : "Standard",
+		"label" : "Display Color",
+		"layout:section" : "Attributes",
 
-	],
+	},
 
-	"attribute:gaffer:transformBlur" : [
+	"attribute:gaffer:transformBlur" : {
 
-		"defaultValue", True,
-		"description",
+		"defaultValue" : True,
+		"description" :
 		"""
 		Whether or not transformation animation on the object
 		is taken into account in the rendered image. Use the
 		`gaffer:transformBlurSegments` attribute to specify
 		the number of segments used to represent the motion.
 		""",
-		"category", "Standard",
-		"label", "Transform Blur",
-		"layout:section", "Motion Blur",
+		"category" : "Standard",
+		"label" : "Transform Blur",
+		"layout:section" : "Motion Blur",
 
-	],
+	},
 
-	"attribute:gaffer:transformBlurSegments" : [
+	"attribute:gaffer:transformBlurSegments" : {
 
-		"defaultValue", 1,
-		"minValue", 1,
-		"description",
+		"defaultValue" : 1,
+		"minValue" : 1,
+		"description" :
 		"""
 		The number of segments of transform animation to
 		pass to the renderer when Transform Blur is on.
 		""",
-		"category", "Standard",
-		"label", "Transform Segments",
-		"layout:section", "Motion Blur",
+		"category" : "Standard",
+		"label" : "Transform Segments",
+		"layout:section" : "Motion Blur",
 
-	],
+	},
 
-	"attribute:gaffer:deformationBlur" : [
+	"attribute:gaffer:deformationBlur" : {
 
-		"defaultValue", True,
-		"description",
+		"defaultValue" : True,
+		"description" :
 		"""
 		Whether or not deformation animation on the object
 		is taken into account in the rendered image. Use the
 		`gaffer:deformationBlurSegments` attribute to specify
 		the number of segments used to represent the motion.
 		""",
-		"category", "Standard",
-		"label", "Deformation Blur",
-		"layout:section", "Motion Blur",
+		"category" : "Standard",
+		"label" : "Deformation Blur",
+		"layout:section" : "Motion Blur",
 
-	],
+	},
 
-	"attribute:gaffer:deformationBlurSegments" : [
+	"attribute:gaffer:deformationBlurSegments" : {
 
-		"defaultValue", 1,
-		"minValue", 1,
-		"description",
+		"defaultValue" : 1,
+		"minValue" : 1,
+		"description" :
 		"""
 		The number of segments of deformation animation to
 		pass to the renderer when Deformation Blur is on.
 		""",
-		"category", "Standard",
-		"label", "Deformation Segments",
-		"layout:section", "Motion Blur",
+		"category" : "Standard",
+		"label" : "Deformation Segments",
+		"layout:section" : "Motion Blur",
 
-	],
+	},
 
-	"attribute:light:mute" : [
+	"attribute:light:mute" : {
 
-		"defaultValue", False,
-		"description",
+		"defaultValue" : False,
+		"description" :
 		"""
 		Whether this light is muted.
 		""",
-		"category", "Standard",
-		"label", "Mute",
-		"layout:section", "Light",
+		"category" : "Standard",
+		"label" : "Mute",
+		"layout:section" : "Light",
 
-	],
+	},
 
-	"attribute:linkedLights" : [
+	"attribute:linkedLights" : {
 
-		"defaultValue", "defaultLights",
-		"description",
+		"defaultValue" : "defaultLights",
+		"description" :
 		"""
 		The lights to be linked to this object. Accepts a set expression or
 		a space separated list of lights. Use \"defaultLights\" to refer to
@@ -192,55 +192,55 @@ Gaffer.Metadata.registerValues( {
 		> Info : Lights can be added to sets either by using the `sets` plug
 		> on the light node itself, or by using a separate Set node.
 		""",
-		"category", "Standard",
-		"label", "Linked Lights",
-		"layout:section", "Light Linking",
+		"category" : "Standard",
+		"label" : "Linked Lights",
+		"layout:section" : "Light Linking",
 
-		"plugValueWidget:type", "GafferSceneUI.SetExpressionPlugValueWidget",
-		"ui:scene:acceptsSetExpression", True
+		"plugValueWidget:type" : "GafferSceneUI.SetExpressionPlugValueWidget",
+		"ui:scene:acceptsSetExpression" : True
 
-	],
+	},
 
-	"attribute:shadowedLights" : [
+	"attribute:shadowedLights" : {
 
-		"defaultValue", "__lights",
-		"description",
+		"defaultValue" : "__lights",
+		"description" :
 		"""
 		The lights that cast shadows from this object. Accepts a set
 		expression or a space separated list of lights.
 		""",
-		"category", "Standard",
-		"label", "Shadowed Lights",
-		"layout:section", "Light Linking",
+		"category" : "Standard",
+		"label" : "Shadowed Lights",
+		"layout:section" : "Light Linking",
 
-		"plugValueWidget:type", "GafferSceneUI.SetExpressionPlugValueWidget",
-		"ui:scene:acceptsSetExpression", True,
+		"plugValueWidget:type" : "GafferSceneUI.SetExpressionPlugValueWidget",
+		"ui:scene:acceptsSetExpression" : True,
 
-	],
+	},
 
-	"attribute:filteredLights" : [
+	"attribute:filteredLights" : {
 
-		"defaultValue", "",
-		"description",
+		"defaultValue" : "",
+		"description" :
 		"""
 		The lights to be filtered by this light filter. Accepts a
 		set expression or a space separated list of lights.
 		Use \"defaultLights\" to refer to all lights that
 		contribute to illumination by default.
 		""",
-		"category", "Standard",
-		"label", "Filtered Lights",
-		"layout:section", "Light Linking",
+		"category" : "Standard",
+		"label" : "Filtered Lights",
+		"layout:section" : "Light Linking",
 
-		"plugValueWidget:type", "GafferSceneUI.SetExpressionPlugValueWidget",
-		"ui:scene:acceptsSetExpression", True,
+		"plugValueWidget:type" : "GafferSceneUI.SetExpressionPlugValueWidget",
+		"ui:scene:acceptsSetExpression" : True,
 
-	],
+	},
 
-	"attribute:gaffer:automaticInstancing" : [
+	"attribute:gaffer:automaticInstancing" : {
 
-		"defaultValue", True,
-		"description",
+		"defaultValue" : True,
+		"description" :
 		"""
 		By default, if Gaffer sees two objects are identical, it will pass them
 		to the renderer only once, saving a lot of memory. You can set this to
@@ -249,35 +249,35 @@ Gaffer.Metadata.registerValues( {
 		copies to displace differently. Disabling is currently only supported by
 		the Arnold and RenderMan renderer backends.
 		""",
-		"category", "Standard",
-		"label", "Automatic Instancing",
-		"layout:section", "Instancing",
+		"category" : "Standard",
+		"label" : "Automatic Instancing",
+		"layout:section" : "Instancing",
 
-	],
+	},
 
-	"attribute:surface" : [
+	"attribute:surface" : {
 
-		"category", "Standard",
+		"category" : "Standard",
 
-	],
+	},
 
-	"attribute:displacement" : [
+	"attribute:displacement" : {
 
-		"category", "Standard",
+		"category" : "Standard",
 
-	],
+	},
 
-	"attribute:volume" : [
+	"attribute:volume" : {
 
-		"category", "Standard",
+		"category" : "Standard",
 
-	],
+	},
 
-	"attribute:light" : [
+	"attribute:light" : {
 
-		"category", "Standard",
+		"category" : "Standard",
 
-	],
+	},
 
 } )
 
