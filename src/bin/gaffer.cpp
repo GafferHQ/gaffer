d9ccbf72c2cebba551f10ac26cbc4092206952b4
Eric Mehl <eric@thinkhypothetical.com>
2025-12-10 16:58:26 -0500

    WIP : Windows TBB memory handler

--- src/bin/gaffer.cpp
+++ src/bin/gaffer.cpp
@@ -3,6 +3,10 @@
 #include "Python.h"
 
 #ifdef MS_WINDOWS
+
+#pragma comment(lib, "tbbmalloc_proxy.lib")
+#pragma comment(linker, "/include:__TBB_malloc_proxy")
+
 int wmain( int argc, wchar_t **argv )
 {
 	return Py_Main( argc, argv );
