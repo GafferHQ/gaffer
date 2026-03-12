GafferHQ/gaffer

commit 8efd84d6915ad4141744895a41a0d75a6b983e60

PlugPopup : Improve default title
- Use Gaffer.MetadataAlgo.firstViewableAncestor to hide non-viewable node internals.
- Add "Editing" prefix to match the language used in other parts of the UI, such as TransformToolUI.
- Adjust formatting so only the target node is in bold.

python/GafferUI/PlugPopup.py
@@ -64,29 +64,34 @@ class PlugPopup( GafferUI.PopupWindow ) :
 
 				# Make default title
 
-				if len( plugs ) == 1 :
-					title = plugs[0].relativeName( script )
-				else :
-					nodes = { plug.node() for plug in plugs }
-					title = "{} plugs{}".format(
+				commonNode = Gaffer.MetadataAlgo.firstViewableNode( plugs[0] )
+
+				if len( plugs ) > 1 :
+					nodes = { Gaffer.MetadataAlgo.firstViewableNode( plug ) for plug in plugs }
+					plugSummary = "{} plugs{}".format(
 						len( plugs ),
 						" on {} nodes".format( len( nodes ) ) if len( nodes ) > 1 else ""
 					)
 
-					commonNode = plugs[0].node()
-					for plug in plugs :
+					for plug in plugs[1:] :
 						if not commonNode.isAncestorOf( plug ) :
 							commonNode = commonNode.commonAncestor( plug.node() )
 
-					if commonNode != script :
-						title = "{} ({})".format(
-							commonNode.relativeName( script ),
-							title
-						)
+				else :
+					plugSummary = ""
+
+				target = "<b>{}</b>".format( commonNode.relativeName( script ) ) if script.isAncestorOf( commonNode ) else ""
+				title = "Editing {}{}".format(
+					target,
+					" ({})".format( plugSummary ) if plugSummary != "" and target != "" else plugSummary
+				)
+
+			else :
+				title = "<h4>{}</h4>".format( title )
 
 			if title :
 
-				titleWidget = GafferUI.Label( "<h4>{}</h4>".format( title ) )
+				titleWidget = GafferUI.Label( title )
 				titleWidget.setToolTip(
 					"\n".join( "- " + p.relativeName( script ) for p in plugs )
 				)

commit 3d84e891b07c6e6c785d61d57f61c85d4e0a8499

PlugPopup : Fix unwanted empty title space in Spreadsheet popups
This was broken by 8efd84d6915ad4141744895a41a0d75a6b983e60.

python/GafferUI/PlugPopup.py
@@ -86,7 +86,7 @@ class PlugPopup( GafferUI.PopupWindow ) :
 					" ({})".format( plugSummary ) if plugSummary != "" and target != "" else plugSummary
 				)
 
-			else :
+			elif title :
 				title = "<h4>{}</h4>".format( title )
 
 			if title :