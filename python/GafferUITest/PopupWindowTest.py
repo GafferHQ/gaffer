GafferHQ/gaffer

commit a13abf4d6d5d3a78e3f7239b0be24ede20d06fe7

PopupWindow : Fix problems when used as VectorDataWidget editor
If we close the window before the edit is committed, Qt outputs this message :

```
QAbstractItemView::commitData called with an editor that does not belong to this view
```

This could be seen when hitting Return in one of the numeric fields of a popup colour picker in a VectorDataWidget (a RandomChoice node is an easy way to test). Qt does it's own auto-close for editors anyway, so we can just skip ours.

Closing later triggers another problem though - an expired WeakMethod for the `paintEvent`. We avoid that by using a regular function instead of a method - we didn't need access to `self` anyway.

Not adding an entry to Changes.md because the colour picker didn't even show until the fix in 1c58086ab80b17471e41dce5d220fe679cbfe1a7 and I expect this commit to go out in the same release as that.

python/GafferUI/PopupWindow.py
@@ -54,7 +54,7 @@ class PopupWindow( GafferUI.Window ) :
 
 		self._qtWidget().setWindowFlags( QtCore.Qt.Popup )
 		self._qtWidget().setAttribute( QtCore.Qt.WA_TranslucentBackground )
-		self._qtWidget().paintEvent = Gaffer.WeakMethod( self.__paintEvent )
+		self._qtWidget().paintEvent = _paintWindowBackground.__get__( self._qtWidget() )
 
 		self.keyPressSignal().connect( Gaffer.WeakMethod( self.__keyPress ) )
 
@@ -94,19 +94,26 @@ class PopupWindow( GafferUI.Window ) :
 
 		cls.__warningPopup.popup( parent = parent, center = center )
 
-	def __paintEvent( self, event ) :
-
-		painter = QtGui.QPainter( self._qtWidget() )
-		painter.setRenderHint( QtGui.QPainter.Antialiasing )
-
-		painter.setBrush( QtGui.QColor( 35, 35, 35 ) )
-		painter.setPen( QtGui.QColor( 0, 0, 0, 0 ) )
-
-		radius = self._qtWidget().layout().contentsMargins().left()
-		size = self.size()
-		painter.drawRoundedRect( QtCore.QRectF( 0, 0, size.x, size.y ), radius, radius )
-
 	def __keyPress( self, widget, event ) :
 
+		## \todo This automatic-close behaviour is not always wanted.
+		# `PlugPopup` has its own version anyway and we have to disable it
+		# manually for `VectorDataWidget`. I suspect we can consolidate
+		# most other usages into a `PopupWindow.showWarning()` function,
+		# and move the auto-close there.
 		if event.key == "Return" :
-			self.close()
+			# The QAbstractItemView in VectorDataWidget handles auto-close
+			# itself, and gets upset if we do it first.
+			if self.ancestor( GafferUI.VectorDataWidget ) is None :
+				self.close()
+
+def _paintWindowBackground( qWidget, event ) :
+
+	painter = QtGui.QPainter( qWidget )
+	painter.setRenderHint( QtGui.QPainter.Antialiasing )
+
+	painter.setBrush( QtGui.QColor( 35, 35, 35 ) )
+	painter.setPen( QtGui.QColor( 0, 0, 0, 0 ) )
+
+	radius = qWidget.layout().contentsMargins().left()
+	painter.drawRoundedRect( QtCore.QRectF( 0, 0, qWidget.width(), qWidget.height() ), radius, radius )

commit 4bac9efd5118de78889fbfcdc5d174f4dea47bd3

PopupWindow : Fix circular reference involving QWidget
As the new unit test demonstrates, this can lead to PySide crashes if the widget is later garbage collected on a background thread. We believe this test models crashes that we have seen both at Image Engine and Cinesite. In Image Engine's case the popup was commonly in a Spreadsheet, and in Cinesite's case the common trigger seemed to have been a custom render pass name widget that the RenderPassEditor pops up.

The circular reference was caused by a13abf4, where I claimed to be "using a regular function rather than a method". But what I failed to understand was that `__get__` was actually creating a bound method, and hence a circular reference. The solution is to revert to the original code, using WeakMethod to avoid the circular reference. This has the problem that motivated the change in the first place though - warnings about an expired WeakMethod when closing a popup editor in VectorDataWidget. This time we fix them by passing `fallbackResult = None` to WeakMethod, so it doesn't complain about expiration.

python/GafferUI/PopupWindow.py
@@ -54,7 +54,7 @@ class PopupWindow( GafferUI.Window ) :
 
 		self._qtWidget().setWindowFlags( QtCore.Qt.Popup | QtCore.Qt.FramelessWindowHint )
 		self._qtWidget().setAttribute( QtCore.Qt.WA_TranslucentBackground )
-		self._qtWidget().paintEvent = _paintWindowBackground.__get__( self._qtWidget() )
+		self._qtWidget().paintEvent = Gaffer.WeakMethod( self.__paintEvent, fallbackResult = None )
 
 		self.keyPressSignal().connect( Gaffer.WeakMethod( self.__keyPress ) )
 
@@ -94,6 +94,18 @@ class PopupWindow( GafferUI.Window ) :
 
 		cls.__warningPopup.popup( parent = parent, center = center )
 
+	def __paintEvent( self, event ) :
+
+		painter = QtGui.QPainter( self._qtWidget() )
+		painter.setRenderHint( QtGui.QPainter.Antialiasing )
+
+		painter.setBrush( QtGui.QColor( 35, 35, 35 ) )
+		painter.setPen( QtGui.QColor( 0, 0, 0, 0 ) )
+
+		radius = self._qtWidget().layout().contentsMargins().left()
+		size = self.size()
+		painter.drawRoundedRect( QtCore.QRectF( 0, 0, size.x, size.y ), radius, radius )
+
 	def __keyPress( self, widget, event ) :
 
 		## \todo This automatic-close behaviour is not always wanted.
@@ -106,14 +118,3 @@ class PopupWindow( GafferUI.Window ) :
 			# itself, and gets upset if we do it first.
 			if self.ancestor( GafferUI.VectorDataWidget ) is None :
 				self.close()
-
-def _paintWindowBackground( qWidget, event ) :
-
-	painter = QtGui.QPainter( qWidget )
-	painter.setRenderHint( QtGui.QPainter.Antialiasing )
-
-	painter.setBrush( QtGui.QColor( 35, 35, 35 ) )
-	painter.setPen( QtGui.QColor( 0, 0, 0, 0 ) )
-
-	radius = qWidget.layout().contentsMargins().left()
-	painter.drawRoundedRect( QtCore.QRectF( 0, 0, qWidget.width(), qWidget.height() ), radius, radius )

python/GafferUITest/PopupWindowTest.py
@@ -34,9 +34,13 @@
 #
 ##########################################################################
 
+import gc
 import unittest
 import weakref
 
+import IECore
+
+import Gaffer
 import GafferUI
 import GafferUITest
 
@@ -75,6 +79,25 @@ class PopupWindowTest( GafferUITest.TestCase ) :
 
 		self.assertIsNone( weakWindow() )
 		self.assertIsNone( weakPopup() )
+
+	def testGarbageCollectionOnBackgroundThread( self ) :
+
+		# Make a PopupWindow, and delete it.
+		with GafferUI.PopupWindow() as p :
+			p = GafferUI.VectorDataWidget( IECore.IntVectorData() )
+		del p
+
+		# If the above were to create any reference cycles involving a QWidget,
+		# then the QWidget will eventually be destroyed by Python's garbage
+		# collector. We don't control when that runs, and if it runs on a
+		# background thread then PySide will attempt to send the C++ QWidget to
+		# the main thread using `Py_AddPendingCall()`, to be deleted there later.
+		# That causes crashes in `~QObject` in `Shiboken::BindingManager::runDeletionInMainThread()`,
+		# potentially due to double-deletion of the QObject. Our conclusion :
+		# PySide is buggy, and we must never make reference cycles involving
+		# QWidget.
+		b = Gaffer.ParallelAlgo.callOnBackgroundThread( None, gc.collect )
+		b.wait()
 
 if __name__ == "__main__":
 	unittest.main()