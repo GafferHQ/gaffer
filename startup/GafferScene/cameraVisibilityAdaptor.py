GafferHQ/gaffer

commit 8f52d5f75e9bab965dfd599cccbdbefb8c8a71b8

cameraVisibilityAdaptor, matteAdaptor : Only output attributes for the current renderer
This is not strictly necessary for 1.6, it just conforms the behaviour of these adaptors so users only see the attributes for the current renderer in the Scene Inspector after specifying the `render:defaultRenderer` option. This could otherwise be held back for `main`.

startup/GafferScene/cameraVisibilityAdaptor.py
@@ -43,9 +43,22 @@ import GafferScene
 
 def __cameraVisibilityAdaptor() :
 
-	__cameraVisibilityAttributes = [ "ai:visibility:camera", "cycles:visibility:camera", "dl:visibility.camera", "ri:visibility:camera" ]
-
 	processor = GafferScene.SceneProcessor()
+
+	processor["renderer"] = Gaffer.StringPlug()
+
+	processor["__attributeSpreadsheet"] = Gaffer.Spreadsheet()
+	processor["__attributeSpreadsheet"]["selector"].setInput( processor["renderer"] )
+	processor["__attributeSpreadsheet"]["rows"].addColumn( Gaffer.StringPlug( "name" ) )
+	for renderer, attributeName in (
+		( "Arnold", "ai:visibility:camera" ),
+		( "Cycles", "cycles:visibility:camera" ),
+		( "3Delight*", "dl:visibility.camera" ),
+		( "RenderMan*", "ri:visibility:camera" )
+	) :
+		row = processor["__attributeSpreadsheet"]["rows"].addRow()
+		row["name"].setValue( renderer )
+		row["cells"]["name"]["value"].setValue( attributeName )
 
 	processor["__optionQuery"] = GafferScene.OptionQuery()
 	processor["__optionQuery"]["scene"].setInput( processor["in"] )
@@ -75,8 +88,8 @@ def __cameraVisibilityAdaptor() :
 
 	processor["__cameraInclusions"] = GafferScene.AttributeTweaks()
 	processor["__cameraInclusions"]["in"].setInput( processor["in"] )
-	for attribute in __cameraVisibilityAttributes :
-		processor["__cameraInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attribute, Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__cameraInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__cameraInclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
 
 	processor["__cameraInclusions"]["filter"].setInput( processor["__cameraInclusionsFilter"]["out"] )
 	# __cameraInclusions is only required when `render:cameraInclusions` exists
@@ -87,8 +100,8 @@ def __cameraVisibilityAdaptor() :
 
 	processor["__cameraExclusions"] = GafferScene.AttributeTweaks()
 	processor["__cameraExclusions"]["in"].setInput( processor["__cameraInclusions"]["out"] )
-	for attribute in __cameraVisibilityAttributes :
-		processor["__cameraExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attribute, Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__cameraExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__cameraExclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
 
 	processor["__cameraExclusions"]["filter"].setInput( processor["__cameraExclusionsFilter"]["out"] )
 	# __cameraExclusions is only required when `render:cameraExclusions` exists
@@ -99,8 +112,8 @@ def __cameraVisibilityAdaptor() :
 
 	processor["__rootExclusions"] = GafferScene.AttributeTweaks()
 	processor["__rootExclusions"]["in"].setInput( processor["__cameraExclusions"]["out"] )
-	for attribute in __cameraVisibilityAttributes :
-		processor["__rootExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attribute, Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__rootExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__rootExclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
 
 	processor["__rootExclusions"]["filter"].setInput( processor["__rootPaths"]["out"] )
 	# All children of / are excluded from camera visibility when `render:cameraInclusions` exists and does not match the root of the scene.

startup/GafferScene/matteAdaptor.py
@@ -43,9 +43,22 @@ import GafferScene
 
 def __matteAdaptor() :
 
-	__matteAttributes = [ "ai:matte", "cycles:use_holdout", "dl:matte", "ri:Ri:Matte" ]
-
 	processor = GafferScene.SceneProcessor()
+
+	processor["renderer"] = Gaffer.StringPlug()
+
+	processor["__attributeSpreadsheet"] = Gaffer.Spreadsheet()
+	processor["__attributeSpreadsheet"]["selector"].setInput( processor["renderer"] )
+	processor["__attributeSpreadsheet"]["rows"].addColumn( Gaffer.StringPlug( "name" ) )
+	for renderer, attributeName in (
+		( "Arnold", "ai:matte" ),
+		( "Cycles", "cycles:use_holdout" ),
+		( "3Delight*", "dl:matte" ),
+		( "RenderMan*", "ri:Ri:Matte" )
+	) :
+		row = processor["__attributeSpreadsheet"]["rows"].addRow()
+		row["name"].setValue( renderer )
+		row["cells"]["name"]["value"].setValue( attributeName )
 
 	processor["__optionQuery"] = GafferScene.OptionQuery()
 	processor["__optionQuery"]["scene"].setInput( processor["in"] )
@@ -75,8 +88,8 @@ def __matteAdaptor() :
 
 	processor["__matteInclusions"] = GafferScene.AttributeTweaks()
 	processor["__matteInclusions"]["in"].setInput( processor["in"] )
-	for attribute in __matteAttributes :
-		processor["__matteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attribute, Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__matteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__matteInclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
 
 	processor["__matteInclusions"]["filter"].setInput( processor["__matteInclusionsFilter"]["out"] )
 	# __matteInclusions is only required when `render:matteInclusions` exists and the root of the scene hasn't been set as matte
@@ -90,8 +103,8 @@ def __matteAdaptor() :
 
 	processor["__matteExclusions"] = GafferScene.AttributeTweaks()
 	processor["__matteExclusions"]["in"].setInput( processor["__matteInclusions"]["out"] )
-	for attribute in __matteAttributes :
-		processor["__matteExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attribute, Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__matteExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__matteExclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
 
 	processor["__matteExclusions"]["filter"].setInput( processor["__matteExclusionsFilter"]["out"] )
 	# __matteExclusions is only required when `render:matteExclusions` exists
@@ -102,8 +115,8 @@ def __matteAdaptor() :
 
 	processor["__rootMatteInclusions"] = GafferScene.AttributeTweaks()
 	processor["__rootMatteInclusions"]["in"].setInput( processor["__matteExclusions"]["out"] )
-	for attribute in __matteAttributes :
-		processor["__rootMatteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attribute, Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__rootMatteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
+	processor["__rootMatteInclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
 
 	processor["__rootMatteInclusions"]["filter"].setInput( processor["__rootPaths"]["out"] )
 	# All children of / are matte if `render:matteInclusions` matches the root of the scene.

python/GafferSceneTest/RenderAdaptorTest.py
@@ -476,30 +476,42 @@ class RenderAdaptorTest( GafferSceneTest.SceneTestCase ) :
 				"/groupA/sphere",
 			}
 
-			renderer = GafferScene.Private.IECoreScenePreview.CapturingRenderer(
-				GafferScene.Private.IECoreScenePreview.Renderer.RenderType.Batch
-			)
-			GafferScene.Private.RendererAlgo.outputObjects(
-				testAdaptors["out"], GafferScene.Private.RendererAlgo.RenderOptions( testAdaptors["out"] ),
-				GafferScene.Private.RendererAlgo.RenderSets( testAdaptors["out"] ),
-				GafferScene.Private.RendererAlgo.LightLinks( renderer ),
-				renderer
-			)
+			for rendererName, attribute in (
+				( "Arnold", "ai:visibility:camera" ),
+				( "Cycles", "cycles:visibility:camera" ),
+				( "3Delight", "dl:visibility.camera" ),
+				( "3Delight Cloud", "dl:visibility.camera" ),
+				( "RenderMan", "ri:visibility:camera" ),
+				( "RenderMan XPU", "ri:visibility:camera" ),
+			) :
 
-			if paths != {} :
-				self.assertTrue( paths.issubset( allPaths ) )
+				with self.subTest( rendererName = rendererName, attribute = attribute ) :
 
-			for path in allPaths :
-				capturedObject = renderer.capturedObject( path )
-				for attribute in [ "ai:visibility:camera", "cycles:visibility:camera", "dl:visibility.camera", "ri:visibility:camera" ] :
-					if path in paths :
-						# path is visible by the absence of the attribute, or its presence with a value of True
-						if attribute in capturedObject.capturedAttributes().attributes() :
-							self.assertTrue( capturedObject.capturedAttributes().attributes()[attribute].value )
-					else :
-						# path is invisible only by the presence of the attribute with a value of False
-						self.assertTrue( attribute in capturedObject.capturedAttributes().attributes() )
-						self.assertFalse( capturedObject.capturedAttributes().attributes()[attribute].value )
+					testAdaptors["renderer"].setValue( rendererName )
+
+					renderer = GafferScene.Private.IECoreScenePreview.CapturingRenderer(
+						GafferScene.Private.IECoreScenePreview.Renderer.RenderType.Batch
+					)
+					GafferScene.Private.RendererAlgo.outputObjects(
+						testAdaptors["out"], GafferScene.Private.RendererAlgo.RenderOptions( testAdaptors["out"] ),
+						GafferScene.Private.RendererAlgo.RenderSets( testAdaptors["out"] ),
+						GafferScene.Private.RendererAlgo.LightLinks( renderer ),
+						renderer
+					)
+
+					if paths != {} :
+						self.assertTrue( paths.issubset( allPaths ) )
+
+					for path in allPaths :
+						capturedObject = renderer.capturedObject( path )
+						if path in paths :
+							# path is visible by the absence of the attribute, or its presence with a value of True
+							if attribute in capturedObject.capturedAttributes().attributes() :
+								self.assertTrue( capturedObject.capturedAttributes().attributes()[attribute].value )
+						else :
+							# path is invisible only by the presence of the attribute with a value of False
+							self.assertTrue( attribute in capturedObject.capturedAttributes().attributes() )
+							self.assertFalse( capturedObject.capturedAttributes().attributes()[attribute].value )
 
 		# By default everything should be camera visible
 		assertCameraVisibleObjects( { "/groupA/cube", "/groupA/sphere" } )
@@ -619,27 +631,39 @@ class RenderAdaptorTest( GafferSceneTest.SceneTestCase ) :
 				"/groupA/sphere",
 			}
 
-			renderer = GafferScene.Private.IECoreScenePreview.CapturingRenderer(
-				GafferScene.Private.IECoreScenePreview.Renderer.RenderType.Batch
-			)
-			GafferScene.Private.RendererAlgo.outputObjects(
-				testAdaptors["out"], GafferScene.Private.RendererAlgo.RenderOptions( testAdaptors["out"] ),
-				GafferScene.Private.RendererAlgo.RenderSets( testAdaptors["out"] ),
-				GafferScene.Private.RendererAlgo.LightLinks( renderer ),
-				renderer
-			)
+			for rendererName, attribute in (
+				( "Arnold", "ai:matte" ),
+				( "Cycles", "cycles:use_holdout" ),
+				( "3Delight", "dl:matte" ),
+				( "3Delight Cloud", "dl:matte" ),
+				( "RenderMan", "ri:Ri:Matte" ),
+				( "RenderMan XPU", "ri:Ri:Matte" ),
+			) :
 
-			for path in allPaths :
-				capturedObject = renderer.capturedObject( path )
-				for attribute in [ "ai:matte", "cycles:use_holdout", "dl:matte", "ri:Ri:Matte" ] :
-					if path in paths :
-						# path is matte only by the presence of the attribute with a value of True
-						self.assertTrue( attribute in capturedObject.capturedAttributes().attributes() )
-						self.assertTrue( capturedObject.capturedAttributes().attributes()[attribute].value )
-					else :
-						# path isn't matte by the absence of the attribute, or its presence with a value of False
-						if attribute in capturedObject.capturedAttributes().attributes() :
-							self.assertFalse( capturedObject.capturedAttributes().attributes()[attribute].value )
+				with self.subTest( rendererName = rendererName, attribute = attribute ) :
+
+					testAdaptors["renderer"].setValue( rendererName )
+
+					renderer = GafferScene.Private.IECoreScenePreview.CapturingRenderer(
+						GafferScene.Private.IECoreScenePreview.Renderer.RenderType.Batch
+					)
+					GafferScene.Private.RendererAlgo.outputObjects(
+						testAdaptors["out"], GafferScene.Private.RendererAlgo.RenderOptions( testAdaptors["out"] ),
+						GafferScene.Private.RendererAlgo.RenderSets( testAdaptors["out"] ),
+						GafferScene.Private.RendererAlgo.LightLinks( renderer ),
+						renderer
+					)
+
+					for path in allPaths :
+						capturedObject = renderer.capturedObject( path )
+						if path in paths :
+							# path is matte only by the presence of the attribute with a value of True
+							self.assertTrue( attribute in capturedObject.capturedAttributes().attributes() )
+							self.assertTrue( capturedObject.capturedAttributes().attributes()[attribute].value )
+						else :
+							# path isn't matte by the absence of the attribute, or its presence with a value of False
+							if attribute in capturedObject.capturedAttributes().attributes() :
+								self.assertFalse( capturedObject.capturedAttributes().attributes()[attribute].value )
 
 		# Nothing should be matte when matte inclusions and exclusions are empty or undefined
 		assertMatte( {} )

commit 288b0ffb5c59b3d783657def2555de7736a41eb7

cameraVisibilityAdaptor, matteAdaptor : Output all attributes to OpenGL
The previous change in 8f52d5f75e9bab965dfd599cccbdbefb8c8a71b8 introduced a regression in behaviour when attempting to visualise these attributes in OpenGL, such as when using the Viewer's renderer-specific "Matte" or "Camera Visibility" diagnostic shading modes, as these adaptors would no longer generate attributes for other renderers (such as "ai:matte", "ri:visibility:camera", etc) with the Viewer renderer set to "OpenGL".

We still want to keep the behaviour of outputting only the necessary attributes for the current renderer for renderers other than OpenGL, so rather than reverting, we instead refactor these adaptors into individual renderer-specific adaptors that are registered to both the target renderer and "OpenGL".

startup/GafferScene/cameraVisibilityAdaptor.py
@@ -34,6 +34,7 @@
 #
 ##########################################################################
 
+import functools
 import inspect
 
 import IECore
@@ -41,24 +42,9 @@ import IECore
 import Gaffer
 import GafferScene
 
-def __cameraVisibilityAdaptor() :
+def __cameraVisibilityAdaptor( attributeName ) :
 
 	processor = GafferScene.SceneProcessor()
-
-	processor["renderer"] = Gaffer.StringPlug()
-
-	processor["__attributeSpreadsheet"] = Gaffer.Spreadsheet()
-	processor["__attributeSpreadsheet"]["selector"].setInput( processor["renderer"] )
-	processor["__attributeSpreadsheet"]["rows"].addColumn( Gaffer.StringPlug( "name" ) )
-	for renderer, attributeName in (
-		( "Arnold", "ai:visibility:camera" ),
-		( "Cycles", "cycles:visibility:camera" ),
-		( "3Delight*", "dl:visibility.camera" ),
-		( "RenderMan*", "ri:visibility:camera" )
-	) :
-		row = processor["__attributeSpreadsheet"]["rows"].addRow()
-		row["name"].setValue( renderer )
-		row["cells"]["name"]["value"].setValue( attributeName )
 
 	processor["__optionQuery"] = GafferScene.OptionQuery()
 	processor["__optionQuery"]["scene"].setInput( processor["in"] )
@@ -88,8 +74,7 @@ def __cameraVisibilityAdaptor() :
 
 	processor["__cameraInclusions"] = GafferScene.AttributeTweaks()
 	processor["__cameraInclusions"]["in"].setInput( processor["in"] )
-	processor["__cameraInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
-	processor["__cameraInclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
+	processor["__cameraInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attributeName, Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
 
 	processor["__cameraInclusions"]["filter"].setInput( processor["__cameraInclusionsFilter"]["out"] )
 	# __cameraInclusions is only required when `render:cameraInclusions` exists
@@ -100,8 +85,7 @@ def __cameraVisibilityAdaptor() :
 
 	processor["__cameraExclusions"] = GafferScene.AttributeTweaks()
 	processor["__cameraExclusions"]["in"].setInput( processor["__cameraInclusions"]["out"] )
-	processor["__cameraExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
-	processor["__cameraExclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
+	processor["__cameraExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attributeName, Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
 
 	processor["__cameraExclusions"]["filter"].setInput( processor["__cameraExclusionsFilter"]["out"] )
 	# __cameraExclusions is only required when `render:cameraExclusions` exists
@@ -112,8 +96,7 @@ def __cameraVisibilityAdaptor() :
 
 	processor["__rootExclusions"] = GafferScene.AttributeTweaks()
 	processor["__rootExclusions"]["in"].setInput( processor["__cameraExclusions"]["out"] )
-	processor["__rootExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
-	processor["__rootExclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
+	processor["__rootExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attributeName, Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
 
 	processor["__rootExclusions"]["filter"].setInput( processor["__rootPaths"]["out"] )
 	# All children of / are excluded from camera visibility when `render:cameraInclusions` exists and does not match the root of the scene.
@@ -126,4 +109,12 @@ def __cameraVisibilityAdaptor() :
 
 	return processor
 
-GafferScene.SceneAlgo.registerRenderAdaptor( "CameraVisibilityAdaptor", __cameraVisibilityAdaptor )
+for renderer, attributeName in (
+	( "Arnold", "ai:visibility:camera" ),
+	( "Cycles", "cycles:visibility:camera" ),
+	( "3Delight*", "dl:visibility.camera" ),
+	( "RenderMan*", "ri:visibility:camera" )
+) :
+	# Adaptors are registered to both `renderer` and "OpenGL" so these attributes are also
+	# available for use with the Viewer's OpenGL diagnostic shading modes.
+	GafferScene.SceneAlgo.registerRenderAdaptor( "{}CameraVisibilityAdaptor".format( renderer.rstrip( "*" ) ), functools.partial( __cameraVisibilityAdaptor, attributeName ), renderer = "{} OpenGL".format( renderer ) )

startup/GafferScene/matteAdaptor.py
@@ -34,6 +34,7 @@
 #
 ##########################################################################
 
+import functools
 import inspect
 
 import IECore
@@ -41,24 +42,9 @@ import IECore
 import Gaffer
 import GafferScene
 
-def __matteAdaptor() :
+def __matteAdaptor( attributeName ) :
 
 	processor = GafferScene.SceneProcessor()
-
-	processor["renderer"] = Gaffer.StringPlug()
-
-	processor["__attributeSpreadsheet"] = Gaffer.Spreadsheet()
-	processor["__attributeSpreadsheet"]["selector"].setInput( processor["renderer"] )
-	processor["__attributeSpreadsheet"]["rows"].addColumn( Gaffer.StringPlug( "name" ) )
-	for renderer, attributeName in (
-		( "Arnold", "ai:matte" ),
-		( "Cycles", "cycles:use_holdout" ),
-		( "3Delight*", "dl:matte" ),
-		( "RenderMan*", "ri:Ri:Matte" )
-	) :
-		row = processor["__attributeSpreadsheet"]["rows"].addRow()
-		row["name"].setValue( renderer )
-		row["cells"]["name"]["value"].setValue( attributeName )
 
 	processor["__optionQuery"] = GafferScene.OptionQuery()
 	processor["__optionQuery"]["scene"].setInput( processor["in"] )
@@ -88,8 +74,7 @@ def __matteAdaptor() :
 
 	processor["__matteInclusions"] = GafferScene.AttributeTweaks()
 	processor["__matteInclusions"]["in"].setInput( processor["in"] )
-	processor["__matteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
-	processor["__matteInclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
+	processor["__matteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attributeName, Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
 
 	processor["__matteInclusions"]["filter"].setInput( processor["__matteInclusionsFilter"]["out"] )
 	# __matteInclusions is only required when `render:matteInclusions` exists and the root of the scene hasn't been set as matte
@@ -103,8 +88,7 @@ def __matteAdaptor() :
 
 	processor["__matteExclusions"] = GafferScene.AttributeTweaks()
 	processor["__matteExclusions"]["in"].setInput( processor["__matteInclusions"]["out"] )
-	processor["__matteExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
-	processor["__matteExclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
+	processor["__matteExclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attributeName, Gaffer.BoolPlug(), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
 
 	processor["__matteExclusions"]["filter"].setInput( processor["__matteExclusionsFilter"]["out"] )
 	# __matteExclusions is only required when `render:matteExclusions` exists
@@ -115,8 +99,7 @@ def __matteAdaptor() :
 
 	processor["__rootMatteInclusions"] = GafferScene.AttributeTweaks()
 	processor["__rootMatteInclusions"]["in"].setInput( processor["__matteExclusions"]["out"] )
-	processor["__rootMatteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( "", Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
-	processor["__rootMatteInclusions"]["tweaks"][0]["name"].setInput( processor["__attributeSpreadsheet"]["out"]["name"] )
+	processor["__rootMatteInclusions"]["tweaks"].addChild( Gaffer.TweakPlug( attributeName, Gaffer.BoolPlug( defaultValue = True ), mode = Gaffer.TweakPlug.Mode.CreateIfMissing ) )
 
 	processor["__rootMatteInclusions"]["filter"].setInput( processor["__rootPaths"]["out"] )
 	# All children of / are matte if `render:matteInclusions` matches the root of the scene.
@@ -128,4 +111,12 @@ def __matteAdaptor() :
 
 	return processor
 
-GafferScene.SceneAlgo.registerRenderAdaptor( "MatteAdaptor", __matteAdaptor )
+for renderer, attributeName in (
+	( "Arnold", "ai:matte" ),
+	( "Cycles", "cycles:use_holdout" ),
+	( "3Delight*", "dl:matte" ),
+	( "RenderMan*", "ri:Ri:Matte" )
+) :
+	# Adaptors are registered to both `renderer` and "OpenGL" so these attributes are also
+	# available for use with the Viewer's OpenGL diagnostic shading modes.
+	GafferScene.SceneAlgo.registerRenderAdaptor( "{}MatteAdaptor".format( renderer.rstrip( "*" ) ), functools.partial( __matteAdaptor, attributeName ), renderer = "{} OpenGL".format( renderer ) )